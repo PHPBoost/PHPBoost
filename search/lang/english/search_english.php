74b51726766e08fe14123845e9b7f493fa59a603
mederic d <mederic.d@gmail.com>
2008-03-19 14:47:50 +0000

    Derni?re relecture par PiJean

--- search/lang/english/search_english.php
+++ search/lang/english/search_english.php
@@ -33,7 +33,7 @@
 //Généralités
 $LANG['title_search'] = 'Search';
 $LANG['search_results'] = 'Search results';
-$LANG['warning_length_string_searched'] = 'The size of the string you\'re searching for must be at least 4 characters!';
+$LANG['warning_length_string_searched'] = 'The size of the string you\'re searching for must be at least 4 characters long!';
 $LANG['search_min_length'] = 'Keywords (at least 4 characters)';
 $LANG['title_all_results'] = 'All results';
 $LANG['forms'] = 'Forms';
