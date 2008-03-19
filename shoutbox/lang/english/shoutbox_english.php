71b54d8076cd2c187e52d97cb96b9372e1ca8bed
mederic d <mederic.d@gmail.com>
2008-03-19 14:52:46 +0000

    Derni?re relecture par PiJean

--- shoutbox/lang/english/shoutbox_english.php
+++ shoutbox/lang/english/shoutbox_english.php
@@ -31,8 +31,8 @@
 ####################################################
 
 //Admin
-$LANG['shoutbox_max_msg'] = 'Maximum number of message to conserve';
-$LANG['shoutbox_max_msg_explain'] = 'Delete each days, set to -1 to desactivate';
+$LANG['shoutbox_max_msg'] = 'Maximum number of message to keep';
+$LANG['shoutbox_max_msg_explain'] = 'Delete each days, set to -1 to deactivate';
 $LANG['shoutbox_config'] = 'Shoutbox configuration';
 $LANG['rank_post'] = 'Rank to post';
 $LANG['shoutbox_refresh_delay'] = 'Delay between discussion automatic refresh';
