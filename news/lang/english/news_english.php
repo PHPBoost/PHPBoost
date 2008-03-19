23e73b25067f1fa08107f744fc683f896b3ed95a
mederic d <mederic.d@gmail.com>
2008-03-19 14:11:42 +0000

    Derni?re relecture par PiJean. Petite confusion entre les "news" (en g?n?ral) et les "piece/item of news" (? l'unit?) ^^

--- news/lang/english/news_english.php
+++ news/lang/english/news_english.php
@@ -31,19 +31,19 @@
 ####################################################
 
 //Admin
-$LANG['confirm_del_news'] = 'Delete this news ?';
-$LANG['add_news'] = 'Add a news';
+$LANG['confirm_del_news'] = 'Delete this piece of news?';
+$LANG['add_news'] = 'Add an item of news';
 $LANG['configuration_news'] = 'News configuration';
 $LANG['category_news'] = 'News category';
 $LANG['img_management'] = 'Picture management';
 $LANG['preview_image'] = 'Picture preview';
 $LANG['preview_image_explain'] = 'Default: on right';
-$LANG['img_link'] = 'Url\'s picture';
+$LANG['img_link'] = 'Picture URL';
 $LANG['img_desc'] = 'Picture description';
 $LANG['news_management'] = 'News management';
 $LANG['edit_news'] = 'Edit news';
-$LANG['edito'] = 'Edito';
-$LANG['edito_where'] = 'Visible message in top of the reception';
+$LANG['edito'] = 'Editorial';
+$LANG['edito_where'] = 'Visible message in the top of the home page';
 $LANG['config_news'] = 'News configuration';
 $LANG['nbr_news_p'] = 'Number of news per page';
 $LANG['nbr_news_p_explain'] = 'Default 6';
@@ -52,8 +52,8 @@ $LANG['nbr_arch_p_explain'] = 'Default 15';
 $LANG['module_management'] = 'Modules management';
 $LANG['activ_pagination'] = 'Activate pagination';
 $LANG['activ_pagination_explain'] = 'Else, show a link to archives';
-$LANG['activ_edito'] = 'Activate Edito';
-$LANG['activ_edito_explain'] = 'Visible message in top of the reception';
+$LANG['activ_edito'] = 'Activate editorial';
+$LANG['activ_edito_explain'] = 'Visible message in top of the home page';
 $LANG['activ_news_block'] = 'Activate news in block';
 $LANG['activ_com_n'] = 'Activate news comments';
 $LANG['activ_icon_n'] = 'Show news category icon';
@@ -73,24 +73,24 @@ $LANG['e_unexist_news'] = 'This news doesn\'t exist';
 $LANG['title_news'] = 'News';
 
 //Alerts
-$LANG['alert_delete_news'] = 'Delete this News ?';
+$LANG['alert_delete_news'] = 'Delete this piece of news??';
 
 //News
 $LANG['news'] = 'News';
-$LANG['propose_news'] = 'Suggest news';
-$LANG['xml_news_desc'] = 'Track the last actualities on';
-$LANG['add_succes_news'] = 'News sent successfully, please wait approval';
-$LANG['add_news'] = 'Add a News';
+$LANG['propose_news'] = 'Suggest an item of news';
+$LANG['xml_news_desc'] = 'Track the last news on';
+$LANG['add_succes_news'] = 'Item of news sent successfully, please wait approval';
+$LANG['add_news'] = 'Add a item of news';
 $LANG['last_news'] = 'Last news';
-$LANG['extend_contents'] = 'Read the rest...';
+$LANG['extend_contents'] = 'Read the continuation...';
 $LANG['no_news_available'] = 'No news currently available';
 $LANG['archive'] = 'Archives';
 $LANG['display_archive'] = 'Display archives';
 
 //Add news.
 $MAIL['new_news_website'] = 'New news on your website';
-$MAIL['new_news'] = 'A new news was added on your website ' . HOST . ', 
-it will have to be approved before being visible on the site by everyone. 
+$MAIL['new_news'] = 'A new item of news was added on your website ' . HOST . ', 
+it will have to be approved before being visible on the website by everyone. 
 
 News\'s title: %s
 Contents: %s...[next]
