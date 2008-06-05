31f4204972daad4412ca032d151264a8a3d8ac85
akhenathon2 <akhenathon2@localhost>
2008-06-05 21:13:07 +0000

    mea culpa, j'avais oublier le wiki, j'ai aussi ajouter des commentaires avec la licence GPL

--- wiki/xmlhttprequest.php
+++ wiki/xmlhttprequest.php
@@ -1,5 +1,26 @@
 <?php
-header('Content-type: text/html; charset=iso-8859-15');
+/*##################################################
+ *                                xmlhttprequest.php
+ *                            -------------------
+ *
+ *  
+###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+###################################################*/
 
 require_once('../kernel/begin.php');
 define('TITLE', 'Utilisation d\'AJAX');
@@ -7,11 +28,11 @@ require_once('../kernel/header_no_display.php');
 
 $Template->Set_filenames(array('wiki_explorer'=> 'wiki/explorer.tpl'));
 
-$id_cat = !empty($_POST['id_cat']) ? numeric($_POST['id_cat']) : 0;
+$id_cat = retrieve(POST, 'id_cat', 0);
 $select_cat = !empty($_GET['select_cat']) ? true : false;
-$selected_cat = !empty($_POST['selected_cat']) ? numeric($_POST['selected_cat']) : 0;
+$selected_cat = retrieve(POST, 'selected_cat', 0);
 $display_select_link = !empty($_GET['display_select_link']) ? 1 : 0;
-$open_cat = !empty($_POST['open_cat']) ? numeric($_POST['open_cat']) : 0;
+$open_cat = retrieve(POST, 'open_cat', 0);
 $root = !empty($_GET['root']) ? 1 : 0;
 
 
