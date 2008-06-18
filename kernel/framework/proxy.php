8dba885c9845e69d3d9c6f8cda5e7eb0ffe26e04
horn37 <horn37@localhost>
2008-06-18 20:48:30 +0000

    Ajout d'un proxy web s?curis? permettant de faire des requ?tes http, https, ou ftp sur un serveur distant

---
+++ kernel/framework/proxy.php
@@ -0,0 +1,57 @@
+<?php
+/*##################################################
+ *                               footer.php
+ *                            -------------------
+ *   begin                : June 18, 2008
+ *   copyright            : (C) 2008 Loïc Rouchon
+ *   email                : horn@phpboost.com
+ *
+ *
+###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+###################################################*/
+
+define('ERROR_REPORTING', E_ALL | E_NOTICE);
+define('UNAUTHORIZED_PROTOCOL', 'UNAUTHORIZED PROTOCOL');
+define('INVALID_URL', 'INVALID URL');
+
+require_once('functions.inc.php');
+
+$url = retrieve(GET, 'url', '', TSTRING_UNSECURE);;
+
+if( !empty($url) )
+{
+    $authorized_protocols = array('http', 'https', 'ftp');
+    $used_protocol = explode('://', $url);
+    if( count($used_protocol) > 1 )
+    {
+        $used_protocol = $used_protocol[0];
+        foreach( $authorized_protocols as $protocol )
+        {
+            if( $used_protocol === $protocol )
+            {
+                echo @file_get_contents_emulate($url);
+                exit(0);
+            }
+        }
+        die(UNAUTHORIZED_PROTOCOL);
+    }
+    die(UNAUTHORIZED_PROTOCOL);
+}
+die(INVALID_URL);
+
+?>
