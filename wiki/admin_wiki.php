2c9b984af9db00943733f10a2c54f82c20b9f824
ben.popeye <ben.popeye@localhost>
2008-06-23 17:54:26 +0000

    Correction du wiki (bug dans l'administration)

--- wiki/admin_wiki.php
+++ wiki/admin_wiki.php
@@ -35,7 +35,7 @@ include_once('../wiki/wiki_functions.php');
 $Cache->Load_file('wiki');
 
 $wiki_name = retrieve(POST, 'wiki_name', $LANG['wiki'], TSTRING_UNSECURE);
-$index_text = wiki_parse(retrieve(POST, 'contents', '', TSTRING_UNSECURE));
+$index_text = stripslashes(wiki_parse(retrieve(POST, 'contents', '', TSTRING_UNSECURE)));
 $last_articles = retrieve(POST, 'last_articles', 0);
 $display_cats = !empty($_POST['display_cats']) ? 1 : 0;
 $count_hits = !empty($_POST['count_hits']) ? 1 : 0;
