d33041e3d74044c6d28b13ad4c5223447fdbe1dc
horn37 <horn37@localhost>
2008-07-01 21:48:06 +0000

    Petit oubli

--- admin/admin_cache.php
+++ admin/admin_cache.php
@@ -73,7 +73,6 @@ else    // Génération du cache des rss
         foreach( $syndication_modules as $module )
             $module->functionnality('syndication_cache', array());
         
-        exit ( 0 );
         redirect(HOST . DIR . '/admin/admin_cache.php?cache=syndication&s=1');
     }
     else //Sinon on rempli le formulaire
