957b74739168ec67b44ffa0921d517fb5f4e817b
ben.popeye <ben.popeye@localhost>
2008-07-10 20:43:20 +0000

    Progression sur le panneau de contributions. Difficile de faire une interface coh?rente et simple ? comprendre.

--- member/contribution_panel.php
+++ member/contribution_panel.php
@@ -34,50 +34,70 @@ require_once('../kernel/header.php');
 if( !$Member->Check_level(MODO_LEVEL) ) //Si il n'est pas modérateur
 	$Errorh->Error_handler('e_auth', E_USER_REDIRECT); 
 	
+$contribution_id = retrieve(GET, 'id', 0);
+	
 require_once(PATH_TO_ROOT . '/kernel/framework/contribution/contribution.class.php');
 require_once(PATH_TO_ROOT . '/kernel/framework/contribution/contribution_panel.class.php');
 require_once(PATH_TO_ROOT . '/kernel/framework/util/date.class.php');
 
 $template = new Template('contribution_panel.tpl');
 
-$template->assign_vars(array(
-	'C_CONTRIBUTION_LIST' => true
-));
-
-$contribution_panel = new ContributionPanel();
-
-$contribution = new Contribution();
-$contribution->set_entitled('Test');
-$contribution->set_fixing_url('faq/faq.php');
-$contribution->set_module('faq');
-$contribution->set_creation_date(new Date());
-$contribution->set_poster_id(1);
-$contribution->set_poster_id(3);
-//$contribution->create_in_db();
-$contribution->get_creation_date()->format(DATE_FORMAT_SHORT);
-
-//On liste les contributions
-$result = $Sql->Query_while("SELECT id, entitled, fixing_url, module, current_status, creation_date, fixing_date, auth, poster_id, fixer_id, poster_member.login poster_login, fixer_member.login fixer_login, description
-	FROM ".PREFIX."contributions c
-	LEFT JOIN ".PREFIX."member poster_member ON poster_member.user_id = c.poster_id
-	LEFT JOIN ".PREFIX."member fixer_member ON fixer_member.user_id = c.poster_id
-	ORDER BY creation_date DESC", __LINE__, __FILE__);
-
-while( $row = $Sql->Sql_fetch_assoc($result) )
+if( $contribution_id > 0 )
 {
-	$this_contribution = new Contribution;
-	$this_contribution->build_from_db($row['id'], $row['entitled'], $row['description'], $row['fixing_url'], $row['module'], $row['current_status'], new Date(DATE_TIMESTAMP, TIMEZONE_USER, $row['creation_date']), new Date(DATE_TIMESTAMP, TIMEZONE_USER, $row['fixing_date']), $row['auth'], $row['poster_id'], $row['fixer_id']);
-	
-	$template->assign_block_vars('contributions', array(
-		'ENTITLED' => $this_contribution->get_entitled(),
-		'MODULE' => $this_contribution->get_module(),
-		'STATUS' => $this_contribution->get_status_name(),
-		'CREATION_DATE' => $this_contribution->get_creation_date()->format(DATE_FORMAT_SHORT),
-		'FIXING_DATE' => $this_contribution->get_current_status() == CONTRIBUTION_STATUS_PROCESSED ? $fixing_date->format(DATE_FORMAT_SHORT) : '',
-		'POSTER' => $row['poster_login'],
-		'FIXER' => $row['fixer_login'],
-		'ACTIONS' => ''
+	$template->assign_vars(array(
+		'C_CONSULT_CONTRIBUTION' => true
 	));
+	
+	$contribution = new Contribution();
+	$contribution->load_from_db($contribution_id);
+	
+	$template->assign_vars(array(
+		'ENTITLED' => $contribution->get_entitled()
+	));
+}
+else
+{
+	$template->assign_vars(array(
+		'C_CONTRIBUTION_LIST' => true
+	));
+
+	$contribution_panel = new ContributionPanel();
+
+	$contribution = new Contribution();
+	$contribution->set_entitled('Proposition de news');
+	$contribution->set_fixing_url('news/news.php');
+	$contribution->set_module('news');
+	$contribution->set_creation_date(new Date());
+	$contribution->set_poster_id(4);
+	//$contribution->create_in_db();
+
+	//On liste les contributions
+	$result = $Sql->Query_while("SELECT id, entitled, fixing_url, module, current_status, creation_date, fixing_date, auth, poster_id, fixer_id, poster_member.login poster_login, fixer_member.login fixer_login, description
+		FROM ".PREFIX."contributions c
+		LEFT JOIN ".PREFIX."member poster_member ON poster_member.user_id = c.poster_id
+		LEFT JOIN ".PREFIX."member fixer_member ON fixer_member.user_id = c.poster_id
+		ORDER BY creation_date DESC", __LINE__, __FILE__);
+
+	while( $row = $Sql->Sql_fetch_assoc($result) )
+	{
+		$this_contribution = new Contribution;
+		$this_contribution->build_from_db($row['id'], $row['entitled'], $row['description'], $row['fixing_url'], $row['module'], $row['current_status'], new Date(DATE_TIMESTAMP, TIMEZONE_USER, $row['creation_date']), new Date(DATE_TIMESTAMP, TIMEZONE_USER, $row['fixing_date']), $row['auth'], $row['poster_id'], $row['fixer_id']);
+		
+		$template->assign_block_vars('contributions', array(
+			'ENTITLED' => $this_contribution->get_entitled(),
+			'MODULE' => $this_contribution->get_module_name(),
+			'STATUS' => $this_contribution->get_status_name(),
+			'CREATION_DATE' => $this_contribution->get_creation_date()->format(DATE_FORMAT_SHORT),
+			'FIXING_DATE' => $this_contribution->get_fixing_date()->format(DATE_FORMAT_SHORT),
+			'POSTER' => $row['poster_login'],
+			'FIXER' => $row['fixer_login'],
+			'ACTIONS' => '',
+			'U_FIXER_PROFILE' => PATH_TO_ROOT . '/member/' . transid('member.php?id=' . $row['fixer_id'], 'member-' . $row['fixer_id'] . '.php'),
+			'U_POSTER_PROFILE' => PATH_TO_ROOT . '/member/' . transid('member.php?id=' . $row['poster_id'], 'member-' . $row['poster_id'] . '.php'),
+			'U_CONSULT' => PATH_TO_ROOT . '/member/' . transid('contribution_panel.php?id=' . $row['id']),
+			'C_FIXED' => $this_contribution->get_current_status() == CONTRIBUTION_STATUS_PROCESSED
+		));
+	}
 }
 
 $template->parse();
