41147e7e19eec78548581b41becb88af8289bccd
akhenathon2 <akhenathon2@localhost>
2008-07-15 22:09:20 +0000

    Bon voila j'ai r?ussi ? faire une m?thode write() qui accepte les gros fichier sans consommer trop de CPU, par contre pour les gros fichiers il faudra l'appeller en ?criture seul ( deuxi?me param?tre ? WRITE ) sinon ?a plante

--- kernel/framework/files/file.class.php
+++ kernel/framework/files/file.class.php
@@ -25,23 +25,29 @@
  *
 ###################################################*/
 
-require_once(PATH_TO_ROOT . 'kernel/framework/files/fse.class.php');
+require_once(PATH_TO_ROOT . '/kernel/framework/files/fse.class.php');
 
 define('ERASE', false);
 define('ADD', true);
 
+define('READ_WRITE', 0x3);
+define('READ', 0x1);
+define('WRITE', 0x2);
 // fonction de gestion des fichiers
 class File extends FileSystemElement
 {
 	## Private Attributes ##
 	var $lines = array();
 	var $contents;
+	var $mode;
 	
 	## Public Methods ##	
 	// Constructeur
-	function File($path, $whenopen = OPEN_AFTER)
+	function File($path, $mode = READ_WRITE, $whenopen = OPEN_AFTER)
 	{
 		parent::FileSystemElement($path);
+		
+		$this->mode = $mode;
 		
 		if( @file_exists($this->path) )
 		{
@@ -62,50 +68,81 @@ class File extends FileSystemElement
 	{
 		parent::open();
 		
-		$this->lines = file($this->path);
-		$this->contents = implode("\n", $this->lines);
+		if($this->mode & READ)
+		{
+			$this->contents = file_get_contents($this->path);
+			$this->lines = explode("\n", $this->contents);
+		}
 	}
 	
 	// renvoie le contenu du fichier en commençant à l'octet $start
 	function get_contents($start = 0, $len = -1)
 	{
-		parent::get();
-		
-		if( !$start && $len == -1 )
-			return $this->contents;
-		else if( $len == -1 )
-			return substr($this->contents, $start);
+		if($this->mode & READ)
+		{
+			parent::get();
+			
+			if( !$start && $len == -1 )
+				return $this->contents;
+			else if( $len == -1 )
+				return substr($this->contents, $start);
+			else
+				return substr($this->contents, $start, $len);
+		}
 		else
-			return substr($this->contents, $start, $len);
+			user_error('File '.$this->path.' is open in read only');
 	}
 	
 	// renvoie le contenu du fichier sous forme de tableau
 	function get_lines($start = 0, $n = -1)
 	{
-		parent::get();
-		
-		if( !$start && $n == -1 )
-			return $this->lines;
-		else if( $n == -1 )
-			return array_slice($this->lines, $start);
+		if($this->mode & READ)
+		{
+			parent::get();
+			
+			if( !$start && $n == -1 )
+				return $this->lines;
+			else if( $n == -1 )
+				return array_slice($this->lines, $start);
+			else
+				return array_slice($this->lines, $start, $n);
+		}
 		else
-			return array_slice($this->lines, $start, $n);
+			user_error('File '.$this->path.' is open in read only');
 	}
 	
 	// écrit $data dans le fichier, soit en écrasant les données ( par défaut ), soit passant en troisième paramètre la constante ADD
 	function write($data, $what = ERASE)
 	{
-		$mode = ( $what == ADD ) ? 'a' : 'w';
-		
-		if( !($fp = @fopen($this->path, $mode)) )
-			return false;
-		
-		fwrite($fp, $data);
-		fclose($fp);
-		
-		parent::write();
-		
-		return true;
+		if($this->mode & WRITE)
+		{	
+			if( !($fp = @fopen($this->path, ( $what == ADD ) ? 'a' : 'w')) )
+				return false;
+			
+			$bytes_to_write = strlen($data);
+			$bytes_written = 0;
+			while ( $bytes_written < $bytes_to_write )
+			{
+				// on écrit par bloc de 4Ko
+				$bytes = fwrite($fp, substr($data, $bytes_written, 4096));
+
+				if ( $bytes === false || $bytes == 0 )
+					break;
+
+				$bytes_written += $bytes;
+				
+				// on fait une petit pause pour ne pas consommer toute la CPU d'un centième de seconde
+				usleep(100);
+			}
+			
+			fclose($fp);
+			
+			parent::write();
+			
+			return ( $bytes_written == $bytes_to_write ) ? true : false;
+		}
+		else
+			user_error('File '.$this->path.' is open in read only');
 	}
 	
 	// supprime le fichier
