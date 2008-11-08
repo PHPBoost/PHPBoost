5d36b00e28b79f16e7023ecd8cfc9e12055d65cb
ben.popeye <ben.popeye@localhost>
2008-11-08 19:50:39 +0000

    Avancement sur TinyMCE. Il semble que toutes les balises passent au parser. Reste maintenant ? adapter le unparser ? cette nouvelle version de TinyMCE qui n'a quasiment plus aucun bug.

--- kernel/framework/ajax/content_xmlhttprequest.php
+++ kernel/framework/ajax/content_xmlhttprequest.php
@@ -38,6 +38,7 @@ $editor = retrieve(GET, 'editor', $CONFIG['editor']);
 
 $contents = utf8_decode(retrieve(POST, 'contents', '', TSTRING_UNCHANGE));
 
+//echo '<pre>' . htmlentities($contents) . '</pre><hr />';
 $ftags = retrieve(POST, 'ftags', TSTRING_UNSECURE);
 
 //On prend le bon parseur avec la bonne configuration
