fe60bfbf59aed9fd24bf97528784dd2c18f7e038
ben.popeye <ben.popeye@localhost>
2008-12-23 10:12:37 +0000

    Correction d'un bug dans le mini module FAQ et modification de la todo.

--- faq/faq_mini.php
+++ faq/faq_mini.php
@@ -3,10 +3,10 @@
  *                               faq_mini.php
  *                            -------------------
  *   begin                : June 21, 2008
- *   copyright          : (C) 2008 Sautel Benoit
+ *   copyright            : (C) 2008 Sautel Benoit
  *   email                : ben.popeye@phpboost.com
  *
-  *
+ *
 ###################################################
  *
  *   This program is free software; you can redistribute it and/or modify
@@ -39,30 +39,48 @@ function faq_mini()
     
     $tpl = new Template('faq/faq_mini.tpl');
     
+    $no_random_question = array(
+    	'L_FAQ_RANDOM_QUESTION' => $FAQ_LANG['random_question'],
+    	'FAQ_QUESTION' => $FAQ_LANG['no_random_question'],
+    	'U_FAQ_QUESTION' => PATH_TO_ROOT . '/faq/' . url('faq.php')
+    );
+    
+    //Aucune question à afficher
+    if (empty($RANDOM_QUESTIONS))
+    {
+    	$tpl->assign_vars($no_random_question);
+    	return $tpl->parse(TEMPLATE_STRING_MODE);
+    }
+    
     $random_question = $RANDOM_QUESTIONS[array_rand($RANDOM_QUESTIONS)];
     
     $faq_cats = new FaqCats();
     
     $i = 0;
     
+    //Tant que la question tirée aléatoirement n'est pas lisible par le visiteur, on en choisit une
+    //On met un "timeout" de 5 essais pour ne pas perdre trop de temps
     while (!$faq_cats->check_auth($random_question['idcat']) && $i < 5)
     {
     	$random_question = $RANDOM_QUESTIONS[array_rand($RANDOM_QUESTIONS)];
     	$i++;
     }
     
+    //Question trouvée avant 5 essais
     if ($i < 5 && !empty($random_question['question']))
+    {
     	$tpl->assign_vars(array(
     		'L_FAQ_RANDOM_QUESTION' => $FAQ_LANG['random_question'],
     		'FAQ_QUESTION' => $random_question['question'],
     		'U_FAQ_QUESTION' => PATH_TO_ROOT . '/faq/' . ($random_question['idcat'] > 0 ? url('faq.php?id=' . $random_question['idcat'] . '&amp;question=' . $random_question['id'], 'faq-' . $random_question['idcat'] . '+' . url_encode_rewrite($FAQ_CATS[$random_question['idcat']]['name']) . '.php?question=' . $random_question['id']) . '#q' . $random_question['id'] : url('faq.php?question=' . $random_question['id'], 'faq.php?question=' . $random_question['id']) . '#q' . $random_question['id'])
     	));
+    }
+    //Echec
     else
-    	$tpl->assign_vars(array(
-    		'L_FAQ_RANDOM_QUESTION' => $FAQ_LANG['random_question'],
-    		'FAQ_QUESTION' => $FAQ_LANG['no_random_question'],
-    		'U_FAQ_QUESTION' => PATH_TO_ROOT . '/faq/' . url('faq.php')
-    	));
+    {
+    	$tpl->assign_vars($no_random_question);
+    }
+    //On retourne le contenu du bloc
     return $tpl->parse(TEMPLATE_STRING_MODE);
 }
 ?>
