ca3697e5673c47634c6415efcefc7add41347b2f
ad gandon <ad.gandon@wanadoo.fr>
2009-01-05 20:24:06 +0000

    Version initiale

---
+++ panel/lang/english/panel_english.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *                             panel_french.php
+ *                            -------------------
+ *   begin                :  November 29, 2008
+ *  @author Alain Gandon
+ *   @copyright (C) 2008 Gandon Alain
+ * @license GPL
+ *
+ *  
+ ###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ **/
+
+
+####################################################
+#                                                          French                                                                        #
+####################################################
+
+//Admin
+$LANG['panel_config'] = 'Configuration du panel';
+$LANG['rank_post'] = 'Niveau pour pouvoir poster';
+
+//Titre
+$LANG['title_panel'] = 'Panel';
+
+?>
