486d0b6eb6561310ea02e17fc6d91355568dcffb
ben.popeye <ben.popeye@localhost>
2009-01-06 22:28:11 +0000

    Modification de la distribution qui sera distribu?e dans la version alpha. C'est la distribution compl?te pour pouvoit tout tester d'un coup.

--- install/distribution/distribution_french.php
+++ install/distribution/distribution_french.php
@@ -26,12 +26,12 @@
 ###################################################*/
 
 //Nom de la distribution
-define('DISTRIBUTION_NAME', 'Publication');
+define('DISTRIBUTION_NAME', 'Complet');
 
 //Description de la distribution
 define('DISTRIBUTION_DESCRIPTION', '<img src="distribution/publication.png" alt="" style="float:right;padding-right:35px"/>
-<p>Vous allez installer la distribution <strong>Publication</strong> de PHPBoost.</p>
-<p>Cette distribution est idéale pour créer un site qui servira à proposer aux visiteurs du contenu, que ce soit sous forme de texte, d\'images ou autres.</p>');
+<p>Vous allez installer la distribution <strong>Complète</strong> de PHPBoost.</p>
+<p>Cette distribution contient l\'ensemble des modules officiels publiés par l\'équipe de développement de PHPBoost. Elle devrait répondre à une grande variété de besoins grâce à la diversité de ses modules.</p>');
 
 //Thème de la distribution
 define('DISTRIBUTION_THEME', 'base');
@@ -43,6 +43,6 @@ define('DISTRIBUTION_START_PAGE', '/news/news.php');
 define('DISTRIBUTION_ENABLE_USER', true);
 
 //Liste des modules
-$DISTRIBUTION_MODULES = array('articles', 'contact', 'connect', 'news', 'pages', 'search', 'web');
+$DISTRIBUTION_MODULES = array('articles', 'calendar', 'contact', 'connect', 'download', 'faq', 'forum', 'gallery', 'guestbook', 'news', 'newsletter', 'online', 'pages', 'poll', 'search', 'shoutbox', 'stats', 'web', 'wiki');
 
 ?>
