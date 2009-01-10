f09ea82ac1b79935d67073322e1239af6bcbe8eb
ben.popeye <ben.popeye@localhost>
2009-01-10 09:43:29 +0000

    Modification de la redirection lors du t?l?chargement d'un fichier dans le module t?l?chargements.

--- download/count.php
+++ download/count.php
@@ -3,7 +3,7 @@
  *                               count.php
  *                            -------------------
  *   begin                : July 27, 2005
- *   copyright          : (C) 2005 Viarre Régis
+ *   copyright            : (C) 2005 Viarre Régis
  *   email                : crowkait@phpboost.com
  *
  *   
@@ -37,18 +37,27 @@ if (!empty($idurl))
 	if (empty($info_file['url']))
 		$Errorh->handler('e_unexist_file_download', E_USER_REDIRECT);
 	
-	//Redirection vers le fichier demandé!
-	$filesize = @filesize(str_replace(HOST . DIR . '/', '../', $info_file['url']));
-	$filesize = ($filesize !== false) ? $filesize : (!empty($info_file) ? number_round($info_file['size']*1048576, 0) : false);
-	if ($filesize !== false)
-		header('Content-Length: ' . $filesize);
-	header('content-type:application/force-download');
-	header('Content-Disposition:attachment;filename="' . substr(strrchr($info_file['url'], '/'), 1) . '"');
-	header('Expires:0');
-	header('Cache-Control:must-revalidate');
-	header('Pragma:public');
-	if (@readfile($info_file['url']) === false)
-		redirect($info_file['url']);
+	//Si c'est une adresse relative
+	if (strpos($info_file['url'], '://') === false)	
+	{
+    	//Redirection vers le fichier demandé
+    	$filesize = @filesize(str_replace(HOST . DIR . '/', '../', $info_file['url']));
+    	$filesize = ($filesize !== false) ? $filesize : (!empty($info_file) ? number_round($info_file['size']*1048576, 0) : false);
+    	if ($filesize !== false)
+    		header('Content-Length: ' . $filesize);
+    	header('content-type:application/force-download');
+    	header('Content-Disposition:attachment;filename="' . substr(strrchr($info_file['url'], '/'), 1) . '"');
+    	header('Expires:0');
+    	header('Cache-Control:must-revalidate');
+    	header('Pragma:public');
+    	if (@readfile($info_file['url']) === false)
+    		redirect($info_file['url']);
+	}
+	//Si c'est une adresse absolue, ce n'est pas la peine d'aller chercher les informations du fichier
+	else
+	{
+	    redirect($info_file['url']);
+	}
 }
 else
 	$Errorh->handler('e_unexist_file_download', E_USER_REDIRECT);
