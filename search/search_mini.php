7b434e6d223853a8e4058711aaa05397c1682d3c
ben.popeye <ben.popeye@localhost>
2009-01-19 19:49:11 +0000

    Quelques petites corrections de bug, je n'ai pas le temps d'en faire plus.

--- search/search_mini.php
+++ search/search_mini.php
@@ -40,7 +40,7 @@ function search_mini($position)
     $tpl->assign_vars(Array(
         'TITLE_SEARCH' => TITLE,
         'SEARCH' => $LANG['title_search'],
-        'TEXT_SEARCHED' => !empty($search) ? htmlentities(retrieve(REQUEST, 'q', '', TSTRING_AS_RECEIVED)) : $LANG['search'] . '...',
+        'TEXT_SEARCHED' => !empty($search) ? stripslashes(retrieve(REQUEST, 'q', '')) : $LANG['search'] . '...',
         'WARNING_LENGTH_STRING_SEARCH' => addslashes($LANG['warning_length_string_searched']),
     	'L_SEARCH' => $LANG['search'],
         'U_FORM_VALID' => url(PATH_TO_ROOT . '/search/search.php#results'),
