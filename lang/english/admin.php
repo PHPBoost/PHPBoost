bef7c7e477acf7ca28d1a7d6975bddb583fd59d3
horn37 <horn37@localhost>
2009-02-24 21:54:24 +0000

    Langues pour les MAJ (needs php5 or above)

--- lang/english/admin.php
+++ lang/english/admin.php
@@ -690,5 +690,8 @@ $LANG['download_the_complete_pack'] = 'Download the complete pack';
 $LANG['download_the_update_pack'] = 'Download the update pack';
 $LANG['no_availables_updates'] = 'No updates is available for the moment.';
 $LANG['incompatible_php_version'] = 'Incompatible PHP Version, please upgrade to %s or above';
+$LANG['incompatible_php_version'] = 'Can\'t check for updates.
+Please upgrade to PHP version %s or above.<br />If you can\'t use PHP5,
+check for updates on our <a href="http://www.phpboost.com">official website</a>.';
 
 ?>
