e80cb56af6ad2a2f93d16dd5916e7f7836cc25a4
mickaelhemri <mickaelhemri@localhost>
2009-02-27 16:21:59 +0000

    Ajout docx Todo

--- admin/admin_files_config.php
+++ admin/admin_files_config.php
@@ -81,7 +81,7 @@ else
 	$array_extensions_type = array(
 		$LANG['files_image'] => array('jpg', 'jpeg', 'bmp', 'gif', 'png', 'tif', 'svg', 'ico'),
 		$LANG['files_archives'] => array('rar', 'zip', 'gz'), 
-		$LANG['files_text'] => array('txt', 'doc', 'pdf', 'ppt', 'xls', 'odt', 'odp', 'ods', 'odg', 'odc', 'odf', 'odb', 'xcf'),
+		$LANG['files_text'] => array('txt', 'doc', 'docx', 'pdf', 'ppt', 'xls', 'odt', 'odp', 'ods', 'odg', 'odc', 'odf', 'odb', 'xcf'),
 		$LANG['files_media'] => array('flv', 'mp3', 'ogg', 'mpg', 'mov', 'swf', 'wav', 'wmv', 'midi', 'mng', 'qt'), 
 		$LANG['files_prog'] => array('c', 'h', 'cpp', 'java', 'py', 'css', 'html', 'xml'),
 		$LANG['files_misc'] => array('ttf', 'tex', 'rtf', 'psd')
