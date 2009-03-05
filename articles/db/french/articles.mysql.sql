cf73b061f4c491454c61bca34944ad465341eac5
mickaelhemri <mickaelhemri@localhost>
2009-03-05 22:00:25 +0000

    Ajout d'une cat?gorie ? l'install des articles

--- articles/db/french/articles.mysql.sql
+++ articles/db/french/articles.mysql.sql
@@ -15,6 +15,8 @@ CREATE TABLE `phpboost_articles_cats` (
 	KEY `id_left` (`id_left`)
 ) ENGINE=MyISAM;
 
+INSERT INTO phpboost_articles_cats (`id`, `id_left`, `id_right`, `level`, `name`, `contents`, `nbr_articles_visible`, `nbr_articles_unvisible`, `icon`, `aprob`, `auth`) VALUES ('1', '1', '2', '0', 'Catégorie de test', 'Articles de démonstration', '0', '0', 'articles.png', '1', 'a:3:{s:3:"r-1";i:1;s:2:"r0";i:1;s:2:"r1";i:1;}');
+
 DROP TABLE IF EXISTS `phpboost_articles`;
 CREATE TABLE `phpboost_articles` (
 	`id` int(11) NOT NULL auto_increment,
