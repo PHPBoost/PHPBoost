9d6c900c737c5c23f3f742991881dd7b63b55be7
benoit.sautel <benoit.sautel@localhost>
2009-04-06 20:38:40 +0000

    Correction d'une incoh?rence dans la FAQ. :'(, moi qui avais consid?r? ?a comme une feature.

--- faq/faq.php
+++ faq/faq.php
@@ -130,8 +130,6 @@ if ($num_rows > 0)
 {
 	//Display mode : if this category has a particular display mode we use it, else we use default display mode. If the category is the root we use default mode.
 	$faq_display_block = ($FAQ_CATS[$id_faq]['display_mode'] > 0) ? ($FAQ_CATS[$id_faq]['display_mode'] == 2 ? true : false ) : $FAQ_CONFIG['display_block'];
-	if ($id_question > 0)
-		$faq_display_block = false;
 	
 	//Displaying administration tools
 	$template->assign_vars(array(
