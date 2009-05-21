84f396862ca709e75e0d5f540f8419ee90b7550c
loic.rouchon <loic.rouchon@localhost>
2009-05-21 09:43:58 +0000

    Corrections sur la classes URL

--- kernel/framework/util/url.class.php
+++ kernel/framework/util/url.class.php
@@ -51,274 +51,279 @@ class Url
      * to the website root if beginning with a "/" or an absolute url
      * @param string $path_to_root url context. default is PATH_TO_ROOT
      */
-    function Url($url = '', $path_to_root = null)
+    function Url($url = '', $path_to_root = null, $server_url = null)
     {
-        if (!empty($url))
-        {
-            $this->set_url($url);
-        }
-        
         if ($path_to_root !== null)
         {
-            $this->path_to_root = Url::path_to_root($path_to_root);
+            $this->path_to_root = $path_to_root;
         }
         else
         {
             $this->path_to_root = Url::path_to_root();
         }
+
+        if ($server_url !== null)
+        {
+            $this->server_url = $server_url;
+        }
+        else
+        {
+            $this->server_url = Url::server_url();
+        }
+
+        $this->set_url($url);
     }
 
-	/**
-	 * @desc Set the url
-	 * @param string $url the url string relative to the current path,
-	 * to the website root if beginning with a "/" or an absolute url
-	 */
-	function set_url($url)
-	{
-		$url = str_replace(HOST . DIR . '/', '/', $url);
-		if (!strpos($url, '://'))
-		{
-			if (substr($url, 0, 1) == '/')
-			{   // Relative url from the website root (good form)
-				$this->relative = $url;
-			}
-			else
-			{   // The url is relative to the current foler
-				$this->relative = $this->root_to_local() . $url;
-			}
-			$this->relative = Url::compress($this->relative);
-		}
+    /**
+     * @desc Set the url
+     * @param string $url the url string relative to the current path,
+     * to the website root if beginning with a "/" or an absolute url
+     */
+    function set_url($url)
+    {
+        $url = str_replace(HOST . DIR . '/', '/', $url);
+        if (!strpos($url, '://'))
+        {
+            if (substr($url, 0, 1) == '/')
+            {   // Relative url from the website root (good form)
+                $this->relative = $url;
+            }
+            else
+            {   // The url is relative to the current foler
+                $this->relative = $this->root_to_local() . $url;
+            }
+            $this->relative = Url::compress($this->relative);
+        }
 
-		global $CONFIG;
-		if (!empty($this->relative))
-		{
-			$this->absolute = Url::compress(Url::get_absolute_root() . $this->relative);
-		}
-		else
-		{
-			$this->absolute = Url::compress($url);
-		}
-	}
+        global $CONFIG;
+        if (!empty($this->relative))
+        {
+            $this->absolute = Url::compress(Url::get_absolute_root() . $this->relative);
+        }
+        else
+        {
+            $this->absolute = Url::compress($url);
+        }
+    }
 
-	/**
-	 * @return bool true if the url is a relative one
-	 */
-	function is_relative()
-	{
-		return !empty($this->relative);
-	}
+    /**
+     * @return bool true if the url is a relative one
+     */
+    function is_relative()
+    {
+        return !empty($this->relative);
+    }
 
-	/**
-	 * @desc Returns the relative url if defined, else the absolute one
-	 * @return string the relative url if defined, else the absolute one
-	 */
-	function relative()
-	{
-		if ($this->is_relative())
-		{
-			return $this->relative;
-		}
-		else
-		{
-			return $this->absolute;
-		}
-	}
+    /**
+     * @desc Returns the relative url if defined, else the absolute one
+     * @return string the relative url if defined, else the absolute one
+     */
+    function relative()
+    {
+        if ($this->is_relative())
+        {
+            return $this->relative;
+        }
+        else
+        {
+            return $this->absolute;
+        }
+    }
 
-	/**
-	 * @desc Returns the absolute url
-	 * @return string the absolute url
-	 */
-	function absolute()
-	{
-		return $this->absolute;
-	}
+    /**
+     * @desc Returns the absolute url
+     * @return string the absolute url
+     */
+    function absolute()
+    {
+        return $this->absolute;
+    }
 
 
-	/**
-	 * @static
-	 * @desc Compress a url by removing all "folder/.." occurrences
-	 * @param string $url the url to compress
-	 * @return string the compressed url
-	 */
-	/* static */ function compress($url)
-	{
-		$url = preg_replace(array('`([^:]|^)/+`', '`(?<!\.)\./`'), array('$1/', ''), $url);
+    /**
+     * @static
+     * @desc Compress a url by removing all "folder/.." occurrences
+     * @param string $url the url to compress
+     * @return string the compressed url
+     */
+    /* static */ function compress($url)
+    {
+        $url = preg_replace(array('`([^:]|^)/+`', '`(?<!\.)\./`'), array('$1/', ''), $url);
 
-		do
-		{
-			$url = preg_replace('`/?[^/]+/\.\.`', '', $url);
+        do
+        {
+            $url = preg_replace('`/?[^/]+/\.\.`', '', $url);
 
-		}
-		while (preg_match('`/?[^/]+/\.\.`', $url) > 0);
-		return $url;
-	}
+        }
+        while (preg_match('`/?[^/]+/\.\.`', $url) > 0);
+        return $url;
+    }
 
-	/**
-	 * @static
-	 * @desc Returns the relative path from the website root to the current path if working on a relative url
-	 * @return string the relative path from the website root to the current path if working on a relative url
-	 */
-	/* static */ function root_to_local()
-	{
-		global $CONFIG;
+    /**
+     * @desc Returns the relative path from the website root to the current path if working on a relative url
+     * @return string the relative path from the website root to the current path if working on a relative url
+     */function root_to_local()
+    {
+        global $CONFIG;
 
-		$local_path = Url::server_url();
-		$local_path = substr(trim($local_path, '/'), strlen(trim($CONFIG['server_path'], '/')));
-		$file_begun = strrpos($local_path, '/');
-		if ($file_begun >= 0)
-		{
-			$local_path = substr($local_path, 0, $file_begun) . '/';
-		}
+        $local_path = $this->server_url;
+        $local_path = substr(trim($local_path, '/'), strlen(trim($CONFIG['server_path'], '/')));
+        $file_begun = strrpos($local_path, '/');
+        if ($file_begun >= 0)
+        {
+            $local_path = substr($local_path, 0, $file_begun) . '/';
+        }
 
-		return $local_path;
-	}
+        return $local_path;
+    }
 
-	/**
-	 * @static
-	 * @desc Returns the absolute website root Url
-	 * @return string the absolute website root Url
-	 */
-	/* static */ function get_absolute_root()
-	{
-		global $CONFIG;
-		return trim(trim($CONFIG['server_name'], '/') . '/' . trim($CONFIG['server_path'], '/'), '/');
-	}
+    /**
+     * @static
+     * @desc Returns the absolute website root Url
+     * @return string the absolute website root Url
+     */
+    /* static */ function get_absolute_root()
+    {
+        global $CONFIG;
+        return trim(trim($CONFIG['server_name'], '/') . '/' . trim($CONFIG['server_path'], '/'), '/');
+    }
 
-	/**
-	 * @static
-	 * @desc Returns the HTML text with only absolutes urls
-	 * @param string $html_text The HTML text in which we gonna search for
-	 * root relatives urls (only those beginning by '/') to convert into absolutes ones.
-	 * @param string $path_to_root Path to root of the page to which you want to fit the URL.
-	 * @param string $server_url Path from the site root of the page to which you want to fit the URL.
-	 * @return string The HTML text with only absolutes urls
-	 */
-	/* static */ function html_convert_root_relative2absolute($html_text, $path_to_root = PATH_TO_ROOT, $server_url = SERVER_URL)
-	{
-		Url::path_to_root($path_to_root);
-		Url::server_url($server_url);
-		return preg_replace_callback(
+    /**
+     * @static
+     * @desc Returns the HTML text with only absolutes urls
+     * @param string $html_text The HTML text in which we gonna search for
+     * root relatives urls (only those beginning by '/') to convert into absolutes ones.
+     * @param string $path_to_root Path to root of the page to which you want to fit the URL.
+     * @param string $server_url Path from the site root of the page to which you want to fit the URL.
+     * @return string The HTML text with only absolutes urls
+     */
+    /* static */ function html_convert_root_relative2absolute($html_text, $path_to_root = PATH_TO_ROOT, $server_url = SERVER_URL)
+    {
+        Url::path_to_root($path_to_root);
+        Url::server_url($server_url);
+        return preg_replace_callback(
             '`(<script type="text/javascript">.*insert(?:Sound|Movie)Player\(")(/[^"]+)(".*</script>)`sU',
-            array('Url', '_convert_url_to_absolute'),
-            preg_replace_callback(
+        array('Url', '_convert_url_to_absolute'),
+        preg_replace_callback(
 		        '`((?:<[^>]+) (?:' . URL_TAGS . ')=")(/[^"]+)("(?:[^<]*>))`',
-		        array('Url', '_convert_url_to_absolute'),
-		        $html_text
-            )
-		);
-	}
-
-	/**
-	 * @static
-	 * @desc Returns the HTML text with only relatives urls
-	 * @param string $html_text The HTML text in which we gonna search for absolutes urls to convert into relatives ones.
-	 * @param string $path_to_root Path to root of the page to which you want to fit the URL.
-	 * @param string $server_url Path from the site root of the page to which you want to fit the URL.
-	 * @return string The HTML text with only absolutes urls
-	 */
-	/* static */ function html_convert_absolute2relative($html_text, $path_to_root = PATH_TO_ROOT, $server_url = SERVER_URL)
-	{
-		Url::path_to_root($path_to_root);
-		Url::server_url($server_url);
-		return preg_replace_callback(
-            '`(<script type="text/javascript">.*insert(?:Sound|Movie)Player\(")([^"]+)(".*</script>)`sU',
-            array('Url', '_convert_url_to_relative'),
-            preg_replace_callback(
-	            '`((?:<[^>]+) (?:' . URL_TAGS . ')(?:="))([^"]+)("(?:[^<]*>))`',
-                array('Url', '_convert_url_to_relative'),
-                $html_text
-            )
+        array('Url', '_convert_url_to_absolute'),
+        $html_text
+        )
         );
-	}
-	
-	/**
-	 * @static
-	 * @desc Transforms the relative URL whose base is the site root (for instance /images/mypic.png) to the real relative path fited to the current page.
-	 * @param string $html_text The HTML text in which you want to replace the paths
-	 * @param string $path_to_root Path to root of the page to which you want to fit the URL.
-	 * @return string The transformed string
-	 */
-	/* static */ function html_convert_root_relative2relative($html_text, $path_to_root = PATH_TO_ROOT)
-	{
-		return  preg_replace(
+    }
+
+    /**
+     * @static
+     * @desc Returns the HTML text with only relatives urls
+     * @param string $html_text The HTML text in which we gonna search for absolutes urls to convert into relatives ones.
+     * @param string $path_to_root Path to root of the page to which you want to fit the URL.
+     * @param string $server_url Path from the site root of the page to which you want to fit the URL.
+     * @return string The HTML text with only absolutes urls
+     */
+    /* static */ function html_convert_absolute2relative($html_text, $path_to_root = PATH_TO_ROOT, $server_url = SERVER_URL)
+    {
+        Url::path_to_root($path_to_root);
+        Url::server_url($server_url);
+        return preg_replace_callback(
+            '`(<script type="text/javascript">.*insert(?:Sound|Movie)Player\(")([^"]+)(".*</script>)`sU',
+        array('Url', '_convert_url_to_relative'),
+        preg_replace_callback(
+	            '`((?:<[^>]+) (?:' . URL_TAGS . ')(?:="))([^"]+)("(?:[^<]*>))`',
+        array('Url', '_convert_url_to_relative'),
+        $html_text
+        )
+        );
+    }
+
+    /**
+     * @static
+     * @desc Transforms the relative URL whose base is the site root (for instance /images/mypic.png) to the real relative path fited to the current page.
+     * @param string $html_text The HTML text in which you want to replace the paths
+     * @param string $path_to_root Path to root of the page to which you want to fit the URL.
+     * @return string The transformed string
+     */
+    /* static */ function html_convert_root_relative2relative($html_text, $path_to_root = PATH_TO_ROOT)
+    {
+        return  preg_replace(
 	            '`((?:<[^>]+) (?:' . URL_TAGS . ')(?:="))(/[^"]+)("(?:[^<]*>))`',
                 '$1' . $path_to_root . '$2$3',
-                $html_text);
-	}
+        $html_text);
+    }
 
-	/**
-	 * @static
-	 * @desc replace a relative url by the corresponding absolute one
-	 * @param string[] $url_params Array containing the attributes containing the url and the url
-	 * @return string the replaced url
-	 */
-	/* static */ function _convert_url_to_absolute($url_params)
-	{
-		$url = new Url($url_params[2]);
-		$url_params[2] = $url->absolute();
-		return $url_params[1] . $url_params[2] . $url_params[3];
-	}
+    /**
+     * @static
+     * @desc replace a relative url by the corresponding absolute one
+     * @param string[] $url_params Array containing the attributes containing the url and the url
+     * @return string the replaced url
+     */
+    /* static */ function _convert_url_to_absolute($url_params)
+    {
+        $url = new Url($url_params[2]);
+        $url_params[2] = $url->absolute();
+        return $url_params[1] . $url_params[2] . $url_params[3];
+    }
 
-	/**
-	 * @static
-	 * @desc replace an absolute url by the corresponding relative one if possible
-	 * @param string[] $url_params Array containing the attributes containing the url and the url
-	 * @return string the replaced url
-	 */
-	/* static */ function _convert_url_to_relative($url_params)
-	{
-		$url = new Url($url_params[2]);
-		$url_params[2] = $url->relative();
-		return $url_params[1] . $url_params[2] . $url_params[3];
-	}
+    /**
+     * @static
+     * @desc replace an absolute url by the corresponding relative one if possible
+     * @param string[] $url_params Array containing the attributes containing the url and the url
+     * @return string the replaced url
+     */
+    /* static */ function _convert_url_to_relative($url_params)
+    {
+        $url = new Url($url_params[2]);
+        $url_params[2] = $url->relative();
+        return $url_params[1] . $url_params[2] . $url_params[3];
+    }
 
-	/**
-	 * @static
-	 * @param string $url the url to "relativize"
-	 * @return string the relative url of the $url parameter
-	 */
-	/* static */ function get_relative($url)
-	{
-		$o_url = new Url($url);
-		return $o_url->relative();
-	}
+    /**
+     * @static
+     * @param string $url the url to "relativize"
+     * @return string the relative url of the $url parameter
+     */
+    /* static */ function get_relative($url, $path_to_root = null, $server_url = null)
+    {
+        $o_url = new Url($url, $path_to_root, $server_url);
+        return $o_url->relative();
+    }
 
-	/**
-	 * @static
-	 * @desc Overrides the used PATH_TO_ROOT. if the argument is null, the value is only returned.
-	 * Please note this is a PHP4 hack to allow a Class variable.
-	 * @param string $path the new PATH_TO_ROOT to use
-	 * @return string the used PATH_TO_ROOT
-	 */
-	function path_to_root($path = null)
-	{
-		static $path_to_root = PATH_TO_ROOT;
-		if ($path != null)
-		{
-			$path_to_root = $path;
-		}
-		return $path_to_root;
-	}
+    /**
+     * @static
+     * @desc Overrides the used PATH_TO_ROOT. if the argument is null, the value is only returned.
+     * Please note this is a PHP4 hack to allow a Class variable.
+     * @param string $path the new PATH_TO_ROOT to use
+     * @return string the used PATH_TO_ROOT
+     */
+    function path_to_root($path = null)
+    {
+        static $path_to_root = PATH_TO_ROOT;
+        if ($path != null)
+        {
+            $path_to_root = $path;
+        }
+        return $path_to_root;
+    }
 
-	/**
-	 * @static
-	 * @desc Overrides the used SERVER URL. if the argument is null, the value is only returned.
-	 * Please note this is a PHP4 hack to allow a Class variable.
-	 * @param string $path the new SERVER URL to use
-	 * @return string the used SERVER URL
-	 */
-	function server_url($url = null)
-	{
-		static $server_url = SERVER_URL;
-		if ($url != null)
-		{
-			$server_url = $url;
-		}
-		return $server_url;
-	}
+    /**
+     * @static
+     * @desc Overrides the used SERVER URL. if the argument is null, the value is only returned.
+     * Please note this is a PHP4 hack to allow a Class variable.
+     * @param string $path the new SERVER URL to use
+     * @return string the used SERVER URL
+     */
+    function server_url($url = null)
+    {
+        static $server_url = SERVER_URL;
+        if ($url !== null)
+        {
+            $server_url = $url;
+        }
+        return $server_url;
+    }
 
-	var $relative = '';
-	var $absolute = '';
-	var $path_to_root = '';
+    var $relative = '';
+    var $absolute = '';
+    var $path_to_root = '';
+    var $server_url = '';
 }
 ?>
