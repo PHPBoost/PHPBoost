6cc923edd74b4b0738bec7b318ad82510f58f607
benoit.sautel <benoit.sautel@localhost>
2009-06-07 17:30:30 +0000

    Th?me publishing sur la distribution publication

--- install/distribution/distribution_publication_french.php
+++ install/distribution/distribution_publication_french.php
@@ -34,7 +34,7 @@ define('DISTRIBUTION_DESCRIPTION', '<img src="distribution/publication.png" alt=
 <p>Cette distribution est idéale pour créer un site qui servira à proposer aux visiteurs du contenu, que ce soit sous forme de texte, d\'images ou autres.</p>');
 
 //Thème de la distribution
-define('DISTRIBUTION_THEME', 'bluedream2');
+define('DISTRIBUTION_THEME', 'publishing');
 
 //Page de démarrage de la distribution (commencer à la racine du site avec /)
 define('DISTRIBUTION_START_PAGE', '/news/news.php');
