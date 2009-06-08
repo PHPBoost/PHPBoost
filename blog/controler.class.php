25786d5afbb73485083207a01530dc5c8604ab3f
loic.rouchon <loic.rouchon@localhost>
2009-06-08 19:26:47 +0000

    Un premier exemple d'utilisation du dispatcheur

---
+++ blog/controler.class.php
@@ -0,0 +1,45 @@
+<?php
+/*##################################################
+ *                           controleur.class.php
+ *                            -------------------
+ *   begin                : June 08 2009
+ *   copyright            : (C) 2009 Loïc Rouchon
+ *   email                : loic.rouchon@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class Controler
+{   
+    function __construct()
+    {
+        
+    }
+    
+    function view()
+    {
+    	echo 'Je suis view';
+    }
+    
+    function view_by_id($id)
+    {
+    	echo 'Je suis view by id avec comme id : ' . $id;
+    }
+}
+?>
