93356e9d519380c70084a4baa8388789db4a9a90
benoit.sautel <benoit.sautel@localhost>
2009-07-25 15:40:27 +0000

    Mise ? jour de FlowPlayer en 3.1.1.

--- kernel/framework/js/players/flowplayer.js
+++ kernel/framework/js/players/flowplayer.js
@@ -7,7 +7,7 @@ function flowPlayerBuild(id) {
 		return;
 	}
 	//On lance le flowplayer
-	flowplayer(id, PATH_TO_ROOT + '/kernel/data/flowplayer/flowplayer-3.0.3.swf', { 
+	flowplayer(id, PATH_TO_ROOT + '/kernel/data/flowplayer/flowplayer-3.1.1.swf', { 
 		    clip: { 
 		        url: $(id).href,
 		        autoPlay: false 
