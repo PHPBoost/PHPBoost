6fa11eab18e5baf4fc67fef2215cf43f3c090927
mickaelhemri <mickaelhemri@localhost>
2009-07-27 20:32:39 +0000

    Correction pour mysql 5

--- update/migration_2.0_to_3.0.sql
+++ update/migration_2.0_to_3.0.sql
@@ -76,7 +76,7 @@ ALTER TABLE `phpboost_member` CHANGE `new_pass` `new_pass`  varchar(64) NOT NULL
 
 CREATE TABLE IF NOT EXISTS `phpboost_member_extend` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `last_view_forum` int(11) NOT NULL,
+  `last_view_forum` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
