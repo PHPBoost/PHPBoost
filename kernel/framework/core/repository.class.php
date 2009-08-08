c87183d31b2725bf394bb84b35ce22387ede631d
loic.rouchon <loic.rouchon@localhost>
2009-08-08 21:49:51 +0000

    Correction bug page blanche

--- kernel/framework/core/repository.class.php
+++ kernel/framework/core/repository.class.php
@@ -7,7 +7,7 @@
  *   email                : loic.rouchon@phpboost.com
  *
  *
-###################################################
+ ###################################################
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
-###################################################*/
+ ###################################################*/
 
 import('core/application');
 
@@ -34,65 +34,70 @@ import('core/application');
  */
 class Repository
 {
-    /**
-	* @desc constructor of the class
-	* @param $url
-	*/
-    function Repository($url)
-    {
-        $this->url = $url;
-        $this->xml = @simplexml_load_file($this->url);
-        if ($this->xml == false)
-            $this->xml = null;
-    }
+	/**
+	 * @desc constructor of the class
+	 * @param $url
+	 */
+	function Repository($url)
+	{
+		$this->url = $url;
+		if (function_exists('simplexml_load_file'))
+		{
+			$this->xml = simplexml_load_file($this->url);
+			if ($this->xml == false)
+			{
+				$this->xml = null;
+			}
+		}
+	}
 
-    /**
-	* @desc Check Application
-	* @param $app
-	*/
-    function check($app)
-    {
-        global $CONFIG;
-        $xpath_query = '//app[@id=\'' . $app->get_id() . '\' and @type=\'' .  $app->get_type() . '\']/version[@language=\'' . $app->get_language() . '\']';
-        // can't compare strings with XPath, so we check the version number with PHP.
-        if ($this->xml != null)
-        {
-            $newerVersions = array();
-            $versions = $this->xml->xpath($xpath_query);
-            $nbVersions = $versions != false ? count($versions) : 0;
-            // Retrieves all the available updates for the current application
-            for ($i = 0; $i < $nbVersions; $i++)
-            {
-                $rep_app = clone($app);
-                $rep_app->load($versions[$i]);
-                
-                if (version_compare($app->get_version(), $rep_app->get_version(), '<') > 0)
-                {
-                    if ($rep_app->check_compatibility())
-                    {
-                        $newerVersions[$rep_app->get_version()] = $i;
-                    }
-                }
-            }
-            
-            // Keep only the first applyable update
-            $firstNewVersion = count($newerVersions) > 0 ? min(array_keys($newerVersions)) : '';
-            if (!empty($firstNewVersion))
-            {
-                $app->load($versions[$newerVersions[$firstNewVersion]]);
-                return $app;
-            }
-        }
-        return null;
-    }
+	/**
+	 * @desc Check Application
+	 * @param $app
+	 */
+	function check($app)
+	{
+		global $CONFIG;
+		$xpath_query = '//app[@id=\'' . $app->get_id() . '\' and @type=\'' .  $app->get_type() . '\']/version[@language=\'' . $app->get_language() . '\']';
+		// can't compare strings with XPath, so we check the version number with PHP.
+		if ($this->xml != null)
+		{
+			$newerVersions = array();
+			$versions = $this->xml->xpath($xpath_query);
+			$nbVersions = $versions != false ? count($versions) : 0;
+			// Retrieves all the available updates for the current application
+			for ($i = 0; $i < $nbVersions; $i++)
+			{
+				$rep_app = clone($app);
+				$rep_app->load($versions[$i]);
 
-    /**
-	* @desc Accessor of url
-	*/
-    function get_url() { return $this->url; }
-    
-    var $url = '';
-    var $xml = null;
+				if (version_compare($app->get_version(), $rep_app->get_version(), '<') > 0)
+				{
+					if ($rep_app->check_compatibility())
+					{
+						$newerVersions[$rep_app->get_version()] = $i;
+					}
+				}
+			}
+
+			// Keep only the first applyable update
+			$firstNewVersion = count($newerVersions) > 0 ? min(array_keys($newerVersions)) : '';
+			if (!empty($firstNewVersion))
+			{
+				$app->load($versions[$newerVersions[$firstNewVersion]]);
+				return $app;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @desc Accessor of url
+	 */
+	function get_url() { return $this->url; }
+
+	var $url = '';
+	var $xml = null;
 };
 
 ?>
