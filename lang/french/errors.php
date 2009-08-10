efa9c03f2999596986404e54ce9f071282e95a8c
mickaelhemri <mickaelhemri@localhost>
2009-08-10 22:11:10 +0000

    Correction de l'erreur lors d'un acc?s ? un module d?sactiv?.

--- lang/french/errors.php
+++ lang/french/errors.php
@@ -36,6 +36,7 @@ $LANG['unknow_error'] = 'Erreur inconnue';
 $LANG['e_auth'] = 'Vous n\'avez pas le niveau requis !';
 $LANG['e_unexist_module'] = 'Le module associé n\'existe pas !';
 $LANG['e_uninstalled_module'] = 'Ce module n\'est pas installé !';
+$LANG['e_unactivated_module'] = 'Ce module n\'est pas activé !';
 $LANG['e_incomplete'] = 'Tous les champs obligatoires doivent être remplis !';
 $LANG['e_auth_post'] = 'Vous devez être inscrit pour poster !';
 $LANG['e_readonly'] = 'Vous ne pouvez exécuter cette action, car vous avez été placé en lecture seule !';
