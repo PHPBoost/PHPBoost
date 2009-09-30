6b268a9b04afc0ce68eb2c100c921a45a45b3457
benoit.sautel <benoit.sautel@localhost>
2009-09-30 19:40:03 +0000

    Fichier begin.php compl?tement nettoy?, il utilise enti?rement la classe Environment. Reste ? s'attaquer ? header et footer maintenant :)

--- kernel/framework/core/environment.class.php
+++ kernel/framework/core/environment.class.php
@@ -25,6 +25,13 @@
  *
  ###################################################*/
 
+/**
+ * This class manages all the environment that PHPBoost need to run.
+ * <p>It's able to initialize the environment that contains services (database, 
+ * users management...) as well as the graphical environment.</p>
+ * @author Benoit Sautel <ben.popeye@phpboost.com>
+ *
+ */
 class Environment
 {
 	/**
@@ -55,8 +62,13 @@ class Environment
 	 * @var Session
 	 */
 	private $session;
+	/**
+	 * @var User
+	 */
+	private $user;
 
 	/**
+	 * Returns the instance of the Environment object (singleton design pattern)
 	 * @return Environment
 	 */
 	public static function get_instance()
@@ -77,81 +89,101 @@ class Environment
 		$this->db_connection = new Sql();
 	}
 
+	/**
+	 * Sets the page title
+	 * @param $title The current page's title
+	 */
 	public function set_title($title)
 	{
 		$this->title = $title;
 	}
 
+	/**
+	 * Returns the current page's title
+	 * @return string The title
+	 */
 	public function get_title()
 	{
 		return $this->title;
 	}
 
+	/**
+	 * Enables the graphical environment display. By default, it's displayed.
+	 */
 	public function enable_display()
 	{
 		$this->display_enabled = true;
 	}
 
+	/**
+	 * Disables the graphical environment display. By default, it's displayed.
+	 */
 	public function disable_display()
 	{
 		$this->display_enabled = false;
 	}
 
+	/**
+	 * Tells whether the graphical environment has to be displayed.
+	 * @return bool true if it has, false otherwise.
+	 */
 	public function is_display_enabled()
 	{
 		return $this->display_enabled;
 	}
 
+	/**
+	 * Returns the current page's bread crumb/
+	 * @return BreadCrumb
+	 */
 	public function get_breadcrumb()
 	{
 		return $this->breadcrumb;
 	}
 
+	/**
+	 * Returns the data base connection
+	 * @return Sql
+	 */
 	public function get_db_connection()
 	{
 		return $this->db_connection;
 	}
 
+	/**
+	 * Returns the current user's session
+	 * @return Session
+	 */
 	public function get_session()
 	{
 		return $this->session;
 	}
 
-	public function init()
+	/**
+	 * Returns the current user
+	 * @return User
+	 */
+	public function get_user()
 	{
-		self::load_static_constants();
-		self::write_http_headers();
-		self::load_cache();
-		self::load_dynamic_constants();
-		$this->init_session();
+		return $this->user;
 	}
 
-	private static function load_static_constants()
+	private function init_services()
 	{
-		if (@include(PATH_TO_ROOT . '/cache/debug.php'))
-		{
-			define('DEBUG', (bool)$DEBUG['debug_mode']);
-		}
-		else
-		{
-			define('DEBUG', false);
-		}
+		$this->db_connection->auto_connect();
+
+		$this->session = new Session();
 	}
-
-	private static function load_dynamic_constants()
-	{
-		global $CONFIG;
-
-		define('DIR', $CONFIG['server_path']);
-		define('HOST', $CONFIG['server_name']);
-		define('TPL_PATH_TO_ROOT', !empty($CONFIG['server_path']) ? $CONFIG['server_path'] : '');
-	}
-
+	
+	/**
+	 * Loads all the files that the environment requires 
+	 */
 	public static function load_imports()
 	{
-		require_once PATH_TO_ROOT . '/kernel/constant.php'; //Constante utiles.
-		require_once PATH_TO_ROOT . '/kernel/framework/functions.inc.php'; //Fonctions de base.
+		require_once PATH_TO_ROOT . '/kernel/constant.php';
+		require_once PATH_TO_ROOT . '/kernel/framework/functions.inc.php';
 
+		//Now we have the import function, we can user it :)
 		import('content/parser/content_formatting_factory');
 		import('core/breadcrumb');
 		import('core/cache');
@@ -166,25 +198,40 @@ class Environment
 		import('util/bench');
 	}
 
+	/**
+	 * Inits the environment and all its services.
+	 */
+	public function init()
+	{
+		self::load_static_constants();
+		self::write_http_headers();
+		self::load_cache();
+		self::load_dynamic_constants();
+		$this->init_session();
+		self::init_output_bufferization();
+		self::load_lang_files();
+		self::perform_changeday();
+		$this->check_current_page_auth();
+		$this->csrf_protect_post_requests();
+	}
+	
+	private static function load_static_constants()
+	{
+		if (@include(PATH_TO_ROOT . '/cache/debug.php'))
+		{
+			define('DEBUG', (bool)$DEBUG['debug_mode']);
+		}
+		else
+		{
+			define('DEBUG', false);
+		}
+	}
 
 	private static function write_http_headers()
 	{
 		header('Content-type: text/html; charset=iso-8859-1');
 		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 		header('Pragma: no-cache');
-	}
-
-	private function init_services()
-	{
-		$this->db_connection->auto_connect();
-
-		$this->session = new Session();
-	}
-	
-	private function init_session()
-	{
-		$this->session->load();
-		$this->session->act();
 	}
 	
 	private function load_cache()
@@ -196,8 +243,162 @@ class Environment
 		$Cache->load('modules');
 		$Cache->load('themes');
 		$Cache->load('langs');
+		$Cache->load('day');
 	}
 
+	private static function load_dynamic_constants()
+	{
+		global $CONFIG;
+
+		define('DIR', $CONFIG['server_path']);
+		define('HOST', $CONFIG['server_name']);
+		define('TPL_PATH_TO_ROOT', !empty($CONFIG['server_path']) ? $CONFIG['server_path'] : '');
+	}
+
+	private function init_session()
+	{
+		global $CONFIG, $THEME_CONFIG, $LANGS_CONFIG, $CONFIG_USER;
+		$this->session->load();
+		$this->session->act();
+
+		$this->user = new User();
+
+		// TODO do we need to keep that feature? It's not supported every where
+		if ($this->session->supports_cookies())
+		{
+			define('SID', 'sid=' . $this->user->get_attribute('session_id') .
+				'&amp;suid=' . $this->user->get_attribute('user_id'));
+			define('SID2', 'sid=' . $this->user->get_attribute('session_id') .
+				'&suid=' . $this->user->get_attribute('user_id'));
+		}
+		else
+		{
+			define('SID', '');
+			define('SID2', '');
+		}
+
+		$user_theme = $this->user->get_attribute('user_theme');
+		//Is that theme authorized for this member? If not, we assign it the default theme
+		if ($CONFIG_USER['force_theme'] == 1 || !isset($THEME_CONFIG[$user_theme]['secure'])
+		|| !$this->user->check_level($THEME_CONFIG[$user_theme]['secure']))
+		{
+			$user_theme = $CONFIG['theme'];
+		}
+		//If the user's theme doesn't exist, we assign it a default one which exists
+		$user_theme = find_require_dir(PATH_TO_ROOT . '/templates/', $user_theme);
+		$this->user->set_user_theme($user_theme);
+
+		$user_lang = $this->user->get_attribute('user_lang');
+		//Is that member authorized to use this lang? If not, we assign it the default lang
+		if (!isset($LANGS_CONFIG[$user_lang]['secure']) ||
+		!$this->user->check_level($LANGS_CONFIG[$user_lang]['secure']))
+		{
+			$user_lang = $CONFIG['lang'];
+		}
+		$user_lang = find_require_dir(PATH_TO_ROOT . '/lang/', $user_lang);
+		$this->user->set_user_lang($user_lang);
+	}
+
+	private static function init_output_bufferization()
+	{
+		global $CONFIG;
+		if ($CONFIG['ob_gzhandler'] == 1)
+		{
+			ob_start('ob_gzhandler');
+		}
+		else
+		{
+			ob_start();
+		}
+	}
+
+	private static function load_lang_files()
+	{
+		global $LANG;
+		$LANG = array();
+		require_once(PATH_TO_ROOT . '/lang/' . get_ulang() . '/main.php');
+		require_once(PATH_TO_ROOT . '/lang/' . get_ulang() . '/errors.php');
+	}
+
+	private static function perform_changeday()
+	{
+		global $_record_day;
+		//If the day changed compared to the last request, we execute the daily tasks
+		if (gmdate_format('j', time(), TIMEZONE_SITE) != $_record_day && !empty($_record_day))
+		{
+			import('io/filesystem/file');
+			$lock_file = new File(PATH_TO_ROOT . '/cache/changeday_lock');
+			if (!$lock_file->exists())
+			{
+				$lock_file->write('');
+				$lock_file->flush();
+			}
+			if ($lock_file->lock(false))
+			{
+				$yesterday_timestamp = time() - 86400;
+
+				$num_entry_today = $this->db_connection->query("SELECT COUNT(*) FROM " . DB_TABLE_STATS
+				. " WHERE stats_year = '" . gmdate_format('Y', $yesterday_timestamp,
+				TIMEZONE_SYSTEM) . "' AND stats_month = '" . gmdate_format('m',
+				$yesterday_timestamp, TIMEZONE_SYSTEM) . "' AND stats_day = '" . gmdate_format(
+				  'd', $yesterday_timestamp, TIMEZONE_SYSTEM) . "'", __LINE__, __FILE__);
+
+				if ((int) $num_entry_today == 0)
+				{
+					$Cache->generate_file('day');
+
+					require_once(PATH_TO_ROOT . '/kernel/changeday.php');
+					change_day();
+				}
+			}
+			$lock_file->close();
+		}
+	}
+
+	private function check_current_page_auth()
+	{
+		global $MODULES, $Errorh;
+		//We verify if the user can display this page
+		define('MODULE_NAME', get_module_name());
+
+		if (isset($MODULES[MODULE_NAME]))
+		{
+			//Is the module disabled?
+			if ($MODULES[MODULE_NAME]['activ'] == 0)
+			{
+				$Errorh->handler('e_unactivated_module', E_USER_REDIRECT);
+			}
+			//Is the module forbidden?
+			else if(!$this->user->check_auth($MODULES[MODULE_NAME]['auth'], ACCESS_MODULE))
+			{
+				$Errorh->handler('e_auth', E_USER_REDIRECT);
+			}
+		}
+		//Otherwise it can be a kernel page (they are in specific folders) => it's ok
+		elseif (in_array(MODULE_NAME, array('member', 'admin', 'kernel', '')))
+		{
+		}
+		//It's maybe a forbidden page!
+		else
+		{
+			//We try to see if it can be an uninstalled module
+			$array_info_module = load_ini_file(PATH_TO_ROOT . '/' . MODULE_NAME . '/lang/', get_ulang());
+			//If it's an unistalled module, we forbid access!
+			if (!empty($array_info_module['name']))
+			{
+				$Errorh->handler('e_uninstalled_module', E_USER_REDIRECT);
+			}
+		}
+	}
+
+	private function csrf_protect_post_requests()
+	{
+		// Verify that the user really wanted to do this POST (only for the registered ones)
+		if ($this->user->check_level(MEMBER_LEVEL))
+		{
+			$this->session->csrf_post_protect();
+		}
+	}
 }
 
 ?>
