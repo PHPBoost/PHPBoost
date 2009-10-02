ef7f2fac6049ef4c1e5f0d7cc00bd8f758394f2a
benoit.sautel <benoit.sautel@localhost>
2009-10-02 15:36:31 +0000

    Travail de Lo?c.
    Mise en place d'un mod?le de mapping objet propri?t? en vue de faire le mapping ORM.

---
+++ kernel/framework/mvc/model/mapping_model_exception.class.php
@@ -0,0 +1,45 @@
+<?php
+/*##################################################
+ *                           mapping_model_exception.class.php
+ *                            -------------------
+ *   begin                : October 2, 2009
+ *   copyright            : (C) 2009 Loic Rouchon
+ *   email                : horn@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+class MappingModelException extends Exception
+{
+    public function __construct($classname, $message)
+    {
+        parent::__construct('mapping model "' . $classname . '" ' . $message);
+    }
+}
+class MappingModelFieldException extends Exception {}
+class MappingModelFieldTableNotSpecifiedException extends MappingModelFieldException
+{
+    public function __construct($property)
+    {
+        parent::__construct('mapping model field "' . $property . '" has no table specified');
+    }
+}
+
+?>
---
+++ kernel/framework/mvc/model/mapping_model_field.class.php
@@ -0,0 +1,92 @@
+<?php
+/*##################################################
+ *                           mapping_model_field.class.php
+ *                            -------------------
+ *   begin                : October 2, 2009
+ *   copyright            : (C) 2009 Loic Rouchon
+ *   email                : horn@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+class MappingModelField
+{
+    const DEFAULT_PROPERTY_NAME = 0x01;
+
+    /**
+     * @var string
+     */
+    private $db_table;
+
+    /**
+     * @var string
+     */
+    private $db_field_name;
+
+    /**
+     * @var string
+     */
+    private $property_name;
+
+    public function __construct($property_name,
+    $db_field_name = self::DEFAULT_PROPERTY_NAME, $db_table = null)
+    {
+        $this->property_name = $property_name;
+        if ($db_field_name === self::DEFAULT_PROPERTY_NAME)
+        {
+            $this->db_field_name = $db_field_name;
+        }
+        else
+        {
+            $this->db_field_name = $this->property_name;
+        }
+        $this->db_table = $db_table;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_table()
+    {
+        if ($this->db_table === null)
+        {
+            throw new MappingModelFieldTableNotSpecifiedException($this->property);
+        }
+        return $this->db_table;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_db_field_name()
+    {
+        return $this->db_field_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_property_name()
+    {
+        return $this->property_name;
+    }
+}
+
+?>
