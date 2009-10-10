e3602bec581aaf219e787c9d2e65229996d4f0de
regis.viarre <regis.viarre@localhost>
2009-10-10 13:39:58 +0000

    Correction sur le mini module article pour qu'il puisse fonctionner

--- articles/articles_mini.php
+++ articles/articles_mini.php
@@ -27,54 +27,54 @@
 
 if (defined('PHPBOOST') !== true) exit;
 
+require_once PATH_TO_ROOT . '/articles/articles_constants.php';
+
 function articles_mini($position, $block)
 {
     global $Cache, $LANG, $CONFIG_ARTICLES;
    
-   $Cache->load('articles'); 
+	$Cache->load('articles'); 
 
 	load_module_lang('articles');
+	global $ARTICLES_LANG;
 	
 	$tpl = new Template('articles/articles_mini.tpl');
 	import('core/menu_service');
 	MenuService::assign_positions_conditions($tpl, $block);
 
-	$mini_conf=unserialize($CONFIG_ARTICLES['mini']);
-	
-	echo $mini_conf;
+	$mini_conf = unserialize($CONFIG_ARTICLES['mini']);
 	switch ($mini_conf['type'])
 	{
 		case 'note' :	
 			$sort = 'note';	
-			$l_type=$ARTICLES_LANG['articles_best_note'];
+			$l_type = $ARTICLES_LANG['articles_best_note'];
 			break;
 		case 'com' :
 			$sort = 'nbr_com';
-			$l_type=$ARTICLES_LANG['articles_more_com'];
+			$l_type = $ARTICLES_LANG['articles_more_com'];
 			break;
 		case 'date' :
 			$sort = 'timestamp';
-			$l_type=$ARTICLES_LANG['articles_by_date'];
+			$l_type = $ARTICLES_LANG['articles_by_date'];
 			break;
 		case 'view' :
 			$sort = 'views';
-			$l_type=$ARTICLES_LANG['articles_most_popular'];
+			$l_type = $ARTICLES_LANG['articles_most_popular'];
 			break;
 		default :
 			$sort = 'date';
-			$l_type=$ARTICLES_LANG['articles_by_date'];
+			$l_type = $ARTICLES_LANG['articles_by_date'];
 			break;
 	}
 	
 	//echo "sort : ".$sort."   - nbr : ".$mini_conf['nbr_articles'];
 	import('content/note');
-	$result = $this->sql_querier->query_while("SELECT a.id, a.title,a.description, a.icon, a.timestamp, a.views, a.note, a.nbrnote, a.nbr_com,a.user_id,m.user_id,m.login,m.level
-	FROM " . DB_TABLE_ARTICLES . " a
+	$result = EnvironmentServices::get_sql()->query_while("SELECT a.id, a.title, a.description, a.icon, a.timestamp, a.views, a.note, a.nbrnote, a.nbr_com, a.user_id
+	FROM " . DB_TABLE_ARTICLES . " a	
 	WHERE a.visible = 1 
 	ORDER BY " . $sort . " DESC ".
-	$this->sql_querier->limit(0,$mini_conf['nbr_articles']), __LINE__, __FILE__);
-	
-	while ($row = $this->sql_querier->fetch_assoc($result))
+	EnvironmentServices::get_sql()->limit(0, $mini_conf['nbr_articles']), __LINE__, __FILE__);
+	while ($row = EnvironmentServices::get_sql()->fetch_assoc($result))
 	{		
 		$tpl->assign_block_vars('articles', array(
 			'ID' => $row['id'],
@@ -85,13 +85,10 @@ function articles_mini($position, $block)
 	}
 	
 	$tpl->assign_vars(array(
-		'L_TYPE_MINI'=>$l_type,
-		'L_VOTE' => $LANG['poll_vote'],
-		'L_POLL_RESULT' => $LANG['poll_result'],
-		'U_POLL_RESULT' => url('.php?id=' . $poll_mini['id'] . '&amp;r=1', '-' . $poll_mini['id'] . '-1.php')
+		'L_TYPE_MINI' => $l_type
 	));
 
 	return $tpl->parse(Template::TEMPLATE_PARSER_STRING);
-
 }
+
 ?>
