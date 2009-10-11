118945934247c41cbf87ad3f069474b0d287b63a
regis.viarre <regis.viarre@localhost>
2009-10-11 17:48:23 +0000

    Correction parse error

--- member/member.php
+++ member/member.php
@@ -478,7 +478,7 @@ if (!empty($id_get)) //Espace membre
 				if ($_FILES['avatars']['size'] > 0)
 				{
 					$Upload->file('avatars', '`([a-z0-9()_-])+\.(jpg|gif|png|bmp)+$`i', UNIQ_NAME, $CONFIG_USER['weight_max']*1024);
-					if (!empty($Upload->get_error())) //Erreur, on arrête ici
+					if ($Upload->get_error() != '') //Erreur, on arrête ici
 						redirect('/member/member' . url('.php?id=' .  $id_get . '&edit=1&erroru=' . $Upload->get_error()) . '#errorh');
 					else
 					{
