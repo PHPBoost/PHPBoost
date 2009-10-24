57e98a643249e33514d7470ea352fd908e6d52c8
benoit.sautel <benoit.sautel@localhost>
2009-10-24 16:16:58 +0000

    Correction d'un bug dans la newsletter.

--- newsletter/newsletter_service.class.php
+++ newsletter/newsletter_service.class.php
@@ -46,7 +46,7 @@ class NewsletterService
 		{
 			$nbr = $Sql->count_table('newsletter', __LINE__, __FILE__);
 			//On enregistre dans les archives la newsletter envoyée
-			$Sql->query_inject("INSERT INTO " . PREFIX . "newsletter_arch (title,message,timestamp,type,nbr) VALUES('" . $mail_object . "','" . $message . "', '" . time() . "', 'html', '" . $nbr . "')", __LINE__, __FILE__);
+			$Sql->query_inject("INSERT INTO " . PREFIX . "newsletter_arch (title,message,timestamp,type,nbr) VALUES('" . $mail_object . "','" . addslashes($message) . "', '" . time() . "', 'html', '" . $nbr . "')", __LINE__, __FILE__);
 			
 			$mailing_list = array();
 			$result = $Sql->query_while("SELECT id, mail 
@@ -107,7 +107,7 @@ class NewsletterService
 		{
 			$nbr = $Sql->count_table('newsletter', __LINE__, __FILE__);
 			//On enregistre dans les archives la newsletter envoyée
-			$Sql->query_inject("INSERT INTO " . PREFIX . "newsletter_arch (title,message,timestamp,type,nbr) VALUES('" . $mail_object . "', '" . $message . "', '" . time() . "', 'bbcode', '" . $nbr . "')", __LINE__, __FILE__);
+			$Sql->query_inject("INSERT INTO " . PREFIX . "newsletter_arch (title,message,timestamp,type,nbr) VALUES('" . $mail_object . "', '" . addslashes($message) . "', '" . time() . "', 'bbcode', '" . $nbr . "')", __LINE__, __FILE__);
 			
 			$mailing_list = array();
 			$result = $Sql->query_while("SELECT id, mail 
