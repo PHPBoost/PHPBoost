6050a9b5cad317190079ba1ad825157f9c4450cc
horn37 <horn37@localhost>
2009-11-07 10:20:05 +0000

    Proposition de la liste des propri?t?s ? g?rer dans la cr?ation d'une table

--- kernel/framework/io/db/dbms/MySQLDBMSUtils.class.php
+++ kernel/framework/io/db/dbms/MySQLDBMSUtils.class.php
@@ -25,10 +25,6 @@
  *
  ###################################################*/
 
-
-
-
-
 /**
  * @author loic rouchon <loic.rouchon@phpboost.com>
  * @package db
@@ -131,6 +127,43 @@ class MySQLDBMSUtils implements DBMSUtils
 		}
 		return $fields;
 	}
+
+    public function create_table($table_name, array $fields, array $options = array())
+    {
+        $options_sample = array(
+            'primary' => array('event_id', 'person_id'),
+            'character_set' => 'utf8',
+            'collate' => 'utf8_unicode_ci',
+            'foreignKeys' => array(
+                'fk_name' => array(
+                    'local'   => 'ext_id_fk',
+                    'foreign' => 'id',
+                    'foreignTable' => 'events',
+                )
+                'onDelete' => 'CASCADE',
+            )
+        )
+        $fields_sample = array(
+            'id' => array(
+                'type' => 'integer',
+                'primary' => true,
+                'autoincrement' => true
+            ),
+            'name' => array(
+                'type' => 'string',
+                'length' => 255,
+                'full_text' => true
+            ),
+            'datetime' => array(
+                'type' => 'timestamp'
+            )
+            'ext_id_fk' => array(
+                'type' => 'integer',
+            ),
+        );
+        $create_table_query = 'CREATE TABLE IF NOT EXISTS `' . $table_name . '`';
+        $this->inject($create_table_query . ';');
+    }
 
 	public function drop($tables)
 	{
