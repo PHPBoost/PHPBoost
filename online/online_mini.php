fbec0cdaa3eee119acf84a57a12fb2c8ff3f5ba2
benoit.sautel <benoit.sautel@localhost>
2009-11-28 18:57:44 +0000

    Int?gration du cache des contributions non lues dans le projet. Suppression de l'ancien cache.

--- online/online_mini.php
+++ online/online_mini.php
@@ -3,7 +3,7 @@
  *                              online_mini.php
  *                            -------------------
  *   begin                : July 20, 2005
- *   copyright          : (C) 2005 Viarre Régis
+ *   copyright            : (C) 2005 Viarre Régis
  *   email                : crowkait@phpboost.com
  *
  *
