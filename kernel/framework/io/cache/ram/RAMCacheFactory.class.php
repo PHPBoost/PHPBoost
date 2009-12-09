24a85ee622141424b4b757a2d7044225c3575cbe
benoit.sautel <benoit.sautel@localhost>
2009-12-09 12:56:56 +0000

    Ajout d'un gestionnaire de cache RAM qui permet de tout stocker dans APC pour ne plus avoir lire les fichiers dans le syst?me de fichier si APC est activ?, sinon la dur?e de vie de ce cache en RAM sera seulement l'ex?cution de la page.

---
+++ kernel/framework/io/cache/ram/RAMCacheFactory.class.php
@@ -0,0 +1,64 @@
+<?php
+/*##################################################
+ *                           RAMCacheFatory.class.php
+ *                            -------------------
+ *   begin                : December 09, 2009
+ *   copyright            : (C) 2009 Benoit Sautel, Loic Rouchon
+ *   email                : ben.popeye@phpboost.com, horn@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @package io
+ * @subpackage cache/ram
+ * @desc
+ * @author Benoit Sautel <ben.popeye@phpboost.com>, Loic Rouchon <horn@phpboost.com>
+ *
+ */
+class RAMCacheFactory
+{
+	private static $apc_enabled = null;
+
+	public static function get($id)
+	{
+		if (self::is_apc_enabled())
+		{
+			return new ApcRAMCache($id);
+		}
+		return new DefaultRAMCache();
+	}
+
+	private static function is_apc_enabled()
+	{
+		if (self::$apc_enabled === null)
+		{
+			if (function_exists('apc_cache_info') && @apc_cache_info('user') !== false)
+			{
+				self::$apc_enabled = true;
+			}
+			else
+			{
+				self::$apc_enabled = false;
+			}
+		}
+		return self::$apc_enabled;
+	}
+}
+?>
