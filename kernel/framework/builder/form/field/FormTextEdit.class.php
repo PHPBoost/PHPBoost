a5d87458376abff780aa0a181832842a5764422e
loic.rouchon <loic.rouchon@localhost>
2009-12-20 10:22:24 +0000

    Travail sur les FormBuilders

--- kernel/framework/builder/form/field/FormTextEdit.class.php
+++ kernel/framework/builder/form/field/FormTextEdit.class.php
@@ -6,7 +6,7 @@
  *   copyright            : (C) 2009 Viarre Régis
  *   email                : crowkait@phpboost.com
  *
-###################################################
+ ###################################################
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,76 +22,72 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
-###################################################*/
-
-
+ ###################################################*/
 
 /**
  * @author Régis Viarre <crowkait@phpboost.com>
- * @desc This class manage multi-line text fields.
+ * @desc This class manage single-line text fields.
+ * It provides you additionnal field options :
+ * <ul>
+ * 	<li>size : The maximum size for the field</li>
+ * 	<li>maxlength : The maximum length for the field</li>
+ * 	<li>required_alert : Text displayed if field is empty (javscript only)</li>
+ * </ul>
  * @package builder
  * @subpackage form
  */
-class FormTextarea extends FormField
+class FormTextEdit extends FormField
 {
-	private $rows = 10; //Rows for the textarea.
-	private $cols = 47; //Cols for the textarea.
-	private $editor = true; //Allow to hide the editor.
-	private $forbidden_tags = array(); //Forbiddend tags in the content.
-	
-	public function __construct($field_id, $field_value, array $field_options = array(), array $constraints = array())
+	private $size = '';
+	private $maxlength = '';
+
+	public function __construct($field_id, $field_options = array(), array $constraints = array())
 	{
-		parent::__construct($field_id, $field_value, $field_options, $constraints);
+		parent::__construct($field_id, $field_options, $constraints);
 		$this->compute_fields_options($field_options);
 	}
-	
+
 	/**
-	 * @return string The html code for the textarea.
+	 * @return string The html code for the input.
 	 */
 	public function display()
 	{
-		$Template = new Template('framework/builder/forms/field_extended.tpl');
+		$template = new Template('framework/builder/forms/field.tpl');
 			
-		$field = '<textarea type="text" ';
-		$field .= 'rows="' . $this->rows . '" ';
-		$field .= 'cols="' . $this->cols . '" ';
-		$field .= 'name="' . $this->name . '" ';
+		$field = '<input type="text" ';
+		$field .= !empty($this->size) ? 'size="' . $this->size . '" ' : '';
+		$field .= !empty($this->maxlength) ? 'maxlength="' . $this->maxlength . '" ' : '';
+		$field .= !empty($this->name) ? 'name="' . $this->name . '" ' : '';
 		$field .= !empty($this->id) ? 'id="' . $this->id . '" ' : '';
-		$field .= !empty($this->css_class) ? 'class="' . $this->css_class . '"> ' : '>';
-		$field .= $this->value;
-		$field .= '</textarea>';
-		
-		$Template->assign_vars(array(
+		$field .= 'value="' . $this->value . '" ';
+		$field .= !empty($this->css_class) ? 'class="' . $this->css_class . '" ' : '';
+		$field .= !empty($this->on_blur) ? 'onblur="' . $this->on_blur . '" ' : '';
+		$field .= '/>';
+
+		$template->assign_vars(array(
 			'ID' => $this->id,
 			'FIELD' => $field,
-			'KERNEL_EDITOR' => $this->editor ? display_editor($this->id, $this->forbidden_tags) : '',
 			'L_FIELD_TITLE' => $this->title,
 			'L_EXPLAIN' => $this->sub_title,
 			'L_REQUIRE' => $this->required ? '* ' : ''
-		));	
-		
-		return $Template->parse(Template::TEMPLATE_PARSER_STRING);
+		));
+
+		return $template->parse(Template::TEMPLATE_PARSER_STRING);
 	}
 
 	private function compute_fields_options(array $field_options)
 	{
-		foreach($field_options as $attribute => $value)
+		foreach ($field_options as $attribute => $value)
 		{
 			$attribute = strtolower($attribute);
 			switch ($attribute)
 			{
-				case 'rows' :
-					$this->rows = $value;
-				break;
-				case 'cols' :
-					$this->cols = $value;
-				break;
-				case 'editor' :
-					$this->editor = $value;
-				break;
-				case 'forbiddentags' :
-					$this->forbidden_tags = $value;
-				break;
+				case 'size' :
+					$this->size = $value;
+					break;
+				case 'maxlength' :
+					$this->maxlength = $value;
+					break;
 				default :
 					throw new FormBuilderException(sprintf('Unsupported option %s with field ' . __CLASS__, $attribute));
 			}
