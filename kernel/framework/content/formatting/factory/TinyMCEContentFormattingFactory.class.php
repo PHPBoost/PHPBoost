c33bd9ffcac07a4a3ab06020dd830456a7ff1a3a
benoit.sautel <benoit.sautel@localhost>
2009-12-20 10:46:08 +0000

    Refactoring des ContentFormattingFactory

---
+++ kernel/framework/content/formatting/factory/TinyMCEContentFormattingFactory.class.php
@@ -0,0 +1,58 @@
+<?php
+/*##################################################
+ *                    AbstractContentFormattingFactory.class.php
+ *                            -------------------
+ *   begin                : December 20, 2000
+ *   copyright            : (C) 2009 Benoit Sautel
+ *   email                :  ben.popeye@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @package content
+ * @subpackage formatting/factory
+ * @author Benoît Sautel <ben.popeye@phpboost.com>
+ * @desc This class is a factory which generates every formatting element required by the TinyMCE
+ * formatting system.
+ */
+abstract class AbstractContentFormattingFactory
+{
+	public function get_parser()
+	{
+		return new TinyMCEParser();
+	}
+
+	public function get_unparser()
+	{
+		return new TinyMCEUnparser();
+	}
+
+	public function get_second_parser()
+	{
+		return new ContentSecondParser();
+	}
+
+	public function get_editor()
+	{
+		return new TinyMCEEditor();
+	}
+}
+
+?>
