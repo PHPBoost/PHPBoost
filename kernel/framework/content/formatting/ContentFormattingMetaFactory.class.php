44bd7f46e58974039650e955fa55d72bd19aa9c9
benoit.sautel <benoit.sautel@localhost>
2010-01-09 20:53:15 +0000

    Correction d'un bug suite ? un refactoring pas enti?rement fait.

--- kernel/framework/content/formatting/ContentFormattingMetaFactory.class.php
+++ kernel/framework/content/formatting/ContentFormattingMetaFactory.class.php
@@ -78,7 +78,7 @@ class ContentFormattingMetaFactory
 	 * @desc Returns the name of the editor of the current user (chosen in its profile).
 	 * @return string The editor used by the current user.
 	 */
-	private static function get_default_language()
+	public static function get_default_language()
 	{
 		return AppContext::get_user()->get_attribute('user_editor');
 	}
