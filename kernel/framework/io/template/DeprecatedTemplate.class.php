c62f6676513f9ac4be9a4452c1527fa9cb30440d
benoit.sautel <benoit.sautel@localhost>
2010-02-14 11:58:14 +0000

    Correction d'un bug sur la classe DepreactedTemplate.

--- kernel/framework/io/template/DeprecatedTemplate.class.php
+++ kernel/framework/io/template/DeprecatedTemplate.class.php
@@ -54,7 +54,7 @@ class DeprecatedTemplate extends FileTemplate
 	{
 		foreach ($array_tpl as $identifier => $filename)
 		{
-			$new_template = new FileTemplate($filename, Template::DONOT_LOAD_FREQUENT_VARS);
+			$new_template = new FileTemplate($filename);
 			$module_data_path = $new_template->get_data()->get_var('PICTURES_DATA_PATH');
 				
 			$this->bind_vars($new_template);
