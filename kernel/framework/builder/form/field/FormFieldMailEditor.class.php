fd32a744603d74dcb7e6ea7c62bb5f038a4d5bbb
benoit.sautel <benoit.sautel@localhost>
2010-03-12 20:24:07 +0000

    Cr?ation d'une page dans le bac ? sable pour tester l'envoi de mail.

---
+++ kernel/framework/builder/form/field/FormFieldMailEditor.class.php
@@ -0,0 +1,50 @@
+<?php
+/*##################################################
+ *                             FormFieldTextEditor.class.php
+ *                            -------------------
+ *   begin                : April 28, 2009
+ *   copyright            : (C) 2009 Viarre Régis
+ *   email                : crowkait@phpboost.com
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @author Régis Viarre <crowkait@phpboost.com>
+ * @desc This class manages a mail address.
+ * @package builder
+ * @subpackage form
+ */
+class FormFieldMailEditor extends FormFieldTextEditor
+{
+    /**
+     * @desc Constructs a FormFieldMailEditor.
+     * @param string $id Field identifier
+     * @param string $label Field label
+     * @param string $value Default value
+     * @param string[] $field_options Map containing the options
+     * @param FormFieldConstraint[] $constraints The constraints checked during the validation
+     */
+    public function __construct($id, $label, $value, $field_options = array(), array $constraints = array())
+    {
+        $this->css_class = "text";
+        $constraints[] = new MailFormFieldConstraint();
+        parent::__construct($id, $label, $value, $field_options, $constraints);
+    }
+}
+?>
