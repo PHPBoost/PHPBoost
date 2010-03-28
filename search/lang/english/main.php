b7539729537d75144e37ae35437e4d2cbd7ad974
loic.rouchon <loic.rouchon@localhost>
2010-03-28 10:40:18 +0000

    R??criture partielle en cours du module recherche

---
+++ search/lang/english/main.php
@@ -0,0 +1,38 @@
+<?php
+/*##################################################
+ *                              main.php
+ *                            -------------------
+ *   begin                : March 28, 2010
+ *   copyright            : (C) 2008 Rouchon Loïc
+ *   email                : loic.rouchon@phpboost.com
+ *
+ *  
+ ###################################################
+ *
+ *   This program is a free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+ #                     English                      #
+ ####################################################
+
+$lang = array();
+
+$lang['page_title'] = 'Search';
+$lang['title'] = 'Search';
+$lang['do_search'] = 'Search';
+?>
