0676f2abf5f15eae64c81247e7e846aa27eceff0
loic.rouchon <loic.rouchon@localhost>
2010-04-14 18:38:22 +0000

    Composant de formulaire permettant de g?rer des actions link

---
+++ kernel/framework/builder/form/field/FormFieldActionLink.class.php
@@ -0,0 +1,67 @@
+<?php
+/*##################################################
+ *                        FormFieldActionLink.class.php
+ *                            -------------------
+ *   begin                : April 14, 2010
+ *   copyright            : (C) 2010 Loic Rouchon
+ *   email                : loic.rouchon@phpboost.com
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @author Loic Rouchon <loic.rouchon@phpboost.com>
+ * @desc This class manage an action link.
+ * @package builder
+ * @subpackage form/field
+ */
+class FormFieldActionLink extends AbstractFormField
+{
+	/**
+	 * @var FormFieldActionLinkElement
+	 */
+	private $action;
+
+	/**
+     * @param string $id the form field id
+     * @param string $title the action title
+     * @param Url $url the action url
+     * @param Url $img the action icon url
+	 */
+	public function __construct($id, $title, $url, $img)
+	{
+		$this->action = new FormFieldActionLinkElement($title, $url, $img);
+		parent::__construct($id, '', '');
+	}
+
+	/**
+	 * @return string The html code for the field.
+	 */
+	public function display()
+	{
+	    $field = new FormFieldActionLinkList($this->id, array($this->action));
+	    return $field->display();
+	}
+
+    protected function get_default_template()
+    {
+        return new FileTemplate('framework/builder/form/FormFieldActionLinkList.tpl');
+    }
+}
+
+?>
---
+++ templates/default/framework/builder/form/FormFieldActionLinkList.tpl
@@ -0,0 +1,12 @@
+<div style="text-align:center;">
+    <ul style="margin:5px;list-style-type:none;">
+# START action #
+        <li style="margin:2px 10px;display:inline-table;">
+            <a href="{action.U_LINK}">
+                <img src="{action.U_IMG}" alt="{action.E_TITLE}" />
+            </a><br />
+            <a href="{action.U_LINK}">{action.E_TITLE}</a>
+        </li>
+# END action #
+    </ul>
+</div>
