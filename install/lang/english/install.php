9d2aa1a633eaf3ed5ab9209c80bd11937e6c5ed3
soldier weasel <soldier.weasel@gmail.com>
2010-05-30 09:13:16 +0000

    Ajout de l'insertion des donn?es dans le nouvel installateur pour pouvoir tester l'installateur au complet ( n'est pas encore op?rationnel )

---
+++ install/lang/english/install.php
@@ -0,0 +1,47 @@
+<?php
+/*##################################################
+ *                                install.php
+ *                            -------------------
+ *   begin                : May 30, 2010
+ *   copyright            : (C) 2010 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+#                      English                      #
+ ####################################################
+
+
+$lang = array(
+   'install.rank.admin' => 'Administrator',
+   'install.rank.modo' => 'Moderator',
+   'install.rank.inactiv' => 'Inactive booster',
+   'install.rank.fronde' => 'Slingshot booster',
+   'install.rank.minigun' => 'Minigun booster',
+   'install.rank.fuzil' => 'Fuzil booster',
+   'install.rank.bazooka' => 'Bazooka booster',
+   'install.rank.roquette' => 'Rocket booster',
+   'install.rank.mortier' => 'Mortar booster',
+   'install.rank.missile' => 'Missile booster',
+   'install.rank.fusee' => 'Spaceship boost'
+);
+?>
---
+++ install/lang/french/install.php
@@ -0,0 +1,47 @@
+<?php
+/*##################################################
+ *                                install.php
+ *                            -------------------
+ *   begin                : May 30, 2010
+ *   copyright            : (C) 2010 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+#                      French                      #
+ ####################################################
+
+
+$lang = array(
+   'install.rank.admin' => 'Administrateur',
+   'install.rank.modo' => 'Modérateur',
+   'install.rank.inactiv' => 'Boosteur Inactif',
+   'install.rank.fronde' => 'Booster Fronde',
+   'install.rank.minigun' => 'Booster Minigun',
+   'install.rank.fuzil' => 'Booster Fuzil',
+   'install.rank.bazooka' => 'Booster Bazooka',
+   'install.rank.roquette' => 'Booster Roquette',
+   'install.rank.mortier' => 'Booster Mortier',
+   'install.rank.missile' => 'Booster Missile',
+   'install.rank.fusee' => 'Booster Fusée'
+);
+?>
