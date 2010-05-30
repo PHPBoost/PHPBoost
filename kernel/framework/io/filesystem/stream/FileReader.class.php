72d5d06063f1387faa913f28f0b38a4f3a45f430
benoit.sautel <benoit.sautel@localhost>
2010-05-30 08:12:16 +0000

    Cr?ation d'un BufferedFileReader pour lire les gros dumps sans saturer la m?moire maximale allou?e au script PHP.

--- kernel/framework/io/filesystem/stream/FileReader.class.php
+++ kernel/framework/io/filesystem/stream/FileReader.class.php
@@ -1,6 +1,6 @@
 <?php
 /*##################################################
- *                       BufferedFileWriter.class.php
+ *                          FileReader.class.php
  *                            -------------------
  *   begin                : May 29, 2010
  *   copyright            : (C) 2010 Benoit Sautel
@@ -25,49 +25,10 @@
  *
  ###################################################*/
 
-class BufferedFileWriter implements FileWriter
+interface FileReader
 {
-	const DEFAULT_BUFFER_SIZE = 100000;
+	function read_all();
 	
-	/**
-	 * @var File
-	 */
-	private $file;
-	private $buffer = '';
-	private $buffer_max_size;
-	
-	public function __construct(File $file, $buffer_size = self::DEFAULT_BUFFER_SIZE)
-	{
-		$this->file = $file;
-		$this->buffer_max_size = $buffer_size;
-	}
-	
-	public function append($content)
-	{
-		if ($this->will_exceed_buffer_size($content))
-		{
-			$this->flush();
-			$this->buffer = $content;
-		}
-		else
-		{
-			$this->append_to_buffer($content);
-		}
-	}
-	
-	private function will_exceed_buffer_size($content)
-	{
-		return strlen($this->buffer) + strlen($content) > $this->buffer_max_size;
-	}
-	
-	public function flush()
-	{
-		$this->file->append($this->buffer);
-		
-	}
-	
-	private function append_to_buffer($content)
-	{
-		$this->buffer .= $content;
-	}
+	function read_line();
 }
+?>
