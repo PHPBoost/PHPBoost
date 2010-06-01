5c702dfec696df5c050f541b8488ad6cab135fe8
soldier weasel <soldier.weasel@gmail.com>
2010-06-01 18:03:53 +0000

    MAJ du module news, cr?ation du "fausse cat?gorie" dans le fichier de cache avec les autorisations du module ( pour ?viter de redevoir d?finir les autorisations et cr?er du surplus de code, petite correction sur les dates de parutions

--- news/news.php
+++ news/news.php
@@ -50,17 +50,9 @@ if (!empty($idnews)) // On affiche la news correspondant à l'id envoyé.
 	$news = $Sql->fetch_assoc($result);
 	$Sql->query_close($result);
 
-	$cat = !empty($NEWS_CAT[$news['idcat']]) && $User->check_auth($NEWS_CAT[$news['idcat']]['auth'], AUTH_NEWS_READ)
+	if (!empty($news['id']) && !empty($NEWS_CAT[$news['idcat']]) && $User->check_auth($NEWS_CAT[$news['idcat']]['auth'], AUTH_NEWS_READ)
 		&& ($User->check_auth($NEWS_CAT[$news['idcat']]['auth'], AUTH_NEWS_MODERATE) || ($news['visible'] && $news['start'] < $now->get_timestamp())
-		|| $User->check_auth($NEWS_CAT[$news['idcat']]['auth'], AUTH_NEWS_WRITE) && $news['user_id'] == $User->get_attribute('user_id'));
-		
-	$not_cat = $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_READ)
-		&& ($User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_MODERATE) || ($news['visible'] && $news['start'] < $now->get_timestamp())
-		|| $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_WRITE) && $news['user_id'] == $User->get_attribute('user_id'));
-		
-	$read_id = $news['idcat'] > 0 ? $cat : $not_cat;
-	
-	if (!empty($news['id']) && $read_id)
+		|| $User->check_auth($NEWS_CAT[$news['idcat']]['auth'], AUTH_NEWS_WRITE) && $news['user_id'] == $User->get_attribute('user_id')))
 	{	
 		
 		$Sql->query_inject("UPDATE " . DB_TABLE_NEWS . " SET compt = compt + 1 WHERE id = '" . $idnews . "'", __LINE__, __LINE__); //MAJ du compteur.
@@ -83,10 +75,10 @@ if (!empty($idnews)) // On affiche la news correspondant à l'id envoyé.
 		$news_cat->build_children_id_list(0, $array_cat, RECURSIVE_EXPLORATION, DO_NOT_ADD_THIS_CATEGORY_IN_LIST, AUTH_NEWS_READ);
 
 		// News suivante.
-		$next_news = $Sql->query_array(DB_TABLE_NEWS, "title", "id", "WHERE visible = 1 AND timestamp > '" . $news['timestamp'] . "' AND start <= '" . $now->get_timestamp() . "' AND idcat IN (" . implode(', ', $array_cat) . ") ORDER BY timestamp ASC" . $Sql->limit(0, 1), __LINE__, __FILE__);
+		$next_news = $Sql->query_array(DB_TABLE_NEWS, "title", "id", "WHERE visible = 1 AND timestamp > '" . $news['timestamp'] . "' AND start <= '" . $now->get_timestamp() . "' ORDER BY timestamp ASC" . $Sql->limit(0, 1), __LINE__, __FILE__);
 
 		// News précédente
-		$previous_news = $Sql->query_array(DB_TABLE_NEWS, "title", "id", "WHERE visible = 1 AND timestamp < '" . $news['timestamp'] . "' AND start <= '" . $now->get_timestamp() . "' AND idcat IN (" . implode(', ', $array_cat) . ") ORDER BY timestamp DESC" . $Sql->limit(0, 1), __LINE__, __FILE__);
+		$previous_news = $Sql->query_array(DB_TABLE_NEWS, "title", "id", "WHERE visible = 1 AND timestamp < '" . $news['timestamp'] . "' AND start <= '" . $now->get_timestamp() . "' ORDER BY timestamp DESC" . $Sql->limit(0, 1), __LINE__, __FILE__);
 
 		// Suggestion de news.
 		$nbr_suggested = 0;
@@ -191,65 +183,57 @@ elseif ($user)
 	// Build array with the children categories.
 	$array_cat = array();
 	$news_cat->build_children_id_list(0, $array_cat, RECURSIVE_EXPLORATION, DO_NOT_ADD_THIS_CATEGORY_IN_LIST, AUTH_NEWS_WRITE);
-
-	if (!empty($array_cat))
-	{
 		
-		$result = $Sql->query_while("SELECT n.contents, n.extend_contents, n.title, n.id, n.idcat, n.timestamp, n.user_id, n.img, n.alt, n.nbr_com, m.login, m.level
-		FROM " . DB_TABLE_NEWS . " n
-		LEFT JOIN " . DB_TABLE_MEMBER . " m ON m.user_id = n.user_id
-		WHERE (n.start > '" . $now->get_timestamp() . "' OR n.visible = '0') AND n.user_id = '" . $User->get_attribute('user_id') . "' AND idcat IN (" . implode(', ', $array_cat) . ")
-		ORDER BY n.timestamp DESC", __LINE__, __FILE__);
-		while ($row = $Sql->fetch_assoc($result))
-		{
-			$timestamp = new Date(DATE_TIMESTAMP, TIMEZONE_AUTO, $row['timestamp']);
-
-			$tpl->assign_block_vars('news', array(
-				'ID' => $row['id'],
-				'U_SYNDICATION' => url('../syndication.php?m=news&amp;cat=' . $row['idcat']),
-				'U_LINK' => 'news' . url('.php?id=' . $row['id'], '-' . $row['idcat'] . '-' . $row['id'] . '+' . Url::encode_rewrite($row['title']) . '.php'),
-				'TITLE' => $row['title'],
-				'C_EDIT' => true,
-				'C_DELETE' => true,
-				'C_IMG' => !empty($row['img']),
-				'IMG' => FormatingHelper::second_parse_url($row['img']),
-				'IMG_DESC' => $row['alt'],
-				'C_ICON' => $NEWS_CONFIG['activ_icon'],
-				'U_CAT' => $row['idcat'] > 0 ? 'news' . url('.php?cat=' . $row['idcat'], '-' . $row['idcat'] . '+' . Url::encode_rewrite($NEWS_CAT[$row['idcat']]['name']) . '.php') : '',
-				'ICON' => $row['idcat'] > 0 ? FormatingHelper::second_parse_url($NEWS_CAT[$row['idcat']]['image']) : '',
-				'CONTENTS' => FormatingHelper::second_parse($row['contents']),
-				'EXTEND_CONTENTS' => !empty($row['extend_contents']) ? '<a style="font-size:10px" href="' . PATH_TO_ROOT . '/news/news' . url('.php?id=' . $row['id'], '-0-' . $row['id'] . '.php') . '">[' . $NEWS_LANG['extend_contents'] . ']</a><br /><br />' : '',
-				'PSEUDO' => $NEWS_CONFIG['display_author'] && !empty($row['login']) ? $row['login'] : '',
-				'U_USER_ID' => '../member/member' . url('.php?id=' . $row['user_id'], '-' . $row['user_id'] . '.php'),
-				'LEVEL' =>	isset($row['level']) ? $level[$row['level']] : '',
-				'DATE' => $NEWS_CONFIG['display_date'] ? sprintf($NEWS_LANG['on'], $timestamp->format(DATE_FORMAT_SHORT, TIMEZONE_AUTO)) : '',
-			    'FEED_MENU' => Feed::get_feed_menu(FEED_URL)
-			));
-
-			$i++;
-		}
-
-		$Sql->query_close($result);
-
-		if ($i == 0)
-		{
-			$tpl->assign_vars(array(
-				'C_NEWS_NO_AVAILABLE' => true,
-				'L_LAST_NEWS' => $NEWS_LANG['waiting_news'],
-				'L_NO_NEWS_AVAILABLE' => $NEWS_LANG['no_news_available'],
-				'C_ADD' => $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_CONTRIBUTE) || $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_WRITE),
-				'U_ADD' => url('management.php?new=1'),
-				'L_ADD' => $NEWS_LANG['add_news']
-			));
-		}
-
-		$tpl->display();
-	}
-	else
+	$result = $Sql->query_while("SELECT n.contents, n.extend_contents, n.title, n.id, n.idcat, n.timestamp, n.user_id, n.img, n.alt, n.nbr_com, m.login, m.level
+	FROM " . DB_TABLE_NEWS . " n
+	LEFT JOIN " . DB_TABLE_MEMBER . " m ON m.user_id = n.user_id
+	WHERE (n.start > '" . $now->get_timestamp() . "' OR n.visible = '0') AND n.user_id = '" . $User->get_attribute('user_id') . "'
+	ORDER BY n.timestamp DESC", __LINE__, __FILE__);
+	while ($row = $Sql->fetch_assoc($result))
 	{
-		AppContext::get_response()->redirect(Environment::get_home_page());
-		exit;
+		$timestamp = new Date(DATE_TIMESTAMP, TIMEZONE_AUTO, $row['timestamp']);
+
+		$tpl->assign_block_vars('news', array(
+			'ID' => $row['id'],
+			'U_SYNDICATION' => url('../syndication.php?m=news&amp;cat=' . $row['idcat']),
+			'U_LINK' => 'news' . url('.php?id=' . $row['id'], '-' . $row['idcat'] . '-' . $row['id'] . '+' . Url::encode_rewrite($row['title']) . '.php'),
+			'TITLE' => $row['title'],
+			'C_EDIT' => true,
+			'C_DELETE' => true,
+			'C_IMG' => !empty($row['img']),
+			'IMG' => FormatingHelper::second_parse_url($row['img']),
+			'IMG_DESC' => $row['alt'],
+			'C_ICON' => $NEWS_CONFIG['activ_icon'],
+			'U_CAT' => $row['idcat'] > 0 ? 'news' . url('.php?cat=' . $row['idcat'], '-' . $row['idcat'] . '+' . Url::encode_rewrite($NEWS_CAT[$row['idcat']]['name']) . '.php') : '',
+			'ICON' => $row['idcat'] > 0 ? FormatingHelper::second_parse_url($NEWS_CAT[$row['idcat']]['image']) : '',
+			'CONTENTS' => FormatingHelper::second_parse($row['contents']),
+			'EXTEND_CONTENTS' => !empty($row['extend_contents']) ? '<a style="font-size:10px" href="' . PATH_TO_ROOT . '/news/news' . url('.php?id=' . $row['id'], '-0-' . $row['id'] . '.php') . '">[' . $NEWS_LANG['extend_contents'] . ']</a><br /><br />' : '',
+			'PSEUDO' => $NEWS_CONFIG['display_author'] && !empty($row['login']) ? $row['login'] : '',
+			'U_USER_ID' => '../member/member' . url('.php?id=' . $row['user_id'], '-' . $row['user_id'] . '.php'),
+			'LEVEL' =>	isset($row['level']) ? $level[$row['level']] : '',
+			'DATE' => $NEWS_CONFIG['display_date'] ? sprintf($NEWS_LANG['on'], $timestamp->format(DATE_FORMAT_SHORT, TIMEZONE_AUTO)) : '',
+			'FEED_MENU' => Feed::get_feed_menu(FEED_URL)
+		));
+
+		$i++;
 	}
+
+	$Sql->query_close($result);
+
+	if ($i == 0)
+	{
+		$tpl->assign_vars(array(
+			'C_NEWS_NO_AVAILABLE' => true,
+			'L_LAST_NEWS' => $NEWS_LANG['waiting_news'],
+			'L_NO_NEWS_AVAILABLE' => $NEWS_LANG['no_news_available'],
+			'C_ADD' => $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_CONTRIBUTE) || $User->check_auth($NEWS_CONFIG['global_auth'], AUTH_NEWS_WRITE),
+			'U_ADD' => url('management.php?new=1'),
+			'L_ADD' => $NEWS_LANG['add_news']
+		));
+	}
+
+	$tpl->display();
+
 }
 else
 {
