281388f351afe41badb199bbf3a465e0e88eed0c
loic.rouchon <loic.rouchon@localhost>
2010-09-12 17:43:46 +0000

    Avancement sur le nouvel installateur

---
+++ install/controllers/InstallLicenseController.class.php
@@ -0,0 +1,77 @@
+<?php
+/*##################################################
+ *                         InstallLicenseController.class.php
+ *                            -------------------
+ *   begin                : September 12 2010
+ *   copyright            : (C) 2010 Loic Rouchon
+ *   email                : loic.rouchon@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class InstallLicenseController extends InstallController
+{
+
+	public function execute(HTTPRequest $request)
+	{
+        parent::load_lang($request);
+		$view = new FileTemplate('install/license.tpl');
+		$this->add_build_form($view);
+		$this->add_navigation($view);
+		return $this->create_response($view);
+	}
+
+	/**
+	 * @param Template $view
+	 * @return InstallDisplayResponse
+	 */
+	private function create_response(Template $view)
+	{
+        $step_title = $this->lang['step.license.title'];
+		$response = new InstallDisplayResponse(1, $step_title, $view);
+		return $response;
+	}
+
+	private function add_build_form(Template $view)
+    {
+    	$form = new HTMLForm('licenseForm', InstallUrlBuilder::server_configuration()->absolute());
+    	$fieldset = new FormFieldsetHTML('agreementFieldset', $this->lang['step.license.terms.title']);
+    	$form->add_fieldset($fieldset);
+    	$agreement = new FormFieldHTML('agreementExplanation', $this->lang['step.license.require.agreement'] . '<br /><br />');
+    	$fieldset->add_field($agreement);
+    	$license_content = file_get_contents_emulate('license.txt');
+    	$license_block = '<div style="width:auto;height:340px;overflow-y:scroll;border:1px solid #DFDFDF;background-color:#F1F4F1">' . $license_content . '</div>';
+    	$license = new FormFieldHTML('licenseContent', $license_block);
+    	$fieldset->add_field($license);
+    	$agree_checkbox = new FormFieldCheckbox('agree', $this->lang['step.license.please_agree']);
+    	$agree_checkbox->add_constraint(new FormFieldConstraintNotEmpty());
+    	$fieldset->add_field($agree_checkbox);
+    	$form->add_button(new FormButtonSubmit('submit', 'submit'));
+		$view->add_subtemplate('LICENSE_FORM', $form->display());
+    }
+
+	private function add_navigation(Template $view)
+    {
+    	$view->assign_vars(array(
+    		'PREVIOUS_STEP_URL' => InstallUrlBuilder::welcome()->absolute(),
+    		'NEXT_STEP_URL' => InstallUrlBuilder::server_configuration()->absolute()
+    	));
+    }
+}
+?>
---
+++ install/templates/license.tpl
@@ -0,0 +1,28 @@
+<h1>${i18n('step.license.agreement')}</h1>
+# INCLUDE LICENSE_FORM #
+<!--<form action="{NEXT_STEP_URL}" method="post" onsubmit="return checkLicenseAgreement" class="fieldset_content">-->
+<!--	<fieldset>-->
+<!--		<legend>-->
+<!--			${i18n('step.license.terms.title')}-->
+<!--		</legend>-->
+<!--		${i18nraw('step.license.require.agreement')}-->
+<!--		<br />-->
+<!--		<br />-->
+<!--		<div style="width:auto;height:340px;overflow-y:scroll;border:1px solid #DFDFDF;background-color:#F1F4F1">-->
+<!--			{GPL_LICENSE}-->
+<!--		</div>-->
+<!--		<div style="text-align:center;margin:15px;margin-bottom:10px;">-->
+<!--			<label style="cursor:pointer;">-->
+<!--				<input type="checkbox" name="license_agreement" id="license_agreement" class="valign_middle" />-->
+<!--				${i18n('step.license.please_agree')}-->
+<!--			</label>-->
+<!--		</div>-->
+<!--	</fieldset>-->
+<!--	-->
+<!--	<fieldset class="submit_case">-->
+<!--		<a href="{PREVIOUS_STEP_URL}" title="${i18n('step.welcome.title')}" ><img src="templates/images/left.png" alt="${i18n('step.welcome.title')}" class="valign_middle" /></a>-->
+<!--		&nbsp;-->
+<!--		<input type="image" src="templates/images/right.png" title="${i18n('step.server.title')}" class="img_submit" />-->
+<!--		<input type="hidden"  name="submit" value="next" />-->
+<!--	</fieldset>		-->
+<!--</form>-->
