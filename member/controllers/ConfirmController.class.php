73a06e3b54654ed950a35bd8e076c743dcf54046
soldier weasel <soldier.weasel@gmail.com>
2010-09-19 09:29:38 +0000

    Avancement sur la mise ? jour de tout le dossier member.

---
+++ member/controllers/ConfirmController.class.php
@@ -0,0 +1,91 @@
+<?php
+/*##################################################
+ *                       ConfirmController.class.php
+ *                            -------------------
+ *   begin                : September 18, 2010 2009
+ *   copyright            : (C) 2010 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class ConfirmController extends AbstractController
+{
+	public function execute(HTTPRequest $request)
+	{
+		$view = new StringTemplate('# INCLUDE form #');
+
+		if(AppContext::get_user()->check_level(MEMBER_LEVEL))
+		{
+			// TODO
+			$form = $this->already_registered();
+		}
+		else
+		{
+			$form = $this->check_activation($request->get_getint('key'));
+		}
+		
+		
+		$view->add_subtemplate('form', $form->display());
+		return new SiteDisplayResponse($view);
+	}
+	
+	private function already_registered()
+	{
+		// You are already registered !
+		// Redirect ...
+		// TODO
+		
+		$form = new HTMLForm('already_register');
+		
+		$fieldset = new FormFieldsetHTML('already_register', 'Vous êtes déjà enregistré !');
+		$fieldset->set_description('Vous êtes déjà enregistré !');
+		$form->add_fieldset($fieldset);
+		
+		return $form;
+	}
+	
+	private function check_activation($key)
+	{
+		$check_mbr = ConfirmHelper::check_activation_pass_exist($key);
+		if ($check_mbr)
+		{
+			ConfirmHelper::update_aprobation($key);
+			
+			$this->activation_sucess();
+		}
+		else
+		{
+			$this->activation_error();
+		}
+	}
+	
+	private function activation_sucess()
+	{
+		return LangLoader::get_message('activ_mbr_mail_success', 'main');
+	}
+	
+	private function activation_error()
+	{
+		return LangLoader::get_message('activ_mbr_mail_error', 'main');
+	}
+	
+}
+
+?>
---
+++ member/controllers/MemberHomeController.class.php
@@ -0,0 +1,53 @@
+<?php
+/*##################################################
+ *                       MemberHomeController.class.php
+ *                            -------------------
+ *   begin                : February 10, 2010
+ *   copyright            : (C) 2010 Benoit Sautel
+ *   email                : ben.popeye@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class MemberHomeController extends AbstractController
+{
+	private static $tpl_src = '<h1>parts</h1>
+	<ul class="bb_ul">
+	# START parts #
+		<li><a href="{parts.URL}">{parts.NAME}</a>
+	# END parts #
+	</ul>';
+
+	private static $links = array('Register' => '?url=/register', 'Confirm key member' => '?url=/confirm');
+
+	public function execute(HTTPRequest $request)
+	{
+		$tpl = new StringTemplate(self::$tpl_src);
+
+		foreach (self::$links as $name => $url)
+		{
+			$tpl->assign_block_vars('parts', array(
+				'URL' => $url,
+				'NAME' => $name
+			));
+		}
+		return new SiteDisplayResponse($tpl);
+	}
+}
+?>
