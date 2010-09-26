84cf8ad911feb62e6d4e3dcb9205462e38f38a42
benoit.sautel <benoit.sautel@localhost>
2010-09-26 09:41:47 +0000

    La commande de lancement de test unitaire est maintenant capable de lancer plusieurs tests qui lui sont pass?s en param?tre.

--- test/cli/RunUnitTestCommand.class.php
+++ test/cli/RunUnitTestCommand.class.php
@@ -25,34 +25,50 @@
  *
  ###################################################*/
 
-class RunUnitTestCommand implements CLICommand 
+class RunUnitTestCommand implements CLICommand
 {
+	private $tests = null;
+
 	public function short_description()
 	{
 		return "executes a unit test";
 	}
-	
+
 	public function execute(array $args)
 	{
-		if (count($args) !== 1)
+		if (count($args) == 0)
 		{
-			CLIOutput::err('Must take 1 parameter');
+			CLIOutput::err('No test to run');
 			return;
 		}
-		$class_to_test = $args[0];
-		$class_path = $this->get_class_to_test_path($class_to_test);
-		CLIOutput::writeln('Running test ' . $class_to_test);
 		import('/test/PHPUnit/TextUI/TestRunner', '.php');
 		import('/test/PHPUnit/Framework/TestCase', '.php');
+		foreach ($args as $class_to_test)
+		{
+			try
+			{
+				$this->run_test($class_to_test);
+			}
+			catch (TestNotFoundException $e)
+			{
+				CLIOutput::err('Test ' . $e->get_test_name() . ' not found. Please check that it exists.');
+			}
+		}
+	}
+
+	private function run_test($class_to_test)
+	{
+		$class_path = $this->get_class_to_test_path($class_to_test);
+		CLIOutput::writeln('Running test ' . $class_to_test);
 		include_once $class_path;
 		$class = new ReflectionClass($class_to_test);
 		PHPUnit_TextUI_TestRunner::run($class);
+		CLIOutput::writeln();
 	}
-	
+
 	private function get_class_to_test_path($class_name)
 	{
-		import('/test/util/phpboost_unit_tests', INC_IMPORT);
-		$tests = list_tu(PATH_TO_ROOT . '/test/kernel');
+		$tests = $this->get_all_tests();
 		$name_to_find = '/' . $class_name . '.php';
 		$name_to_find_length = strlen($name_to_find);
 		foreach ($tests as $test_path)
@@ -63,12 +79,23 @@ class RunUnitTestCommand implements CLICommand
 				return $test_path;
 			}
 		}
-		throw new Exception('Test class ' . $class_name .  ' not found');
+		throw new TestNotFoundException($class_name);
 	}
-	
+
+	private function get_all_tests()
+	{
+		if ($this->tests === null)
+		{
+			import('/test/util/phpboost_unit_tests', INC_IMPORT);
+			$this->tests = list_tu(PATH_TO_ROOT . '/test/kernel');
+		}
+		return $this->tests;
+	}
+
 	public function help(array $args)
 	{
-		return "";
+		return "Runs unit tests. Parameters are the test class names, there can be as much test as you want.\n" .
+		'To know the available unit tests, run the list goal';
 	}
 }
 
