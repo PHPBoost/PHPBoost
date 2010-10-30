85004cf9ddb3c2549e69032b621d886e04955d5b
loic.rouchon <loic.rouchon@localhost>
2010-10-30 21:41:33 +0000

    Validation de la license

--- install/controllers/InstallLicenseController.class.php
+++ install/controllers/InstallLicenseController.class.php
@@ -27,47 +27,68 @@
 
 class InstallLicenseController extends InstallController
 {
+	/**
+	 * @var HTMLForm
+	 */
+	private $form;
+	/**
+	 * @var FormButtonSubmit
+	 */
+	private $submit;
 
 	public function execute(HTTPRequest $request)
 	{
         parent::load_lang($request);
-		$view = new FileTemplate('install/license.tpl');
-		$this->build_form($view);
-		return $this->create_response($view);
+		$this->build_form();
+		if ($this->submit->has_been_submited())
+		{
+			$this->handle_form();
+		}
+		return $this->create_response();
 	}
 
-	/**
-	 * @param Template $view
-	 * @return InstallDisplayResponse
-	 */
-	private function create_response(Template $view)
+	private function handle_form()
 	{
-        $step_title = $this->lang['step.license.title'];
-		$response = new InstallDisplayResponse(1, $step_title, $view);
-		return $response;
+		if ($this->form->validate())
+		{
+			AppContext::get_response()->redirect(InstallUrlBuilder::server_configuration());
+		}
 	}
 
-	private function build_form(Template $view)
+	private function build_form()
     {
-    	$form = new HTMLForm('licenseForm', InstallUrlBuilder::server_configuration());
+    	$this->form = new HTMLForm('licenseForm');
     	$fieldset = new FormFieldsetHTML('agreementFieldset', $this->lang['step.license.terms.title']);
-    	$form->add_fieldset($fieldset);
+    	$this->form->add_fieldset($fieldset);
     	$agreement = new FormFieldHTML('agreementExplanation', $this->lang['step.license.require.agreement'] . '<br /><br />');
     	$fieldset->add_field($agreement);
-    	$license_content = file_get_contents_emulate('license.txt');
+    	$license_content = file_get_contents_emulate('gpl-license.txt');
     	$license_block = '<div style="width:auto;height:340px;overflow-y:scroll;border:1px solid #DFDFDF;background-color:#F1F4F1">' . $license_content . '</div>';
     	$license = new FormFieldHTML('licenseContent', $license_block);
     	$fieldset->add_field($license);
     	$agree_checkbox = new FormFieldCheckbox('agree', $this->lang['step.license.please_agree']);
     	$agree_checkbox->add_constraint(new FormFieldConstraintNotEmpty());
     	$fieldset->add_field($agree_checkbox);
+
     	$action_fieldset = new FormFieldsetButtons('actions');
 		$back = new FormButtonLink($this->lang['step.previous'], InstallUrlBuilder::welcome(), 'templates/images/left.png');
 		$action_fieldset->add_button($back);
-		$next = new FormButtonSubmitImg($this->lang['step.next'], 'templates/images/right.png', 'license');
-		$action_fieldset->add_button($next);
-		$form->add_fieldset($action_fieldset);
-		$view->put('LICENSE_FORM', $form->display());
+		$this->submit = new FormButtonSubmitImg($this->lang['step.next'], 'templates/images/right.png', 'license');
+		$action_fieldset->add_button($this->submit);
+		$this->form->add_fieldset($action_fieldset);
     }
+
+	/**
+	 * @param Template $view
+	 * @return InstallDisplayResponse
+	 */
+	private function create_response()
+	{
+		$view = new FileTemplate('install/license.tpl');
+		$view->put('LICENSE_FORM', $this->form->display());
+        $step_title = $this->lang['step.license.title'];
+		$response = new InstallDisplayResponse(1, $step_title, $view);
+		return $response;
+	}
 }
 ?>
