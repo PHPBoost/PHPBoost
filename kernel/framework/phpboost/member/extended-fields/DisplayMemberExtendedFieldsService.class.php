6f0204e9e48bc4fd756783d4d9133835445b9e6a
soldier weasel <soldier.weasel@gmail.com>
2010-10-31 10:42:25 +0000

    Ajout de champs Date, Theme, Lang dans les champs ?tendus (t'en es ou benoit sur les fields ? ) dans la classe DisplayMemberExtendedFieldsService

--- kernel/framework/phpboost/member/extended-fields/DisplayMemberExtendedFieldsService.class.php
+++ kernel/framework/phpboost/member/extended-fields/DisplayMemberExtendedFieldsService.class.php
@@ -1,6 +1,6 @@
 <?php
 /*##################################################
- *                      	 DisplayMemberExtendedFields.class.php
+ *                      	 DisplayMemberExtendedFieldsService.class.php
  *                            -------------------
  *   begin                : September 27, 2010
  *   copyright            : (C) 2010 Kévin MASSY
@@ -28,7 +28,7 @@
 /**
  * @author Kévin MASSY <soldier.weasel@gmail.com>
  */
-class DisplayMemberExtendedFields
+class DisplayMemberExtendedFieldsService
 {
 	/*
 	 * This function required object DisplayMemberExtendedField containing user_id and Template. If user is not registered, use object DisplayMemberExtendedField, and define user_id of null.
@@ -40,6 +40,13 @@ class DisplayMemberExtendedFields
 
 		if (count($extend_fields_cache) > 0)
 		{
+			$template = $display_extended_field->get_template();
+			
+			//TODO Var lang
+			$fieldset = new FormFieldsetHTML('other', 'Other');
+			$template->add_fieldset($fieldset);
+			$display_extended_field->set_fieldset($fieldset);
+			
 			$user_id = $display_extended_field->get_user_id();
 			if($user_id !== null)
 			{
@@ -130,6 +137,19 @@ class DisplayMemberExtendedFields
 				case 6:
 					return $this->display_multiple_choice($display_extended_field);
 				break;
+				// Add for list type fields
+				case 7:
+					return $this->display_user_theme($display_extended_field);
+				break;
+				case 8:
+					return $this->display_user_lang($display_extended_field);
+				break;
+				case 9:
+					return $this->display_user_born($display_extended_field);
+				break;
+				case 10:
+					return $this->display_mail_adresse($display_extended_field);
+				break;
 				default:
 					// TODO Error
 			}
@@ -138,7 +158,7 @@ class DisplayMemberExtendedFields
 	
 	private function display_short_field(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();
+		$fieldset = $display_extended_field->get_fieldset();
 		
 		/*
 		$fieldset->add_field(new FormFieldTextEditor($display_extended_field->get_field_name(), $display_extended_field->get_name(), $display_extended_field->get_default_values(), array(
@@ -150,7 +170,7 @@ class DisplayMemberExtendedFields
 	
 	private function display_long_field(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();
+		$fieldset = $display_extended_field->get_fieldset();
 		
 		/*
 		$fieldset->add_field(new FormFieldTextEditor($display_extended_field->get_field_name(), $display_extended_field->get_name(), $display_extended_field->get_default_values(), array(
@@ -162,7 +182,7 @@ class DisplayMemberExtendedFields
 	
 	private function display_simple_select(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();
+		$fieldset = $display_extended_field->get_fieldset();
 		
 		/*
 		$field = array();
@@ -175,20 +195,20 @@ class DisplayMemberExtendedFields
 		}
 		
 		$fieldset->add_field(new FormFieldSelectChoice($display_extended_field->get_field_name(), $display_extended_field->get_name(), $display_extended_field->get_default_values(),
-			array($field)
+			$field
 		));
 		*/
 	}
 	
 	private function display_multiple_select(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();	
+		$fieldset = $display_extended_field->get_fieldset();	
 		
 	}
 	
 	private function display_simple_choice(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();
+		$fieldset = $display_extended_field->get_fieldset();
 		
 		/*
 		$field = array();
@@ -201,7 +221,7 @@ class DisplayMemberExtendedFields
 		}
 		
 		$fieldset->add_field(new FormFieldRadioChoiceOption($display_extended_field->get_field_name(), $display_extended_field->get_name(), $display_extended_field->get_default_values(),
-			array($field)
+			$field
 		));
 		*/
 		
@@ -209,8 +229,92 @@ class DisplayMemberExtendedFields
 	
 	private function display_multiple_choice(DisplayMemberExtendedField $display_extended_field)
 	{
-		$template = $display_extended_field->get_template();
+		$fieldset = $display_extended_field->get_fieldset();
 		
+	}
+	
+	private function display_user_theme(DisplayMemberExtendedField $display_extended_field)
+	{
+		$fieldset = $display_extended_field->get_fieldset();
+		
+		/*$fieldset->add_field(new FormFieldSelectChoice($display_extended_field->get_field_name(), $display_extended_field->get_name(), UserAccountsConfig::load()->get_default_theme(),
+			$this->return_array_theme_for_formubuilder(),
+			array('events' => array('change' => 'change_img_theme(\'img_theme\', HTMLForms.getField("'. $display_extended_field->get_field_name() .'").getValue())'))
+		));
+		$fieldset->add_field(new FormFieldFree('preview_theme', $this->lang['preview'], '<img id="img_theme" src="../templates/'. UserAccountsConfig::load()->get_default_theme() .'/theme/images/theme.jpg" alt="" style="vertical-align:top" />'));
+		*/
+	}
+	
+	private function display_user_lang(DisplayMemberExtendedField $display_extended_field)
+	{
+		$fieldset = $display_extended_field->get_fieldset();
+		
+		/*
+		$fieldset->add_field(new FormFieldSelectChoice($display_extended_field->get_field_name(), $display_extended_field->get_name(), UserAccountsConfig::load()->get_default_lang(),
+			$this->return_array_lang_for_formbuilder()
+		));
+		*/
+	}
+	
+	private function display_user_born(DisplayMemberExtendedField $display_extended_field)
+	{
+		$fieldset = $display_extended_field->get_fieldset();
+		
+		/*
+		$fieldset->add_field(new FormFieldDate($display_extended_field->get_field_name(), $display_extended_field->get_name(), null, 
+			array('description' => $this->lang['valid'])
+		));
+		*/
+	}
+	
+	private function display_mail_adresse(DisplayMemberExtendedField $display_extended_field)
+	{
+		$fieldset = $display_extended_field->get_fieldset();
+		
+		$fieldset->add_field(new FormFieldTextEditor($display_extended_field->get_field_name(), $display_extended_field->get_name(), $display_extended_field->get_default_values(), array(
+			'class' => 'text', 'maxlength' => 255), array(new FormFieldConstraintMailAddress()))
+		);
+	}
+	
+	private function return_array_theme_for_formubuilder()
+	{
+		$choices_list = array();
+		
+		$user_accounts_config = UserAccountsConfig::load();
+		
+		if (!$user_accounts_config->is_users_theme_forced()) // If users can choose the theme they use
+		{
+			foreach(ThemesCache::load()->get_installed_themes() as $theme => $theme_properties)
+			{
+				if (UserAccountsConfig::load()->get_default_theme() == $theme || ($User->check_auth($theme_properties['auth'], AUTH_THEME) && $theme != 'default'))
+				{				
+					$info_theme = load_ini_file('../templates/' . $theme . '/config/', UserAccountsConfig::load()->get_default_lang());
+					$choices_list[] = new FormFieldSelectChoiceOption( $info_theme['name'], $theme);
+				}
+			}
+		}
+		else
+		{
+			$choices_list[] = new FormFieldSelectChoiceOption('Base', 'base');
+		}
+		return $choices_list;
+	}
+	
+	private function return_array_lang_for_formbuilder()
+	{
+		$array = array();
+		$langs_cache = LangsCache::load();
+		foreach($langs_cache->get_installed_langs() as $lang => $properties)
+		{
+			if ($properties['auth'] == -1)
+			{
+				$info_lang = load_ini_file('../lang/', $lang);
+
+				$array[] = new FormFieldSelectChoiceOption($info_lang['name'], $lang);
+			}
+			
+		}
+		return $array;
 	}
 }
 ?>
