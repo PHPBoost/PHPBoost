ae536082169f220f6b99bf62f413dc5e6aa85cca
Regis Viarre <Regis Viarre@localhost>
2010-11-14 10:49:47 +0100

    [Fix] Ajout de l'affichage de la requ??te dans le dbquerier

--- kernel/framework/io/db/NotASingleRowFoundException.class.php
+++ kernel/framework/io/db/NotASingleRowFoundException.class.php
@@ -31,9 +31,10 @@
  */
 class NotASingleRowFoundException extends SQLQuerierException
 {
-    public function __construct()
+    public function __construct(SelectQueryResult $query_result)
     {
-        parent::__construct('multiple rows have been found but the query expect only one result');
+        parent::__construct('multiple rows have been found but the query expect only one result<br />-> ' . 
+        $query_result->get_query() . '<br />' . var_export($query_result->get_parameters(), true));
     }
 }
 
