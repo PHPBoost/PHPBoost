a72d7237154704a8bb025078cf77cfd25eeb2973
Regis Viarre <Regis Viarre@localhost>
2010-11-24 00:01:33 +0100

    [Fix] Correction dans les autorisations dans les menus de liens, les invit??s ne pouvaient pas les voir. Loic si tu peux v??rifier que ??a n'a pas d'effet de bord ??a serait cool.

--- kernel/framework/phpboost/menu/Menu.class.php
+++ kernel/framework/phpboost/menu/Menu.class.php
@@ -119,8 +119,14 @@ abstract class Menu
 	 */
     public function cache_export_begin()
     {
-        if (is_array($this->auth))
-            return '\'; $__auth=' . preg_replace('`[\s]+`', '', var_export($this->auth, true)) . ';if ($User->check_auth($__auth,1)){$__menu.=\'';
+        if (is_array($this->auth) && !empty($this->auth))
+        {
+        	return '\'; $__auth=' . preg_replace('`[\s]+`', '', var_export($this->auth, true)) . ';if ($User->check_auth($__auth,1)){$__menu.=\'';
+        }
+        else if (is_array($this->auth)) 
+        {
+        	return '\'; if ($User->check_auth($__auth,1)) { $__menu.=\''; 
+        }
         return '';
     }
     
