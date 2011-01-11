08e5bee677ded950db6412ce3e0603ca431e7a0e
ReidLos <ReidLos@localhost>
2011-01-11 22:04:19 +0100

    [Maj] Modification des autorisations par d?faut pour les champs ?tendus

--- kernel/framework/phpboost/member/extended-fields/ExtendedField.class.php
+++ kernel/framework/phpboost/member/extended-fields/ExtendedField.class.php
@@ -187,7 +187,7 @@ class ExtendedField
 	
 	public function get_authorization()
 	{
-		return !empty($this->authorization) ? $this->authorization : array('r1' => 3, 'r0' => 3, 'r-1' => 1);
+		return !empty($this->authorization) ? $this->authorization : array('r1' => 3, 'r0' => 3, 'r-1' => 2);
 	}
 	
 	public static function rewrite_field_name($field_name)
