461950818d7bdbf5a68d5ba4374f35d9f5152204
ReidLos <ReidLos@localhost>
2011-02-01 14:51:58 +0100

    Passage du module guestbook au nouveau syst?me de cache.

--- guestbook/phpboost/GuestbookExtensionPointProvider.class.php
+++ guestbook/phpboost/GuestbookExtensionPointProvider.class.php
@@ -36,25 +36,6 @@ class GuestbookExtensionPointProvider extends ExtensionPointProvider
         $this->sql_querier = PersistenceContext::get_sql();
         parent::__construct('guestbook');
     }
-
-	//Récupération du cache.
-	function get_cache()
-	{
-		$guestbook_code = "\n\n" . 'global $_guestbook_rand_msg;' . "\n";
-		$guestbook_code .= "\n" . '$_guestbook_rand_msg = array();' . "\n";
-		$result = $this->sql_querier->query_while("SELECT g.id, g.login, g.user_id, g.timestamp, m.login as mlogin, g.contents
-		FROM " . PREFIX . "guestbook g
-		LEFT JOIN " . DB_TABLE_MEMBER . " m ON m.user_id = g.user_id
-		ORDER BY g.timestamp DESC
-		" . $this->sql_querier->limit(0, 10), __LINE__, __FILE__);
-		while ($row = $this->sql_querier->fetch_assoc($result))
-		{
-			$guestbook_code .= '$_guestbook_rand_msg[] = array(\'id\' => ' . var_export($row['id'], true) . ', \'contents\' => ' . var_export(nl2br(TextHelper::substr_html(strip_tags(FormatingHelper::second_parse($row['contents'])), 0, 150)), true) . ', \'user_id\' => ' . var_export($row['user_id'], true) . ', \'login\' => ' . var_export($row['login'], true) . ');' . "\n";
-		}
-		$this->sql_querier->query_close($result);
-
-		return $guestbook_code;
-	}
 }
 
 ?>
