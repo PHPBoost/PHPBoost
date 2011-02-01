8b1339841ddbb8de90fd607de19b2dcac93fa8b9
ReidLos <ReidLos@localhost>
2011-02-01 19:39:15 +0100

    Cr?ation des nouvelles bases du module newsletter

---
+++ newsletter/services/AbstractNewsletterMail.class.php
@@ -0,0 +1,48 @@
+<?php
+/*##################################################
+ *                        AbstractNewsletterMail.class.php
+ *                            -------------------
+ *   begin                : February 1, 2011
+ *   copyright            : (C) 2011 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @author Kévin MASSY <soldier.weasel@gmail.com>
+ */
+abstract class AbstractNewsletterMail implements NewsletterMailType
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function send_mail(NewsletterMailService $newsletter_mail_config)
+	{
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function display_mail(NewsletterMailService $newsletter_mail_config)
+	{
+	}
+}
+
+?>
