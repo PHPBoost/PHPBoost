b304562d6f9894237478602658e39f6aff849149
ReidLos <ReidLos@localhost>
2011-02-10 14:05:07 +0100

    [Fix] Correction d'une erreur dans les sondages si le num?ro g?n?r? n'existe pas.

--- poll/poll_mini.php
+++ poll/poll_mini.php
@@ -35,87 +35,92 @@ function poll_mini($position, $block)
     {
     	//Chargement de la langue du module.
     	load_module_lang('poll');
-    	$poll_mini = $_array_poll[array_rand($_array_poll)]; //Sondage aléatoire.
+		$rand = array_rand($_array_poll);
+		if (in_array($rand, $_array_poll))
+		{
+			$poll_mini = $_array_poll[$rand]; //Sondage aléatoire.
 
-    	$tpl = new FileTemplate('poll/poll_mini.tpl');
+			$tpl = new FileTemplate('poll/poll_mini.tpl');
 
-        MenuService::assign_positions_conditions($tpl, $block);
+			MenuService::assign_positions_conditions($tpl, $block);
 
-    	#####################Résultats######################
-    	//Si le cookie existe, on redirige vers les resulats, sinon on prend en compte le vote (vérification par ip plus tard).
-    	$array_cookie = array();
-    	if (AppContext::get_request()->has_cookieparameter($CONFIG_POLL['poll_cookie']))
-    	{
-    		$array_cookie = explode('/', AppContext::get_request()->get_cookie($CONFIG_POLL['poll_cookie']));
-    	}
-    	if (in_array($poll_mini['id'], $array_cookie))
-    	{
-    		$tpl->put_all(array(
-    			'THEME' => get_utheme(),
-    			'L_MINI_POLL' => $LANG['mini_poll'],
-    			'L_VOTE' => ($poll_mini['total'] > 1) ? $LANG['poll_vote_s'] : $LANG['poll_vote']
-    		));
+			#####################Résultats######################
+			//Si le cookie existe, on redirige vers les resulats, sinon on prend en compte le vote (vérification par ip plus tard).
+			$array_cookie = array();
+			if (AppContext::get_request()->has_cookieparameter($CONFIG_POLL['poll_cookie']))
+			{
+				$array_cookie = explode('/', AppContext::get_request()->get_cookie($CONFIG_POLL['poll_cookie']));
+			}
+			if (in_array($poll_mini['id'], $array_cookie))
+			{
+				$tpl->put_all(array(
+					'THEME' => get_utheme(),
+					'L_MINI_POLL' => $LANG['mini_poll'],
+					'L_VOTE' => ($poll_mini['total'] > 1) ? $LANG['poll_vote_s'] : $LANG['poll_vote']
+				));
 
-    		$tpl->assign_block_vars('result', array(
-    			'QUESTION' => $poll_mini['question'],
-    			'VOTES' => $poll_mini['total'],
-    		));
+				$tpl->assign_block_vars('result', array(
+					'QUESTION' => $poll_mini['question'],
+					'VOTES' => $poll_mini['total'],
+				));
 
-    		foreach ($poll_mini['votes'] as $answer => $width)
-    		{
-    			$tpl->assign_block_vars('result.answers', array(
-    				'ANSWERS' => $answer,
-    				'WIDTH' => NumberHelper::round($width, 0),
-    				'PERCENT' => $width
-    			));
-    		}
-    	}
-    	else
-    	{
-    		#####################Questions######################
-    		$tpl->put_all(array(
-    			'L_MINI_POLL' => $LANG['mini_poll'],
-    			'L_VOTE' => $LANG['poll_vote'],
-    			'L_POLL_RESULT' => $LANG['poll_result'],
-    			'U_POLL_RESULT' => url('.php?id=' . $poll_mini['id'] . '&amp;r=1', '-' . $poll_mini['id'] . '-1.php')
-    		));
+				foreach ($poll_mini['votes'] as $answer => $width)
+				{
+					$tpl->assign_block_vars('result.answers', array(
+						'ANSWERS' => $answer,
+						'WIDTH' => NumberHelper::round($width, 0),
+						'PERCENT' => $width
+					));
+				}
+			}
+			else
+			{
+				#####################Questions######################
+				$tpl->put_all(array(
+					'L_MINI_POLL' => $LANG['mini_poll'],
+					'L_VOTE' => $LANG['poll_vote'],
+					'L_POLL_RESULT' => $LANG['poll_result'],
+					'U_POLL_RESULT' => url('.php?id=' . $poll_mini['id'] . '&amp;r=1', '-' . $poll_mini['id'] . '-1.php')
+				));
 
-    		global $Session;
-    		$tpl->assign_block_vars('question', array(
-    			'ID' => url('.php?id=' . $poll_mini['id'] . '&amp;token=' . $Session->get_token(), '-' . $poll_mini['id'] . '.php?token=' . $Session->get_token()),
-    			'QUESTION' => $poll_mini['question']
-    		));
+				global $Session;
+				$tpl->assign_block_vars('question', array(
+					'ID' => url('.php?id=' . $poll_mini['id'] . '&amp;token=' . $Session->get_token(), '-' . $poll_mini['id'] . '.php?token=' . $Session->get_token()),
+					'QUESTION' => $poll_mini['question']
+				));
 
-    		$z = 0;
-    		if ($poll_mini['type'] == '1')
-    		{
+				$z = 0;
+				if ($poll_mini['type'] == '1')
+				{
 
-    			if (is_array($poll_mini['votes']))
-    			{
-    			    // FIXME should always be an array, needs to patch cache generation
-	    			foreach ($poll_mini['votes'] as $answer => $width)
-	    			{
-	    				$tpl->assign_block_vars('question.radio', array(
-	    					'NAME' => $z,
-	    					'ANSWERS' => $answer
-	    				));
-	    				$z++;
-	    			}
-    			}
-    		}
-    		elseif ($poll_mini['type'] == '0')
-    		{
-    			foreach ($poll_mini['votes'] as $answer => $width)
-    			{
-    				$tpl->assign_block_vars('question.checkbox', array(
-    					'NAME' => $z,
-    					'ANSWERS' => $answer
-    				));
-    				$z++;
-    			}
-    		}
-    	}
-        return $tpl->render();
+					if (is_array($poll_mini['votes']))
+					{
+						// FIXME should always be an array, needs to patch cache generation
+						foreach ($poll_mini['votes'] as $answer => $width)
+						{
+							$tpl->assign_block_vars('question.radio', array(
+								'NAME' => $z,
+								'ANSWERS' => $answer
+							));
+							$z++;
+						}
+					}
+				}
+				elseif ($poll_mini['type'] == '0')
+				{
+					foreach ($poll_mini['votes'] as $answer => $width)
+					{
+						$tpl->assign_block_vars('question.checkbox', array(
+							'NAME' => $z,
+							'ANSWERS' => $answer
+						));
+						$z++;
+					}
+				}
+			}
+			return $tpl->render();
+		}
+		 return '';
     }
     return '';
 }
