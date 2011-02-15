e21450494560d366dec542d27e898f5ce9d2b972
ReidLos <ReidLos@localhost>
2011-02-15 23:22:26 +0100

    [MAJ] Syst?me de notation fonctionnelle, par contre j'ai vu avec Benoit, je vais cr?er une autre table du coup pour les moyennes de note

--- kernel/framework/content/notation/NotationService.class.php
+++ kernel/framework/content/notation/NotationService.class.php
@@ -49,37 +49,33 @@ class NotationService
 	{
 		if ($notation->get_notation_scale() > 0)
 		{
-			$nbr_notes = self::$db_querier->count(DB_TABLE_NOTATION, "WHERE module_name = '" . $notation->get_module_name() . "' AND module_id = '". $notation->get_module_id() ."' ");
-			if ($nbr_notes > 0)
+			$template = new StringTemplate('
+			# START notation #
+				<img src="{PATH_TO_ROOT}/templates/{THEME}/images/{notation.PICTURE}" alt="" class="valign_middle" />
+			# END notation #');
+			
+			$average_notes = self::get_average_notes($notation);
+			for ($i = 1; $i <= $notation->get_notation_scale(); $i++)
 			{
-				$template = new StringTemplate('
-				# START notation #
-					<img src="{PATH_TO_ROOT}/templates/{THEME}/images/{notation.PICTURE}" alt="" class="valign_middle" />
-				# END notation #');
-				
-				$average_notes = self::get_average_notes($notation);
-				for ($i = 1; $i <= $notation->get_notation_scale(); $i++)
+				$star_img = 'stars.png';
+				if ($average_notes < $i)
 				{
-					$star_img = 'stars.png';
-					if ($average_notes < $i)
-					{
-						$decimal = $i - $average_notes;
-						if ($decimal >= 1)
-							$star_img = 'stars0.png';
-						elseif ($decimal >= 0.75)
-							$star_img = 'stars1.png';
-						elseif ($decimal >= 0.50)
-							$star_img = 'stars2.png';
-						else
-							$star_img = 'stars3.png';
-					}
-					
-					$template->assign_block_vars('notation', array(
-						'PICTURE' => $star_img
-					));
+					$decimal = $i - $average_notes;
+					if ($decimal >= 1)
+						$star_img = 'stars0.png';
+					elseif ($decimal >= 0.75)
+						$star_img = 'stars1.png';
+					elseif ($decimal >= 0.50)
+						$star_img = 'stars2.png';
+					else
+						$star_img = 'stars3.png';
 				}
-				return $template->render();
+				
+				$template->assign_block_vars('notation', array(
+					'PICTURE' => $star_img
+				));
 			}
+			return $template->render();
 		}
 		else
 		{
@@ -133,29 +129,53 @@ class NotationService
 				
 				$template->assign_block_vars('notation', array(
 					'I' => $i,
-					'IMAGE' => $star_img
+					'PICTURE' => $star_img
 				));
 			}
 
 			$template->put_all(array(
+				'C_VOTES' => self::get_number_notes($notation) > 0 ? true : false,
 				'CURRENT_URL' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
 				'MODULE_ID' => $notation->get_module_id(),
 				'NOTATION_SCALE' => $notation->get_notation_scale(),
 				'NUMBER_PIXEL' => $notation->get_notation_scale() * 16,
 				'NUMBER_VOTES' => self::get_number_notes($notation),
-				'C_VOTES' => self::get_number_notes($notation) > 0 ? true : false,
-				'L_NO_NOTE' => self::$lang['no_note'],
 				'AVERAGE_NOTES' => $average_notes,
+				'ALREADY_VOTE' => self::get_member_already_notation($notation),
 				'ARRAY_NOTE' => 'array_note[' . $notation->get_module_id() . '] = \'' . $average_notes . '\';',
-				'L_AUTH_ERROR' => /* TODO */ '',
-				'L_ALERT_ALREADY_VOTE' => addslashes(self::$lang['already_vote']),
-				'L_ALREADY_VOTE' => '',
-				'L_NOTE' => addslashes(self::$lang['note']),
-				'L_NOTES' => addslashes(self::$lang['notes']),
+				'L_NO_NOTE' => self::$lang['no_note'],
+				'L_AUTH_ERROR' => LangLoader::get_message('e_auth', 'errors'),
+				'L_ALREADY_VOTE' => self::$lang['already_vote'],
+				'L_NOTES' => self::get_number_notes($notation) > 1 ? self::$lang['notes'] : self::$lang['note'],
+				'L_NOTE' => self::$lang['note'],
 				'L_VALID_NOTE' => self::$lang['valid_note']
 			));
 
 			return $template->render();
+		}
+	}
+	
+	/*
+	 * This function required object Notation containing the module_name and module_id.
+	 */
+	public static function get_average_notes(Notation $notation)
+	{
+		$nbr_notes = self::get_number_notes($notation);
+		if ($nbr_notes > 0)
+		{
+			$result = self::$db_querier->select("SELECT note
+				FROM " . DB_TABLE_NOTATION . "
+				WHERE module_name = '" . $notation->get_module_name() . "' 
+				AND module_id = '". $notation->get_module_id() ."'
+			");
+			
+			$notes = 0;
+			while ($row = $result->fetch())
+			{
+				$notes += $row['note'];
+			}
+
+			return (round(($notes / $nbr_notes) / 0.25) * 0.25);
 		}
 	}
 	
@@ -164,7 +184,7 @@ class NotationService
 		if (self::$user->check_level(MEMBER_LEVEL))
 		{
 			$note_is_valid = $notation->get_note() >= 0 && $notation->get_note() <= $notation->get_notation_scale() ? true : false;
-			$member_already_notation = self::$db_querier->count(DB_TABLE_NOTATION, "WHERE user_id = '" . $notation->get_user_id() . "' AND module_name = '" . $notation->get_module_name() . "' AND module_id = '". $notation->get_module_id() ."' ") > 0 ? true : false;
+			$member_already_notation = self::get_member_already_notation($notation);
 			
 			if (!$member_already_notation && $note_is_valid)
 			{
@@ -195,33 +215,16 @@ class NotationService
 				'note' => $notation->get_note(),				
 		));
 	}
-	
-	private static function get_average_notes(Notation $notation)
-	{
-		$nbr_notes = self::get_number_notes($notation);
-		if ($nbr_notes > 0)
-		{
-			$result = self::$db_querier->select("SELECT note
-				FROM " . DB_TABLE_NOTATION . "
-				WHERE module_name = '" . $notation->get_module_name() . "' 
-				AND module_id = '". $notation->get_module_id() ."'
-			");
-			
-			$notes = 0;
-			while ($row = $result->fetch())
-			{
-				$notes += $row['note'];
-			}
 
-			return (round(($notes / $nbr_notes) / 0.25) * 0.25);
-		}
-	}
-	
 	private static function get_number_notes(Notation $notation)
 	{
 		return self::$db_querier->count(DB_TABLE_NOTATION, "WHERE module_name = '" . $notation->get_module_name() . "' AND module_id = '". $notation->get_module_id() ."' ");
 	}
-
+	
+	private static function get_member_already_notation(Notation $notation)
+	{
+		return self::$db_querier->count(DB_TABLE_NOTATION, "WHERE user_id = '" . $notation->get_user_id() . "' AND module_name = '" . $notation->get_module_name() . "' AND module_id = '". $notation->get_module_id() ."' ") > 0 ? true : false;
+	}
 }
 
 ?>
