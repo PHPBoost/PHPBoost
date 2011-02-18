1278b9e8a27f94511b3bfd014b9d1abc39990ac9
ReidLos <ReidLos@localhost>
2011-02-18 16:40:01 +0100

    [FIX] Divers corrections

--- kernel/framework/phpboost/member/extended-fields/field/MemberUserBornExtendedField.class.php
+++ kernel/framework/phpboost/member/extended-fields/field/MemberUserBornExtendedField.class.php
@@ -67,7 +67,7 @@ class MemberUserBornExtendedField extends AbstractMemberExtendedField
 	public function return_value(HTMLForm $form, MemberExtendedField $member_extended_field)
 	{
 		$field_name = $member_extended_field->get_field_name();
-		return $form->get_value($field_name)->format(TIMESTAMP);
+		return $form->get_value($field_name)->format(DATE_TIMESTAMP);
 	}
 }
 ?>
