e31b5eecc2511373781a3b0a1dfa37fdaabf63ae
ReidLos <ReidLos@localhost>
2011-03-31 16:21:48 +0200

    [Add] D?but de cr?ation de la structure du nouveau syst?me de commentaire

---
+++ kernel/framework/content/comments/CommentsDAO.class.php
@@ -0,0 +1,51 @@
+<?php
+/*##################################################
+ *                              CommentsDAO.class.php
+ *                            -------------------
+ *   begin                : March 31, 2010
+ *   copyright            : (C) 2010 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+ /**
+ * @author Kévin MASSY <soldier.weasel@gmail.com>
+ * @package {@package}
+ */
+class CommentsDAO
+{
+	private static $db_querier;
+	
+	public static function __static()
+	{
+		self::$db_querier = PersistenceContext::get_querier();
+	}
+	
+	public static function add_comment()
+	{
+	
+	}
+	
+	public static function edit_comment()
+	{
+	
+	}
+}
+?>
