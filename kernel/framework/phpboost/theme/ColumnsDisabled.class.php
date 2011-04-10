93a0be2bdaea783c3de7a6585ec84b9ec2e64379
ReidLos <ReidLos@localhost>
2011-04-10 21:06:20 +0200

    [Feature] D?but de refonte de la gestion des th?mes (ajout de la possibilit? de d?sactiver diverses colonnes et pr?paration de la structure pour les g?rer ?galement dans la gestion des th?mes).

---
+++ kernel/framework/phpboost/theme/ColumnsDisabled.class.php
@@ -0,0 +1,129 @@
+<?php
+/*##################################################
+/**
+ *                         ColumnsDisabled.class.php
+ *                            -------------------
+ *   begin                : April 10, 2011
+ *   copyright            : (C) 2011 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ *###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *###################################################
+ */
+
+ /**
+ * @author Kévin MASSY <soldier.weasel@gmail.com>
+ * @package {@package}
+ */
+class ColumnsDisabled
+{   
+	private $disable_header = false;
+	private $disable_sub_header = false;
+	private $disable_top_central = false;
+	private $disable_bottom_central = false;
+	private $disable_top_footer = false;
+	private $disable_footer = false;
+	private $disable_left_columns = false;
+	private $disable_right_columns = false;
+	
+	public function header_is_disabled()
+	{
+		return $this->disable_header;
+	}
+	
+	public function sub_header_is_disabled()
+	{
+		return $this->disable_sub_header;
+	}
+	
+	public function top_central_is_disabled()
+	{
+		return $this->disable_top_central;
+	}
+	
+	public function bottom_central_is_disabled()
+	{
+		return $this->disable_bottom_central;
+	}
+	
+	public function top_footer_is_disabled()
+	{
+		return $this->disable_top_footer;
+	}
+	
+	public function footer_is_disabled()
+	{
+		return $this->disable_footer;
+	}
+	
+	public function left_columns_is_disabled()
+	{
+		return $this->disable_left_columns;
+	}
+	
+	public function right_columns_is_disabled()
+	{
+		return $this->disable_right_columns;
+	}
+	
+	public function set_columns_disabled(Array $disable_columns)
+	{
+		foreach($disable_columns as $columns => $value)
+		{
+			$attribute = trim(strtolower($attribute));
+			switch ($columns)
+			{
+				case 'header':
+					$this->disable_header = !empty($value) ? $value : true;
+					unset($disable_columns['header']);
+					break;
+				case 'sub_header':
+					$this->disable_sub_header = !empty($value) ? $value : true;
+					unset($disable_columns['sub_header']);
+					break;
+				case 'top_central':
+					$this->disable_top_central = !empty($value) ? $value : true;
+					unset($disable_columns['top_central']);
+					break;
+				case 'bottom_central':
+					$this->disable_bottom_central = !empty($value) ? $value : true;
+					unset($disable_columns['bottom_central']);
+					break;
+				case 'top_footer':
+					$this->disable_top_footer = !empty($value) ? $value : true;
+					unset($disable_columns['top_footer']);
+					break;
+				case 'footer':
+					$this->disable_footer = !empty($value) ? $value : true;
+					unset($disable_columns['footer']);
+					break;
+				case 'left':
+					$this->disable_left_columns = !empty($value) ? $value : true;
+					unset($disable_columns['left']);
+					break;
+				case 'right':
+					$this->disable_right_columns = !empty($value) ? $value : true;
+					unset($disable_columns['right']);
+					break;	
+				
+			}
+		}
+	}
+}
+?>
---
+++ kernel/framework/phpboost/theme/ThemeConfigurationManager.class.php
@@ -0,0 +1,87 @@
+<?php
+/*##################################################
+/**
+ *                        ThemeConfigurationManager.class.php
+ *                            -------------------
+ *   begin                : April 10, 2011
+ *   copyright            : (C) 2011 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ *###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *###################################################
+ */
+
+ /**
+ * @author Kévin MASSY <soldier.weasel@gmail.com>
+ * @package {@package}
+ */
+class ThemeConfigurationManager
+{
+	private static $cache_manager = null;
+
+	public static function get($theme_id)
+	{
+		$cache_manager = self::get_cache_manager();
+		if (!$cache_manager->contains($theme_id))
+		{
+			$module_configuration = self::get_module_configuration($theme_id);
+			$cache_manager->store($theme_id, $module_configuration);
+		}
+		return $cache_manager->get($theme_id);
+	}
+
+	private static function get_cache_manager()
+	{
+		if (self::$cache_manager === null)
+		{
+			self::$cache_manager = DataStoreFactory::get_ram_store(__CLASS__);
+		}
+		return self::$cache_manager;
+	}
+
+	private static function get_module_configuration($theme_id)
+	{
+		$config_ini_file = self::find_config_ini_file($theme_id);
+		return new ModuleConfiguration($config_ini_file);
+	}
+
+	private static function find_config_ini_file($theme_id)
+	{
+		$config_ini_folder = PATH_TO_ROOT . '/' . $theme_id . '/lang/';
+
+		$config_ini_file = $config_ini_folder . get_ulang() . '/config.ini';
+		if (file_exists($config_ini_file))
+		{
+			return $config_ini_file;
+		}
+
+		$folder = new Folder($config_ini_folder);
+		foreach ($folder->get_folders() as $lang_folder)
+		{
+			$config_ini_file = $lang_folder->get_path() . '/config.ini';
+			if (file_exists($config_ini_file))
+			{
+				return $config_ini_file;
+			}
+		}
+		throw new Exception('Theme "' . $theme_id . '" config.ini not found in' .
+			    '/' . $theme_id . '/lang/');
+	}
+}
+?>
