4479e40dd6c89dcd7fccadc4947a3985f6713a6e
ReidLos <ReidLos@localhost>
2011-05-01 22:17:12 +0200

    [MAJ] Affichage de l'erreur CSRF avec UserErrorController

--- kernel/framework/phpboost/user/session/SessionData.class.php
+++ kernel/framework/phpboost/user/session/SessionData.class.php
@@ -31,8 +31,6 @@
  */
 class SessionData
 {
-	const SEASURF_ATTACK_ERROR_PAGE = '/member/csrf-attack.php';
-
 	private static $KEY_USER_ID = 'user_id';
 	private static $KEY_SESSION_ID = 'session_id';
 
@@ -388,11 +386,10 @@ class SessionData
 	 *      <li>If the token isn't in the request, we analyse the HTTP referer to be sure that the request comes from the current site and not from another which can be suspect</li>
 	 * </ul>
 	 * If the request doesn't match any of these two cases, this method will consider that it's a CSRF attack.
-	 * @param mixed $redirect if string, redirect to the $redirect error page if the token is wrong
-	 * if false, do not redirect
+	 * @param string $message message display to the error page if the token is wrong
 	 * @return bool true if no csrf attack by post is detected
 	 */
-	public function csrf_post_protect($redirect = self::SEASURF_ATTACK_ERROR_PAGE)
+	public function csrf_post_protect($message = '')
 	{
 		//The user sent a POST request
 		if (!empty($_POST))
@@ -409,7 +406,7 @@ class SessionData
 				return true;
 			}
 			//If those two lines are executed, none of the two cases has been matched. Thow it's a potential attack.
-			$this->csrf_attack($redirect);
+			$this->csrf_attack($message);
 			return false;
 		}
 		//It's not a POST request, there is no problem.
@@ -422,16 +419,15 @@ class SessionData
 	/**
 	 * @desc Check the session against CSRF attacks by GET. Checks that GETs are done from
 	 * this site with a correct token.
-	 * @param mixed $redirect if string, redirect to the $redirect error page if the token is wrong
-	 * if false, do not redirect
+	 * @param string $message message display to the error page if the token is wrong
 	 * @return true if no csrf attack by get is detected
 	 */
-	public function csrf_get_protect($redirect = self::SEASURF_ATTACK_ERROR_PAGE)
+	public function csrf_get_protect($message = '')
 	{
 		$token = $this->get_token();
 		if (empty($token) || $this->get_request_token() !== $token)
 		{
-			$this->csrf_attack($redirect);
+			$this->csrf_attack($message);
 			return false;
 		}
 		return true;
@@ -454,18 +450,17 @@ class SessionData
 	/**
 	 * @desc Redirect to the $redirect error page if the token is wrong
 	 * if false, do not redirect
-	 * @param mixed $redirect if string, redirect to the $redirect error page if the token is wrong
-	 * if false, do not redirect
+	 * @param string $message message display to the error page if the token is wrong
 	 */
-	private function csrf_attack($redirect = self::SEASURF_ATTACK_ERROR_PAGE)
+	private function csrf_attack($message = '')
 	{
 		$bad_token = $this->get_printable_token($this->get_request_token());
 		$good_token = $this->get_printable_token($this->get_token());
-
-		if ($redirect !== false && !empty($redirect))
-		{
-			AppContext::get_response()->redirect(Url::to_absolute($redirect));
-		}
+		
+		$title = LangLoader::get_message('e_warning', 'errors');
+		$message = empty($message)? LangLoader::get_message('csrf_attack', 'main') : $message;
+		$controller = new UserErrorController($title, $message, UserErrorController::WARNING);
+		DispatchManager::redirect($controller);
 	}
 
 	private function get_request_token()
