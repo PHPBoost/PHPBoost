00923fe833f3022ede903ff8e45923d4d3590c77
K?vin MASSY <K?vin MASSY@localhost>
2011-09-12 21:58:24 +0200

    [MAJ] Mise ? jour du MemberLostPasswordController

--- member/controllers/MemberLostPasswordController.class.php
+++ member/controllers/MemberLostPasswordController.class.php
@@ -34,13 +34,11 @@ class MemberLostPasswordController extends AbstractController
 	/**
 	 * @var HTMLForm
 	 */
-	private $send_activation_key_form;
-	private $change_password_form;
+	private $form;
 	/**
 	 * @var FormButtonSubmit
 	 */
-	private $send_activation_key_submit_button;
-	private $change_password_submit_button;
+	private $submit_button;
 
 	public function execute(HTTPRequest $request)
 	{
@@ -53,57 +51,10 @@ class MemberLostPasswordController extends AbstractController
 
 		$this->init();
 
-		if (!empty($this->activation_key))
-		{
-			$this->change_password_build_form();
-				
-			if($this->change_password_submit_button->has_been_submited() && $this->change_password_form->validate())
-			{
-				if ($this->check_activ_pass_exist())
-				{
-					$user_id = $this->get_user_id();
-					 
-					//TODO, connecter l'utilisateur et le rediriger vers la page d'accueil
-					 
-					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_confirm_change'], E_USER_SUCCESS));
-						
-					if ($this->change_password($user_id))
-					{
-						$this->clear_activation_key($user_id);
-					}
-				}
-				else
-				{
-					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_echec_change'], E_USER_NOTICE));
-				}
-			}
-			$this->tpl->put('FORM', $this->change_password_form->display());
-		}
-		else
-		{
-			$this->send_activation_key_build_form();
-				
-			if($this->send_activation_key_submit_button->has_been_submited() && $this->send_activation_key_form->validate())
-			{
-				if($this->check_member_exist())
-				{
-					$activ_pass = KeyGenerator::generate_key(15);
-						
-					$this->save_activ_pass($activ_pass);
-					 
-					$this->send_activation_key_mail($activ_pass);
-					 
-					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_mail_send'], E_USER_SUCCESS));
-				}
-				else
-				{
-					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_mail_forget'], E_USER_NOTICE));
-				}
-			}
-				
-			$this->tpl->put('FORM', $this->send_activation_key_form->display());
-		}
+		$this->tpl->put('FORM', $this->build_form()->display());
 
+		$this->validate_form();
+		
 		return $this->build_response($this->tpl);
 	}
 
@@ -116,55 +67,96 @@ class MemberLostPasswordController extends AbstractController
 
 		$this->tpl->add_lang($this->lang);
 	}
+	
+	private function build_form()
+	{
+		if (!empty($this->activation_key))
+		{
+			return $this->change_password_build_form();
+		}
+		else
+		{
+			return $this->send_activation_key_build_form();
+		}
+	}
+	
+	private function validate_form()
+	{
+		if (!empty($this->activation_key))
+		{
+			if($this->submit_button->has_been_submited() && $this->form->validate())
+			{
+				if ($this->check_activ_pass_exist())
+				{
+					$user_id = $this->get_user_id();
+					$new_password = $this->form->get_value('new_password');
+					if (!empty($new_password))
+					{
+						MemberUpdateProfileHelper::change_password(KeyGenerator::string_hash($new_password), $user_id);
+						$this->clear_activation_key($user_id);
+						$this->connect_user($user_id, $new_password);
+						$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_confirm_change'], E_USER_SUCCESS));
+					}
+				}
+				else
+				{
+					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_echec_change'], E_USER_NOTICE));
+				}
+			}
+		}
+		else 
+		{
+			if($this->submit_button->has_been_submited() && $this->form->validate())
+			{
+				if($this->check_member_exist())
+				{
+					$activ_pass = KeyGenerator::generate_key(15);
+					$this->save_activ_pass($activ_pass);
+					$this->send_activation_key_mail($activ_pass);
+					 
+					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_forget_mail_send'], E_USER_SUCCESS));
+				}
+				else
+				{
+					$this->tpl->put('MSG', MessageHelper::display($this->error_lang['e_mail_forget'], E_USER_NOTICE));
+				}
+			}
+		}
+	}
 
 	private function send_activation_key_build_form()
 	{
 		$form = new HTMLForm('send_activation_key');
 		$fieldset = new FormFieldsetHTML('fieldset', $this->lang['forget_pass']);
-			
+		$form->add_fieldset($fieldset);
+		
 		$fieldset->add_field(new FormFieldLabel($this->lang['forget_pass_send']));
 		
-		$default_select_option = new FormFieldSelectChoiceOption($this->lang['mail'], 1);
 		$fieldset->add_field(new FormFieldSimpleSelectChoice('field_choice', 'Sélectionnez le champ que vous voulez renseigner (mail ou pseudo)',
-			$default_select_option, 
+			'1', 
 			array(
-				$default_select_option, 
-				new FormFieldSelectChoiceOption($this->lang['pseudo'], 2)
-				),
-			array('events' => array('change' => 
-					'if (HTMLForms.getField("field_choice").getValue() == 1) { 
-						HTMLForms.getField("mail").enable(); 
-						HTMLForms.getField("user_login").setValue("");
-						HTMLForms.getField("user_login").disable(); 
-					} else { 
-						HTMLForms.getField("user_login").enable(); 
-						HTMLForms.getField("mail").setValue("");
-						HTMLForms.getField("mail").disable(); 
-					}')
-				)
+				new FormFieldSelectChoiceOption($this->lang['mail'], '1'), 
+				new FormFieldSelectChoiceOption($this->lang['pseudo'], '2')
+			)
 		));
 		
-		$fieldset->add_field(new FormFieldMailEditor('mail', $this->lang['mail'], '', array(
+		$fieldset->add_field(new FormFieldTextEditor('information', $this->lang['pseudo'] .' / '.$this->lang['mail'], '', array(
 			'class' => 'text', 'description' => '', 'required' => true)
 		));
 		
-		$fieldset->add_field(new FormFieldTextEditor('user_login', $this->lang['pseudo'], '', array(
-			'class' => 'text', 'description' => '', 'required' => true, 'hidden' => true, 'disabled' => true)
-		));
-		
-		$form->add_fieldset($fieldset);
+		$this->submit_button = new FormButtonSubmit($this->lang['submit'], 'forget_password');
+		$form->add_button($this->submit_button);
 			
-		$this->send_activation_key_submit_button = new FormButtonSubmit($this->lang['submit'], 'forget_password');
-		$form->add_button($this->send_activation_key_submit_button);
-			
-		$this->send_activation_key_form = $form;
+		$this->form = $form;
+		return $this->form;
 	}
 
 	private function change_password_build_form()
 	{
 		$form = new HTMLForm('change_password_form');
 		$fieldset = new FormFieldsetHTML('fieldset', $this->lang['change_password']);
-			
+		$form->add_fieldset($fieldset);
+		
 		$fieldset->add_field($password = new FormFieldPasswordEditor('new_password', $this->lang['new_password'], '', array(
 			'class' => 'text', 'maxlength' => 25, 'description' => $this->lang['password_how'], 'required' => true),
 			array(new FormFieldConstraintLength(6))
@@ -175,13 +167,12 @@ class MemberLostPasswordController extends AbstractController
 			array(new FormFieldConstraintLength(6))
 		));
 			
-		$form->add_fieldset($fieldset);
-			
-		$this->change_password_submit_button = new FormButtonSubmit($this->lang['submit'], 'change_password');
-		$form->add_button($this->change_password_submit_button);
+		$this->submit_button = new FormButtonSubmit($this->lang['submit'], 'change_password');
+		$form->add_button($this->submit_button);
 		$form->add_constraint(new FormConstraintFieldsEquality($password, $password_bis));
 			
-		$this->change_password_form = $form;
+		$this->form = $form;
+		return $this->form;
 	}
 
 	private function check_activ_pass_exist()
@@ -192,17 +183,16 @@ class MemberLostPasswordController extends AbstractController
 
 	private function check_member_exist()
 	{
-		
-		if($this->send_activation_key_form->get_value('field_choice')->get_raw_value() == 1)
+		if($this->form->get_value('field_choice')->get_raw_value() == 1)
 		{
 			return (bool)PersistenceContext::get_querier()->count(DB_TABLE_MEMBER, "WHERE user_mail = :mail",
-				array('mail' => $this->send_activation_key_form->get_value('mail')
+				array('mail' => $this->form->get_value('information')
 			));
 		}
-		elseif($this->send_activation_key_form->get_value('field_choice')->get_raw_value() == 2)
+		elseif($this->form->get_value('field_choice')->get_raw_value() == 2)
 		{
 			return (bool)PersistenceContext::get_querier()->count(DB_TABLE_MEMBER, "WHERE login = :login",
-				array('login' => $this->send_activation_key_form->get_value('user_login')
+				array('login' => $this->form->get_value('information')
 			));
 		}
 		else 
@@ -220,60 +210,48 @@ class MemberLostPasswordController extends AbstractController
 
 	private function save_activ_pass($activ_pass)
 	{
-		if($this->send_activation_key_form->get_value('field_choice')->get_raw_value() == 1)
+		if($this->form->get_value('field_choice')->get_raw_value() == 1)
 		{
 			PersistenceContext::get_querier()->update(DB_TABLE_MEMBER, array('activ_pass' => $activ_pass),
-				"WHERE user_mail = :mail", array('mail' => $this->send_activation_key_form->get_value('mail')));
+				"WHERE user_mail = :mail", array('mail' => $this->form->get_value('information')));
 		}
 		else 
 		{	
 			PersistenceContext::get_querier()->update(DB_TABLE_MEMBER, array('activ_pass' => $activ_pass),
-				"WHERE login = :login", array('login' => $this->send_activation_key_form->get_value('user_login')));
+				"WHERE login = :login", array('login' => $this->form->get_value('information')));
 		}
 	}
 
 	private function send_activation_key_mail($activ_pass)
 	{
-		if($this->send_activation_key_form->get_value('field_choice')->get_raw_value() == 1)
+		if($this->form->get_value('field_choice')->get_raw_value() == 1)
 		{
-			$sql_member = PersistenceContext::get_querier()->select_single_row(DB_TABLE_MEMBER, array('login'),
-				"WHERE user_mail = :mail", array('mail' => $this->send_activation_key_form->get_value('mail')));
-			$user_login = $sql_member['login'];
-			$user_mail = $this->send_activation_key_form->get_value('mail');
+			$sql_member = PersistenceContext::get_querier()->select_single_row(DB_TABLE_MEMBER, array('user_mail'),
+				"WHERE user_mail = :mail", array('mail' => $this->form->get_value('information')));
+			$user_mail = $sql_member['user_mail'];
 		}
 		else 
 		{
-			$sql_member = PersistenceContext::get_querier()->select_single_row(DB_TABLE_MEMBER, array('user_mail'),
-				"WHERE login = :login", array('login' => $this->send_activation_key_form->get_value('user_login')));
+			$sql_member = PersistenceContext::get_querier()->select_single_row(DB_TABLE_MEMBER, array('login'),
+				"WHERE login = :login", array('login' => $this->form->get_value('information')));
 			$user_mail = $sql_member['user_mail'];
-			$user_login = $this->send_activation_key_form->get_value('user_login');
 		}
 		
 		$subject = $this->lang['forget_pass'] . ' - ' . GeneralConfig::load()->get_site_name();
 		$content = StringVars::replace_vars($this->lang['forget_mail_pass'], array('login' => $user_login, 'host' => HOST,
 			'host_dir' => (HOST . DIR), 'key' => $activ_pass, 'signature' => MailServiceConfig::load()->get_mail_signature()));
 
+		$this->send_mail($user_mail, $subject, $content);
+	}
+
+	private function send_mail($mail, $subject, $content)
+	{
 		$mail = new Mail();
-		$mail->add_recipient($user_mail, $user_login);
+		$mail->add_recipient($mail);
 		$mail->set_sender(MailServiceConfig::load()->get_default_mail_sender(), GeneralConfig::load()->get_site_name());
 		$mail->set_subject($subject);
 		$mail->set_content($content);
 		AppContext::get_mail_service()->try_to_send($mail);
-	}
-
-	private function change_password($user_id)
-	{
-		$new_password = $this->change_password_form->get_value('new_password');
-
-		if (!empty($new_password))
-		{
-			MemberUpdateProfileHelper::change_password(KeyGenerator::string_hash($new_password), $user_id);
-			return true;
-		}
-		else 
-		{
-			return false;
-		}
 	}
 
 	private function clear_activation_key($user_id)
@@ -282,6 +260,12 @@ class MemberLostPasswordController extends AbstractController
 		array('activ_pass' => 0, 'user_id' => $user_id));
 	}
 
+	private function connect_user($user_id, $password)
+	{
+		AppContext::get_session()->start($user_id, 
+		$password, 0, SCRIPT, QUERY_STRING, '', true);
+	}
+	
 	private function build_response(View $view)
 	{
 		$response = new SiteDisplayResponse($view);
@@ -290,5 +274,4 @@ class MemberLostPasswordController extends AbstractController
 		return $response;
 	}
 }
-
 ?>
