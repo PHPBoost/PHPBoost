e56dc25dfcdc99f6f66e6a1e67fe3187da0d46df
daaxwizeman <daaxwizeman@gmail.com>
2011-09-18 10:07:19 -0400

    [MAJ]Modification du traitement de la langue afin d'?tre similaire aux autres contr?leurs

--- admin/config/util/AdminConfigDisplayResponse.class.php
+++ admin/config/util/AdminConfigDisplayResponse.class.php
@@ -27,25 +27,16 @@
 
 class AdminConfigDisplayResponse extends AdminMenuDisplayResponse
 {
-	private $lang;
-	
 	public function __construct($view)
 	{ 
         parent::__construct($view);
         
-        $this->load_lang();
-        
-        $view->add_lang($this->lang);
-        $this->set_title($this->lang['general-config']);
+        $lang = LangLoader::get('admin-config-common');
+        $this->set_title($lang['general-config']);
         $img = '/templates/' . get_utheme() . '/images/admin/configuration.png';
-        $this->add_link($this->lang['general-config'], DispatchManager::get_url('/admin/config/index.php', '/general'), $img);
-        $this->add_link($this->lang['advanced-config'], DispatchManager::get_url('/admin/config/index.php', '/advanced'), $img);
-        $this->add_link($this->lang['mail-config'], DispatchManager::get_url('/admin/config/index.php', '/mail'), $img);
-	}
-	
-	private function load_lang()
-	{
-		$this->lang = LangLoader::get('admin-config-common');
+        $this->add_link($lang['general-config'], DispatchManager::get_url('/admin/config/index.php', '/general'), $img);
+        $this->add_link($lang['advanced-config'], DispatchManager::get_url('/admin/config/index.php', '/advanced'), $img);
+        $this->add_link($lang['mail-config'], DispatchManager::get_url('/admin/config/index.php', '/mail'), $img);
 	}
 }
 ?>
