a4e0967edcbebb645dad8131003b9d3022ba2518
Kevin MASSY <Kevin MASSY@localhost>
2011-10-08 00:17:50 +0200

    [MAJ] Oubli de changement d'appel du begin.php ? init.php

--- kernel/framework/ajax/dispatcher.php
+++ kernel/framework/ajax/dispatcher.php
@@ -27,7 +27,7 @@
 
 define('PATH_TO_ROOT', '../../..');
 
-require_once PATH_TO_ROOT . '/kernel/begin.php';
+require_once PATH_TO_ROOT . '/kernel/init.php';
 
 $url_controller_mappers = array(
 	//new UrlControllerMapper('', '`^/category/display/?$`'),
