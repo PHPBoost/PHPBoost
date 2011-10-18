34fa0016d8bcbe8d4183ced88a97e60690b98e56
soldier.weasel <soldier.weasel@localhost>
2011-10-18 21:58:33 +0200

    [MAJ] Config module articles

--- articles/phpboost/ArticlesConfig.class.php
+++ articles/phpboost/ArticlesConfig.class.php
@@ -1,6 +1,6 @@
 <?php
 /*##################################################
- *                         ArticlesConfig.class.php
+ *		                   ArticlesConfig.class.php
  *                            -------------------
  *   begin                : October 15, 2011
  *   copyright            : (C) 2011 Patrick DUBEAU
@@ -27,84 +27,88 @@
 
 class ArticlesConfig extends AbstractConfigData
 {
-        const AUTHORIZATION = 'authorization';
-        
-        public function get_authorizations()
-        {
-                return $this->get_property(self::AUTHORIZATION);
-        }
-        
-        public function set_authorizations(Array $array)
-        {
-                $this->set_property(self::AUTHORIZATION, $array);
-        }
-        
-        public function get_max_nbr_articles_displayed()
-        {
-                return $this->get_property('nbr_articles_max');
-        }
-        
-        public function set_max_nbr_articles_displayed($nbr_articles) 
-        {
-                $this->set_property('nbr_articles_max', $nbr_articles);
-        }
-        
-        public function get_max_nbr_category_displayed()
-        {
-                return $this->get_property('nbr_cat_max');
-        }
-        
-        public function set_max_nbr_category_displayed($nbr_category) 
-        {
-                $this->set_property('nbr_cat_max', $nbr_category);
-        }
-        
-        public function get_number_columns_per_page()
-        {
-                return $this->get_property('nbr_column');
-        }
-        
-        public function set_number_columns_per_page($nbr_cols) 
-        {
-                $this->set_property('nbr_column', $nbr_cols);
-        }
-        
-        public function get_max_notation_scale()
-        {
-                return $this->get_property('note_max');
-        }
-        
-        public function set_max_notation_scale($note) 
-        {
-                $this->set_property('note_max', $note);
-        }
-        
-        public function get_default_values()
-        {
-                return array(
-                        self::AUTHORIZATION => array('r-1' => 1, 'r0' => 3, 'r1' => 7),
-                        'nbr_articles_max' => 10,
-                        'nbr_cat_max' => 10,
-                        'nbr_column' => 3,
-                        'note_max' => 5
-                );
-        }
-        
-        /**
-         * Returns the configuration.
-         * @return ArticlesConfig
-         */
-        public static function load()
-        {
-                return ConfigManager::load(__CLASS__, 'module', 'articles-config');
-        }
+	const AUTHORIZATIONS = 'authorizations';
+	const NUMBER_ARTICLES_PER_PAGE = 'number_articles_per_page';
+	const NUMBER_CATEGORIES_PER_PAGE = 'number_categories_per_page';
+	const NUMBER_COLUMNS_DISPLAYED = 'number_columns_displayed';
+	const NOTATION_SCALE = 'notation_scale';
+	
+	public function get_authorizations()
+	{
+		return $this->get_property(self::AUTHORIZATIONS);
+	}
+	
+	public function set_authorizations(Array $array)
+	{
+		$this->set_property(self::AUTHORIZATIONS, $array);
+	}
+	
+	public function get_number_articles_per_page()
+	{
+		return $this->get_property(self::NUMBER_ARTICLES_PER_PAGE);
+	}
+	
+	public function set_number_articles_per_page($number) 
+	{
+		$this->set_property(self::NUMBER_ARTICLES_PER_PAGE, $number);
+	}
+	
+	public function get_number_categories_per_page()
+	{
+		return $this->get_property(self::NUMBER_CATEGORIES_PER_PAGE);
+	}
+	
+	public function set_number_categories_per_page($number) 
+	{
+		$this->set_property(self::NUMBER_CATEGORIES_PER_PAGE, $number);
+	}
+	
+	public function get_number_columns_displayed()
+	{
+		return $this->get_property(self::NUMBER_COLUMNS_DISPLAYED);
+	}
+	
+	public function set_number_columns_displayed($number) 
+	{
+		$this->set_property(self::NUMBER_COLUMNS_DISPLAYED, $number);
+	}
+	
+	public function get_notation_scale()
+	{
+		return $this->get_property(self::NOTATION_SCALE);
+	}
+	
+	public function set_notation_scale($notation_scale) 
+	{
+		$this->set_property(self::NOTATION_SCALE, $notation_scale);
+	}
+	
+	public function get_default_values()
+	{
+		return array(
+			self::AUTHORIZATIONS => array(),
+			self::NUMBER_ARTICLES_PER_PAGE => 10,
+			self::NUMBER_CATEGORIES_PER_PAGE => 10,
+			self::NUMBER_COLUMNS_DISPLAYED => 3,
+			self::NOTATION_SCALE => 5
+		);
+	}
+	
+	/**
+	 * Returns the configuration.
+	 * @return ArticlesConfig
+	 */
+	public static function load()
+	{
+		return ConfigManager::load(__CLASS__, 'module', 'articles-config');
+	}
 
-        /**
-         * Saves the configuration in the database. Has it become persistent.
-         */
-        public static function save()
-        {
-                ConfigManager::save('module', self::load(), 'articles-config');
-        }
+	/**
+	 * Saves the configuration in the database. Has it become persistent.
+	 */
+	public static function save()
+	{
+		ConfigManager::save('module', self::load(), 'articles-config');
+	}
 }
 ?>
