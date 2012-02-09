9c3a9156d1cb0adc220faa2ad2991d46904976a8
julien briswalter <julien.briswalter@gmail.com>
2012-02-09 18:10:30 +0100

    [Feature] Int?gration du PagesHomePageExtensionPoint

--- pages/pages.php
+++ pages/pages.php
@@ -260,103 +260,12 @@ elseif (!empty($error))
 }
 else
 {
-	$Template = new FileTemplate('pages/index.tpl');
-	
-	$num_pages = $Sql->query("SELECT COUNT(*) FROM " . PREFIX . "pages WHERE redirect = '0'", __LINE__, __FILE__);
-	$num_coms = $Sql->query("SELECT COUNT(*) FROM " . DB_TABLE_COM . " WHERE script = 'pages'", __LINE__, __FILE__);
-	
-	$Template->put_all(array(
-		'NUM_PAGES' => sprintf($LANG['pages_num_pages'], $num_pages),
-		'NUM_COMS' => sprintf($LANG['pages_num_coms'], $num_coms, ($num_pages > 0 ? $num_coms / $num_pages : 0)),
-		'L_EXPLAIN_PAGES' => $LANG['pages_explain'],
-		'L_TOOLS' => $LANG['pages_tools'],
-		'L_STATS' => $LANG['pages_stats']
-	));
-	
-	$tools = array(
-		$LANG['pages_create'] => url('post.php'),
-		$LANG['pages_redirections'] => url('action.php'),
-		$LANG['pages_explorer'] => url('explorer.php'),
-	);
-	if ($User->check_level(User::ADMIN_LEVEL))
-		$tools[$LANG['pages_config']] = url('admin_pages.php');
-	
-	foreach ($tools as $tool => $url)
-		$Template->assign_block_vars('tools', array(
-			'L_TOOL' => $tool,
-			'U_TOOL' => $url
-		));
-	
-	//Liste des dossiers de la racine
-	$root = '';
-	foreach ($_PAGES_CATS as $key => $value)
+	$modulesLoader = AppContext::get_extension_provider_service();
+	$module = $modulesLoader->get_provider('pages');
+	if ($module->has_extension_point(HomePageExtensionPoint::EXTENSION_POINT))
 	{
-		if ($value['id_parent'] == 0)
-		{
-			//Autorisation particulière ?
-			$special_auth = !empty($value['auth']);
-			//Vérification de l'autorisation d'éditer la page
-			if (($special_auth && $User->check_auth($value['auth'], READ_PAGE)) || (!$special_auth && $User->check_auth($_PAGES_CONFIG['auth'], READ_PAGE)))
-			{
-				$root .= '<tr><td class="row2"><img src="' . $Template->get_pictures_data_path() . '/images/closed_cat.png" alt="" style="vertical-align:middle" />&nbsp;<a href="javascript:open_cat(' . $key . '); show_cat_contents(' . $value['id_parent'] . ', 0);">' . $value['name'] . '</a></td></tr>';
-			}
-		}
+		echo $module->get_extension_point(HomePageExtensionPoint::EXTENSION_POINT)->get_home_page()->get_view()->display();
 	}
-	//Liste des fichiers de la racine
-	$result = $Sql->query_while("SELECT title, id, encoded_title, auth
-		FROM " . PREFIX . "pages
-		WHERE id_cat = 0 AND is_cat = 0
-		ORDER BY is_cat DESC, title ASC", __LINE__, __FILE__);
-	while ($row = $Sql->fetch_assoc($result))
-	{
-		//Autorisation particulière ?
-		$special_auth = !empty($row['auth']);
-		$array_auth = unserialize($row['auth']);
-		//Vérification de l'autorisation d'éditer la page
-		if (($special_auth && $User->check_auth($array_auth, READ_PAGE)) || (!$special_auth && $User->check_auth($_PAGES_CONFIG['auth'], READ_PAGE)))
-		{
-			$root .= '<tr><td class="row2"><img src="' . $Template->get_pictures_data_path() . '/images/page.png" alt=""  style="vertical-align:middle" />&nbsp;<a href="' . PagesUrlBuilder::get_link_item($row['encoded_title']) . '">' . $row['title'] . '</a></td></tr>';
-		}
-	}
-	$Sql->query_close($result);
-
-	$Template->put_all(array(
-		'TITLE' => $LANG['pages'],
-		'L_ROOT' => $LANG['pages_root'],
-		'ROOT_CONTENTS' => $root,
-		'L_CATS' => $LANG['pages_cats_tree'],
-		'L_EXPLORER' => $LANG['pages_explorer'],
-		'SELECTED_CAT' => 0,
-		'CAT_0' => 'pages_selected_cat',
-		'CAT_LIST' => ''
-	));
-
-	$contents = '';
-	$result = $Sql->query_while("SELECT c.id, p.title, p.encoded_title
-	FROM " . PREFIX . "pages_cats c
-	LEFT JOIN " . PREFIX . "pages p ON p.id = c.id_page
-	WHERE c.id_parent = 0
-	ORDER BY p.title ASC", __LINE__, __FILE__);
-	while ($row = $Sql->fetch_assoc($result))
-	{
-		$sub_cats_number = $Sql->query("SELECT COUNT(*) FROM " . PREFIX . "pages_cats WHERE id_parent = '" . $row['id'] . "'", __LINE__, __FILE__);
-		if ($sub_cats_number > 0)
-		{	
-			$Template->assign_block_vars('list', array(
-				'DIRECTORY' => '<li><a href="javascript:show_cat_contents(' . $row['id'] . ', 0);"><img src="' . $Template->get_pictures_data_path() . '/images/plus.png" alt="" id="img2_' . $row['id'] . '"  style="vertical-align:middle" /></a> 
-				<a href="javascript:show_cat_contents(' . $row['id'] . ', 0);"><img src="' . $Template->get_pictures_data_path() . '/images/closed_cat.png" id ="img_' . $row['id'] . '" alt="" style="vertical-align:middle" /></a>&nbsp;<span id="class_' . $row['id'] . '" class=""><a href="javascript:open_cat(' . $row['id'] . ');">' . $row['title'] . '</a></span><span id="cat_' . $row['id'] . '"></span></li>'
-			));
-		}
-		else
-		{
-			$Template->assign_block_vars('list', array(
-				'DIRECTORY' => '<li style="padding-left:17px;"><img src="' . $Template->get_pictures_data_path() . '/images/closed_cat.png" alt=""  style="vertical-align:middle" />&nbsp;<span id="class_' . $row['id'] . '" class=""><a href="javascript:open_cat(' . $row['id'] . ');">' . $row['title'] . '</a></span><span id="cat_' . $row['id'] . '"></span></li>'
-			));
-		}
-	}
-	$Sql->query_close($result);
-	
-	$Template->display();
 }
 
 require_once('../kernel/footer.php'); 
