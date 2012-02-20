9c8fe6f8d7a065d34de0b62491c16c0a2575c557
soldier.weasel <soldier.weasel@localhost>
2012-02-20 15:05:40 +0100

    [MAJ] Administration des modules BBCode et TinyMCE

--- BBCode/index.php
+++ BBCode/index.php
@@ -1,10 +1,3 @@
-<html>
-<head>
-<title></title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-</head>
-
-<body>
-
-</body>
-</html>
+<?php 
+header('Location: ../admin/admin_content_config.php');
+?>
