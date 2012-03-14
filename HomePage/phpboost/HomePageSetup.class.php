fde5b9dce60ef216a2b52407e0716a1c6f2ec056
soldier.weasel <soldier.weasel@localhost>
2012-03-14 10:11:03 +0100

    [MAJ] Avancement sur le module HomePage

--- HomePage/phpboost/HomePageSetup.class.php
+++ HomePage/phpboost/HomePageSetup.class.php
@@ -1,4 +1,30 @@
 <?php
+/*##################################################
+ *                           HomePageSetup.class.php
+ *                            -------------------
+ *   begin                : March 14, 2012
+ *   copyright            : (C) 2012 Kévin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
 class HomePageSetup extends DefaultModuleSetup
 {
 	public static $home_page_table;
@@ -12,13 +38,11 @@ class HomePageSetup extends DefaultModuleSetup
 	{
 		$this->drop_tables();
 		$this->create_tables();
-		$this->create_field_member();
 	}
 
 	public function uninstall()
 	{
 		$this->drop_tables();
-		$this->delete_field_member();
 	}
 
 	private function drop_tables()
