28d66163cdd63d46c6229248d82d7db2ff13a749
soldier.weasel <soldier.weasel@localhost>
2012-03-15 10:31:49 +0100

    [MAJ] Installation => Suppression du cache avant le lancement de l'installateur

--- install/controllers/InstallWelcomeController.class.php
+++ install/controllers/InstallWelcomeController.class.php
@@ -29,6 +29,7 @@ class InstallWelcomeController extends InstallController
 {
 	public function execute(HTTPRequest $request)
 	{
+		AppContext::get_cache_service()->clear_cache();
         parent::load_lang($request);
 		$view = new FileTemplate('install/welcome.tpl');
 		$this->add_navigation($view);
