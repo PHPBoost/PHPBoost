7bb12233c24d73f19a464feb4ecae7816e22cf60
soldier.weasel <soldier.weasel@localhost>
2012-04-19 14:21:23 +0200

    [MAJ] Texte de l'article ? l'installation

--- articles/lang/english/install.php
+++ articles/lang/english/install.php
@@ -32,13 +32,21 @@ $lang['default.category.description'] = 'Demonstration of an article';
 
 $lang['default.article.name'] = 'Items begin with the module';
 $lang['default.article.description'] = '';
-$lang['default.article.contents'] = 'This brief article will give you some simple tips to take control of this module.<br />
-\r\n<br />\r\n<ul class="bb_ul">\r\n	<li class="bb_li">To configure your module, <a href="/articles/admin_articles_config.php">click here</a>\r\n	
-</li><li class="bb_li">To add categories : <a href="/admin_articles_cat_add.php">click here</a> (categories and subcategories are infinitely)\r\n	</li>
-<li class="bb_li">To add an item, you have two solutions (the two arrive at the same link)<br />\r\n		<ul class="bb_ul">\r\n			
-<li class="bb_li">In the desired category, click \"Add\"\r\n			</li>
-<li class="bb_li"><a href="/articles/admin_articles_add.php">Click here</a> to add it via the admin panel of the module.<br />\r\n		</li></ul>\r\n	</li>
-<li class="bb_li">To format your articles, you can use bbcode language or the WYSIWYG editor (cf this <a href="http://www.phpboost.com/articles/articles-6-61+mise-en-page-du-contenu.php">article</a>)<br />\r\n</li>
-</ul><br />\r\n<br />\r\nFor more information, please see the module documentation on the site <a href="http://www.phpboost.com">PHPBoost</a>.
-<br />\r\n<br />\r\n<br />\r\nGood use of this module.';
+$lang['default.article.contents'] = 'This brief article will give you some simple tips to take control of this module.<br /> 
+<br /> 
+<ul class="bb_ul"> 
+<li class="bb_li">To configure your module, <a href="/articles/admin_articles_config.php">click here</a> 
+</li><li class="bb_li">To add categories :<a href="/admin_articles_cat_add.php">click here</a> (categories and subcategories are infinitely)
+</li><li class="bb_li">To add an item, you have two solutions (the two arrive at the same link)<br /> 
+<ul class="bb_ul"> 
+<li class="bb_li">In the desired category, click \'Add\' 
+</li><li class="bb_li"><a href="/articles/admin_articles_add.php">Click here</a> to add it via the admin panel of the module.<br /> 
+</li></ul> 
+</li><li class="bb_li">To format your articles, you can use bbcode language or the WYSIWYG editor (cf this (cf cet <a href="http://www.phpboost.com/articles/articles-6-61+mise-en-page-du-contenu.php">article</a>)<br /> 
+</li></ul><br /> 
+<br /> 
+For more information, please see the module documentation on the site <a href="http://www.phpboost.com">PHPBoost</a>.<br /> 
+<br /> 
+<br /> 
+Good use of this module.';
 ?>
