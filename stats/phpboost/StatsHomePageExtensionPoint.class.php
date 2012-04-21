1173743b2e301cb07f443ffed71b5d6a4367112f
soldier.weasel <soldier.weasel@localhost>
2012-04-21 08:36:19 +0200

    [FIX] stats

--- stats/phpboost/StatsHomePageExtensionPoint.class.php
+++ stats/phpboost/StatsHomePageExtensionPoint.class.php
@@ -121,9 +121,10 @@ class StatsHomePageExtensionPoint implements HomePageExtensionPoint
 			}
 
 			$stats_array = array();
-			$result = $this->sql_querier->query_while ("SELECT count(user_sex) as compt, user_sex
-			FROM " . PREFIX . "member
-			GROUP BY user_sex
+			$result = $this->sql_querier->query_while ("SELECT count(ext_field.user_sex) as compt, ext_field.user_sex
+			FROM " . PREFIX . "member member
+			LEFT JOIN " . DB_TABLE_MEMBER_EXTENDED_FIELDS . " ext_field ON ext_field.user_id = member.user_id
+			GROUP BY ext_field.user_sex
 			ORDER BY compt", __LINE__, __FILE__);
 			while ($row = $this->sql_querier->fetch_assoc($result))
 			{
