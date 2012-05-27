1871787efadbbb9bbb69f1ca67dc7b8981840d84
soldier.weasel <soldier.weasel@localhost>
2012-05-27 13:56:30 +0200

    alpha 21

---
+++ lang/french/admin-langs-common.php
@@ -0,0 +1,65 @@
+<?php
+/*##################################################
+ *                                admin-langs-common.php
+ *                            -------------------
+ *   begin                : April 12, 2012
+ *   copyright            : (C) 2012 Kevin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+ ####################################################
+ #                     French                       #
+ ####################################################
+ 
+$lang = array();
+
+//Title
+$lang['langs'] = 'Langues';
+$lang['langs.management'] = 'Gestion des langues';
+$lang['langs.installed'] = 'Langues installées';
+$lang['langs.uninstall'] = 'Suppression de la langue';
+$lang['langs.install'] = 'Installer';
+
+//Errors
+$lang['langs.already_exist'] = 'La langue existe déjà';
+$lang['langs.not_compatible'] = 'La langue n\'est pas compatible avec votre version de PHPBoost';
+$lang['themes.add.not_lang'] = 'Aucune langue à ajouter';
+
+//Uninstall
+$lang['langs.uninstall.drop_files'] = 'Supprimer tous les fichiers de la langue';
+$lang['langs.default_lang_explain'] = 'La langue par défaut ne peut pas être désinstallée, désactivée ou réservée';
+
+$lang['langs.upload'] = 'Uploader une langue';
+$lang['langs.upload.description'] = 'L\'archive uploadée doit être au format zip ou gzip';
+
+
+$lang['langs.not_installed'] = 'Langues non installées';
+$lang['langs.name'] = 'Nom';
+$lang['langs.authorisations'] = 'Autorisations';
+$lang['langs.activated'] = 'Activé';
+$lang['langs.compatibility'] = 'Compatibilité';
+$lang['langs.author'] = 'Auteur';
+$lang['langs.description'] = 'Description';
+
+$lang['langs.no'] = 'Non';
+$lang['langs.yes'] = 'Oui';
+$lang['langs.visitor'] = 'Visiteur';
+?>
