315515cfe1f7a8e1371d133685dc6c330df013d4
soldier.weasel <soldier.weasel@localhost>
2012-06-12 22:09:27 +0200

    [FIX] Enregistrement d'un membre

--- install/services/InstallationServices.class.php
+++ install/services/InstallationServices.class.php
@@ -485,12 +485,13 @@ class InstallationServices
 
 	private function send_installation_mail($login, $password, $email, $unlock_admin)
 	{
+		$general_config = GeneralConfig::load();
 		$mail = new Mail();
 		$mail->set_sender($email, 'admin');
 		$mail->add_recipient($email);
 		$mail->set_subject($this->messages['admin.created.email.object']);
 		$mail->set_content(sprintf($this->messages['admin.created.email.unlockCode'], stripslashes($login),
-		stripslashes($login), $password, $unlock_admin, HOST . DIR));
+		stripslashes($login), $password, $unlock_admin, $general_config->get_default_site_url() . $general_config->get_default_site_url()));
 		AppContext::get_mail_service()->try_to_send($mail);
 	}
 
