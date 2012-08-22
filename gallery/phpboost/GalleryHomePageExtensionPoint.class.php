c1c175232df107216962a9bf5378c4b3d50124bf
soldier.weasel <soldier.weasel@localhost>
2012-08-22 21:47:15 +0200

    [FIX] Url du module Galerie

--- gallery/phpboost/GalleryHomePageExtensionPoint.class.php
+++ gallery/phpboost/GalleryHomePageExtensionPoint.class.php
@@ -337,7 +337,7 @@ class GalleryHomePageExtensionPoint implements HomePageExtensionPoint
 							$Gallery->Resize_pics(PATH_TO_ROOT . '/gallery/pics/' . $row['path']); //Redimensionnement + création miniature
 	
 						//Affichage de la liste des miniatures sous l'image.
-						$array_pics[] = '<td class="row2" style="text-align:center;height:' . ($CONFIG_GALLERY['height'] + 16) . 'px"><span id="thumb' . $i . '"><a href="' . GalleryUrlBuilder::get_link_item($row['idcat'],$row['id'],null,$g_sort) . '#pics_max' . '"><img src="pics/thumbnails/' . $row['path'] . '" alt="" / ></a></span></td>';
+						$array_pics[] = '<td class="row2" style="text-align:center;height:' . ($CONFIG_GALLERY['height'] + 16) . 'px"><span id="thumb' . $i . '"><a href="gallery' . url('.php?cat=' . $row['idcat'] . '&amp;id=' . $row['id'] . '&amp;sort=' . $g_sort, '-' . $row['idcat'] . '-' . $row['id'] . '.php?sort=' . $g_sort) . '#pics_max' . '"><img src="pics/thumbnails/' . $row['path'] . '" alt="" / ></a></span></td>';
 	
 						if ($row['id'] == $g_idpics)
 						{
