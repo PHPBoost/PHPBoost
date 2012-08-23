ad485f2d7232ecbccf91e8a57c836262fe5fe287
soldier.weasel <soldier.weasel@localhost>
2012-08-23 15:08:47 +0200

    [MAJ] Affichage des commentaires d'un membre

--- lang/french/comments-common.php
+++ lang/french/comments-common.php
@@ -47,4 +47,5 @@ $lang['allComments'] = 'Voir les autres commentaires';
 $lang['comments.user.read-only'] = 'Vous êtes actuellement en lecture seule, vous ne pouvez pas poster de commentaire pour le moment';
 
 $lang['module.choice'] = 'Choisissez un module';
+$lang['no_comment'] = 'Aucun commentaire';
 ?>
