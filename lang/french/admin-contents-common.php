c3064abeee200dfe130b8382bd3a744298ae389d
soldier.weasel <soldier.weasel@localhost>
2012-08-24 12:10:32 +0200

    [MAJ] Nettoyage de fichier

---
+++ lang/french/admin-contents-common.php
@@ -0,0 +1,58 @@
+<?php
+/*##################################################
+ *                           admin-contents-common.php
+ *                            -------------------
+ *   begin                : August 10, 2011
+ *   copyright            : (C) 2011 Kevin MASSY
+ *   email                : soldier.weasel@gmail.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+ ####################################################
+#                     French                       #
+ ####################################################
+ 
+$lang = array();
+
+$lang['comments'] = 'Commentaires';
+$lang['comments.config'] = 'Configuration des commentaires';
+$lang['comments.management'] = 'Gestion des commentaires';
+
+$lang['comments.config.display-captcha'] = 'Code de vérification visuel';
+$lang['comments.config.captcha-difficulty'] = 'Difficulté du code de vérification';
+$lang['comments.config.number-comments-display'] = 'Nombre de commentaires à afficher par défaut';
+$lang['comments.config.order-display-comments'] = 'Ordre d\'affichage des commentaires';
+$lang['comments.config.order-display-comments.asc'] = 'Du plus vieux au plus récent';
+$lang['comments.config.order-display-comments.desc'] = 'Du plus récent au plus vieux';
+
+$lang['comments.config.authorization'] = 'Autorisations';
+$lang['comments.config.authorization-read'] = 'Autorisation de voir les commentaires';
+$lang['comments.config.authorization-post'] = 'Autorisation de poster un commentaire';
+$lang['comments.config.authorization-moderation'] = 'Autorisation de gérer les commentaires';
+$lang['comments.config.authorization-note'] = 'Autorisation de noter les commentaires';
+$lang['comments.config.max-links-comment'] = 'Nombre de liens autorisés dans un commentaire';
+$lang['comments.config.forbidden-tags'] = 'Types de formatage interdits';
+$lang['comments.config.approbation'] = 'Approbation des commentaires';
+$lang['comments.config.approbation.auto'] = 'Automatiquement';
+$lang['comments.config.approbation.moderator'] = 'Modérateur';
+$lang['comments.config.approbation.administrator'] = 'Administrateur';
+
+$lang['number-required'] = 'La valeur saisi doit être un nombre';
+?>
