dfcceadc2602ffc59d3ed21245ac1b0445cf1bad
soldier.weasel <soldier.weasel@localhost>
2012-08-29 13:47:38 +0200

    [FIX] Lien de l'avatar par d?faut (probl?me de path)

--- kernel/framework/content/comments/CommentsService.class.php
+++ kernel/framework/content/comments/CommentsService.class.php
@@ -211,7 +211,7 @@ class CommentsService
 				$path = PATH_TO_ROOT . $row['path'];
 				
 				if (empty($row['user_avatar']))
-					$user_avatar = $user_accounts_config->is_default_avatar_enabled() == '1' ? Url::to_rel(PATH_TO_ROOT .'/templates/' . get_utheme() . '/images/' .  $user_accounts_config->get_default_avatar_name()) : '';
+					$user_avatar = $user_accounts_config->is_default_avatar_enabled() == '1' ? Url::to_rel('/templates/' . get_utheme() . '/images/' .  $user_accounts_config->get_default_avatar_name()) : '';
 				else
 					$user_avatar = Url::to_rel($row['user_avatar']);
 				
