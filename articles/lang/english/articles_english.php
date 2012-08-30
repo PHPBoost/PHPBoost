2f51c1936c8a490761477f9149f5e6fde9c95b0b
soldier.weasel <soldier.weasel@localhost>
2012-08-30 13:39:53 +0200

    [FIX] Langue anglaise dans le module articles

--- articles/lang/english/articles_english.php
+++ articles/lang/english/articles_english.php
@@ -30,29 +30,13 @@
 #                                                           English                                                                             #
  ####################################################
 
-//Admin
-
-//Error
+//Erreurs
 $LANG['e_unexist_articles'] = 'This article doesn\'t exist';
-
-//Add articles
-$MAIL['new_article_website'] = 'New article on your website';
-$MAIL['new_article'] = 'A new article was added on your website ' . HOST . ',
-it will have to be approved before being readable by everyone.
-
-Title of the article: %s
-Contents: %s...[next]
-Posted by: %s
-
-Go into the articles management panel of the administration, and approve it.
-' . HOST . DIR . '/admin/admin_articles_gestion.php';
-
 
 global $ARTICLES_LANG;
 // contribution
 $ARTICLES_LANG = array(
 	'articles_management' => 'Articles management',
-	'configuration_articles' => 'Articles configuration',
 	'recount' => 'Recount',
 	'explain_articles_count' => 'Recount the number of articles per category',
 	'nbr_articles_max' => 'Maximum number of displayed articles',
@@ -77,7 +61,6 @@ $ARTICLES_LANG = array(
 	'contribution_counterpart_explain' => 'Tell us why you want us to add this article. This field is not required, but it may help the moderator to take his decision.',
 	'contribution_entitled' => '[Articles] %s',
 	'contribution_success' => 'Your contribution has been saved.',	
-	'notice_contribution' => 'You aren\'t authorized to add an article, however you can contribute by submitting a one. Your contribution will be processed by an moderator.',
 	'global_auth' => 'Overall permissions',
 	'global_auth_explain' => 'Here you can define overall permissions of the module. You can change these permissions locally in each category',
 	'auth_contribute' => 'Contribution permissions',
@@ -87,8 +70,9 @@ $ARTICLES_LANG = array(
 	'add_articles' => 'Add an article',
 	'release_date' => 'Release date',
 	'removing_category' => 'Removing category',
-	'require_cat' => 'Please choice a category!',
-	'required_fields_empty' => 'Whole required files are not be typed, please correctly redo the operation',	
+	'require_cat' => 'Please select a category!',
+	'articles_date' => 'Article date',
+	'required_fields_empty' => 'Required fields have not been filled, thank you repeat the operation correctly',	
 	'category_name' => 'Name of the category',
 	'category_desc' => 'Description of the category',
 	'category_image' => 'Icon of the category',
@@ -96,7 +80,9 @@ $ARTICLES_LANG = array(
 	'special_auth' => 'Special permissions',
 	'special_auth_explain' => 'The category will have the general configuration of the module. You can apply particular permissions.',
 	'add_category' => 'Add a category',
+	'configuration_articles' => 'Articles configuration',
 	'category_articles' => 'Categories management',
+	'required_fields_empty' => 'Whole required files are not be typed, please correctly redo the operation',	
 	'unexisting_category' => 'The category you have selected does not exist',
 	'new_cat_does_not_exist' => 'The target category does not exist',
 	'infinite_loop' => 'You want to move the category in one of its subcategories or in itself, that makes no sense. Please choose another category',
@@ -105,10 +91,32 @@ $ARTICLES_LANG = array(
 	'delete_category_and_its_content' => 'Delete the category and all its contents',
 	'move_category_content' => 'Move its contents in:',
 	'edit_articles' =>  'Edit articles',
-	'use_tab'=>"Use tab for articles pagination",
+	'notice_contribution' => 'You aren\'t authorized to add an article, however you can contribute by submitting a one. Your contribution will be processed by an moderator.',
 	'or_direct_path' => 'Or direct path',
-	'waiting_articles' => 'Waiting articles of :',
+	'waiting_articles' => 'Waiting articles',
 	'no_articles_available' => 'No articles avaible',
+	
+	'article_description' => 'Description',
+	'no_articles_waiting' => 'No items available for wait time',
+	'publicate_articles' => 'Articles published',
+	'source' => 'Sources',
+	'add_source' => 'Add a source',
+	'source_link' => 'Source link',
+	'special_auth_explain_articles' => 'Article will default the general configuration of its class. You can apply special permissions.',
+	'special_option_explain' => 'Article will default the general configuration of its class. You can apply particulères options.',
+	'special_option' => 'Special options',	
+	'articles_best_note' => 'Items rated',
+	'articles_more_com' => 'Articles with the most comments',
+	'articles_by_date' => 'Recent articles',
+	'articles_most_popular' => 'Most popular articles',
+	'author' => 'Author',
+	'more_article' => 'More article',
+	'hide' => 'Hide',
+	'enable' =>' Enable',
+	'desable' => 'Disable',
+	'order_by' => 'Order by ',
+	
+	'print' => 'Print',
+	'read_article' => 'Read more',
 );
-
 ?>
