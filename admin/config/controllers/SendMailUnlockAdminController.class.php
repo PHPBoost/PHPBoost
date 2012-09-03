5cd80eee499cdbeafa94c0fa7bd57bcdee8a829f
soldier.weasel <soldier.weasel@localhost>
2012-09-03 23:25:13 +0200

    [MAJ]

--- admin/config/controllers/SendMailUnlockAdminController.class.php
+++ admin/config/controllers/SendMailUnlockAdminController.class.php
@@ -75,7 +75,7 @@ class SendMailUnlockAdminController extends AdminController
         	$mail->add_recipient($mail_address);
         }
 		
-        $mail->set_sender(MailServiceConfig::load()->get_default_mail_sender(), GeneralConfig::load()->get_site_name());
+        $mail->set_sender(MailServiceConfig::load()->get_default_mail_sender());
         $mail->set_subject($subject);
         $mail->set_content($content);
         $mail->set_is_html(true);
