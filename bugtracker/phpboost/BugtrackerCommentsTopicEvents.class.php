27acae75209acda7affe8852c44dab4fbdbf5e3d
julien.briswalter <julien.briswalter@localhost>
2012-10-11 16:28:24 +0200

    [MAJ] BugtrackerCommentsTopicEvents.class.php

--- bugtracker/phpboost/BugtrackerCommentsTopicEvents.class.php
+++ bugtracker/phpboost/BugtrackerCommentsTopicEvents.class.php
@@ -28,18 +28,12 @@
 class BugtrackerCommentsTopicEvents extends CommentsTopicEvents
 {	
 	private $sql_querier;
-	protected $comments_topic;
-	
-	public function __construct(CommentsTopic $comments_topic)
-	{
-        $this->sql_querier = PersistenceContext::get_sql();
-		$this->comments_topic = $comments_topic;
-	}
 	
 	public function execute_add_comment_event()
 	{
-		global $LANG, $User;
-		
+		global $LANG;
+		$this->sql_querier = PersistenceContext::get_querier();
+		$User = AppContext::get_current_user();
 		$now = new Date(DATE_NOW, TIMEZONE_AUTO);
 		
 		// Récupéreration du contenu du commentaire
@@ -49,36 +43,41 @@ class BugtrackerCommentsTopicEvents extends CommentsTopicEvents
 		$bug_id = $this->comments_topic->get_id_in_module();
 		
 		//Récupération de l'id de l'auteur du bug
-		$author_id = $this->sql_querier->query("SELECT author_id FROM " . PREFIX . "bugtracker WHERE id = '" . $bug_id . "'", __LINE__, __FILE__);
+		$author_id = $this->sql_querier->select_single_row(PREFIX . 'bugtracker', array('author_id'), 'WHERE id=:id', array(
+			'id' => $bug_id
+		));
 		
 		//Récupération de l'id des personnes qui ont mis à jour le bug
 		$updaters_ids = array($author_id);
-		$result = $this->sql_querier->query_while("SELECT updater_id
-		FROM " . PREFIX . "bugtracker_history
-		WHERE bug_id = '" . $bug_id . "'
-		GROUP BY updater_id
-		", __LINE__, __FILE__);
-		while ($row = $this->sql_querier->fetch_assoc($result))
+		
+		$result = $this->sql_querier->select('SELECT updater_id FROM ' . PREFIX . 'bugtracker_history WHERE bug_id=:id GROUP BY updater_id', array(
+			'id' => $bug_id
+		), SelectQueryResult::FETCH_ASSOC .'ou'. SelectQueryResult::FETCH_NUM);
+		while ($row = $result->fetch())
 		{
 			if ($row['updater_id'] != $author_id)
 				$updaters_ids[] = $row['updater_id'];
 		}
-		$this->sql_querier->query_close($result);
 		
 		//Ajout d'une ligne dans l'historique du bug
-		$this->sql_querier->query_inject("INSERT INTO " . PREFIX . "bugtracker_history (bug_id, updater_id, update_date, change_comment)
-		VALUES('" . $bug_id . "', '" . $User->get_id() . "', '" . $now->get_timestamp() . "', '" . $LANG['bugs.notice.new_comment'] . "')", __LINE__, __FILE__);
+		$this->sql_querier->insert(PREFIX . 'bugtracker_history', array(
+			'bug_id' => $bug_id, 
+			'updater_id' => $User->get_id(),
+			'update_date' => $now->get_timestamp(),
+			'change_comment' => $LANG['bugs.notice.new_comment'],
+		));
 		
 		// Envoi d'un MP à chaque utilisateur qui a contribué au bug lorsqu'un commentaire est posté
 		foreach ($updaters_ids as $updater_id)
 		{
+			
 			if ($User->get_attribute('user_id') != $updater_id)
 			{
 				$Privatemsg = new PrivateMsg();
 				$Privatemsg->start_conversation(
 					$updater_id, 
-					sprintf($LANG['bugs.pm.comment.title'], $LANG['bugs.module_title'], $bug_id), 
-					sprintf($LANG['bugs.pm.comment.contents'], $bug_id, $comment, '[url]' . HOST . DIR . '/bugtracker/bugtracker.php?view&id=' . $bug_id . '&com=0#comments_list[/url]'), 
+					sprintf($LANG['bugs.pm.comment.title'], $LANG['bugs.module_title'], $bug_id, $User->get_login()), 
+					sprintf($LANG['bugs.pm.comment.contents'], '[url="' . UserUrlBuilder::profile($row['old_value'])->absolute() . '"]' . $User->get_login() . '[/url]', $bug_id, $comment, '[url]' . HOST . DIR . '/bugtracker/bugtracker.php?view&id=' . $bug_id . '&com=0#comments_list[/url]'), 
 					'-1', 
 					PrivateMsg::SYSTEM_PM
 				);
