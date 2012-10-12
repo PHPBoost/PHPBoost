6515eb515d3756f89943cbee3aa1a52efa24182d
soldier.weasel <soldier.weasel@localhost>
2012-10-12 20:00:58 +0200

    [FIX] Web division par z?ro

--- web/phpboost/WebHomePageExtensionPoint.class.php
+++ web/phpboost/WebHomePageExtensionPoint.class.php
@@ -66,12 +66,10 @@ class WebHomePageExtensionPoint implements HomePageExtensionPoint
 		//On créé une pagination si le nombre de catégories est trop important.
 		 
 		$Pagination = new DeprecatedPagination();
-
-		$nbr_column = $web_config->get_number_columns();
-		if ($total_cat < $web_config->get_number_columns($nbr_column))
-		{
-			$nbr_column = $total_cat;
-		}
+		
+		$nbr_column_config = $web_config->get_number_columns();
+		$nbr_column = ($total_cat > $nbr_column_config) ? $nbr_column_config : $total_cat;
+        $nbr_column = !empty($nbr_column) ? $nbr_column : 1;
 		
 		$tpl->put_all(array(
 			'C_WEB_CAT' => true,
