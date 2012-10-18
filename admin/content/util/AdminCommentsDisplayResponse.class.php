5dbd4aef377a99ab9ffb4ac3c62521329a8a3ee2
soldier.weasel <soldier.weasel@localhost>
2012-10-18 18:23:45 +0200

    [FIX] Comments URL

--- admin/content/util/AdminCommentsDisplayResponse.class.php
+++ admin/content/util/AdminCommentsDisplayResponse.class.php
@@ -34,7 +34,7 @@ class AdminCommentsDisplayResponse extends AdminMenuDisplayResponse
 		$lang = LangLoader::get('admin-contents-common');
 		$picture = '/templates/' . get_utheme() . '/images/com.png';
 		$this->set_title($lang['comments']);
-		$this->add_link($lang['comments.config'], DispatchManager::get_url('/admin/contents/', '/comments/config/'), $picture);
+		$this->add_link($lang['comments.config'], DispatchManager::get_url('/admin/content/', '/comments/config/'), $picture);
 		$this->add_link($lang['comments.management'], UserUrlBuilder::comments(), $picture);
 
 		$env = $this->get_graphical_environment();
