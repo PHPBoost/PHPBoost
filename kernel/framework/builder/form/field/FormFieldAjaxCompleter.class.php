509ddbaa820caa21910ecdcd69ab869dbb146dad
soldier.weasel <soldier.weasel@localhost>
2012-11-14 17:31:54 +0100

    Ajax.Autocompleter

--- kernel/framework/builder/form/field/FormFieldAjaxCompleter.class.php
+++ kernel/framework/builder/form/field/FormFieldAjaxCompleter.class.php
@@ -35,7 +35,7 @@ class FormFieldAjaxCompleter extends FormFieldTextEditor
     private $maxlength = 255;
 	private $method = 'post';
 	private $file;
-	private $name_parameter = 'name';
+	private $name_parameter = 'value';
 	private static $tpl_src = '<input type="text" size="{SIZE}" maxlength="{MAX_LENGTH}" name="${escape(NAME)}" id="${escape(ID)}" value="${escape(VALUE)}"
 	class="${escape(CLASS)}" # IF C_DISABLED # disabled="disabled" # ENDIF # autocomplete="off" /><div id="${escape(ID)}_completer" class="form_autocompleter"></div>
 	<script type="text/javascript">new Ajax.Autocompleter(${escapejs(ID)}, "${escape(ID)}_completer", ${escapejs(FILE)}, { method: ${escapejs(METHOD)}, paramName: ${escapejs(NAME_PARAMETER)} : HTMLForms.getField(${escape(ID)})});</script>';	
