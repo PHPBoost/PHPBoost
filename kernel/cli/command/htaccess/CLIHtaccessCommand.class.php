6042e7f71fa5622da8eff3677b32e3ec6fc6d2c1
soldier.weasel <soldier.weasel@localhost>
2013-01-25 13:29:43 +0100

    [MAJ] CliHtaccessCommand

--- kernel/cli/command/htaccess/CLIHtaccessCommand.class.php
+++ kernel/cli/command/htaccess/CLIHtaccessCommand.class.php
@@ -27,7 +27,7 @@
 
 class CLIHtaccessCommand extends CLIMultipleGoalsCommand
 {
-	private static $name = 'cache';
+	private static $name = 'htaccess';
 	private static $goals = array('content' => 'CLIHtaccessContentCommand', 'rewriting' => 'CLIHtaccessRewritingCommand');
 
 	public function __construct()
