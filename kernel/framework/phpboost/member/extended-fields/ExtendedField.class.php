b3b2b76736a8b90b8a2c0a141cfb2c7b7889b84a
soldier.weasel <soldier.weasel@localhost>
2013-03-12 10:20:26 +0100

    [FIX] ExtendedField

--- kernel/framework/phpboost/member/extended-fields/ExtendedField.class.php
+++ kernel/framework/phpboost/member/extended-fields/ExtendedField.class.php
@@ -189,7 +189,7 @@ class ExtendedField
 	
 	public function get_authorization()
 	{
-		return $this->authorization;
+		return !empty($this->authorization) ? $this->authorization : array('r1' => 3, 'r0' => 3, 'r-1' => 2);
 	}
 	
 	public function set_is_not_installer($is_not_installer)
