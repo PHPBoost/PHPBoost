c7958c0f2fed11ac02b9a89bcdcf8c1117ef03ec
julien.briswalter <julien.briswalter@localhost>
2013-04-16 07:39:24 +0200

    [REVERT] Affichage des auteurs dans la configuration des modules

--- admin/modules/controllers/AdminModuleUpdateController.class.php
+++ admin/modules/controllers/AdminModuleUpdateController.class.php
@@ -85,15 +85,17 @@ class AdminModuleUpdateController extends AdminController
 			if (ModulesManager::module_is_upgradable($module->get_id()))
 			{
 				$configuration = $module->get_configuration();
-				$authors_website = $configuration->get_authors_website();
+				$author = $configuration->get_author();
+				$author_email = $configuration->get_author_email();
+				$author_website = $configuration->get_author_website();
 				
 				$this->view->assign_block_vars('modules_upgradable', array(
 					'ID' => $module->get_id(),
 					'NAME' => ucfirst($configuration->get_name()),
 					'ICON' => $module->get_id(),
 					'VERSION' => $configuration->get_version(),
-					'AUTHOR' => $configuration->get_authors_list(),
-					'AUTHOR_WEBSITE' => !empty($authors_website) ? '<a href="' . $authors_website . '"><img src="' . TPL_PATH_TO_ROOT . '/templates/' . get_utheme() . '/images/' . get_ulang() . '/user_web.png" alt="" /></a>' : '',
+					'AUTHOR' => !empty($author_email) ? '<a href="mailto:' . $author_email . '">' . $author . '</a>' : $author,
+					'AUTHOR_WEBSITE' => !empty($author_website) ? '<a href="' . $author_website . '"><img src="' . TPL_PATH_TO_ROOT . '/templates/' . get_utheme() . '/images/' . get_ulang() . '/user_web.png" alt="" /></a>' : '',
 					'DESCRIPTION' => $configuration->get_description(),
 					'COMPATIBILITY' => $configuration->get_compatibility(),
 					'PHP_VERSION' => $configuration->get_php_version(),
