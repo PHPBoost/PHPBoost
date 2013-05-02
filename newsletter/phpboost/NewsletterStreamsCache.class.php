2893630fb2110d6980d8184f6237562dd17f6f05
soldier.weasel <soldier.weasel@localhost>
2013-05-02 09:56:28 +0200

    [FIX]

--- newsletter/phpboost/NewsletterStreamsCache.class.php
+++ newsletter/phpboost/NewsletterStreamsCache.class.php
@@ -52,7 +52,7 @@ class NewsletterStreamsCache implements CacheData
 
 		while ($row = $result->fetch())
 		{
-			$auth = unserialize($row['auth']);
+			$auth = @unserialize($row['auth']);
 			$this->streams[$row['id']] = array(
 				'id' => $row['id'],
 				'name' => $row['name'],
