da904421c6dd88ee7c1891b70730ec7ffce40706
daaxwizeman <daaxwizeman@localhost>
2013-05-16 15:47:38 -0400

    [MAJ] Home page du module articles, corrections au contr?leur d'ajout/?dition d'articles

--- articles/phpboost/ArticlesModuleHomePage.class.php
+++ articles/phpboost/ArticlesModuleHomePage.class.php
@@ -71,13 +71,13 @@ class ArticlesModuleHomePage implements ModuleHomePage
 		$pagination_cat->set_url(Category::ROOT_CATEGORY, Url::encode_rewrite(LangLoader::get_message('root', 'main')));
 		
 		$nbr_articles_root_cat = PersistenceContext::get_querier()->count(ArticlesSetup::$articles_table, 
-					'WHERE id_category=0 AND (published=1 OR (published=2 AND (publishing_start_date < :timestamp_now 
-					AND publishing_end_date=0) OR publishing_end_date > :timestamp_now))', 
+					'WHERE id_category = 0 AND (published = 1 OR (published = 2 AND (publishing_start_date < :timestamp_now 
+					AND publishing_end_date = 0) OR publishing_end_date > :timestamp_now))', 
 					array(
 					    'timestamp_now' => $now->get_timestamp()
 		));
 		
-		$number_articles_not_published = PersistenceContext::get_querier()->count(ArticlesSetup::$articles_table, 'WHERE published=0');
+		$number_articles_not_published = PersistenceContext::get_querier()->count(ArticlesSetup::$articles_table, 'WHERE published = 0');
 		
 		$this->view->put_all(array(
 			'C_ADD' => $this->add_auth,
@@ -91,6 +91,7 @@ class ArticlesModuleHomePage implements ModuleHomePage
 			'L_WRITTEN' => LangLoader::get_message('written_by', 'main'),
 			'L_ARTICLES_INDEX' => $this->lang['articles'],
 			'L_ADD_ARTICLES' => $this->lang['articles.add'],
+			'L_ALERT_DELETE_ARTICLE' => $this->lang['articles.form.alert_delete_article'],
 			'L_SUBCATEGORIES' => $this->lang['articles.sub_categories'],
 			'L_MANAGE_CATEGORIES' => $this->lang['admin.categories.manage'],
 			'L_EDIT_CONFIG' => $this->lang['articles_configuration'],
@@ -166,24 +167,23 @@ class ArticlesModuleHomePage implements ModuleHomePage
 			$field = $request->get_getstring('field', '');
 			
 			$current_page = ($request->get_getint('page', 1) > 0) ? $request->get_getint('page', 1) : 1;
-			$number_articles_per_page = ArticlesConfig::load()->get_number_articles_per_page();
+			$nbr_articles_per_page = ArticlesConfig::load()->get_number_articles_per_page();
 			
-			$pagination = new ArticlesPagination($current_page, $nbr_articles_root_cat, $number_articles_per_page);
+			$pagination = new ArticlesPagination($current_page, $nbr_articles_root_cat, $nbr_articles_per_page);
 			
 			$limit_page = $pagination->get_display_from();
 
-			$result = PersistenceContext::get_querier()->select('SELECT articles.*, member.level, member.user_groups, member.login
+			$result = PersistenceContext::get_querier()->select('SELECT articles.*, member.level, member.user_groups, member.login, note.number_notes
 			FROM ' . ArticlesSetup::$articles_table . ' articles
 			LEFT JOIN ' . DB_TABLE_MEMBER . ' member ON member.user_id = articles.author_user_id
 			LEFT JOIN ' . DB_TABLE_COMMENTS_TOPIC . ' com ON com.id_in_module = articles.id AND com.module_id = "articles"
 			LEFT JOIN ' . DB_TABLE_AVERAGE_NOTES . ' note ON note.id_in_module = articles.id AND note.module_name = "articles"
 			WHERE articles.id_category = :id_category AND (articles.published = 1 OR (articles.published = 2 AND (articles.publishing_start_date < :timestamp_now 
-			AND articles.publishing_end_date > :timestamp_now) OR articles.publishing_end_date = 0)) 
-			ORDER BY :field :sort LIMIT ' . $number_articles_per_page . ' OFFSET :start_limit', 
+			AND articles.publishing_end_date = 0) OR articles.publishing_end_date > :timestamp_now)) 
+			ORDER BY :field :sort LIMIT ' . $nbr_articles_per_page . ' OFFSET :start_limit', 
 				array(
 				'id_category' => Category::ROOT_CATEGORY,
 				'timestamp_now' => $now->get_timestamp(),
-				'limit' => $number_articles_per_page,
 				'start_limit' => $limit_page,
 				'field' => $field,
 				'sort' => $mode
@@ -196,8 +196,9 @@ class ArticlesModuleHomePage implements ModuleHomePage
 
 			$this->view->put_all(array(
 				    'C_ARTICLES_FILTERS' => true,
-				    'PAGINATION' => $pagination->display()->render(),
-				    'L_TOTAL_ARTICLES' => $nbr_articles_root_cat
+				    'L_ARTICLES_FILTERS_TITLE' => $this->lang['articles.sort_filter_title'],
+				    'PAGINATION' => ($nbr_articles_root_cat > $nbr_articles_per_page) ? $pagination->display()->render() : '',
+				    'L_TOTAL_ARTICLES' => sprintf($this->lang['articles.nbr_articles_category'],$nbr_articles_root_cat)
 			));
 
 			$notation = new Notation();
@@ -208,7 +209,7 @@ class ArticlesModuleHomePage implements ModuleHomePage
 			{
 				$notation->set_id_in_module($row['id']);
 
-				$group_color = User::get_group_color($row['user_group'], $row['level']);
+				$group_color = User::get_group_color($row['user_groups'], $row['level']);
 
 				$this->view->assign_block_vars('articles', array(
 					'C_GROUP_COLOR' => !empty($group_color),
@@ -219,11 +220,11 @@ class ArticlesModuleHomePage implements ModuleHomePage
 					'L_NUMBER_COM' => empty($row['number_comments']) ? '0' : $row['number_comments'],
 					'NOTE' => $row['number_notes'] > 0 ? NotationService::display_static_image($notation, $row['average_notes']) : $this->lang['articles.no_notes'],
 					'DESCRIPTION' =>FormatingHelper::second_parse($row['description']),                                    
-					'U_ARTICLES_LINK_COM' => ArticlesUrlBuilder::home()->absolute() . $row['id'] . '-' . $row['rewrited_title'] . '/comments/',
-					'U_AUTHOR' => '<a href="' . UserUrlBuilder::profile($row['user_id'])->absolute() . '" class="' . UserService::get_level_class($row['level']) . '"' . (!empty($group_color) ? ' style="color:' . $group_color . '"' : '') . '>' . TextHelper::wordwrap_html($row['login'], 19) . '</a>',
-					'U_ARTICLES_LINK' => ArticlesUrlBuilder::display_article(Category::ROOT_CATEGORY, LangLoader::get_message('root', 'main'), $row['id'], $row['rewrited_title'])->absolute(),
-					'U_ARTICLES_EDIT' => ArticlesUrlBuilder::edit_article($row['id'])->absolute(),
-					'U_ARTICLES_DELETE' => ArticlesUrlBuilder::delete_article($row['id'])->absolute()
+					'U_ARTICLE_LINK_COM' => ArticlesUrlBuilder::home()->absolute() . $row['id'] . '-' . $row['rewrited_title'] . '/comments/',
+					'U_AUTHOR' => '<a href="' . UserUrlBuilder::profile($row['author_user_id'])->absolute() . '" class="' . UserService::get_level_class($row['level']) . '"' . (!empty($group_color) ? ' style="color:' . $group_color . '"' : '') . '>' . TextHelper::wordwrap_html($row['login'], 19) . '</a>',
+					/* @ todo: à voir si je ne devrais pas créer un objet RichCategory*/'U_ARTICLE_LINK' => ArticlesUrlBuilder::display_article(Category::ROOT_CATEGORY, 'root', $row['id'], $row['rewrited_title'])->absolute(),
+					'U_EDIT_ARTICLE' => ArticlesUrlBuilder::edit_article($row['id'])->absolute(),
+					'U_DELETE_ARTICLE' => ArticlesUrlBuilder::delete_article($row['id'])->absolute()
 				));
 			}
 			
@@ -232,7 +233,7 @@ class ArticlesModuleHomePage implements ModuleHomePage
 		else 
 		{
 			$this->view->put_all(array(
-				'L_NO_ARTICLES' => $this->lang['articles.no_article']
+				'L_NO_ARTICLE' => $this->lang['articles.no_article']
 			));
 		}
 		
