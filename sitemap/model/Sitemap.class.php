74583e6e64a0caabf095e55c776a50a193c30afc
soldier.weasel <soldier.weasel@localhost>
2013-07-08 13:52:19 +0200

    [FIX] Sitemap

--- sitemap/model/Sitemap.class.php
+++ sitemap/model/Sitemap.class.php
@@ -177,12 +177,10 @@ class Sitemap
 	 */
 	private function build_modules_maps($auth_mode = self::AUTH_PUBLIC)
 	{
-		$providers = array_keys(AppContext::get_extension_provider_service()->get_providers(SitemapExtensionPoint::EXTENSION_POINT));
 		$providers_sitemap = AppContext::get_extension_provider_service()->get_providers(SitemapExtensionPoint::EXTENSION_POINT);
-		
 		foreach (ModulesManager::get_installed_modules_map_sorted_by_localized_name() as $id => $module)
 		{
-			if (in_array($module->get_id(), $providers))
+			if (array_key_exists($module->get_id(), $providers_sitemap))
 			{
 				$sitemap_provider = $providers_sitemap[$module->get_id()]->sitemap();
 				if ($auth_mode == self::AUTH_PUBLIC)
