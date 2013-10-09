322a9c0c55be8cbc54e16cfcf4b79988a3090c6a
soldier.weasel <soldier.weasel@localhost>
2013-10-09 09:08:02 +0200

    [MAJ] Suppression de l'image msn.png

--- user/pm.php
+++ user/pm.php
@@ -706,7 +706,7 @@ elseif (!empty($pm_id_get)) //Messages associés à la conversation.
 			'USER_MSG' => ($is_admin) ? '' : $user_msg,
 			'USER_LOCAL' => ($is_admin) ? '' : $user_local,
 			'USER_MAIL' => ($is_admin) ? '' : ( !empty($row['user_mail']) && ($row['user_show_mail'] == '1' ) ) ? '<a href="mailto:' . $row['user_mail'] . '"><img src="../templates/' . get_utheme() . '/images/' . get_ulang() . '/email.png" alt="' . $row['user_mail']  . '" title="' . $row['user_mail']  . '" /></a>' : '',
-			'USER_MSN' => ($is_admin) ? '' : (!empty($row['user_msn'])) ? '<a href="mailto:' . $row['user_msn'] . '"><img src="../templates/' . get_utheme() . '/images/' . get_ulang() . '/msn.png" alt="' . $row['user_msn']  . '" title="' . $row['user_msn']  . '" /></a>' : '',
+			'USER_MSN' => ($is_admin) ? '' : (!empty($row['user_msn'])) ? '<a href="mailto:' . $row['user_msn'] . '"><span class="label">MSN</span></a>' : '',
 			'USER_YAHOO' => ($is_admin) ? '' : (!empty($row['user_yahoo'])) ? '<a href="mailto:' . $row['user_yahoo'] . '"><span class="label">Yahoo</span></a>' : '',
 			'USER_SIGN' => ($is_admin) ? '' : (!empty($row['user_sign'])) ? '____________________<br />' . FormatingHelper::second_parse($row['user_sign']) : '',
 			'USER_WEB' => ($is_admin) ? '' : (!empty($row['user_website'])) ? '<a href="' . $row['user_website'] . '"><span class="label">Web</span></a>' : '',
