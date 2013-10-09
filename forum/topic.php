b47094507238d71fbc1b326c9627db6613e87289
soldier.weasel <soldier.weasel@localhost>
2013-10-09 09:08:02 +0200

    [MAJ] Suppression de l'image msn.png

--- forum/topic.php
+++ forum/topic.php
@@ -391,7 +391,7 @@ while ( $row = $Sql->fetch_assoc($result) )
 		'USER_MSG' => (!$is_guest) ? $user_msg : '',
 		'USER_LOCAL' => $user_local,
 		'USER_MAIL' => ( !empty($row['user_mail']) && ($row['user_show_mail'] == '1' ) ) ? '<a href="mailto:' . $row['user_mail'] . '"><img src="../templates/' . get_utheme() . '/images/' . get_ulang() . '/email.png" alt="' . $row['user_mail']  . '" title="' . $row['user_mail']  . '" /></a>' : '',			
-		'USER_MSN' => (!empty($row['user_msn'])) ? '<a href="mailto:' . $row['user_msn'] . '"><img src="../templates/' . get_utheme() . '/images/' . get_ulang() . '/msn.png" alt="' . $row['user_msn']  . '" title="' . $row['user_msn']  . '" /></a>' : '',
+		'USER_MSN' => (!empty($row['user_msn'])) ? '<a href="mailto:' . $row['user_msn'] . '"><span class="label">MSN</span></a>' : '',
 		'USER_YAHOO' => (!empty($row['user_yahoo'])) ? '<a href="mailto:' . $row['user_yahoo'] . '"><span class="label">Yahoo</span></a>' : '',
 		'USER_SIGN' => (!empty($row['user_sign'])) ? '____________________<br />' . FormatingHelper::second_parse($row['user_sign']) : '',
 		'USER_WEB' => (!empty($row['user_website'])) ? '<a href="' . $row['user_website'] . '"><span class="label">Web</span></a>' : '',
