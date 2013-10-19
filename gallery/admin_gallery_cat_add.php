5ecf5048a5aad275ddfe2fc9318ced13b7c30241
soldier.weasel <soldier.weasel@localhost>
2013-10-19 18:14:42 +0200

    [FIX] #715 Probl?me avec les cat?gories du module gallery

--- gallery/admin_gallery_cat_add.php
+++ gallery/admin_gallery_cat_add.php
@@ -49,7 +49,7 @@ if (!empty($_POST['add'])) //Nouvelle galerie/catégorie.
 	
 	if (!empty($name))
 	{	
-		if (isset($CAT_GALLERY[$parent_category])) //Insertion sous galerie de niveau x.
+		if (!empty($idcat) && isset($CAT_GALLERY[$parent_category])) //Insertion sous galerie de niveau x.
 		{
 			//Galerie parente de la galerie cible.
 			$list_parent_cats = '';
