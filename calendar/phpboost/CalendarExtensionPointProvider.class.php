05650efe46f43db5c33bdf65e33a3b9d81ab70c8
genet.arnaud <genet.arnaud@localhost>
2013-12-30 11:06:36 +0100

    [MAJ] Suppression du calendar_mini.css

--- calendar/phpboost/CalendarExtensionPointProvider.class.php
+++ calendar/phpboost/CalendarExtensionPointProvider.class.php
@@ -41,7 +41,6 @@ class CalendarExtensionPointProvider extends ExtensionPointProvider
 	{
 		$module_css_files = new ModuleCssFiles();
 		$module_css_files->adding_always_displayed_file('calendar.css');
-		$module_css_files->adding_running_module_displayed_file('calendar.css');
 		return $module_css_files;
 	}
 	
