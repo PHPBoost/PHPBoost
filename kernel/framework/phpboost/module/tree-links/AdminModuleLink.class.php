f644d19661d6d518f1c175ce916a91070605c4b0
soldier.weasel <soldier.weasel@localhost>
2014-01-25 12:55:39 +0100

    [MAJ] Petites optimisations

--- kernel/framework/phpboost/module/tree-links/AdminModuleLink.class.php
+++ kernel/framework/phpboost/module/tree-links/AdminModuleLink.class.php
@@ -32,8 +32,7 @@ class AdminModuleLink extends ModuleLink
 {
 	public function __construct($name, $url)
 	{
-		parent::__construct($name, $url);
-		$this->visibility = AppContext::get_current_user()->check_level(User::ADMIN_LEVEL);
+		parent::__construct($name, $url, AppContext::get_current_user()->check_level(User::ADMIN_LEVEL));
 	}
 }
 ?>
