e85c3afcc25bd96dfcd33a8b649c2f6f74d124f8
soldier.weasel <soldier.weasel@localhost>
2014-01-30 10:10:18 +0100

    [MAJ] JS

--- kernel/lib/js/phpboost/global.js
+++ kernel/lib/js/phpboost/global.js
@@ -488,3 +488,53 @@ function flowPlayerDisplay(id)
 	    }
 	);
 }
+
+var delay = 1000; //Délai après lequel le bloc est automatiquement masqué, après le départ de la souris.
+var timeout;
+var displayed = false;
+var previous_bblock;
+
+//Affiche le bloc.
+function bb_display_block(divID, field)
+{
+	var i;
+	
+	if( timeout )
+		clearTimeout(timeout);
+	
+	var block = document.getElementById('bb-block' + divID + field);
+	if( block.style.display == 'none' )
+	{
+		if( document.getElementById(previous_bblock) )
+			document.getElementById(previous_bblock).style.display = 'none';
+		block.style.display = 'block';
+		displayed = true;
+		previous_bblock = 'bb-block' + divID + field;
+	}
+	else
+	{
+		block.style.display = 'none';
+		displayed = false;
+	}
+}
+
+//Cache le bloc.
+function bb_hide_block(bbfield, field, stop)
+{
+	var nav = navigator.appName; //Recupère le nom du navigateur
+	if( nav == 'Microsoft Internet Explorer' ) // Internet Explorer
+	{
+		if (window.event.toElement == null) //Hack pour ie... encore une fois!
+			return;
+	}
+	
+	if( stop && timeout )
+	{	
+		clearTimeout(timeout);
+	}
+	else if( displayed )
+	{
+		clearTimeout(timeout);
+		timeout = setTimeout('bb_display_block(\'' + bbfield + '\',  \'' + field + '\')', delay);
+	}	
+}
