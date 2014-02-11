f8cd630008a064a2cf0ff2c573b807e215425dd9
soldier.weasel <soldier.weasel@localhost>
2014-02-11 10:35:41 +0100

    [FIX]

--- admin/controllers/AdminLoginController.class.php
+++ admin/controllers/AdminLoginController.class.php
@@ -33,10 +33,6 @@ class AdminLoginController extends AbstractController
 		$lang = LangLoader::get_class(__CLASS__);
 		$view->add_lang($lang);
 
-		$view->put_all(array(
-			'SITE_NAME' => GeneralConfig::load()->get_site_name()
-		));
-			
 		$flood = $request->get_getint('flood', 0); 
 		if ($flood > 0)
 		{
@@ -47,7 +43,7 @@ class AdminLoginController extends AbstractController
 			));
 		}
 
-		return new AdminNodisplayResponse($view);
+		return new AdminDisplayFrameResponse($view);
 	}
 }
 ?>
