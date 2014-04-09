f3655d1358c762cd9758261fd2f37663307d9d94
soldier.weasel <soldier.weasel@localhost>
2014-04-09 22:10:34 +0200

    [FIX] Forum

--- forum/topic.php
+++ forum/topic.php
@@ -29,7 +29,6 @@ require_once('../kernel/begin.php');
 require_once('../forum/forum_begin.php');
 require_once('../forum/forum_tools.php');
 
-$page = retrieve(GET, 'pt', 1);
 $id_get = retrieve(GET, 'id', 0);
 $quote_get = retrieve(GET, 'quote', 0);	
 
@@ -203,7 +202,6 @@ $array_ranks = array(-1 => $LANG['guest_s'], 0 => $LANG['member_s'], 1 => $LANG[
 
 list($track, $track_pm, $track_mail, $poll_done) = array(false, false, false, false);
 $ranks_cache = ForumRanksCache::load()->get_ranks(); //Récupère les rangs en cache.
-$page = retrieve(GET, 'pt', 0); //Redéfinition de la variable $page pour prendre en compte les redirections.
 $quote_last_msg = ($page > 1) ? 1 : 0; //On enlève 1 au limite si on est sur une page > 1, afin de récupérer le dernier msg de la page précédente.
 $i = 0;	
 $j = 0;	
