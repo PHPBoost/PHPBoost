274d511ee25d6f6868867b2a98ada97b5dfa5422
julienseth78 <julienseth78@localhost>
2014-05-09 15:57:34 +0200

    [FIX] #737 Parsage d'url impossible avec le symbole !

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -48,8 +48,8 @@ class Url
 	const DOMAIN_REGEX = '(?:[a-z0-9-_~]+\.)*[a-z0-9-_~]+(?::[0-9]{1,5})?/';
 	const FOLDERS_REGEX = '/*(?:[a-z0-9~_\.-]+/+)*';
 	const FILE_REGEX = '[a-z0-9-+_,~:\.\%]+';
-	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[a-z0-9-+=,_~:;/\.\?\'\%]+(?:=[a-z0-9-+=_~:;/\.\?\'\%]+)?)*)?';
-	const ANCHOR_REGEX = '\#[a-z0-9-_/]*';
+	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[a-z0-9-+=,_~:;/\.\?\'\%!]+(?:=[a-z0-9-+=_~:;/\.\?\'\%]+)?)*)?';
+	const ANCHOR_REGEX = '\#[a-z0-9-_/+!]*';
 
 	private static $root = TPL_PATH_TO_ROOT;
 	private static $server = SERVER_URL;
