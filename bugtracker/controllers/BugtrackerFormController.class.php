49a1353f06f91b4aff4fb914afaa518f9a353711
julienseth78 <julienseth78@localhost>
2014-05-09 09:34:19 +0200

    [FIX] Bugtracker

--- bugtracker/controllers/BugtrackerFormController.class.php
+++ bugtracker/controllers/BugtrackerFormController.class.php
@@ -626,7 +626,7 @@ class BugtrackerFormController extends ModuleController
 			$response = new SiteDisplayResponse($body_view);
 			$graphical_environment = $response->get_graphical_environment();
 			$graphical_environment->set_page_title($this->lang['titles.add']);
-			$graphical_environment->get_seo_meta_data()->set_canonical_url(BugtrackerUrlBuilder::add();
+			$graphical_environment->get_seo_meta_data()->set_canonical_url(BugtrackerUrlBuilder::add());
 			
 			$breadcrumb = $graphical_environment->get_breadcrumb();
 			$breadcrumb->add($this->lang['module_title'], BugtrackerUrlBuilder::home());
