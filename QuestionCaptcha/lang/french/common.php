66f4659160fcf643e4a9639a807c4d9f19b6ead5
julienseth78 <julienseth78@localhost>
2014-05-20 09:21:06 +0200

    [FEATURE] Nouveau captcha : QuestionCaptcha

---
+++ QuestionCaptcha/lang/french/common.php
@@ -0,0 +1,38 @@
+<?php
+/*##################################################
+ *		                         common.php
+ *                            -------------------
+ *   begin                : May 9, 2014
+ *   copyright            : (C) 2014 Julien BRISWALTER
+ *   email                : julienseth78@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+#####################################################
+#						French						#
+#####################################################
+
+$lang['form.questions'] = 'Questions';
+$lang['form.questions.explain'] = 'Séparez les réponses par le symbole ; quand plusieurs réponses sont possibles';
+$lang['form.question'] = 'Question';
+$lang['form.answer'] = 'Réponse';
+$lang['form.change_question'] = 'Changer la question';
+$lang['config.title'] = 'Configuration des questions';
+?>
---
+++ QuestionCaptcha/lang/french/install.php
@@ -0,0 +1,37 @@
+<?php
+/*##################################################
+ *                            install.php
+ *                            -------------------
+ *   begin                : May 9, 2014
+ *   copyright            : (C) 2014 Julien BRISWALTER
+ *   email                : julienseth78@phpboost.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+ #						French						#
+ ####################################################
+
+$lang['question1_label'] = 'Combien font 4 + cinq ?';
+$lang['question1_answers'] = '9;neuf';
+$lang['question2_label'] = 'Combien y a-t-il de voyelles dans le mot PHPBoost ?';
+$lang['question2_answers'] = '2;deux';
+?>
