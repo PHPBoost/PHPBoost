040497f0ebc12146971aabd2f6d96c00b10e2d56
julienseth78 <julienseth78@localhost>
2014-05-23 17:32:22 +0200

    [MAJ] script d'update du module newsletter

--- update/services/modules/NewsletterModuleUpdateVersion.class.php
+++ update/services/modules/NewsletterModuleUpdateVersion.class.php
@@ -34,16 +34,63 @@ class NewsletterModuleUpdateVersion extends ModuleUpdateVersion
 	
 	public function execute()
 	{
+		$tables = $this->db_utils->list_tables(true);
+		
+		if (in_array(PREFIX . 'newsletter_streams', $tables))
+			$this->update_newsletter_streams_table();
+		
 		$this->delete_old_files();
+	}
+	
+	private function update_newsletter_streams_table()
+	{
+		$columns = $this->db_utils->desc_table(PREFIX . 'newsletter_streams');
+		
+		if (isset($columns['picture']))
+			$this->querier->inject('ALTER TABLE '. PREFIX .'newsletter_streams CHANGE picture image VARCHAR(255)');
+		
+		if (isset($columns['visible']))
+			$this->db_utils->drop_column(PREFIX .'newsletter_streams', 'visible');
+		
+		if (!isset($columns['rewrited_name']))
+			$this->db_utils->add_column(PREFIX .'newsletter_streams', 'rewrited_name', array('type' => 'string', 'length' => 250, 'default' => "''"));
+		if (!isset($columns['c_order']))
+			$this->db_utils->add_column(PREFIX .'newsletter_streams', 'c_order', array('type' => 'integer', 'length' => 11, 'unsigned' => 1, 'notnull' => 1, 'default' => 0));
+		if (!isset($columns['id_parent']))
+			$this->db_utils->add_column(PREFIX .'newsletter_streams', 'id_parent', array('type' => 'integer', 'length' => 11, 'notnull' => 1, 'default' => 0));
+		
+		$i = 1;
+		$result = $this->querier->select_rows(PREFIX .'newsletter_streams', array('id', 'name'));
+		while ($row = $result->fetch())
+		{
+			$this->querier->update(PREFIX . 'newsletter_streams', array(
+				'rewrited_name' => Url::encode_rewrite($row['name']),
+				'c_order' => $i,
+				'id_parent' => 0
+			), 'WHERE id=:id', array('id' => $row['id']));
+			$i++;
+		}
 	}
 	
 	private function delete_old_files()
 	{
+		$file = new File(Url::to_rel('/' . $this->module_id . '/controllers/AdminNewsletterAddStreamController.class.php'));
+		$file->delete();
+		$file = new File(Url::to_rel('/' . $this->module_id . '/controllers/AdminNewsletterDeleteStreamController.class.php'));
+		$file->delete();
+		$file = new File(Url::to_rel('/' . $this->module_id . '/controllers/AdminNewsletterEditStreamController.class.php'));
+		$file->delete();
+		$file = new File(Url::to_rel('/' . $this->module_id . '/controllers/AdminNewsletterStreamsListController.class.php'));
+		$file->delete();
 		$file = new File(Url::to_rel('/' . $this->module_id . '/lang/english/' . $this->module_id . '_common.php'));
 		$file->delete();
 		$file = new File(Url::to_rel('/' . $this->module_id . '/lang/french/' . $this->module_id . '_common.php'));
 		$file->delete();
 		$file = new File(Url::to_rel('/' . $this->module_id . '/phpboost/NewsletterModuleHomePage.class.php'));
+		$file->delete();
+		$file = new File(Url::to_rel('/' . $this->module_id . '/phpboost/NewsletterStreamsCache.class.php'));
+		$file->delete();
+		$file = new File(Url::to_rel('/' . $this->module_id . '/templates/AdminNewsletterStreamsListController.tpl'));
 		$file->delete();
 		$file = new File(Url::to_rel('/' . $this->module_id . '/templates/index.php'));
 		$file->delete();
