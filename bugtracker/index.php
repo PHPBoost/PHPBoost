864c26f4d2642740a22bdbbeeaf3920227288513
julienseth78 <julienseth78@localhost>
2014-05-27 21:00:37 +0200

    [FIX] #1066 Message d'erreur bugtracker

--- bugtracker/index.php
+++ bugtracker/index.php
@@ -32,35 +32,35 @@ require_once PATH_TO_ROOT . '/kernel/init.php';
 $url_controller_mappers = array(
 	//Admin
 	new UrlControllerMapper('AdminBugtrackerConfigController', '`^/admin(?:/config)?/?$`'),
-	new UrlControllerMapper('AdminBugtrackerAuthorizationsController', '`^/admin/authorizations?/?$`'),
-	new UrlControllerMapper('AdminBugtrackerDeleteParameterController', '`^/admin/delete/([a-z]+)/([0-9]+)?/?$`', array('parameter', 'id')),
-	new UrlControllerMapper('AdminBugtrackerDeleteDefaultParameterController', '`^/admin/delete/default/([a-z]+)?/?$`', array('parameter')),
+	new UrlControllerMapper('AdminBugtrackerAuthorizationsController', '`^/admin/authorizations/?$`'),
+	new UrlControllerMapper('AdminBugtrackerDeleteParameterController', '`^/admin/delete/([a-z]+)/([0-9]+)/?$`', array('parameter', 'id')),
+	new UrlControllerMapper('AdminBugtrackerDeleteDefaultParameterController', '`^/admin/delete/default/([a-z]+)/?$`', array('parameter')),
 	
 	//Lists
-	new UrlControllerMapper('BugtrackerUnsolvedListController', '`^/unsolved/success/([a-z_-]+)?/?([0-9]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('success', 'id', 'page', 'filter', 'filter_id')),
-	new UrlControllerMapper('BugtrackerUnsolvedListController', '`^/unsolved?/?([a-z]+)?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('field', 'sort', 'page', 'filter', 'filter_id')),
-	new UrlControllerMapper('BugtrackerSolvedListController', '`^/solved/success/([a-z_-]+)?/?([0-9]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('success', 'id', 'page', 'filter', 'filter_id')),
-	new UrlControllerMapper('BugtrackerSolvedListController', '`^/solved(?:/([a-z]+))?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('field', 'sort', 'page', 'filter', 'filter_id')),
-	new UrlControllerMapper('BugtrackerRoadmapListController', '`^/roadmap(?:/([0-9]+))?-?([a-z0-9_-]+)?/?([a-z_-]+)?/?([a-z]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('id_version', 'version', 'status', 'field', 'sort', 'page')),
+	new UrlControllerMapper('BugtrackerUnsolvedListController', '`^/unsolved/success/([a-z_-]+)/?([0-9]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('success', 'id', 'page', 'filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerUnsolvedListController', '`^/unsolved/?([a-z]+)?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('field', 'sort', 'page', 'filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerSolvedListController', '`^/solved/success/([a-z_-]+)/?([0-9]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('success', 'id', 'page', 'filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerSolvedListController', '`^/solved/?([a-z]+)?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9_-]+)?/?$`', array('field', 'sort', 'page', 'filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerRoadmapListController', '`^/roadmap/?([0-9]+)?-?([a-z0-9_-]+)?/?([a-z_-]+)?/?([a-z]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('id_version', 'version', 'status', 'field', 'sort', 'page')),
 	
 	//Stats
-	new UrlControllerMapper('BugtrackerStatsListController', '`^/stats?/?([0-9]+)?/?$`', array('id')),
+	new UrlControllerMapper('BugtrackerStatsListController', '`^/stats/?([0-9]+)?/?$`', array('id')),
 	
 	//Bug history
-	new UrlControllerMapper('BugtrackerHistoryListController', '`^/history/([0-9]+)?/?([0-9]+)?/?$`', array('id', 'page')),
+	new UrlControllerMapper('BugtrackerHistoryListController', '`^/history/([0-9]+)/?([0-9]+)?/?$`', array('id', 'page')),
 	
 	//Bug detail
-	new UrlControllerMapper('BugtrackerDetailController', '`^/detail/success/([a-z_-]+)?/?([0-9]+)?/?$`', array('success', 'id')),
-	new UrlControllerMapper('BugtrackerDetailController', '`^/detail/([0-9]+)?-?([0-9A-Za-z_-]+)?/?$`', array('id', 'rewrited_title')),
+	new UrlControllerMapper('BugtrackerDetailController', '`^/detail/success/([a-z_-]+)/?([0-9]+)?/?$`', array('success', 'id')),
+	new UrlControllerMapper('BugtrackerDetailController', '`^/detail/([0-9]+)-?([0-9A-Za-z_-]+)?/?$`', array('id', 'rewrited_title')),
 	
 	//New bug
 	new UrlControllerMapper('BugtrackerFormController', '`^/add/?$`'),
 	
 	//Bug edition
-	new UrlControllerMapper('BugtrackerFormController', '`^/edit/([0-9]+)?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9-]+)?/?$`', array('id', 'back_page', 'page', 'back_filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerFormController', '`^/edit/([0-9]+)/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9-]+)?/?$`', array('id', 'back_page', 'page', 'back_filter', 'filter_id')),
 	
 	//Actions
-	new UrlControllerMapper('BugtrackerDeleteBugController', '`^/delete/([0-9]+)?/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9-]+)?/?$`', array('id', 'back_page', 'page', 'back_filter', 'filter_id')),
+	new UrlControllerMapper('BugtrackerDeleteBugController', '`^/delete/([0-9]+)/?([a-z]+)?/?([0-9]+)?/?([a-z_-]+)?/?([a-z0-9-]+)?/?$`', array('id', 'back_page', 'page', 'back_filter', 'filter_id')),
 	new UrlControllerMapper('BugtrackerChangeBugStatusController', '`^/change_status/([0-9]+)?/?$`', array('id')),
 	new UrlControllerMapper('BugtrackerAjaxCheckStatusChangedController', '`^/check_status_changed/?$`'),
 	
