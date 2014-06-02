f7e5bdceace1f6c3efd86a05634e157e658372ef
soldier.weasel <soldier.weasel@localhost>
2014-06-02 11:20:54 +0200

    [MAJ] UrlControllerMapper

--- admin/member/index.php
+++ admin/member/index.php
@@ -41,7 +41,7 @@ $url_controller_mappers = array(
 	new UrlControllerMapper('AdminMemberEditController', '`^/([0-9]+)/edit/?$`', array('id')),
 	new UrlControllerMapper('AdminMemberDeleteController', '`^/([0-9]+)/delete/?$`', array('id')),
 	new UrlControllerMapper('AdminViewAllMembersController', '`^/management(?:/([a-z]+))?/?([a-z]+)?/?([0-9]+)?/?$`', array('field', 'sort', 'page')),
-	new UrlControllerMapper('AdminViewAllMembersController', '`^$`'),
+	new UrlControllerMapper('AdminViewAllMembersController'),
 );
 DispatchManager::dispatch($url_controller_mappers);
 ?>
