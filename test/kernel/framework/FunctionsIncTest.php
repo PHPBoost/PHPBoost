8032a92a02437a8ab9175f0649723f6a167f6478
soldier.weasel <soldier.weasel@localhost>
2014-08-04 17:44:15 +0200

    [MAJ] Suppression de la fonction strtotimestamp()

--- test/kernel/framework/FunctionsIncTest.php
+++ test/kernel/framework/FunctionsIncTest.php
@@ -102,11 +102,6 @@ class FunctionsIncTest extends PHPBoostUnitTestCase {
 		// TODO(__METHOD__);
 	}
 	
-	function test_strtotimestamp()
-	{
-		// TODO(__METHOD__);
-	}
-	
 	function test_strtodate()
 	{
 		// TODO(__METHOD__);
