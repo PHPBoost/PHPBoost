e8874904ca8e675cab66dadc0904aac24056e729
soldier.weasel <soldier.weasel@localhost>
2014-08-04 19:46:04 +0200

    [MAJ] Nettoyage ancienne classe SQL

--- kernel/framework/db/Sql.class.php
+++ kernel/framework/db/Sql.class.php
@@ -321,5 +321,62 @@ class Sql
 		return PersistenceContext::get_querier()->inject(
 		$decoded_query->get_query(), $decoded_query->get_parameters());
 	}
+	
+	/**
+	 * @desc Indents a MySQL query.
+	 * @param string $query Query to indent.
+	 * @return string The indented SQL query.
+	 */
+	public static function indent_query($query)
+	{
+		$query = ' ' . strtolower($query) . ' ';
+
+		//Suppression des espaces en trop.
+		$query = preg_replace('`(\s){2,}(\s){2,}`', '$1', $query);
+
+		//Ajout d'un retour à la ligne devant les mots clés principaux.
+		$query = preg_replace('`\b(' . implode('|', array('select', 'update', 'insert into', 'from', 'left join', 'right join', 'cross join', 'natural join', 'inner join', 'left outer join', 'right outer join', 'full outer join', 'full join', 'drop', 'truncate', 'where', 'order by', 'group by', 'limit', 'having', 'union')) . ')+`', "\r\n" . '$1', $query);
+
+		//Case des mots clés.
+		$key_words = array('select', 'update', 'delete', 'insert into', 'truncate', 'alter', 'table', 'status', 'set', 'drop', 'from', 'values', 'count', 'distinct', 'having', 'left', 'right', 'join', 'natural', 'outer', 'inner', 'between', 'where', 'group by', 'order by', 'limit', 'union', 'or', 'and', 'not', 'in', 'as', 'on', 'all', 'any', 'like', 'concat', 'substring', 'collate', 'collation', 'primary', 'key', 'default', 'null', 'exists', 'status', 'show');
+		$query = preg_replace_callback('`\b(' . implode('|', $key_words) . ')+\b`', create_function('$matches','return strtoupper($matches[1]);'), $query);
+
+		//Suppression des espaces en trop.
+		$query = preg_replace('`(\s){2,}(\s){2,}`', '$1', $query);
+
+		return trim($query);
+	}
+	
+	/**
+	 * @desc Highlights a SQL query to be more readable by a human.
+	 * @param string $query Query to highlight
+	 * @return string HTML code corresponding to the highlighted query.
+	 */
+	public static function highlight_query($query)
+	{
+		$query = ' ' . strtolower($query) . ' ';
+
+		//Suppression des espaces en trop.
+		$query = preg_replace('`(\s){2,}(\s){2,}`', '$1', $query);
+
+		//Ajout d'un retour à la ligne devant les mots clés principaux.
+		$query = preg_replace('`\b(' . implode('|', array('select', 'update', 'insert into', 'from', 'left join', 'right join', 'cross join', 'natural join', 'inner join', 'left outer join', 'right outer join', 'full outer join', 'full join', 'drop', 'truncate', 'where', 'order by', 'group by', 'limit', 'having', 'union')) . ')+`', "\r\n" . '$1', $query);
+
+		//Coloration des opérateurs.
+		$query = preg_replace('`(' . implode('|', array_map('preg_quote', array('*', '=', ',', '!=', '<>', '>', '<', '.', '(', ')'))) . ')+`U', '<span style="color:#FF00FF;">$1</span>', $query);
+
+		//Coloration des mots clés.
+		$key_words = array('select', 'update', 'delete', 'insert into', 'truncate', 'alter', 'table', 'status', 'set', 'drop', 'from', 'values', 'count', 'distinct', 'having', 'left', 'right', 'join', 'natural', 'outer', 'inner', 'between', 'where', 'group by', 'order by', 'limit', 'union', 'or', 'and', 'not', 'in', 'as', 'on', 'all', 'any', 'like', 'concat', 'substring', 'collate', 'collation', 'primary', 'key', 'default', 'null', 'exists', 'status', 'show');
+		$query = preg_replace_callback('`\b(' . implode('|', $key_words) . ')+\b`', create_function('$matches','return \'<span style="color:#990099;">\' . strtoupper($matches[1]) . \'</span>\';'), $query);
+
+		//Coloration finale.
+		$query = preg_replace('`\'(.+)\'`U', '<span style="color:#008000;">\'$1\'</span>', $query); //Coloration du texte échappé.
+		$query = preg_replace('`(?<![\'#])\b([0-9]+)\b(?!\')`', '<span style="color:#008080;">$1</span>', $query); //Coloration des chiffres.
+
+		//Suppression des espaces en trop.
+		$query = preg_replace('`(\s){2,}(\s){2,}`', '$1', $query);
+
+		return nl2br(trim($query));
+	}
 }
 ?>
