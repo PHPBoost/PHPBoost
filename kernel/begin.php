1866a96c5d482c7cca6dc1d674bc61f5cebcf8f1
soldier.weasel <soldier.weasel@localhost>
2014-08-04 18:59:17 +0200

    [MAJ] Suppression de la classe DeprecatedEnvironment

--- kernel/begin.php
+++ kernel/begin.php
@@ -27,5 +27,20 @@
 
 require_once 'init.php';
 
-DeprecatedEnvironment::check_page_auth();
+$running_module_name = Environment::get_running_module_name();
+if (!in_array($running_module_name, array('user', 'admin', 'kernel', '')))
+{
+	if (ModulesManager::is_module_installed($running_module_name))
+	{
+		$module = ModulesManager::get_module($running_module_name);
+		if (!$module->is_activated())
+		{
+			DispatchManager::redirect(PHPBoostErrors::module_not_activated());
+		}
+	}
+	else
+	{
+		DispatchManager::redirect(PHPBoostErrors::module_not_installed());
+	}
+}
 ?>
