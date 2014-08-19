3f021465f39bd51a04e6e9445ac4a173f82cd639
julienseth78 <julienseth78@localhost>
2014-08-19 07:00:50 +0200

    [FIX] #1146 [calendar] Menu de flux fatal error

--- calendar/phpboost/CalendarFeedProvider.class.php
+++ calendar/phpboost/CalendarFeedProvider.class.php
@@ -83,7 +83,6 @@ class CalendarFeedProvider implements FeedProvider
 				$item->set_guid($link);
 				$item->set_desc(FormatingHelper::second_parse($event->get_content()->get_contents()) . ($event->get_content()->get_location() ? '<br />' . $lang['calendar.labels.location'] . ' : ' . $event->get_content()->get_location() . '<br />' : '') . '<br />' . $lang['calendar.labels.start_date'] . ' : ' . $event->get_start_date()->format(Date::FORMAT_DAY_MONTH_YEAR_HOUR_MINUTE) . '<br />' . $lang['calendar.labels.end_date'] . ' : ' . $event->get_end_date()->format(Date::FORMAT_DAY_MONTH_YEAR_HOUR_MINUTE));
 				$item->set_date($event->get_content()->get_creation_date());
-				$item->set_image_url($category->get_image());
 				$item->set_auth(CalendarService::get_categories_manager()->get_heritated_authorizations($category->get_id(), Category::READ_AUTHORIZATIONS, Authorizations::AUTH_PARENT_PRIORITY));
 				$data->add_item($item);
 			}
