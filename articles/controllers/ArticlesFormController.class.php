0eef1d8e20fd7cd6c11cbdc8847dd6a3c39dd631
julienseth78 <julienseth78@localhost>
2014-08-24 09:32:12 +0200

    [FIX] Contribution calendar et articles

--- articles/controllers/ArticlesFormController.class.php
+++ articles/controllers/ArticlesFormController.class.php
@@ -367,7 +367,7 @@ class ArticlesFormController extends ModuleController
 			{
 				$contribution = new Contribution();
 				$contribution->set_id_in_module($id_article);
-				$contribution->set_description(stripslashes($article->get_description()));
+				$contribution->set_description(stripslashes($this->form->get_value('contribution_description')));
 				$contribution->set_entitled(StringVars::replace_vars($this->lang['articles.form.contribution_entitled'], array('module_name' => $this->lang['articles'], 'title' => $article->get_title())));
 				$contribution->set_fixing_url(ArticlesUrlBuilder::edit_article($id_article)->relative());
 				$contribution->set_poster_id(AppContext::get_current_user()->get_id());
