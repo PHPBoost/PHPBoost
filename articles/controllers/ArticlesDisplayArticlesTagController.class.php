2ad7a0fec5c6e29be260a5e295f46c5038f20f59
julienseth78 <julienseth78@localhost>
2014-08-27 18:33:02 +0200

    [FIX] #1162 [Articles] Le tri par auteur ne fonctionne pas dans les cat?gories

--- articles/controllers/ArticlesDisplayArticlesTagController.class.php
+++ articles/controllers/ArticlesDisplayArticlesTagController.class.php
@@ -95,7 +95,7 @@ class ArticlesDisplayArticlesTagController extends ModuleController
 				$sort_field = 'average_notes';
 				break;
 			case 'author':
-				$sort_field = 'author_user_id';
+				$sort_field = 'display_name';
 				break;
 			default:
 				$sort_field = 'date_created';
