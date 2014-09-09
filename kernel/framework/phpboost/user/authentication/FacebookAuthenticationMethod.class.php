cf578a33952b91421dc19542d7f08f3dd6a2c505
soldier.weasel <soldier.weasel@localhost>
2014-09-09 09:12:23 +0200

    [MAJ] FBAuth

---
+++ kernel/framework/phpboost/user/authentication/FacebookAuthenticationMethod.class.php
@@ -0,0 +1,129 @@
+<?php
+/*##################################################
+ *                            FacebookAuthenticationMethod.class.php
+ *                            -------------------
+ *   begin                : November 28, 2010
+ *   copyright            : (C) 2010 loic rouchon
+ *   email                : horn@phpboost.com
+ *
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+/**
+ * @author Loic Rouchon <horn@phpboost.com>
+ * @desc The AuthenticationMethod interface could be implemented in different ways to enable specifics
+ * authentication mecanisms.
+ * PHPBoost comes with a PHPBoostAuthenticationMethod which will be performed on the internal member
+ * list. But it is possible to implement external authentication mecanism by providing others
+ * implementations of this class to support LDAP authentication, OpenID, Facebook connect and more...
+ *
+ * @package {@package}
+ */
+
+require_once PATH_TO_ROOT . '/kernel/lib/php/facebook/facebook.php';
+
+class FacebookAuthenticationMethod extends AuthenticationMethod
+{
+	const AUTHENTICATION_METHOD = 'fb';
+	
+	/**
+	 * @var DBQuerier
+	 */
+	private $querier;
+	
+	protected $fb_user_data = array();
+	
+	public function __construct()
+	{
+		$this->querier = PersistenceContext::get_querier();
+		
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function associate($user_id)
+	{
+		$authentication_method_columns = array(
+			'user_id' => $user_id,
+            'method' => self::AUTHENTICATION_METHOD,
+			'identifier' => $this->fb_user_data['id'],
+			'data' => serialize($this->fb_user_data)
+		);
+		try {
+            $this->querier->insert(DB_TABLE_AUTHENTICATION_METHOD, $authentication_method_columns);
+		} catch (SQLQuerierException $ex) {
+			throw new IllegalArgumentException('User Id ' . $user_id .
+				' is already associated with an authentication method [' . $ex->getMessage() . ']');
+		}
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function authenticate()
+	{
+		$config = AuthenticationConfig::load();
+		
+		$facebook = new Facebook(array(
+			'appId'  => $config->get_fb_app_id(),
+			'secret' => $config->get_fb_app_key(),
+			'cookie' => true
+		));
+		
+		$fb_id = $facebook->getUser();
+		if (!$fb_id)
+		{
+			AppContext::get_response()->redirect($facebook->getLoginUrl(array('scope' => 'email')));
+		}
+		else
+		{
+			$data = $facebook->api('/me');
+
+			try {
+				
+				$condition = 'WHERE method=:method AND identifier=:identifier';
+				$parameters = array('method' => self::AUTHENTICATION_METHOD, 'identifier' => $fb_id);
+				return $this->querier->get_column_value(DB_TABLE_AUTHENTICATION_METHOD, 'user_id', $condition, $parameters);
+				
+			} catch (RowNotFoundException $e) {
+				
+				$email_exists = $this->querier->row_exists(DB_TABLE_MEMBER, 'WHERE email=:email', array('email' => $data['email']));
+				if (!$email_exists)
+				{
+					$user = new User();
+					$user->set_display_name(utf8_decode($data['name']));
+					$user->set_level(User::MEMBER_LEVEL);
+					$user->set_email($data['email']);
+					
+					$auth_method = new FacebookAuthenticationMethod();
+					$auth_method->fb_user_data = $data;
+					$fields_data = array('user_avatar' => 'https://graph.facebook.com/'. $fb_id .'/picture');
+					return UserService::create($user, $auth_method, $fields_data);
+				}
+				else
+				{
+					$this->error_msg = 'Account to this email already exists';
+				}
+				
+			}
+		}
+	}
+}
+?>
