bcdb48a0f6a45242405c13ff26ac2d562111cc64
julienseth78 <julienseth78@localhost>
2014-12-01 12:59:46 +0100

    [FIX] #1204 Erreur PHP lors de la suppression d'un dossier dans la gestion des fichiers

--- kernel/framework/phpboost/member/Uploads.class.php
+++ kernel/framework/phpboost/member/Uploads.class.php
@@ -1,6 +1,6 @@
 <?php
 /*##################################################
- *                             Iploads.class.php
+ *                             Uploads.class.php
  *                            -------------------
  *   begin                : April 18, 2007
  *   copyright            : (C) 2007 Viarre Régis
@@ -95,7 +95,7 @@ class Uploads
 		while ($row = self::$sql_querier->fetch_assoc($result))
 		{
 			if (!empty($row['id']))
-				$this->del_folder($row['id'], false);
+				self::Del_folder($row['id']);
 		}
 	}
 	
