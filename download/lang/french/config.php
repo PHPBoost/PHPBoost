d58a39666c79e1acfff9600ff7db43f0d84ee522
julienseth78 <julienseth78@localhost>
2015-02-08 08:02:56 +0100

    [FIX]

--- download/lang/french/config.php
+++ download/lang/french/config.php
@@ -35,9 +35,9 @@ $lang['root_category_description'] = 'Bienvenue dans l\'espace de téléchargement
 Une catégorie et un fichier ont été créés pour vous montrer comment fonctionne ce module. Voici quelques conseils pour bien débuter sur ce module.
 <br /><br /> 
 <ul class="formatter-ul">
-	<li class="formatter-li"> Pour configurer ou personnaliser l\'accueil de votre module, rendez vous dans l\'<a href="' . DownloadUrlBuilder::configuration()->rel() . '">administration du module</a></li>
-	<li class="formatter-li"> Pour créer des catégories, <a href="' . DownloadUrlBuilder::add_category()->rel() . '">cliquez ici</a> </li>
-	<li class="formatter-li"> Pour ajouter des fichiers en téléchargement, <a href="' . DownloadUrlBuilder::add()->rel() . '">cliquez ici</a></li>
+	<li class="formatter-li"> Pour configurer ou personnaliser l\'accueil de votre module, rendez vous dans l\'<a href="' . DownloadUrlBuilder::configuration()->relative() . '">administration du module</a></li>
+	<li class="formatter-li"> Pour créer des catégories, <a href="' . DownloadUrlBuilder::add_category()->relative() . '">cliquez ici</a> </li>
+	<li class="formatter-li"> Pour ajouter des fichiers en téléchargement, <a href="' . DownloadUrlBuilder::add()->relative() . '">cliquez ici</a></li>
 </ul>
 <br />Pour en savoir plus, n\'hésitez pas à consulter la documentation du module sur le site de <a href="www.phpboost.com">PHPBoost</a>.';
 ?>
--- web/lang/french/config.php
+++ web/lang/french/config.php
@@ -30,5 +30,14 @@
  #						French						#
  ####################################################
 
-$lang['root_category_description'] = 'Bienvenue dans l\'espace consacré aux liens web du site !';
+$lang['root_category_description'] = 'Bienvenue dans l\'espace consacré aux liens web du site !
+<br /><br />
+Une catégorie et un lien ont été créés pour vous montrer comment fonctionne ce module. Voici quelques conseils pour bien débuter sur ce module.
+<br /><br /> 
+<ul class="formatter-ul">
+	<li class="formatter-li"> Pour configurer ou personnaliser l\'accueil de votre module, rendez vous dans l\'<a href="' . WebUrlBuilder::configuration()->relative() . '">administration du module</a></li>
+	<li class="formatter-li"> Pour créer des catégories, <a href="' . WebUrlBuilder::add_category()->relative() . '">cliquez ici</a> </li>
+	<li class="formatter-li"> Pour ajouter des liens, <a href="' . WebUrlBuilder::add()->relative() . '">cliquez ici</a></li>
+</ul>
+<br />Pour en savoir plus, n\'hésitez pas à consulter la documentation du module sur le site de <a href="www.phpboost.com">PHPBoost</a>.';
 ?>
