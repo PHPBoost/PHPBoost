eb49ba4c758dffddab7e55d72f8bacdebb80d04b
julienseth78 <julienseth78@localhost>
2015-02-08 08:02:56 +0100

    [FIX]

--- download/lang/english/config.php
+++ download/lang/english/config.php
@@ -35,9 +35,9 @@ $lang['root_category_description'] = 'Welcome to the downloads section of the we
 One category and one file were created to show you how this module works. Here are some tips to get started on this module.
 <br /><br /> 
 <ul class="formatter-ul">
-	<li class="formatter-li"> To configure or customize the module homepage your module, go into the <a href="' . DownloadUrlBuilder::configuration()->rel() . '">module administration</a></li>
-	<li class="formatter-li"> To create categories, <a href="' . DownloadUrlBuilder::add_category()->rel() . '">clic here</a></li>
-	<li class="formatter-li"> To create download files, <a href="' . DownloadUrlBuilder::add()->rel() . '">clic here</a></li>
+	<li class="formatter-li"> To configure or customize the module homepage your module, go into the <a href="' . DownloadUrlBuilder::configuration()->relative() . '">module administration</a></li>
+	<li class="formatter-li"> To create categories, <a href="' . DownloadUrlBuilder::add_category()->relative() . '">clic here</a></li>
+	<li class="formatter-li"> To create download files, <a href="' . DownloadUrlBuilder::add()->relative() . '">clic here</a></li>
 </ul>
 <br />To learn more, don \'t hesitate to consult the documentation for the module on <a href="www.phpboost.com">PHPBoost</a> website.';
 ?>
