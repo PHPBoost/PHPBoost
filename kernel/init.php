d3381947919fa6ad9c0d2103faaee37273b84c4a
julienseth78 <julien.briswalter@gmail.com>
2015-06-30 13:34:29 +0200

    [MAJ] Suppression de l'ancien système de cache

--- kernel/init.php
+++ kernel/init.php
@@ -29,10 +29,6 @@ defined('PATH_TO_ROOT') or define('PATH_TO_ROOT', '..');
 require_once PATH_TO_ROOT . '/kernel/framework/core/environment/Environment.class.php';
 Environment::load_imports();
 
-/* DEPRECATED VARS */
-$Cache = new Cache();
-/* END DEPRECATED */
-
 Environment::init();
 
 /* DEPRECATED VARS */
