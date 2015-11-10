904526c68930f0954a97a6e96d96d0a94f4d10dc
Julien Briswalter <jbriswalter@users.noreply.github.com>
2015-11-10 17:18:23 +0100

    [FIX] FormFieldMultipleCheckbox.class.php

--- kernel/framework/builder/form/field/FormFieldMultipleCheckbox.class.php
+++ kernel/framework/builder/form/field/FormFieldMultipleCheckbox.class.php
@@ -95,6 +95,8 @@ class FormFieldMultipleCheckbox extends AbstractFormField
 		$template->assign_block_vars('fieldelements', array(
 			'ELEMENT' => $this->generate_html_code()->render()
 		));
+		
+		$template->put('C_HIDE_FOR_ATTRIBUTE', true);
 
 		return $template;
 	}
@@ -127,7 +129,7 @@ class FormFieldMultipleCheckbox extends AbstractFormField
 	{
 		$tpl_src = '# START choice #
 		<div class="form-field-checkbox">
-			<input type="checkbox" name="${escape(choice.HTML_ID)}" id="${escape(choice.HTML_ID)}" # IF choice.C_CHECKED # checked="checked" # ENDIF # />
+			<input type="checkbox" name="${escape(choice.HTML_ID)}" id="${escape(choice.HTML_ID)}" # IF choice.C_CHECKED # checked="checked"# ENDIF # />
 			<label for="${escape(choice.HTML_ID)}"></label>
 		</div>
 		${escape(choice.NAME)}
