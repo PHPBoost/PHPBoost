1c1ada4c582cfd33233bdab572e5560f7db08237
Julien Briswalter <jbriswalter@users.noreply.github.com>
2015-11-13 09:10:39 +0100

    [MAJ] Web

--- web/services/WebLink.class.php
+++ web/services/WebLink.class.php
@@ -455,10 +455,12 @@ class WebLink
 			'NUMBER_COMMENTS' => CommentsService::get_number_comments('web', $this->id),
 			
 			//Category
+			'C_ROOT_CATEGORY' => $category->get_id() == Category::ROOT_CATEGORY,
 			'CATEGORY_ID' => $category->get_id(),
 			'CATEGORY_NAME' => $category->get_name(),
 			'CATEGORY_DESCRIPTION' => $category->get_description(),
 			'CATEGORY_IMAGE' => $category->get_image()->rel(),
+			'U_EDIT_CATEGORY' => $category->get_id() == Category::ROOT_CATEGORY ? WebUrlBuilder::configuration()->rel() : WebUrlBuilder::edit_category($category->get_id())->rel(),
 			
 			'U_SYNDICATION' => SyndicationUrlBuilder::rss('web', $this->id_category)->rel(),
 			'U_AUTHOR_PROFILE' => UserUrlBuilder::profile($this->get_author_user()->get_id())->rel(),
