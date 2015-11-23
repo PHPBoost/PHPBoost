a403edcdb21e1e06085e1d00b07671f9123aa318
Julien Briswalter <jbriswalter@users.noreply.github.com>
2015-11-23 09:17:24 +0100

    [FIX] Forum

--- forum/topic.php
+++ forum/topic.php
@@ -548,7 +548,7 @@ if (!empty($quote_get))
 	}
 	
 	$pseudo = PersistenceContext::get_querier()->get_column_value(DB_TABLE_MEMBER, 'display_name', 'WHERE user_id=:id', array('id' => $quote_msg['user_id']));
-	$contents = '[quote=' . $pseudo . ']' . FormatingHelper::unparse($quote_msg['contents']) . '[/quote]';
+	$contents = '[quote=' . $pseudo . ']' . stripslashes(FormatingHelper::unparse($quote_msg['contents'])) . '[/quote]';
 }
 
 //Formulaire de réponse, non présent si verrouillé.
