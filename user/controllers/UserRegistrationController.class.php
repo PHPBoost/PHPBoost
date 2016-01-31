02c2594ab140b623d579f1bc81966337d2b9abb4
elenwii <genet.arnaud@gmail.com>
2016-01-31 17:45:10 +0100

    [MAJ] Message registration

--- user/controllers/UserRegistrationController.class.php
+++ user/controllers/UserRegistrationController.class.php
@@ -173,11 +173,11 @@ class UserRegistrationController extends AbstractController
 	{
 		if ($this->user_accounts_config->get_member_accounts_validation_method() == UserAccountsConfig::MAIL_USER_ACCOUNTS_VALIDATION)
 		{
-			return '<strong>'. $this->lang['registration.validation.mail.explain'] . '</strong>';
+			return '<div id="registration-validation-mail" class="notice">'. $this->lang['registration.validation.mail.explain'] . '</div>';
 		}
 		elseif ($this->user_accounts_config->get_member_accounts_validation_method() == UserAccountsConfig::ADMINISTRATOR_USER_ACCOUNTS_VALIDATION)
 		{
-			return '<strong>'. $this->lang['registration.validation.administrator.explain'] . '</strong>';
+			return '<div id="registration-validation-admin" class="notice">'. $this->lang['registration.validation.administrator.explain'] . '</div>';
 		}
 		else
 		{
