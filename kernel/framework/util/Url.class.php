aa18d67e1cdf383e0b0ca71627bdd82b09260130
julienseth78 <julien.briswalter@gmail.com>
2016-02-01 06:09:51 +0100

    [FIX] #1471 Bug de lien

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -47,7 +47,7 @@ class Url
 	const USER_REGEX = '[a-z0-9-_]+(?::[a-z0-9-_]+)?@';
 	const DOMAIN_REGEX = '(?:[a-z0-9-_~]+\.)*[a-z0-9-_~]+(?::[0-9]{1,5})?/';
 	const FOLDERS_REGEX = '/*(?:[A-Za-z0-9~_\.+@,-]+/+)*';
-	const FILE_REGEX = '[A-Za-z0-9-+_,~:\.\%!=]+';
+	const FILE_REGEX = '[A-Za-z0-9-+_,~:/\.\%!=]+';
 	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!]+(?:=[A-Za-z0-9-+=_~:;/\.\?\'\%\*!]+)?)*)?';
 	const ANCHOR_REGEX = '\#[a-z0-9-_/+\.!=]*';
 	
