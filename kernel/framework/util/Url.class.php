fbaef3d345ebfae8de5d02e902cf10969055210a
julienseth78 <julien.briswalter@gmail.com>
2016-02-14 08:08:24 +0100

    [FIX] #1486 Ancre dans URL : Les liens ne se génèrent pas

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -49,7 +49,7 @@ class Url
 	const FOLDERS_REGEX = '/*(?:[a-z0-9~_\.+@,-]+/+)*';
 	const FILE_REGEX = '[A-Za-z0-9-+_,~:/\.\%!=]+';
 	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!]+(?:=[A-Za-z0-9-+=_~:;/\.\?\'\%\*!]+)?)*)?';
-	const ANCHOR_REGEX = '\#[a-z0-9-_/+\.!]*';
+	const ANCHOR_REGEX = '\#[A-Za-z0-9-_/+\.!]*';
 
 	private static $root = TPL_PATH_TO_ROOT;
 	private static $server = SERVER_URL;
