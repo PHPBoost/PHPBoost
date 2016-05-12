6750fc2bc96f1d4069c06df5615a95296465b20b
j1.seth <julien.briswalter@gmail.com>
2016-05-12 13:10:07 +0200

    [FIX] #1571 Modif du nom du cookie

--- kernel/framework/phpboost/cache/HtaccessFileCache.class.php
+++ kernel/framework/phpboost/cache/HtaccessFileCache.class.php
@@ -104,7 +104,7 @@ class HtaccessFileCache implements CacheData
 	private function get_domain($hostname)
 	{
 		preg_match("/[a-z0-9\-]{1,63}\.[a-z\.]{2,6}$/", parse_url($hostname, PHP_URL_HOST), $_domain_tld);
-		return $_domain_tld[0];
+		return isset($_domain_tld[0]) ? $_domain_tld[0] : '';
 	}
 	
 	private function add_free_php56()
