0d96bb25e56781a9ece0f8fd0f0cfc611a0e0866
j1.seth <julien.briswalter@gmail.com>
2016-06-19 08:52:50 +0200

    [FIX] #1606 Liens HS dans les pages d'accueil par défaut de certains modules

--- wiki/lang/english/config.php
+++ wiki/lang/english/config.php
@@ -34,10 +34,10 @@ $lang['wiki_name'] = 'Wiki ' . GeneralConfig::load()->get_site_name();
 $lang['index_text'] = 'Welcome on the wiki module!<br /><br />
 Here are some tips for a good start with this module :<br /><br />
 <ul class="formatter-ul">
-<li class="formatter-li">To configure your module, go to the <a href="' . WikiUrlBuilder::configuration()->rel() . '">administration module</a></li>
-<li class="formatter-li">To create categories, <a href="' . WikiUrlBuilder::add_category()->rel() . '">click here</a></li>
-<li class="formatter-li">To create articles, <a href="' . WikiUrlBuilder::add()->rel() . '">click here</a></li>
+<li class="formatter-li">To configure your module, go to the <a href="' . WikiUrlBuilder::configuration()->relative() . '">administration module</a></li>
+<li class="formatter-li">To create categories, <a href="' . WikiUrlBuilder::add_category()->relative() . '">click here</a></li>
+<li class="formatter-li">To create articles, <a href="' . WikiUrlBuilder::add()->relative() . '">click here</a></li>
 </ul><br /><br />
-To customize the home page of this module, <a href="' . WikiUrlBuilder::configuration()->rel() . '">click here</a>.<br /><br />
+To customize the home page of this module, <a href="' . WikiUrlBuilder::configuration()->relative() . '">click here</a>.<br /><br />
 For more information about the features of this module, feel free to ask questions on the <a href="http://www.phpboost.com/forum/">support forum</a>.';
 ?>
