fff26f141bded6d078dd7fb9108bb11cac237b28
j1.seth <julien.briswalter@gmail.com>
2016-07-27 07:03:32 +0200

    [MAJ] Bugtracker : Possibilité de rechercher un bug en entrant son id dans la recherche du site

--- bugtracker/phpboost/BugtrackerSearchable.class.php
+++ bugtracker/phpboost/BugtrackerSearchable.class.php
@@ -35,13 +35,22 @@ class BugtrackerSearchable extends AbstractSearchableExtensionPoint
 	{
 		$weight = isset($args['weight']) && is_numeric($args['weight']) ? $args['weight'] : 1;
 		
+		$search_id = '';
+		$search_words = explode(' ', $args['search']);
+		foreach ($search_words as $word)
+		{
+			$word = str_replace('#', '', $word);
+			if (is_numeric($word))
+				$search_id = $word;
+		}
+		
 		return "SELECT " . $args['id_search'] . " AS id_search,
 			id AS id_content,
 			title,
 			( 2 * FT_SEARCH_RELEVANCE(title, '" . $args['search'] . "') + FT_SEARCH_RELEVANCE(contents, '" . $args['search'] . "') ) / 3 * " . $weight . " AS relevance,
 			CONCAT('" . PATH_TO_ROOT . "/bugtracker/" . (!ServerEnvironmentConfig::load()->is_url_rewriting_enabled() ? "index.php?url=/" : "") . "detail/',id) AS link
 			FROM " . BugtrackerSetup::$bugtracker_table . "
-			WHERE ( FT_SEARCH(title, '" . $args['search'] . "') OR FT_SEARCH(contents, '" . $args['search'] . "') )
+			WHERE ( FT_SEARCH(title, '" . $args['search'] . "') OR FT_SEARCH(contents, '" . $args['search'] . "') ) " . ($search_id ? "OR id = '" . $search_id . "'" : "") . "
 			ORDER BY relevance DESC
 			LIMIT 100 OFFSET 0";
 	}
