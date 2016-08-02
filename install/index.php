8daa8e233b88b21dae5e28d01ca1160491c6fbff
j1.seth <julien.briswalter@gmail.com>
2016-08-02 07:05:49 +0200

    [MAJ] #1632 Rendre l'installateur compatible pour pouvoir installer PHPBoost automatiquement sur installatron ou softaculous

--- install/index.php
+++ install/index.php
@@ -42,6 +42,67 @@ if (!$permissions['/cache']->is_writable() || !$permissions['/cache/tpl']->is_wr
 	die(LangLoader::get_message('chmod.cache.notWritable', 'install', 'install'));
 }
 
+if ($_GET || $_POST)
+{
+	$arguments_list = $_POST ? $_POST : $_GET;
+	
+	if (!empty($arguments_list))
+	{
+		$argv = array('phpboost', 'install');
+		$has_db_pwd = $has_ws_locale = false;
+		foreach ($arguments_list as $id => $arg)
+		{
+			if (!empty($arg) && in_array($id, array('--db-host', '--db-port', '--db-user', '--db-pwd', '--db-schema', '--db-table-prefix', '--ws-server', '--ws-path', '--ws-name', '--ws-slogan', '--ws-desc', '--ws-locale', '--ws-timezone', '--u-login', '--u-pwd', '--u-email')))
+			{
+				switch ($id)
+				{
+					case '--db-pwd' :
+							$has_db_pwd = true;
+							break;
+					
+					case '--ws-locale' :
+							$has_ws_locale = $arg ? true : false;
+							$arg = $has_ws_locale ? ($arg == 'french' ? $arg : 'english') : '';
+							break;
+				}
+				
+				$argv[] = $id;
+				$argv[] = $arg;
+			}
+		}
+		
+		if (!$has_ws_locale)
+		{
+			$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+			switch ($lang)
+			{
+				case 'fr':
+					$locale = 'french';
+					break;
+				default:
+					$locale = 'english';
+					break;
+			}
+			$argv[] = '--ws-locale';
+			$argv[] = $locale;
+		}
+		
+		if ($has_db_pwd)
+		{
+			$launcher = new CLILauncher($argv);
+			$result = $launcher->launch();
+			if ($result)
+			{
+				exit(0);
+			}
+			else
+			{
+				exit(1);
+			}
+		}
+	}
+}
+
 $url_controller_mappers = array(
 	new UrlControllerMapper('InstallWelcomeController', '`^(?:/welcome)?/?$`'),
 	new UrlControllerMapper('InstallLicenseController', '`^/license/?$`'),
