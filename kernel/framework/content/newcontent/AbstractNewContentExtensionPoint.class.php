0fe1b1ad8bbf183675e9100a036059f245e93eab
Arnaud <ElenWii@users.noreply.github.com>
2016-09-30 09:25:00 +0200

    [FIX] check_date in days

--- kernel/framework/content/newcontent/AbstractNewContentExtensionPoint.class.php
+++ kernel/framework/content/newcontent/AbstractNewContentExtensionPoint.class.php
@@ -48,7 +48,7 @@ abstract class AbstractNewContentExtensionPoint implements NewContentExtensionPo
 
 	public function check_date($date)
 	{
-		return date('m', time() - $date) < $this->content_management_config->get_new_content_duration();
+		return date('j', time() - $date) <= $this->content_management_config->get_new_content_duration();
 	}
 }
 ?>
