3904998f410f0880c5c639bab870945fcb3eddb9
elenwii <genet.arnaud@gmail.com>
2016-10-20 23:35:23 +0200

    [FIX] utf8 lang

--- lang/french/date-common.php
+++ lang/french/date-common.php
@@ -29,75 +29,75 @@
  #                     French                       #
  ####################################################
 
-$lang['date_format'] = 'AAAA-MM-JJ';
-$lang['date_format_day_month'] = 'd/m';
-$lang['date_format_day_month_year'] = 'd/m/Y';
-$lang['date_format_day_month_year_hour_minute'] = 'd/m/Y \à H\hi';
-$lang['date_format_day_month_year_long'] = 'j F Y';
-$lang['date_format_day_month_year_text'] = 'l j F Y';
-$lang['date_format_day_month_year_hour_minute_text'] = 'l j F Y \à H\hi';
+$lang['date_format']                                 = 'AAAA-MM-JJ';
+$lang['date_format_day_month']                       = 'd/m';
+$lang['date_format_day_month_year']                  = 'd/m/Y';
+$lang['date_format_day_month_year_hour_minute']      = 'd/m/Y à H\hi';
+$lang['date_format_day_month_year_long']             = 'j F Y';
+$lang['date_format_day_month_year_text']             = 'l j F Y';
+$lang['date_format_day_month_year_hour_minute_text'] = 'l j F Y à H\hi';
 
-$lang['unit.hour'] = 'h';
+$lang['unit.hour']    = 'h';
 $lang['unit.seconds'] = 's';
 
-$lang['date'] = 'Date';
-$lang['today'] = 'Aujourd\'hui';
-$lang['every_day'] = 'Tous les jours';
-$lang['every_week'] = 'Toutes les semaines';
+$lang['date']        = 'Date';
+$lang['today']       = 'Aujourd\'hui';
+$lang['every_day']   = 'Tous les jours';
+$lang['every_week']  = 'Toutes les semaines';
 $lang['every_month'] = 'Tous les mois';
-$lang['every_year'] = 'Tous les ans';
+$lang['every_year']  = 'Tous les ans';
 
-$lang['january'] = 'Janvier';
-$lang['february'] = 'Février';
-$lang['march'] = 'Mars';
-$lang['april'] = 'Avril';
-$lang['may'] = 'Mai';
-$lang['june'] = 'Juin';
-$lang['july'] = 'Juillet';
-$lang['august'] = 'Août';
+$lang['january']   = 'Janvier';
+$lang['february']  = 'Février';
+$lang['march']     = 'Mars';
+$lang['april']     = 'Avril';
+$lang['may']       = 'Mai';
+$lang['june']      = 'Juin';
+$lang['july']      = 'Juillet';
+$lang['august']    = 'Août';
 $lang['september'] = 'Septembre';
-$lang['october'] = 'Octobre';
-$lang['november'] = 'Novembre';
-$lang['december'] = 'Décembre';
+$lang['october']   = 'Octobre';
+$lang['november']  = 'Novembre';
+$lang['december']  = 'Décembre';
 
-$lang['monday_mini'] = 'Lu';
-$lang['tuesday_mini'] = 'Ma';
+$lang['monday_mini']    = 'Lu';
+$lang['tuesday_mini']   = 'Ma';
 $lang['wednesday_mini'] = 'Me';
-$lang['thursday_mini'] = 'Je';
-$lang['friday_mini'] = 'Ve';
-$lang['saturday_mini'] = 'Sa';
-$lang['sunday_mini'] = 'Di';
+$lang['thursday_mini']  = 'Je';
+$lang['friday_mini']    = 'Ve';
+$lang['saturday_mini']  = 'Sa';
+$lang['sunday_mini']    = 'Di';
 
-$lang['monday_short'] = 'Lun';
-$lang['tuesday_short'] = 'Mar';
+$lang['monday_short']    = 'Lun';
+$lang['tuesday_short']   = 'Mar';
 $lang['wednesday_short'] = 'Mer';
-$lang['thursday_short'] = 'Jeu';
-$lang['friday_short'] = 'Ven';
-$lang['saturday_short'] = 'Sam';
-$lang['sunday_short'] = 'Dim';
+$lang['thursday_short']  = 'Jeu';
+$lang['friday_short']    = 'Ven';
+$lang['saturday_short']  = 'Sam';
+$lang['sunday_short']    = 'Dim';
 
-$lang['monday'] = 'Lundi';
-$lang['tuesday'] = 'Mardi';
+$lang['monday']    = 'Lundi';
+$lang['tuesday']   = 'Mardi';
 $lang['wednesday'] = 'Mercredi';
-$lang['thursday'] = 'Jeudi';
-$lang['friday'] = 'Vendredi';
-$lang['saturday'] = 'Samedi';
-$lang['sunday']	= 'Dimanche';
+$lang['thursday']  = 'Jeudi';
+$lang['friday']    = 'Vendredi';
+$lang['saturday']  = 'Samedi';
+$lang['sunday']    = 'Dimanche';
 
-$lang['instantly'] = 'a l\'instant';
-$lang['second'] = 'seconde';
-$lang['seconds'] = 'secondes';
-$lang['minute'] = 'minute';
-$lang['minutes'] = 'minutes';
-$lang['hour'] = 'heure';
-$lang['hours'] = 'heures';
-$lang['day'] = 'jour';
-$lang['days'] = 'jours';
-$lang['week'] = 'semaine';
-$lang['weeks'] = 'semaines';
-$lang['month'] = 'mois';
-$lang['months'] = 'mois';
-$lang['year'] = 'année';
-$lang['years'] = 'années';
+$lang['instantly'] = 'à l\'instant';
+$lang['second']    = 'seconde';
+$lang['seconds']   = 'secondes';
+$lang['minute']    = 'minute';
+$lang['minutes']   = 'minutes';
+$lang['hour']      = 'heure';
+$lang['hours']     = 'heures';
+$lang['day']       = 'jour';
+$lang['days']      = 'jours';
+$lang['week']      = 'semaine';
+$lang['weeks']     = 'semaines';
+$lang['month']     = 'mois';
+$lang['months']    = 'mois';
+$lang['year']      = 'année';
+$lang['years']     = 'années';
 
 ?>
