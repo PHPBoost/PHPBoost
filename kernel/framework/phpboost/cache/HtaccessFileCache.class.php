56b91656f894addec77f564f1cf2c9dc50af691a
j1.seth <julien.briswalter@gmail.com>
2016-12-04 08:41:23 +0100

    [FIX] .htaccess

--- kernel/framework/phpboost/cache/HtaccessFileCache.class.php
+++ kernel/framework/phpboost/cache/HtaccessFileCache.class.php
@@ -44,8 +44,6 @@ class HtaccessFileCache implements CacheData
 		$this->htaccess_file_content = '';
 		$this->general_config = GeneralConfig::load();
 		
-		$this->disallow_scripts_execution();
-		
 		$this->set_default_charset();
 		
 		$this->add_free_php56();
@@ -103,13 +101,6 @@ class HtaccessFileCache implements CacheData
 	{
 		$this->add_empty_line();
 		$this->add_line('# ' . $name . ' #');
-	}
-	
-	private function disallow_scripts_execution()
-	{
-		$this->add_section('Disallow scripts execution');
-		$this->add_line('Options -ExecCGI');
-		$this->add_line('AddHandler cgi-script .pl .py .jsp .asp .htm .shtml .sh .cgi');
 	}
 	
 	private function set_default_charset()
