f4d9f13c69a114ab35338665e483ccacc2bc4563
Julien Briswalter <jbriswalter@users.noreply.github.com>
2017-02-02 14:12:10 +0100

    [FIX] #1726 Certaines dates anniversaires des membres ne s'affichent pas dans le calendrier

--- calendar/services/CalendarService.class.php
+++ calendar/services/CalendarService.class.php
@@ -248,7 +248,7 @@ class CalendarService
 		(SELECT member_extended_fields.user_born AS start_date, member_extended_fields.user_born AS end_date, login AS title, 'BIRTHDAY' AS type, 0 AS id_category, '" . CalendarEventContent::YEARLY . "' AS repeat_type, 100 AS repeat_number
 		FROM " . DB_TABLE_MEMBER . " member
 		LEFT JOIN " . DB_TABLE_MEMBER_EXTENDED_FIELDS . " member_extended_fields ON member_extended_fields.user_id = member.user_id
-		WHERE member_extended_fields.user_born <> '' AND MONTH(FROM_UNIXTIME(member_extended_fields.user_born)) = :month AND :year > YEAR(FROM_UNIXTIME(member_extended_fields.user_born)))
+		WHERE member_extended_fields.user_born <> '' AND IF(member_extended_fields.user_born < 0, MONTH(DATE_ADD(FROM_UNIXTIME(0), INTERVAL member_extended_fields.user_born second)), MONTH(FROM_UNIXTIME(member_extended_fields.user_born))) = :month AND :year > IF(member_extended_fields.user_born < 0, YEAR(DATE_ADD(FROM_UNIXTIME(0), INTERVAL member_extended_fields.user_born second)), YEAR(FROM_UNIXTIME(member_extended_fields.user_born))))
 		UNION
 		" : "") . "(SELECT start_date, end_date, title, 'EVENT' AS type, id_category, repeat_type, repeat_number
 		FROM " . CalendarSetup::$calendar_events_table . " event
