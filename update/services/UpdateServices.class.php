9d1a3dff01a3eb4a967c599e60ff639912a21eab
Julien Briswalter <jbriswalter@users.noreply.github.com>
2017-03-13 10:49:53 +0100

    [FIX] Script d'update

--- update/services/UpdateServices.class.php
+++ update/services/UpdateServices.class.php
@@ -253,7 +253,7 @@ class UpdateServices
 		// Mise à jour des tables du noyau - conversion en utf8
 		foreach (self::$db_utils->list_tables(true) as $table_name)
 		{
-			if (!in_array($table_name, array(PREFIX . 'errors_404', PREFIX . 'stats_referer')))
+			if (!in_array($table_name, array(PREFIX . 'configs', PREFIX . 'errors_404', PREFIX . 'stats_referer')))
 			{
 				self::$db_querier->inject('ALTER TABLE `' . $table_name . '` CONVERT TO CHARACTER SET utf8');
 				self::$db_querier->inject('ALTER TABLE `' . $table_name . '` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci');
