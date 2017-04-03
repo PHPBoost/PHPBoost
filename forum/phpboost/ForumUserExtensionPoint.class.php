54387b4b83110a5a07a1fcf7e84715e484206a82
Arnaud <ElenWii@users.noreply.github.com>
2017-04-03 10:33:22 +0200

    [MAJ] MAJ babso

--- forum/phpboost/ForumUserExtensionPoint.class.php
+++ forum/phpboost/ForumUserExtensionPoint.class.php
@@ -51,7 +51,7 @@ class ForumUserExtensionPoint implements UserExtensionPoint
 	 */
 	public function get_messages_list_link_img()
 	{
-		return '<img src="' . TPL_PATH_TO_ROOT . '/forum/forum_mini.png" alt="forum_mini" class="valign-middle" />';
+		return '<i class="fa fa-globe"></i>';
 	}
 	
 	public function get_number_messages($user_id)
