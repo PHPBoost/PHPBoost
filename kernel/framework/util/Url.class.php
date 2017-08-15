1ea2f5a46623b4e6374854c072f14dc76baaded2
j1.seth <julien.briswalter@gmail.com>
2017-08-15 10:49:38 +0200

    [FIX] #1806 Message vide selon conditions de liens

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -48,8 +48,8 @@ class Url
 	const DOMAIN_REGEX = '(?:[a-z0-9-_~]+\.)*[a-z0-9-_~]+(?::[0-9]{1,5})?/';
 	const FOLDERS_REGEX = '/*(?:[A-Za-z0-9~_\.+@,-]+/+)*';
 	const FILE_REGEX = '[A-Za-z0-9-+_,~:/\.\%!=]+';
-	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!]+(?:=[A-Za-z0-9-+=_~:;/\.\?\'\%\*!]+)?)*)?';
-	const ANCHOR_REGEX = '\#[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!&]*';
+	const ARGS_REGEX = '(?:\?(?!&)(?:(?:&amp;|&)?[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!|]+(?:=[A-Za-z0-9-+=_~:;/\.\?\'\%\*!|]+)?)*)?';
+	const ANCHOR_REGEX = '\#[A-Za-z0-9-+=,_~:;/\.\?\'\%\*!&|]*';
 	
 	const STATUS_OK = 200;
 	const STATUS_FOUND = 302;
