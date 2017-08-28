9f6ac1cdb66b42272803f71fa4eaa8b75a4d6999
j1.seth <julien.briswalter@gmail.com>
2017-08-28 06:44:25 +0200

    [FIX] #1815 Download : lien mort avec upload

--- kernel/framework/phpboost/cache/HtaccessFileCache.class.php
+++ kernel/framework/phpboost/cache/HtaccessFileCache.class.php
@@ -121,6 +121,7 @@ class HtaccessFileCache implements CacheData
 	{
 		$this->add_section('Hide directory listings');
 		$this->add_line('Options -Indexes');
+		$this->add_line('Options +FollowSymLinks');
 		$this->add_section('Prevent viewing of .htaccess file');
 		if (AppContext::get_request()->get_domain_name() == 'free.fr')
 		{
