3aa92c64071d9b1125473c65112c2d747c6def11
Janus57 <janus57@users.noreply.github.com>
2017-09-08 17:35:02 +0200

    [FIX]#1819 Génération de la news après install
    Génération de la news après install : pas les bons liens

--- news/lang/french/install.php
+++ news/lang/french/install.php
@@ -42,17 +42,17 @@ Supprimez le répertoire /install à la racine de votre site pour des raisons de
 <br />
 <h2 class="formatter-title">Administrez votre site</h2><br />
 <br />
-Accédez au <a href="' . UserUrlBuilder::administration()->rel() . '">panneau d\'administration de votre site</a> afin de le paramétrer comme vous le souhaitez !  Pour cela : <br />
+Accédez au <a href="' . UserUrlBuilder::administration()->relative() . '">panneau d\'administration de votre site</a> afin de le paramétrer comme vous le souhaitez !  Pour cela : <br />
 <br />
 <ul class="formatter-ul">
-<li class="formatter-li"><a href="' . AdminMaintainUrlBuilder::maintain()->rel() . '">Mettez votre site en maintenance</a> en attendant que vous le configuriez à votre guise.
-</li><li class="formatter-li">Rendez vous à la <a href="' . AdminConfigUrlBuilder::general_config()->rel() . '">Configuration générale du site</a>.
-</li><li class="formatter-li"><a href="' . AdminModulesUrlBuilder::list_installed_modules()->rel() . '">Configurez les modules</a> disponibles et donnez leur les droits d\'accès (si vous n\'avez pas installé le pack complet, tous les modules sont disponibles sur le site de <a href="http://www.phpboost.com/download/">phpboost.com</a> dans la section téléchargement).
-</li><li class="formatter-li"><a href="' . AdminContentUrlBuilder::content_configuration()->rel() . '">Choisissez le langage de formatage du contenu</a> par défaut du site.
-</li><li class="formatter-li"><a href="' . AdminMembersUrlBuilder::configuration()->rel() . '">Configurez l\'inscription des membres</a>.
-</li><li class="formatter-li"><a href="' . AdminThemeUrlBuilder::list_installed_theme()->rel() . '">Choisissez le thème par défaut de votre site</a> pour changer l\'apparence de votre site (vous pouvez en obtenir d\'autres sur le site de <a href="http://www.phpboost.com/download/">phpboost.com</a>).
+<li class="formatter-li"><a href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">Mettez votre site en maintenance</a> en attendant que vous le configuriez à votre guise.
+</li><li class="formatter-li">Rendez vous à la <a href="' . AdminConfigUrlBuilder::general_config()->relative() . '">Configuration générale du site</a>.
+</li><li class="formatter-li"><a href="' . AdminModulesUrlBuilder::list_installed_modules()->relative() . '">Configurez les modules</a> disponibles et donnez leur les droits d\'accès (si vous n\'avez pas installé le pack complet, tous les modules sont disponibles sur le site de <a href="http://www.phpboost.com/download/">phpboost.com</a> dans la section téléchargement).
+</li><li class="formatter-li"><a href="' . AdminContentUrlBuilder::content_configuration()->relative() . '">Choisissez le langage de formatage du contenu</a> par défaut du site.
+</li><li class="formatter-li"><a href="' . AdminMembersUrlBuilder::configuration()->relative() . '">Configurez l\'inscription des membres</a>.
+</li><li class="formatter-li"><a href="' . AdminThemeUrlBuilder::list_installed_theme()->relative() . '">Choisissez le thème par défaut de votre site</a> pour changer l\'apparence de votre site (vous pouvez en obtenir d\'autres sur le site de <a href="http://www.phpboost.com/download/">phpboost.com</a>).
 </li><li class="formatter-li">Avant de donner l\'accès de votre site à vos visiteurs, prenez un peu de temps pour y mettre du contenu.
-</li><li class="formatter-li">Enfin <a href="' . AdminMaintainUrlBuilder::maintain()->rel() . '">désactivez la maintenance</a> de votre site afin qu\'il soit visible par vos visiteurs.<br />
+</li><li class="formatter-li">Enfin <a href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">désactivez la maintenance</a> de votre site afin qu\'il soit visible par vos visiteurs.<br />
 </li></ul><br />
 <br />
 <h2 class="formatter-title">Que faire si vous rencontrez un problème ?</h2><br />
