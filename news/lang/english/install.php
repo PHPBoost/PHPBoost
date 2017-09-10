324dd3b54f063eb8a23e919617ffb5f7503e36a8
Janus57 <janus57@users.noreply.github.com>
2017-09-10 14:45:34 +0200

    [FIX]#1819 Génération de la news après install

--- news/lang/english/install.php
+++ news/lang/english/install.php
@@ -47,14 +47,14 @@ Access the <a href="' . UserUrlBuilder::administration()->rel() . '">Administrat
 To do so : <br />
 <br />
 <ul class="formatter-ul">
-<li class="formatter-li"><a href="' . AdminMaintainUrlBuilder::maintain()->rel() . '">Put your website under maintenance</a> and you won\'t be disturbed while you\'re working on it.
-</li><li class="formatter-li">Now\'s the time to setup the <a href="' . AdminConfigUrlBuilder::general_config()->rel() . '">main configurations</a> of the website.
-</li><li class="formatter-li"><a href="' . AdminModulesUrlBuilder::list_installed_modules()->rel() . '">Configure the installed modules</a> and give them access rights (If you have not installed the complete package, all modules are available on the <a href="http://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
-</li><li class="formatter-li"><a href="' . AdminContentUrlBuilder::content_configuration()->rel() . '">Choose the default content language formatting</a>.
-</li><li class="formatter-li"><a href="' . AdminMembersUrlBuilder::configuration()->rel() . '">Configure the members settings</a>.
-</li><li class="formatter-li"><a href="' . AdminThemeUrlBuilder::list_installed_theme()->rel() . '">Choose the website style</a> to change the look of your site (You can find more styles on the <a href="http://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
+<li class="formatter-li"><a href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">Put your website under maintenance</a> and you won\'t be disturbed while you\'re working on it.
+</li><li class="formatter-li">Now\'s the time to setup the <a href="' . AdminConfigUrlBuilder::general_config()->relative() . '">main configurations</a> of the website.
+</li><li class="formatter-li"><a href="' . AdminModulesUrlBuilder::list_installed_modules()->relative() . '">Configure the installed modules</a> and give them access rights (If you have not installed the complete package, all modules are available on the <a href="http://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
+</li><li class="formatter-li"><a href="' . AdminContentUrlBuilder::content_configuration()->relative() . '">Choose the default content language formatting</a>.
+</li><li class="formatter-li"><a href="' . AdminMembersUrlBuilder::configuration()->relative() . '">Configure the members settings</a>.
+</li><li class="formatter-li"><a href="' . AdminThemeUrlBuilder::list_installed_theme()->relative() . '">Choose the website style</a> to change the look of your site (You can find more styles on the <a href="http://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
 </li><li class="formatter-li">Before giving back access to your members, take time to add content to your website!
-</li><li class="formatter-li">Finally, Finally, <a href="' . AdminMaintainUrlBuilder::maintain()->rel() . '">put your site online</a> in order to restore access to your site to your visitors.<br />
+</li><li class="formatter-li">Finally, Finally, <a href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">put your site online</a> in order to restore access to your site to your visitors.<br />
 </li></ul><br />
 <br />
 <h2 class="formatter-title">What should I do if I have problems ?</h2><br />
