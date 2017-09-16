f9cf84a3cd927b6304dfcf75901f5f727db5d838
Janus57 <janus57@users.noreply.github.com>
2017-09-16 22:55:58 +0200

    [FIX] #1826
    Page 404 dans un sujet avec des guillemets français « et »

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -223,7 +223,7 @@ class Url
 	public static function encode_rewrite($url)
 	{
 		$url = utf8_decode(TextHelper::html_entity_decode($url));
-		$url = TextHelper::strtolower(strtr($url, utf8_decode('²ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöø°ÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ()[]\'"~$&%*@ç!?;,:/\^¨€{}<>|+.- #'),  '2aaaaaaaaaaaaoooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn    --      c  ---    e      --- '));
+		$url = TextHelper::strtolower(strtr($url, utf8_decode('²ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöø°ÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ()[]\'"~$&%*@ç!?;,:/\^¨€{}<>«»`|+.- #'),  '2aaaaaaaaaaaaoooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn    --      c  ---    e         --- '));
 		$url = str_replace(' ', '', $url);
 		$url = str_replace('---', '-', $url);
 		$url = str_replace('--', '-', $url);
