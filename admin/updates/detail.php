decec1baee8d3a7a3d78698c4c438f4703378c75
mipel <mipel@phpboost.com>
2018-03-25 22:08:54 +0200

    [FIX] alarmes-gestion correcte des accents issus du main.xml

--- admin/updates/detail.php
+++ admin/updates/detail.php
@@ -45,7 +45,7 @@ $app = null;
 
 if (($update = AdministratorAlertService::find_by_identifier($identifier, 'updates')) !== null)
 {
-    $app = TextHelper::unserialize($update->get_properties());
+    $app = TextHelper::mb_unserialize($update->get_properties());
 }
 
 if ($app !== null && $app->check_compatibility())
