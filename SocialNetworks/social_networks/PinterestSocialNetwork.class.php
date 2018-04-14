08e1176e8a05b74dd0ebe0d05467a08844a0d512
j1.seth <julien.briswalter@gmail.com>
2018-04-14 10:38:39 +0200

    [MAJ] SocialNetworks : ajout des RS Pinterest et Tumblr

---
+++ SocialNetworks/social_networks/PinterestSocialNetwork.class.php
@@ -0,0 +1,42 @@
+<?php
+/*##################################################
+ *                        PinterestSocialNetwork.class.php
+ *                            -------------------
+ *   begin                : April 10, 2018
+ *   copyright            : (C) 2018 Julien BRISWALTER
+ *   email                : j1.seth@phpboost.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class PinterestSocialNetwork extends AbstractSocialNetwork
+{
+	const SOCIAL_NETWORK_ID = 'pinterest';
+	
+	public function get_name()
+	{
+		return 'Pinterest';
+	}
+	
+	public function get_content_sharing_url()
+	{
+		return 'http://pinterest.com/pin/create/link/?url=' . HOST . REWRITED_SCRIPT;
+	}
+}
+?>
---
+++ SocialNetworks/social_networks/TumblrSocialNetwork.class.php
@@ -0,0 +1,42 @@
+<?php
+/*##################################################
+ *                        TumblrSocialNetwork.class.php
+ *                            -------------------
+ *   begin                : April 10, 2018
+ *   copyright            : (C) 2018 Julien BRISWALTER
+ *   email                : j1.seth@phpboost.com
+ *
+ *  
+ ###################################################
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+class TumblrSocialNetwork extends AbstractSocialNetwork
+{
+	const SOCIAL_NETWORK_ID = 'tumblr';
+	
+	public function get_name()
+	{
+		return 'Tumblr';
+	}
+	
+	public function get_content_sharing_url()
+	{
+		return 'https://www.tumblr.com/widgets/share/tool?canonicalUrl=' . HOST . REWRITED_SCRIPT;
+	}
+}
+?>
