21f43b55dccc1dbccf37f8b3e6f27a0312f065ea
j1.seth <julien.briswalter@gmail.com>
2018-10-26 15:58:12 +0200

    [MAJ] Boutons paypal install et update

--- install/lang/french/install.php
+++ install/lang/french/install.php
@@ -248,8 +248,7 @@ Cordialement l\'équipe PHPBoost.',
 									<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 										<input type="hidden" name="cmd" value="_s-xclick">
 										<input type="hidden" name="hosted_button_id" value="7EFHMABH75HPE">
-										<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
-										<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+										<input type="image" src="https://resources.phpboost.com/documentation/paypal/button_french.png" border="0" name="submit" alt="PHPBoost - PayPal">
 									</form>
 								</div>
 							</div>
