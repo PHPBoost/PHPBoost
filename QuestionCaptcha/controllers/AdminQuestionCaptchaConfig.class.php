34eac2f1c066e4663baac7dfc0a94d667b02767d
Labsoweb\babsolune <babsolune@phpboost.com>
2018-11-18 08:14:10 +0100

    [MAJ] [QuestionCaptcha] placement bouton de suppression dans la config

--- QuestionCaptcha/controllers/AdminQuestionCaptchaConfig.class.php
+++ QuestionCaptcha/controllers/AdminQuestionCaptchaConfig.class.php
@@ -35,75 +35,75 @@ class AdminQuestionCaptchaConfig extends AdminModuleController
 	 * @var FormButtonSubmit
 	 */
 	private $submit_button;
-	
+
 	private $lang;
-	
+
 	/**
 	 * @var QuestionCaptchaConfig
 	 */
 	private $config;
-	
+
 	public function execute(HTTPRequestCustom $request)
 	{
 		$this->init();
-		
+
 		$this->build_form();
-		
+
 		$tpl = new StringTemplate('# INCLUDE MSG # # INCLUDE FORM #');
 		$tpl->add_lang($this->lang);
-		
+
 		if ($this->submit_button->has_been_submited() && $this->form->validate())
 		{
 			$this->save();
 			$tpl->put('MSG', MessageHelper::display(LangLoader::get_message('message.success.config', 'status-messages-common'), MessageHelper::SUCCESS, 5));
 		}
-		
+
 		$tpl->put('FORM', $this->form->display());
-		
+
 		return $this->build_response($tpl);
 	}
-	
+
 	private function init()
 	{
 		$this->lang = LangLoader::get('common', 'QuestionCaptcha');
 		$this->config = QuestionCaptchaConfig::load();
 	}
-	
+
 	private function build_form()
 	{
 		$form = new HTMLForm(__CLASS__);
-		
+
 		$fieldset = new FormFieldsetHTMLHeading('config', $this->lang['config.title']);
 		$form->add_fieldset($fieldset);
-		
+
 		$fieldset->add_field(new QuestionCaptchaFormFieldQuestions('questions', $this->lang['form.questions'], $this->config->get_questions(), array(
-			'description' => $this->lang['form.questions.explain']
+			'description' => $this->lang['form.questions.explain'], 'class' => 'full-field'
 		)));
-		
+
 		$this->submit_button = new FormButtonDefaultSubmit();
 		$form->add_button($this->submit_button);
 		$form->add_button(new FormButtonReset());
-		
+
 		$this->form = $form;
 	}
-	
+
 	private function save()
 	{
 		$this->config->set_questions($this->form->get_value('questions'));
-		
+
 		QuestionCaptchaConfig::save();
 	}
-	
+
 	private function build_response(View $tpl)
 	{
 		$title = LangLoader::get_message('configuration', 'admin');
-		
+
 		$response = new AdminMenuDisplayResponse($tpl);
 		$response->set_title($title);
 		$response->add_link($this->lang['config.title'], QuestionCaptchaUrlBuilder::configuration());
 		$env = $response->get_graphical_environment();
 		$env->set_page_title($title);
-		
+
 		return $response;
 	}
 }
