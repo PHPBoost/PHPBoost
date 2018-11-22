e74aa11d8ac89a87fee26058c52eab501eee91e5
Labsoweb\babsolune <babsolune@phpboost.com>
2018-11-22 04:36:50 +0100

    [MAJ] upload move

--- kernel/lib/js/phpboost/upload.js
+++ kernel/lib/js/phpboost/upload.js
@@ -7,14 +7,14 @@ function show_cat_contents(id_cat, display_select_link)
 	var xhr_object = null;
 	var data = null;
 	var filename = PATH_TO_ROOT + "/user/xmlhttprequest.php?token=" + TOKEN + (display_select_link != 0 ? "&display_select_link=1" : "");
-	
+
 	if(window.XMLHttpRequest) // Firefox
 	   xhr_object = new XMLHttpRequest();
 	else if(window.ActiveXObject) // Internet Explorer
 	   xhr_object = new ActiveXObject("Microsoft.XMLHTTP");
 	else // XMLHttpRequest non supporté par le navigateur
 	    return;
-		
+
 	if( id_cat > 0 )
 	{
 		//Si le dossier est fermé on l'ouvre sinon on le ferme
@@ -23,22 +23,22 @@ function show_cat_contents(id_cat, display_select_link)
 			data = "id_cat=" + id_cat;
 			xhr_object.open("POST", filename, true);
 
-			xhr_object.onreadystatechange = function() 
+			xhr_object.onreadystatechange = function()
 			{
-				if( xhr_object.readyState == 4 ) 
+				if( xhr_object.readyState == 4 )
 				{
 					document.getElementById("cat_" + id_cat).innerHTML = xhr_object.responseText;
 					document.getElementById("img_" + id_cat).className = 'fa fa-folder-open';
 					if( document.getElementById("img2_" + id_cat) )
 						document.getElementById("img2_" + id_cat).className = 'far fa-minus-square';
-					
+
 					cat_status[id_cat] = 1;
 				}
 			}
 
 			xhr_object.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 			xhr_object.send(data);
-		}	
+		}
 		else if( cat_status[id_cat] == 0 )
 		{
 			document.getElementById("cat_" + id_cat).style.display = 'block';
@@ -50,7 +50,7 @@ function show_cat_contents(id_cat, display_select_link)
 		else
 		{
 			document.getElementById("cat_" + id_cat).style.display = 'none';
-			document.getElementById("img_" + id_cat).className = 'fa fa-folder fa-2x';
+			document.getElementById("img_" + id_cat).className = 'fa fa-folder';
 			if( document.getElementById("img2_" + id_cat) )
 				document.getElementById("img2_" + id_cat).className = 'far fa-plus-square';
 			cat_status[id_cat] = 0;
@@ -63,7 +63,7 @@ function select_cat(id_cat)
 	var xhr_object = null;
 	var data = null;
 	var filename = PATH_TO_ROOT + "/user/xmlhttprequest.php?select_cat=1&token=" + TOKEN;
-	
+
 	if(window.XMLHttpRequest) // Firefox
 	   xhr_object = new XMLHttpRequest();
 	else if(window.ActiveXObject) // Internet Explorer
@@ -77,9 +77,9 @@ function select_cat(id_cat)
 
 		xhr_object.open("POST", filename, true);
 
-		xhr_object.onreadystatechange = function() 
+		xhr_object.onreadystatechange = function()
 		{
-			if( xhr_object.readyState == 4 ) 
+			if( xhr_object.readyState == 4 )
 			{
 				document.getElementById("id_cat").value = id_cat;
 				document.getElementById("class-" + id_cat).className = "upload-selected-cat";
