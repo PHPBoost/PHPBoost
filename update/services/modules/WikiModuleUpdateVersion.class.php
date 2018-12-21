ed8f96ce0e4185b945ecbc8b44b81f1bbba88e35
j1.seth <julien.briswalter@gmail.com>
2018-12-21 17:38:03 +0100

    [FIX] Script d'update : titres du wiki

--- update/services/modules/WikiModuleUpdateVersion.class.php
+++ update/services/modules/WikiModuleUpdateVersion.class.php
@@ -47,6 +47,8 @@ class WikiModuleUpdateVersion extends ModuleUpdateVersion
 				$this->update_wiki_contents_table();
 			
 			$this->update_content();
+			
+			$this->update_content_titles();
 		}
 		
 		$this->delete_old_files();
@@ -62,9 +64,43 @@ class WikiModuleUpdateVersion extends ModuleUpdateVersion
 			$this->db_utils->add_column(PREFIX . 'wiki_contents', 'change_reason', array('type' => 'text', 'length' => 100, 'notnull' => 0));
 	}
 	
-	public function update_content()
+	private function update_content()
 	{
 		UpdateServices::update_table_content(PREFIX . 'wiki_contents', 'content', 'id_contents');
+	}
+	
+	private function update_content_titles()
+	{
+		$result = $this->querier->select('SELECT id_contents, content
+			FROM ' . PREFIX . 'wiki_contents'
+		);
+		
+		$selected_rows = $result->get_rows_count();
+		$updated_content = 0;
+		
+		while($row = $result->fetch())
+		{
+			$array_preg = array();
+			$array_preg_replace = array();
+			
+			for ($i = 2 ; $i <= 6 ; $i++)
+			{
+				array_push($array_preg, '`&lt;h' . $i . ' class="formatter-title wiki-paragraph-' . $i . '" id="([^"]+)"&gt;(.+)&lt;/h' . $i . '&gt;`isuU');
+				array_push($array_preg_replace, '<h' . $i . ' class="formatter-title wiki-paragraph-' . $i . '" id="$1">$2</h' . $i . '>');
+			}
+			
+			$new_content = preg_replace($array_preg, $array_preg_replace, $row['content']);
+			
+			if ($new_content != $row['content'])
+			{
+				$this->querier->update(PREFIX . 'wiki_contents', array('content' => $new_content), 'WHERE id_contents=:id', array('id' => $row['id_contents']));
+				$updated_content++;
+			}
+		}
+		$result->dispose();
+		
+		$object = new UpdateServices('', false);
+		$object->add_information_to_file('table ' . PREFIX . 'wiki_contents', ': ' . $updated_content . ' contents titles updated');
 	}
 	
 	private function delete_old_files()
