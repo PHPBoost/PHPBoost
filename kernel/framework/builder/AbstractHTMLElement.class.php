31f90326f9b51ec97aab641ec986b6886d5c0a35
Labsoweb\babsolune <babsolune@phpboost.com>
2019-01-02 14:53:45 +0100

    [MAJ] doc php

--- kernel/framework/builder/AbstractHTMLElement.class.php
+++ kernel/framework/builder/AbstractHTMLElement.class.php
@@ -1,50 +1,32 @@
 <?php
-/*##################################################
- *                             HTMLElement.class.php
- *                            -------------------
- *   begin                : December 21, 2009
- *   copyright            : (C) 2009 Loic Rouchon
- *   email                : loic.rouchon@phpboost.com
- *
- ###################################################
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- ###################################################*/
-
 /**
- * @author loic rouchon <loic.rouchon@phpboost.com>
- * @desc This class allows you to manage easily html elements.
- * @package {@package}
- */
+ * This class allows you to manage easily html elements.
+ * @package     Builder
+ * @category    Framework
+ * @copyright   &copy; 2005-2019 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Loic ROUCHON <horn@phpboost.com>
+ * @version     PHPBoost 5.2 - last update: 2015 05 27
+ * @since       PHPBoost 3.0 - 2009 12 21
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
+*/
+
 abstract class AbstractHTMLElement implements HTMLElement
 {
 	protected $css_class = '';
 	protected $css_style = '';
 	protected $id = '';
-	
+
 	public function has_css_style()
 	{
 		return !empty($this->css_style);
 	}
-	
+
 	public function get_css_style()
 	{
 		return $this->css_style;
 	}
-	
+
 	public function set_css_style($style)
 	{
 		$this->css_style = $style;
@@ -54,17 +36,17 @@ abstract class AbstractHTMLElement implements HTMLElement
 	{
 		$this->css_style .= ' ' . $style;
 	}
-	
+
 	public function has_css_class()
 	{
 		return !empty($this->css_class);
 	}
-	
+
 	public function get_css_class()
 	{
 		return $this->css_class;
 	}
-	
+
 	public function set_css_class($class)
 	{
 		$this->css_class = $class;
@@ -74,17 +56,17 @@ abstract class AbstractHTMLElement implements HTMLElement
 	{
 		$this->css_class .= ' ' . $class;
 	}
-	
+
 	public function has_id()
 	{
 		return !empty($this->id);
 	}
-	
+
 	public function get_id()
 	{
 		return $this->id;
 	}
-	
+
 	public function set_id($id)
 	{
 		$this->id = $id;
--- kernel/framework/builder/element/ImgHTMLElement.class.php
+++ kernel/framework/builder/element/ImgHTMLElement.class.php
@@ -1,34 +1,16 @@
 <?php
-/*##################################################
- *                           ImgHTMLElement.class.php
- *                            -------------------
- *   begin                : January 19, 2015
- *   copyright            : (C) 2015 Kevin MASSY
- *   email                : kevin.massy@phpboost.com
- *
- ###################################################
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- ###################################################*/
-
 /**
- * 
- * @author Kevin MASSY <kevin.massy@phpboost.com>
- * @package {@package}
- */
+ * @package     Builder
+ * @subpackage  Element
+ * @category    Framework
+ * @copyright   &copy; 2005-2019 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Kevin MASSY <reidlos@phpboost.com>
+ * @version     PHPBoost 5.2 - last update: 2017 03 19
+ * @since       PHPBoost 4.1 - 2015 01 19
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
+*/
+
 class ImgHTMLElement extends AbstractHTMLElement
 {
 	private $url;
@@ -59,7 +41,7 @@ class ImgHTMLElement extends AbstractHTMLElement
 		foreach ($this->attributs as $type => $value)
 		{
 			$tpl->assign_block_vars('attributs', array(
-				'TYPE' => $type, 
+				'TYPE' => $type,
 				'VALUE' => $value
 			));
 		}
