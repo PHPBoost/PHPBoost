74672619c73af666444086d428c4c425ea059ba6
Labsoweb\babsolune <babsolune@phpboost.com>
2019-01-02 11:28:41 +0100

    [MAJ] doc php indent

--- kernel/framework/ajax/admin_alerts.php
+++ kernel/framework/ajax/admin_alerts.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * @package 	Ajax
- * @category 	Framework
- * @copyright 	&copy; 2005-2019 PHPBoost
- * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @package     Ajax
+ * @subpackage  Controllers
+ * @category    Framework
+ * @copyright   &copy; 2005-2019 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2016 11 28
- * @since   	PHPBoost 2.0 - 2008 08 30
+ * @version     PHPBoost 5.2 - last update: 2016 11 28
+ * @since       PHPBoost 2.0 - 2008 08 30
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
 */
