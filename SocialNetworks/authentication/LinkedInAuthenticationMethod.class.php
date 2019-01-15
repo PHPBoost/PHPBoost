364ba7ffc9ea2905d063185a713af9a829815c04
Labsoweb\babsolune <babsolune@phpboost.com>
2019-01-15 11:00:35 +0100

    [MAJ] docblocks

--- SocialNetworks/authentication/LinkedInAuthenticationMethod.class.php
+++ SocialNetworks/authentication/LinkedInAuthenticationMethod.class.php
@@ -12,37 +12,6 @@
  * @since   	PHPBoost 5.1 - 2018 04 10
 */
 
-/*##################################################
- *                            LinkedInAuthenticationMethod.class.php
- *                            -------------------
- *   begin                : April 10, 2018
- *   copyright            : (C) 2018 Julien BRISWALTER
- *   email                : j1.seth@phpboost.com
- *
- ###################################################
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- ###################################################*/
-
-/**
- * @author Julien BRISWALTER <j1.seth@phpboost.com>
- *
- * @package {@package}
- */
-
 session_start();
 
 require_once PATH_TO_ROOT . '/SocialNetworks/lib/linkedin/LinkedIn.php';
