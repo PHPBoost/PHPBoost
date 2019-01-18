813c501ca09df6d54ac28e3575e082584575d53a
j1.seth <julien.briswalter@gmail.com>
2019-01-18 06:38:27 +0100

    [MAJ] Ajout du forum dans le Plan du site

--- forum/phpboost/ForumExtensionPointProvider.class.php
+++ forum/phpboost/ForumExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2016 09 20
+ * @version   	PHPBoost 5.2 - last update: 2019 01 18
  * @since   	PHPBoost 2.0 - 2008 02 24
  * @contributor Loic ROUCHON <horn@phpboost.com>
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
@@ -14,7 +14,7 @@ define('FORUM_MAX_SEARCH_RESULTS', 50);
 
 class ForumExtensionPointProvider extends ExtensionPointProvider
 {
-	public function __construct() //Constructeur de la classe ForumInterface
+	public function __construct()
 	{
 		parent::__construct('forum');
 	}
@@ -44,6 +44,11 @@ class ForumExtensionPointProvider extends ExtensionPointProvider
 	public function search()
 	{
 		return new ForumSearchable();
+	}
+
+	public function sitemap()
+	{
+		return new ForumSitemapExtensionPoint();
 	}
 
 	public function tree_links()
