779dca382eb4a4a3286f179f3583c6efc2a1faa9
Labsoweb\babsolune <babsolune@phpboost.com>
2019-02-02 07:26:10 +0100

    [FEATURE] templates lang overload

--- kernel/framework/phpboost/theme/ThemeConfigurationManager.class.php
+++ kernel/framework/phpboost/theme/ThemeConfigurationManager.class.php
@@ -5,9 +5,10 @@
  * @copyright   &copy; 2005-2019 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 5.2 - last update: 2015 06 30
+ * @version     PHPBoost 5.2 - last update: 2019 02 02
  * @since       PHPBoost 3.0 - 2011 04 10
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class ThemeConfigurationManager
@@ -43,12 +44,29 @@ class ThemeConfigurationManager
 
 	private static function find_desc_ini_file($theme_id)
 	{
+		$desc_ini_file = PATH_TO_ROOT . '/lang/' . AppContext::get_current_user()->get_locale() . '/templates/' . $theme_id . '/desc.ini';
+		if (file_exists($desc_ini_file))
+		{
+			return $desc_ini_file;
+		}
+
 		$desc_ini_folder = PATH_TO_ROOT . '/templates/' . $theme_id . '/lang/';
 
 		$desc_ini_file = $desc_ini_folder . AppContext::get_current_user()->get_locale() . '/desc.ini';
 		if (file_exists($desc_ini_file))
 		{
 			return $desc_ini_file;
+		}
+
+		$folder = new Folder($desc_ini_folder);
+		$folders = $folder->get_folders();
+		foreach ($folders as $lang_folder)
+		{
+			$desc_ini_file = $lang_folder->get_path() . '/desc.ini';
+			if (file_exists($desc_ini_file))
+			{
+				return $desc_ini_file;
+			}
 		}
 		throw new IOException('Theme "' . $theme_id . '" desc.ini not found in' .
 			    '/' . $theme_id . '/lang/');
