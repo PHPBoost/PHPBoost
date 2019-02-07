42ae40a2a94904cbd6e3138db02b8172dcb5c4c8
j1.seth <julien.briswalter@gmail.com>
2019-02-07 06:14:49 +0100

    [FIX] Smallads update

--- update/services/modules/SmalladsModuleUpdateVersion.class.php
+++ update/services/modules/SmalladsModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2019 01 09
+ * @version   	PHPBoost 5.2 - last update: 2019 02 07
  * @since   	PHPBoost 5.1 - 2018 09 20
 */
 
@@ -79,7 +79,7 @@ class SmalladsModuleUpdateVersion extends ModuleUpdateVersion
 		foreach ($rows_change as $old_name => $new_name)
 		{
 			if (isset($columns[$old_name]))
-				$this->querier->inject('ALTER TABLE ' . PREFIX . 'download CHANGE ' . $old_name . ' ' . $new_name);
+				$this->querier->inject('ALTER TABLE ' . PREFIX . 'smallads CHANGE ' . $old_name . ' ' . $new_name);
 		}
 
 		if (isset($columns['cat_id']))
