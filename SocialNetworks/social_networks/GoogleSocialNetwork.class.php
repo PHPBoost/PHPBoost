24cc9b3e18360220907e1d52e3b3dcab071734c4
j1.seth <julien.briswalter@gmail.com>
2019-02-28 07:03:25 +0100

    [MAJ] Suppression du lien de partage Google+ en vue de la fermeture du service le 02/04/19

--- SocialNetworks/social_networks/GoogleSocialNetwork.class.php
+++ SocialNetworks/social_networks/GoogleSocialNetwork.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2018 09 20
+ * @version   	PHPBoost 5.2 - last update: 2019 02 28
  * @since   	PHPBoost 5.1 - 2018 04 10
 */
 
@@ -13,22 +13,7 @@ class GoogleSocialNetwork extends AbstractSocialNetwork
 
 	public function get_name()
 	{
-		return 'Google+';
-	}
-
-	public function get_icon_name()
-	{
-		return self::SOCIAL_NETWORK_ID . '-plus-g';
-	}
-
-	public function get_css_class()
-	{
-		return self::SOCIAL_NETWORK_ID . 'plus';
-	}
-
-	public function get_content_sharing_url()
-	{
-		return 'https://plus.google.com/share?url=' . (rawurlencode(HOST . REWRITED_SCRIPT));
+		return 'Google';
 	}
 
 	public function get_external_authentication()
