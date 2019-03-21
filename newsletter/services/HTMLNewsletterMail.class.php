d078c469372c6b7fb1af99c8bbecb5e3168ee038
j1.seth <julien.briswalter@gmail.com>
2019-03-21 23:13:34 +0100

    [FIX] #1873 archive de newsletter

--- newsletter/services/HTMLNewsletterMail.class.php
+++ newsletter/services/HTMLNewsletterMail.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2016 10 25
+ * @version   	PHPBoost 5.2 - last update: 2019 03 21
  * @since   	PHPBoost 3.0 - 2011 02 01
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -19,7 +19,7 @@ class HTMLNewsletterMail extends AbstractNewsletterMail
 
 	public function display_mail($subject, $contents)
 	{
-		return stripslashes($contents);
+		return TextHelper::htmlspecialchars_decode(stripslashes($contents));
 	}
 
 	public function parse_contents($contents)
