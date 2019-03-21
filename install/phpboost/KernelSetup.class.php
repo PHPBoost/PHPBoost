fe72d0bba7ff68910790ea2801850a255227e55a
j1.seth <julien.briswalter@gmail.com>
2019-03-21 06:05:30 +0100

    [FIX] #1907 Invalid Query lors de l'installation 5.2

--- install/phpboost/KernelSetup.class.php
+++ install/phpboost/KernelSetup.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2018 02 09
+ * @version   	PHPBoost 5.2 - last update: 2019 03 21
  * @since   	PHPBoost 3.0 - 2010 05 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -593,7 +593,7 @@ class KernelSetup
 
 		self::$db_querier->insert(self::$visit_counter_table, array(
 			'id' => 1,
-			'ip' => '',
+			'ip' => 0,
 			'time' => $now->format(Date::FORMAT_ISO_DAY_MONTH_YEAR),
 			'total' => 0
 		));
