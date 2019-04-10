bc787a8cb3a108b3caa4bad620621b60610caa44
j1.seth <julien.briswalter@gmail.com>
2019-04-10 06:26:38 +0200

    [FIX] smallads update

--- update/services/modules/SmalladsModuleUpdateVersion.class.php
+++ update/services/modules/SmalladsModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2019 02 07
+ * @version   	PHPBoost 5.2 - last update: 2019 04 09
  * @since   	PHPBoost 5.1 - 2018 09 20
 */
 
@@ -228,7 +228,7 @@ class SmalladsModuleUpdateVersion extends ModuleUpdateVersion
 		$file = new File(PATH_TO_ROOT . '/smallads/fields/SmalladsFormFieldSelectSources.class.php');
 		$file->delete();
 
-		$file = new File(PATH_TO_ROOT . '/smallads/lang/english/smallads_french.php');
+		$file = new File(PATH_TO_ROOT . '/smallads/lang/english/smallads_english.php');
 		$file->delete();
 
 		$file = new File(PATH_TO_ROOT . '/smallads/lang/french/smallads_french.php');
