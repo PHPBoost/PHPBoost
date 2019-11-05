31fddb1ade7782c55c586e1a0e913cb3eac6b20f
j1.seth <julien.briswalter@gmail.com>
2019-11-05 06:59:33 +0100

    [UPD] DefaultSitemapCategoriesModule

--- kernel/framework/content/category/bridges/DefaultSitemapCategoriesModule.class.php
+++ kernel/framework/content/category/bridges/DefaultSitemapCategoriesModule.class.php
@@ -3,7 +3,7 @@
  * @copyright	&copy; 2005-2019 PHPBoost
  * @license		https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author		Julien BRISWALTER <j1.seth@phpboost.com>
- * @version		PHPBoost 5.3 - last update: 2019 11 03
+ * @version		PHPBoost 5.3 - last update: 2019 11 05
  * @since		PHPBoost 5.3 - 2019 11 02
 */
 
@@ -14,10 +14,10 @@ class DefaultSitemapCategoriesModule extends SitemapCategoriesModule
 	 */
 	private $module_id;
 
-	public function __construct($module_id)
+	public function __construct($module_id, $id_category = CategoriesItemsParameters::DEFAULT_FIELD_NAME)
 	{
 		$this->module_id = $module_id;
-		parent::__construct(CategoriesService::get_categories_manager($this->module_id));
+		parent::__construct(CategoriesService::get_categories_manager($this->module_id, $id_category));
 	}
 
 	protected function get_category_url(Category $category)
