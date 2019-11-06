c8d3ece351b97006a063759131f122c32b7e9051
j1.seth <julien.briswalter@gmail.com>
2019-11-06 06:24:34 +0100

    [FIX] Mail domains check constraint

--- kernel/framework/builder/form/field/constraint/FormFieldConstraintForbiddenMailDomains.class.php
+++ kernel/framework/builder/form/field/constraint/FormFieldConstraintForbiddenMailDomains.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2019 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.2 - last update: 2019 10 25
+ * @version     PHPBoost 5.2 - last update: 2019 11 06
  * @since       PHPBoost 5.2 - 2019 07 30
 */
 
@@ -19,7 +19,9 @@ class FormFieldConstraintForbiddenMailDomains extends FormFieldConstraintRegex
 		}
 		$this->set_validation_error_message($error_message);
 		
-		$regex = '/^((?!' . implode('|', SecurityConfig::load()->get_forbidden_mail_domains()) . ').)*$/iu';
+		$forbidden_mail_domains = SecurityConfig::load()->get_forbidden_mail_domains();
+		
+		$regex = !empty($forbidden_mail_domains) ? '/^((?!' . implode('|', $forbidden_mail_domains) . ').)*$/iu' : '';
 
 		parent::__construct(
 			$regex,
