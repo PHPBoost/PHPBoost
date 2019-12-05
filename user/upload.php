8200f5ca5bf333311efcfd8169f889e92898435b
mipel <mipel@phpboost.com>
2019-12-05 18:38:45 +0100

    [UPD] personal or public folder count size

--- user/upload.php
+++ user/upload.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version   	PHPBoost 5.3 - last update: 2019 11 27
+ * @version   	PHPBoost 5.3 - last update: 2019 12 05
  * @since   	PHPBoost 1.6 - 2007 07 07
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -31,7 +31,7 @@ if ($item_id)
     PersistenceContext::get_querier()->update(PREFIX . "upload", array('public' => $status), 'WHERE id = :id', array('id' => $item_id));
 }
 
-if ( !empty($popup))
+if (!empty($popup))
 { //Popup.
     $env = new SiteDisplayFrameGraphicalEnvironment();
     Environment::set_graphical_environment($env);
@@ -51,7 +51,7 @@ if ( !empty($popup))
     $popup_noamp = '';
 }
 
-if ( !AppContext::get_current_user()->check_level(User::MEMBER_LEVEL))
+if (!AppContext::get_current_user()->check_level(User::MEMBER_LEVEL))
 { // No visitors !
     $error_controller = PHPBoostErrors::unexisting_page();
     DispatchManager::redirect($error_controller);
@@ -60,7 +60,7 @@ if ( !AppContext::get_current_user()->check_level(User::MEMBER_LEVEL))
 $files_upload_config = FileUploadConfig::load();
 
 // access authorization ?
-if ( !AppContext::get_current_user()->check_auth($files_upload_config->get_authorization_enable_interface_files(), FileUploadConfig::AUTH_FILES_BIT))
+if (!AppContext::get_current_user()->check_auth($files_upload_config->get_authorization_enable_interface_files(), FileUploadConfig::AUTH_FILES_BIT))
 {
     $error_controller = PHPBoostErrors::unexisting_page();
     DispatchManager::redirect($error_controller);
@@ -77,9 +77,10 @@ $move_folder = (int)retrieve(GET, 'movefd', 0);
 $move_file = (int)retrieve(GET, 'movefi', 0);
 $to = retrieve(POST, 'new_cat', -1);
 
-if(!empty($parent_folder))
+if (!empty($parent_folder))
 { // folder change
-    if (empty($parent_folder)) AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=0&' . $popup_noamp, '', '&'));
+    if (empty($parent_folder))
+        AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=0&' . $popup_noamp, '', '&'));
 
     try {
         $info_folder = PersistenceContext::get_querier()->select_single_row(DB_TABLE_UPLOAD_CAT, array('id_parent', 'user_id'), 'WHERE id = :id', array('id' => $parent_folder));
@@ -90,13 +91,16 @@ if(!empty($parent_folder))
 
     if ($info_folder['id_parent'] != 0 || AppContext::get_current_user()->check_level(User::ADMIN_LEVEL))
     {
-        if ($parent_folder['user_id'] == -1) AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?showm=1', '', '&'));
-        else AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $info_folder['id_parent'] . '&' . $popup_noamp, '', '&'));
-    } else AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $parent_folder . '&' . $popup_noamp, '', '&'));
+        if ($parent_folder['user_id'] == -1)
+            AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?showm=1', '', '&'));
+        else
+            AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $info_folder['id_parent'] . '&' . $popup_noamp, '', '&'));
+    } else
+        AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $parent_folder . '&' . $popup_noamp, '', '&'));
 }
 elseif ($home_folder) // Root return
     AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?' . $popup_noamp, '', '&'));
-elseif ( !empty($_FILES['upload_file']['name']) && AppContext::get_request()->has_getparameter('f'))
+elseif (!empty($_FILES['upload_file']['name']) && AppContext::get_request()->has_getparameter('f'))
 { // Adding a file
     $error = '';
     // Groups upload authorizations
@@ -104,13 +108,15 @@ elseif ( !empty($_FILES['upload_file']['name']) && AppContext::get_request()->ha
     $unlimited_data = ($group_limit === -1) || AppContext::get_current_user()->check_level(User::ADMIN_LEVEL);
 
     $member_memory_used = Uploads::Member_memory_used(AppContext::get_current_user()->get_id());
-    if ($member_memory_used >= $group_limit && !$unlimited_data) $error = 'e_max_data_reach';
+    if ($member_memory_used >= $group_limit && !$unlimited_data)
+        $error = 'e_max_data_reach';
     else
     {
         // if folder is not writable, try CHMOD 777
         @clearstatcache();
         $dir = PATH_TO_ROOT . '/upload/';
-        if ( !is_writable($dir)) $is_writable = (@chmod($dir, 0777));
+        if (!is_writable($dir))
+            $is_writable = (@chmod($dir, 0777));
 
         @clearstatcache();
         if (is_writable($dir))
@@ -123,7 +129,8 @@ elseif ( !empty($_FILES['upload_file']['name']) && AppContext::get_request()->ha
             if ($Upload->get_error() != '')
             { // Error, stop here
                 $error = $Upload->get_error();
-                if ($Upload->get_error() == 'e_upload_max_weight') $error = 'e_max_data_reach';
+                if ($Upload->get_error() == 'e_upload_max_weight')
+                    $error = 'e_max_data_reach';
                 AppContext::get_response()->redirect('/user/upload.php?f=' . $folder . '&erroru=' . $error . '&' . $popup_noamp . '#message_helper');
             }
             else
@@ -134,24 +141,26 @@ elseif ( !empty($_FILES['upload_file']['name']) && AppContext::get_request()->ha
                     $id_file = $result->get_last_inserted_id();
                 }
             }
-        } else $error = 'e_upload_failed_unwritable';
+        } else
+            $error = 'e_upload_failed_unwritable';
     }
 
-    $anchor = !empty($error) ? '&error=' . $error . '&' . $popup_noamp . '#message_helper' : '&' . $popup_noamp . ( !empty($id_file) ? '#fifl' . $id_file : '');
+    $anchor = !empty($error) ? '&error=' . $error . '&' . $popup_noamp . '#message_helper' : '&' . $popup_noamp . (!empty($id_file) ? '#fifl' . $id_file : '');
     AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $folder . $anchor, '', '&'));
 }
-elseif ( !empty($del_folder))
+elseif (!empty($del_folder))
 { // delete one folder
     AppContext::get_session()->csrf_get_protect(); // csrf protection
 
-    if (AppContext::get_current_user()->check_level(User::ADMIN_LEVEL)) Uploads::Del_folder($del_folder);
+    if (AppContext::get_current_user()->check_level(User::ADMIN_LEVEL))
+        Uploads::Del_folder($del_folder);
     else
     {
         $check_user_id = 0;
         try {
             $check_user_id = PersistenceContext::get_querier()->get_column_value(DB_TABLE_UPLOAD_CAT, 'user_id', 'WHERE id = :id', array('id' => $del_folder));
         } catch (RowNotFoundException $ex) {
-
+            
         }
 
         // folder and all content delete
@@ -166,7 +175,7 @@ elseif ( !empty($del_folder))
     }
 
     AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $folder . '&' . $popup_noamp, '', '&'));
-} elseif ( !empty($del_file))
+} elseif (!empty($del_file))
 { // File delete
     AppContext::get_session()->csrf_get_protect(); // csrf protection
 
@@ -176,7 +185,7 @@ elseif ( !empty($del_folder))
     } else
     {
         $error = Uploads::Del_file($del_file, AppContext::get_current_user()->get_id());
-        if ( !empty($error))
+        if (!empty($error))
         {
             $error_controller = PHPBoostErrors::unexisting_page();
             DispatchManager::redirect($error_controller);
@@ -184,7 +193,7 @@ elseif ( !empty($del_folder))
     }
 
     AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $folder . '&' . $popup_noamp, '', '&'));
-} elseif ( !empty($move_folder) && $to != -1)
+} elseif (!empty($move_folder) && $to != -1)
 { // folder move
     AppContext::get_session()->csrf_get_protect(); // csrf protection
 
@@ -192,7 +201,7 @@ elseif ( !empty($del_folder))
     try {
         $folder_owner = PersistenceContext::get_querier()->get_column_value(DB_TABLE_UPLOAD_CAT, 'user_id', 'WHERE id = :id', array('id' => $move_folder));
     } catch (RowNotFoundException $ex) {
-
+        
     }
 
     if ($folder_owner == AppContext::get_current_user()->get_id())
@@ -202,21 +211,22 @@ elseif ( !empty($del_folder))
         upload_find_subcats($sub_cats, $move_folder, AppContext::get_current_user()->get_id());
         $sub_cats[] = $move_folder;
         // If we don't move the file to one of his sons or to himself
-        if ( !in_array($to, $sub_cats))
+        if (!in_array($to, $sub_cats))
         {
             if (AppContext::get_current_user()->get_id() || $to == 0)
             {
                 PersistenceContext::get_querier()->update(DB_TABLE_UPLOAD_CAT, array('id_parent' => $to), 'WHERE id = :id', array('id' => $move_folder));
                 AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?f=' . $to . '&' . $popup_noamp, '', '&'));
             }
-        } else AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?movefd=' . $move_folder . '&f=0&error=folder_contains_folder&' . $popup_noamp, '', '&'));
+        } else
+            AppContext::get_response()->redirect(HOST . DIR . url('/user/upload.php?movefd=' . $move_folder . '&f=0&error=folder_contains_folder&' . $popup_noamp, '', '&'));
     }
     else
     {
         $error_controller = PHPBoostErrors::unexisting_page();
         DispatchManager::redirect($error_controller);
     }
-} elseif ( !empty($move_file) && $to != -1)
+} elseif (!empty($move_file) && $to != -1)
 { // file move
     AppContext::get_session()->csrf_get_protect(); // csrf protection
 
@@ -247,7 +257,7 @@ elseif ( !empty($del_folder))
         $error_controller = PHPBoostErrors::unexisting_page();
         DispatchManager::redirect($error_controller);
     }
-} elseif ( !empty($move_folder) || !empty($move_file))
+} elseif (!empty($move_folder) || !empty($move_file))
 {
     $tpl = new FileTemplate('user/upload_move.tpl');
 
@@ -264,7 +274,8 @@ elseif ( !empty($del_folder))
         'L_SUBMIT' => $LANG['submit'],
     ));
 
-    if ($get_error == 'folder_contains_folder') $tpl->put('message_helper', MessageHelper::display($LANG['upload_folder_contains_folder'], MessageHelper::WARNING));
+    if ($get_error == 'folder_contains_folder')
+        $tpl->put('message_helper', MessageHelper::display($LANG['upload_folder_contains_folder'], MessageHelper::WARNING));
 
     // list of available files
     include_once('upload_functions.php');
@@ -349,10 +360,13 @@ elseif ( !empty($del_folder))
 
     // errors management
     $array_error = array('e_upload_invalid_format', 'e_upload_max_weight', 'e_upload_error', 'e_upload_php_code', 'e_upload_failed_unwritable', 'e_unlink_disabled', 'e_max_data_reach');
-    if (in_array($get_error, $array_error)) $tpl->put('message_helper', MessageHelper::display($LANG[$get_error], MessageHelper::WARNING));
-    if ($get_error == 'incomplete') $tpl->put('message_helper', MessageHelper::display($LANG['e_incomplete'], MessageHelper::NOTICE));
+    if (in_array($get_error, $array_error))
+        $tpl->put('message_helper', MessageHelper::display($LANG[$get_error], MessageHelper::WARNING));
+    if ($get_error == 'incomplete')
+        $tpl->put('message_helper', MessageHelper::display($LANG['e_incomplete'], MessageHelper::NOTICE));
 
-    if (isset($LANG[$get_l_error])) $tpl->put('message_helper', MessageHelper::display($LANG[$get_l_error], MessageHelper::WARNING));
+    if (isset($LANG[$get_l_error]))
+        $tpl->put('message_helper', MessageHelper::display($LANG[$get_l_error], MessageHelper::WARNING));
 
     $tpl->put_all(array(
         'C_POPUP' => !empty($popup),
@@ -397,7 +411,7 @@ elseif ( !empty($del_folder))
         'L_CHANGE_PUBLIC' => $upload_lang['change.to.public'],
     ));
 
-    list($total_folder_size, $total_personal_files, $total_public_files, $total_directories) = array(0, 0, 0, 0);
+    list($total_folder_size, $total_public_size, $total_personal_files, $total_public_files, $total_directories) = array(0, 0, 0, 0, 0);
     // folder display
     $result = PersistenceContext::get_querier()->select("SELECT id, name, id_parent, user_id
 	FROM " . DB_TABLE_UPLOAD_CAT . "
@@ -483,7 +497,7 @@ elseif ( !empty($del_folder))
             }
             $is_bbcode_editor = ($editor == 'BBCode');
             $displayed_code = $is_bbcode_editor ? $bbcode : '/upload/' . $row['path'];
-            $inserted_code = !empty($parse) ? ( !empty($no_path) ? $link : PATH_TO_ROOT . $link) : ($is_bbcode_editor ? addslashes($bbcode) : TextHelper::htmlspecialchars($tinymce));
+            $inserted_code = !empty($parse) ? (!empty($no_path) ? $link : PATH_TO_ROOT . $link) : ($is_bbcode_editor ? addslashes($bbcode) : TextHelper::htmlspecialchars($tinymce));
             $tpl->assign_block_vars($loop_id, array(
                 'C_ENABLED_THUMBNAILS' => FileUploadConfig::load()->get_display_file_thumbnail(),
                 'C_IMG' => $get_img_mimetype['img'] == 'far fa-file-image',
@@ -503,8 +517,15 @@ elseif ( !empty($del_folder))
                 'U_MOVE' => url('.php?movefi=' . $row['id'] . '&amp;f=' . $folder . $popup)
             ));
 
-            $total_folder_size += $row['size'];
-            $loop_id == 'public_files' ? $total_public_files ++ : $total_personal_files ++;
+            if ($loop_id == 'public_files')
+            {
+                $total_public_files ++;
+                $total_public_size += $row['size'];
+            } else
+            {
+                $total_personal_files ++;
+                $total_folder_size += $row['size'];
+            }
         }
         $result->dispose();
     }
@@ -516,7 +537,7 @@ elseif ( !empty($del_folder))
     try {
         $total_size = PersistenceContext::get_querier()->get_column_value(DB_TABLE_UPLOAD, 'SUM(size)', 'WHERE user_id = :id', array('id' => AppContext::get_current_user()->get_id()));
     } catch (RowNotFoundException $ex) {
-
+        
     }
 
     $total_size = !empty($folder) ? Uploads::Member_memory_used(AppContext::get_current_user()->get_id()) : $total_size;
@@ -525,6 +546,7 @@ elseif ( !empty($del_folder))
         'SIZE_LIMIT' => !$unlimited_data ? (($group_limit > 1024) ? NumberHelper::round($group_limit / 1024, 2) . ' ' . LangLoader::get_message('unit.megabytes', 'common') : NumberHelper::round($group_limit, 0) . ' ' . LangLoader::get_message('unit.kilobytes', 'common')) : $LANG['illimited'],
         'MAX_FILES_SIZE' => !$unlimited_data ? (($group_limit * 1024 > 1024 * 1024) ? NumberHelper::round($group_limit * 1024, 2) : NumberHelper::round($group_limit * 1024, 0)) : -1,
         'TOTAL_SIZE' => ($total_size > 1024) ? NumberHelper::round($total_size / 1024, 2) . ' ' . LangLoader::get_message('unit.megabytes', 'common') : NumberHelper::round($total_size, 0) . ' ' . LangLoader::get_message('unit.kilobytes', 'common'),
+        'TOTAL_PUBLIC_SIZE' => ($total_public_size > 1024) ? NumberHelper::round($total_public_size / 1024, 2) . ' ' . LangLoader::get_message('unit.megabytes', 'common') : NumberHelper::round($total_public_size, 0) . ' ' . LangLoader::get_message('unit.kilobytes', 'common'),
         'TOTAL_FOLDER_SIZE' => ($total_folder_size > 1024) ? NumberHelper::round($total_folder_size / 1024, 2) . ' ' . LangLoader::get_message('unit.megabytes', 'common') : NumberHelper::round($total_folder_size, 0) . ' ' . LangLoader::get_message('unit.kilobytes', 'common'),
         'TOTAL_FOLDERS' => $total_directories,
         'TOTAL_PERSONAL_FILES' => $total_personal_files,
@@ -533,14 +555,16 @@ elseif ( !empty($del_folder))
 
     $tpl->put_all(array(
         'C_PERSONAL_SUMMARY' => $total_directories > 0 || $total_personal_files > 0,
-        'C_PERSONAL_FILES'   => $total_personal_files > 0,
-        'C_PUBLIC_FILES'     => $total_public_files > 0,
-        'L_NO_ITEM'          => LangLoader::get_message('no_item_now', 'common')
+        'C_PERSONAL_FILES' => $total_personal_files > 0,
+        'C_PUBLIC_FILES' => $total_public_files > 0,
+        'L_NO_ITEM' => LangLoader::get_message('no_item_now', 'common')
     ));
 
     $tpl->display();
 }
 
-if (empty($popup)) require_once('../kernel/footer.php');
-else require_once('../kernel/footer_no_display.php');
+if (empty($popup))
+    require_once('../kernel/footer.php');
+else
+    require_once('../kernel/footer_no_display.php');
 ?>
