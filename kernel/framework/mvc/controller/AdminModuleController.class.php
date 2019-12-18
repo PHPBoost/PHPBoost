62b6b77096e3374a012ae21d421dce2df522fca4
j1.seth <julien.briswalter@gmail.com>
2019-12-18 14:02:43 +0100

    [UPD] Module Controller

--- kernel/framework/mvc/controller/AdminModuleController.class.php
+++ kernel/framework/mvc/controller/AdminModuleController.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2016 10 28
+ * @version     PHPBoost 5.3 - last update: 2019 12 18
  * @since       PHPBoost 3.0 - 2011 10 07
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -32,6 +32,11 @@ abstract class AdminModuleController extends AbstractController
 			return PHPBoostErrors::module_not_installed();
 		}
 		return $this;
+	}
+	
+	public final function get_module()
+	{
+		return ModulesManager::get_module(Environment::get_running_module_name());
 	}
 }
 ?>
--- kernel/framework/mvc/controller/ModuleController.class.php
+++ kernel/framework/mvc/controller/ModuleController.class.php
@@ -6,8 +6,9 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2014 12 22
+ * @version     PHPBoost 5.3 - last update: 2019 12 18
  * @since       PHPBoost 3.0 - 2009 12 14
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 abstract class ModuleController extends AbstractController
@@ -27,6 +28,11 @@ abstract class ModuleController extends AbstractController
 			return PHPBoostErrors::module_not_installed();
 		}
 		return $this;
+	}
+	
+	public final function get_module()
+	{
+		return ModulesManager::get_module(Environment::get_running_module_name());
 	}
 }
 ?>
