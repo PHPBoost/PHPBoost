0efbb6155999ab4822aa3e54872d92c11aae9301
Labsoweb\babsolune <babsolune@phpboost.com>
2019-12-19 05:47:07 +0100

    [UPD] Rontudju indent

--- kernel/framework/content/category/controllers/DefaultRichCategoriesFormController.class.php
+++ kernel/framework/content/category/controllers/DefaultRichCategoriesFormController.class.php
@@ -3,8 +3,8 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.    3 - last update: 2019 11 05
- * @since       P  HPBoost 5.3 - 2019 11 02
+ * @version     PHPBoost 5.3 - last update: 2019 11 05
+ * @since       PHPBoost 5.3 - 2019 11 02
 */
 
 class DefaultRichCategoriesFormController extends AbstractRichCategoriesFormController
