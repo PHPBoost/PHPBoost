1f12c4ad08fc406799b3803e344ced403b8c3000
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-12-19 07:30:48 +0100

    [FIX] Social Networks

--- SocialNetworks/social_networks/ViberSocialNetwork.class.php
+++ SocialNetworks/social_networks/ViberSocialNetwork.class.php
@@ -3,11 +3,11 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 18
+ * @version     PHPBoost 5.3 - last update: 2019 12 19
  * @since       PHPBoost 5.1 - 2018 09 19
 */
 
-class WhatsappSocialNetwork extends AbstractSocialNetwork
+class ViberSocialNetwork extends AbstractSocialNetwork
 {
 	const SOCIAL_NETWORK_ID = 'viber';
 
