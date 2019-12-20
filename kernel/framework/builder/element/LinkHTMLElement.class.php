50ceb51d7decbe7f3070e3afe5e588a984541e0b
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-12-20 14:31:58 +0100

    [UPD] LinkHTMLElement

--- kernel/framework/builder/element/LinkHTMLElement.class.php
+++ kernel/framework/builder/element/LinkHTMLElement.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2017 03 19
+ * @version     PHPBoost 5.3 - last update: 2019 12 20
  * @since       PHPBoost 4.1 - 2015 01 19
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -15,8 +15,9 @@ class LinkHTMLElement extends AbstractHTMLElement
 	private $url;
 	private $content;
 	private $attributs = array();
+	private $use_icon;
 
-	public function __construct($url, $content, $attributs = array(), $css_class = '')
+	public function __construct($url, $content, $attributs = array(), $css_class = '', $use_icon = true)
 	{
 		if ($url instanceof Url)
 		{
@@ -27,6 +28,7 @@ class LinkHTMLElement extends AbstractHTMLElement
 		$this->content = $content;
 		$this->attributs = $attributs;
 		$this->css_class = $css_class;
+		$this->use_icon = ($css_class ? $use_icon : false);
 	}
 
 	public function display()
@@ -34,7 +36,8 @@ class LinkHTMLElement extends AbstractHTMLElement
 		$tpl = new FileTemplate('framework/builder/element/LinkHTMLElement.tpl');
 
 		$tpl->put_all(array(
-			'C_HAS_CSS_CLASSES' => $this->has_css_class(),
+			'C_HAS_ICON' => $this->use_icon,
+			'C_HAS_CSS_CLASSES' => $this->has_css_class() && !$this->use_icon,
 			'CSS_CLASSES' => $this->get_css_class(),
 			'URL' => $this->url,
 			'CONTENT' => $this->content,
