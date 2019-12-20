5e1cce6d0583b1f00ad939a10f6b81ae0dacb73c
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-12-20 15:03:40 +0100

    Create DeleteLinkHTMLElement.class.php

---
+++ kernel/framework/builder/element/DeleteLinkHTMLElement.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Builder
+ * @subpackage  Element
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Julien BRISWALTER <j1.seth@phpboost.com>
+ * @version     PHPBoost 5.3 - last update: 2019 12 20
+ * @since       PHPBoost 5.3 - 2019 12 20
+*/
+
+class DeleteLinkHTMLElement extends LinkHTMLElement
+{
+	public function __construct($url, $content = '')
+	{
+		parent::__construct($url, $content, array('aria-label' => LangLoader::get_message('delete', 'common')), 'far fa-fw fa-trash-alt')
+	}
+}
+?>
