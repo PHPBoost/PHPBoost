29998fec37d2bd2dcd60dbe6e551e631b4f67e76
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-12-23 10:32:19 +0100

    [FIX] CalendarCategory

--- calendar/services/CalendarCategory.class.php
+++ calendar/services/CalendarCategory.class.php
@@ -21,17 +21,17 @@ class CalendarCategory extends Category
 		return $this->color;
 	}
 
-	protected static function get_additional_properties()
+	public static function get_additional_properties()
 	{
 		return array('color' => $this->get_color());
 	}
 
-	protected static function set_additional_properties(array $properties)
+	public static function set_additional_properties(array $properties)
 	{
 		$this->set_color($properties['color']);
 	}
 
-	protected static function get_categories_table_additional_fields()
+	public static function get_categories_table_additional_fields()
 	{
 		return array(
 			'color' => array('type' => 'string', 'length' => 250, 'default' => "''")
