accdcbbf36e9fefbd4c4330d8efde86e5fa17c2b
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-12-26 17:00:36 +0100

    [FIX] RichRootCategory

--- kernel/framework/content/category/RichRootCategory.class.php
+++ kernel/framework/content/category/RichRootCategory.class.php
@@ -5,15 +5,17 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2016 10 24
+ * @version     PHPBoost 5.3 - last update: 2019 12 26
  * @since       PHPBoost 4.0 - 2013 01 31
  * @contributor Arnaud GENET <elenwii@phpboost.com>
+ * @contrinutor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class RichRootCategory extends RichCategory
 {
 	public function __construct()
 	{
+		self::__static();
 		$this->set_id(self::ROOT_CATEGORY);
 		$this->set_id_parent(self::ROOT_CATEGORY);
 		$this->set_name(LangLoader::get_message('root', 'main'));
