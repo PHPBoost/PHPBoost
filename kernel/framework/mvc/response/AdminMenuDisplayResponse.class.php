2264dc24a626f1f776ad43bea5c6c1dff2ffc67d
j1.seth <julien.briswalter@gmail.com>
2019-12-28 09:07:47 +0100

    [FIX] AdminMenuDisplayResponse

--- kernel/framework/mvc/response/AdminMenuDisplayResponse.class.php
+++ kernel/framework/mvc/response/AdminMenuDisplayResponse.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 18
+ * @version     PHPBoost 5.3 - last update: 2019 12 28
  * @since       PHPBoost 3.0 - 2009 10 18
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -26,7 +26,7 @@ class AdminMenuDisplayResponse extends AbstractResponse
 		$this->full_view = new FileTemplate('admin/AdminMenuDisplayResponse.tpl');
 		$this->full_view->put('content', $view);
 		$env->display_kernel_message($this->full_view);
-		parent::__construct($env , $this->full_view);
+		parent::__construct($env, $this->full_view);
 
 		$module_name = Environment::get_running_module_name();
 		if (!empty($module_name))
@@ -34,11 +34,11 @@ class AdminMenuDisplayResponse extends AbstractResponse
 			$this->module = ModulesManager::get_module($module_name);
 			if ($this->module)
 			{
+				$this->set_title(StringVars::replace_vars(LangLoader::get_message('configuration.module.title', 'admin-common'), array('module_name' => $this->module->get_configuration()->get_name())));
+				
 				$home_page = $this->module->get_configuration()->get_home_page();
 				if (!empty($home_page))
 				{
-					$this->set_title(StringVars::replace_vars(LangLoader::get_message('configuration.module.title', 'admin-common'), array('module_name' => $this->module->get_configuration()->get_name())));
-					
 					$this->links[] = array(
 						'LINK' => LangLoader::get_message('home', 'main'),
 						'U_LINK' => Url::to_rel('/' . $this->module->get_id() . '/' . $home_page)
