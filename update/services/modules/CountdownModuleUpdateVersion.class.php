6b8edaed97644ddf6f7be0484344d38d05e50ad3
Labsoweb\babsolune <babsolune@phpboost.com>
2019-12-28 09:24:50 +0100

    [UPD] countdown lastcoms

---
+++ update/services/modules/CountdownModuleUpdateVersion.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 5.3 - last update: 2019 12 28
+ * @since       PHPBoost 5.3 - 2019 12 27
+*/
+
+class CountdownModuleUpdateVersion extends ModuleUpdateVersion
+{
+	public function __construct()
+	{
+		parent::__construct('countdown');
+		$this->delete_old_folders_list = array(
+			'/util'
+		);
+	}
+}
+?>
