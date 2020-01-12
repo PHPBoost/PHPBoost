e3aef7ceb0de26c0fb0ac4cd6019245818b76170
Labsoweb\babsolune <babsolune@phpboost.com>
2020-01-12 13:00:56 +0100

    [FIX] delete item - web

--- web/controllers/WebDeleteController.class.php
+++ web/controllers/WebDeleteController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 31
+ * @version     PHPBoost 5.3 - last update: 2020 01 12
  * @since       PHPBoost 4.1 - 2014 08 21
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Mipel <mipel@phpboost.com>
@@ -20,13 +20,17 @@ class WebDeleteController extends ModuleController
 
 		$weblink = $this->get_weblink($request);
 
-		$this->check_authorizations();
+		if (!$weblink->is_authorized_to_delete())
+		{
+			$error_controller = PHPBoostErrors::user_not_authorized();
+			DispatchManager::redirect($error_controller);
+		}
 
 		WebService::delete($weblink->get_id());
 
 		WebService::clear_cache();
 
-		AppContext::get_response()->redirect(($request->get_url_referrer() && !TextHelper::strstr($request->get_url_referrer(), WebUrlBuilder::display($this->weblink->get_category()->get_id(), $this->weblink->get_category()->get_rewrited_name(), $this->weblink->get_id(), $this->weblink->get_rewrited_title())->rel()) ? $request->get_url_referrer() : WebUrlBuilder::home()), StringVars::replace_vars(LangLoader::get_message('web.message.success.delete', 'common', 'web'), array('name' => $this->weblink->get_title())));
+		AppContext::get_response()->redirect(($request->get_url_referrer() && !TextHelper::strstr($request->get_url_referrer(), WebUrlBuilder::display($weblink->get_category()->get_id(), $weblink->get_category()->get_rewrited_name(), $weblink->get_id(), $weblink->get_rewrited_title())->rel()) ? $request->get_url_referrer() : WebUrlBuilder::home()), StringVars::replace_vars(LangLoader::get_message('web.message.success.delete', 'common', 'web'), array('name' => $weblink->get_title())));
 	}
 
 	private function get_weblink(HTTPRequestCustom $request)
@@ -36,25 +40,11 @@ class WebDeleteController extends ModuleController
 		if (!empty($id))
 		{
 			try {
-				$this->weblink = WebService::get_weblink('WHERE web.id=:id', array('id' => $id));
+				return WebService::get_weblink('WHERE web.id=:id', array('id' => $id));
 			} catch (RowNotFoundException $e) {
 				$error_controller = PHPBoostErrors::unexisting_page();
 				DispatchManager::redirect($error_controller);
 			}
-		}
-	}
-
-	private function check_authorizations()
-	{
-		if (!$this->weblink->is_authorized_to_delete())
-		{
-			$error_controller = PHPBoostErrors::user_not_authorized();
-			DispatchManager::redirect($error_controller);
-		}
-		if (AppContext::get_current_user()->is_readonly())
-		{
-			$error_controller = PHPBoostErrors::user_in_read_only();
-			DispatchManager::redirect($error_controller);
 		}
 	}
 }
