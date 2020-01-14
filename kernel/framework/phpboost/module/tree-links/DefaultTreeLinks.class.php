4ae5eef7834b3941dad41d8eeec2f213ef7fe5f6
j1.seth <julien.briswalter@gmail.com>
2020-01-14 06:58:07 +0100

    [UPD] DefaultTreeLinks

--- kernel/framework/phpboost/module/tree-links/DefaultTreeLinks.class.php
+++ kernel/framework/phpboost/module/tree-links/DefaultTreeLinks.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 13
+ * @version     PHPBoost 5.3 - last update: 2020 01 14
  * @since       PHPBoost 5.3 - 2019 12 20
 */
 
@@ -20,6 +20,11 @@ class DefaultTreeLinks implements ModuleTreeLinksExtensionPoint
 	 * @var bool display of items links or not
 	 */
 	private $display_items_links;
+
+	/**
+	 * @var mixed[] authorizations checker
+	 */
+	private $authorizations;
 
 	/**
 	 * DefaultTreeLinks constructor
@@ -52,13 +57,13 @@ class DefaultTreeLinks implements ModuleTreeLinksExtensionPoint
 	{
 		$tree = new ModuleTreeLinks();
 		$has_categories = ModulesManager::get_module($this->module_id)->get_configuration()->has_categories();
-		$authorizations = $has_categories ? CategoriesAuthorizationsService::check_authorizations(Category::ROOT_CATEGORY, $this->module_id) : ItemsAuthorizationsService::check_authorizations($this->module_id);
+		$this->authorizations = $has_categories ? CategoriesAuthorizationsService::check_authorizations(Category::ROOT_CATEGORY, $this->module_id) : ItemsAuthorizationsService::check_authorizations($this->module_id);
 		
 		if ($has_categories)
 		{
-			$manage_categories_link = new ModuleLink(LangLoader::get_message('categories.manage', 'categories-common'), CategoriesUrlBuilder::manage_categories($this->module_id), $authorizations->manage_categories());
-			$manage_categories_link->add_sub_link(new ModuleLink(LangLoader::get_message('categories.manage', 'categories-common'), CategoriesUrlBuilder::manage_categories($this->module_id), $authorizations->manage_categories()));
-			$manage_categories_link->add_sub_link(new ModuleLink(LangLoader::get_message('category.add', 'categories-common'), CategoriesUrlBuilder::add_category(AppContext::get_request()->get_getint('id_category', Category::ROOT_CATEGORY), $this->module_id), $authorizations->manage_categories()));
+			$manage_categories_link = new ModuleLink(LangLoader::get_message('categories.manage', 'categories-common'), CategoriesUrlBuilder::manage_categories($this->module_id), $this->authorizations->manage_categories());
+			$manage_categories_link->add_sub_link(new ModuleLink(LangLoader::get_message('categories.manage', 'categories-common'), CategoriesUrlBuilder::manage_categories($this->module_id), $this->authorizations->manage_categories()));
+			$manage_categories_link->add_sub_link(new ModuleLink(LangLoader::get_message('category.add', 'categories-common'), CategoriesUrlBuilder::add_category(AppContext::get_request()->get_getint('id_category', Category::ROOT_CATEGORY), $this->module_id), $this->authorizations->manage_categories()));
 			$tree->add_link($manage_categories_link);
 		}
 		
@@ -66,22 +71,22 @@ class DefaultTreeLinks implements ModuleTreeLinksExtensionPoint
 		{
 			$lang = ItemsService::get_items_lang($this->module_id);
 			
-			$manage_items_link = new ModuleLink($lang['items.manage'], ItemsUrlBuilder::manage($this->module_id), $authorizations->moderation());
-			$manage_items_link->add_sub_link(new ModuleLink($lang['items.manage'], ItemsUrlBuilder::manage($this->module_id), $authorizations->moderation()));
-			$manage_items_link->add_sub_link(new ModuleLink($lang['item.add'], ItemsUrlBuilder::add(AppContext::get_request()->get_getint('id_category', Category::ROOT_CATEGORY), $this->module_id), $authorizations->moderation()));
+			$manage_items_link = new ModuleLink($lang['items.manage'], ItemsUrlBuilder::manage($this->module_id), $this->authorizations->moderation());
+			$manage_items_link->add_sub_link(new ModuleLink($lang['items.manage'], ItemsUrlBuilder::manage($this->module_id), $this->authorizations->moderation()));
+			$manage_items_link->add_sub_link(new ModuleLink($lang['item.add'], $this->get_add_item_url(), $this->authorizations->moderation()));
 			$tree->add_link($manage_items_link);
 			
-			if (!$authorizations->moderation())
+			if (!$this->authorizations->moderation())
 			{
-				$tree->add_link(new ModuleLink($lang['item.add'], ItemsUrlBuilder::add(AppContext::get_request()->get_getint('id_category', Category::ROOT_CATEGORY), $this->module_id), $authorizations->write() || $authorizations->contribution()));
+				$tree->add_link(new ModuleLink($lang['item.add'], $this->get_add_item_url(), $this->check_write_authorization()));
 			}
 			
-			$this->get_module_additional_items_actions_tree_links();
+			$this->get_module_additional_items_actions_tree_links($tree);
 			
-			$tree->add_link(new ModuleLink($lang['items.pending'], ItemsUrlBuilder::display_pending($this->module_id), $authorizations->write() || $authorizations->contribution() || $authorizations->moderation()));
+			$tree->add_link(new ModuleLink($lang['items.pending'], ItemsUrlBuilder::display_pending($this->module_id), $this->check_write_authorization() || $this->authorizations->moderation()));
 		}
 		
-		$this->get_module_additional_actions_tree_links();
+		$this->get_module_additional_actions_tree_links($tree);
 		
 		if (ModulesManager::get_module($this->module_id)->get_configuration()->get_admin_main_page())
 			$tree->add_link(new AdminModuleLink(LangLoader::get_message('configuration', 'admin-common'), ModulesUrlBuilder::configuration($this->module_id)));
@@ -89,7 +94,7 @@ class DefaultTreeLinks implements ModuleTreeLinksExtensionPoint
 		if (ModulesManager::get_module($this->module_id)->get_configuration()->get_documentation())
 		{
 			if ($has_categories)
-				$tree->add_link(new ModuleLink(LangLoader::get_message('module.documentation', 'admin-modules-common'), ModulesManager::get_module($this->module_id)->get_configuration()->get_documentation(), $authorizations->write() || $authorizations->contribution() || $authorizations->moderation()));
+				$tree->add_link(new ModuleLink(LangLoader::get_message('module.documentation', 'admin-modules-common'), ModulesManager::get_module($this->module_id)->get_configuration()->get_documentation(), $this->check_write_authorization() || $this->authorizations->moderation()));
 			else
 				$tree->add_link(new AdminModuleLink(LangLoader::get_message('module.documentation', 'admin-modules-common'), ModulesManager::get_module($this->module_id)->get_configuration()->get_documentation()));
 		}
@@ -97,8 +102,18 @@ class DefaultTreeLinks implements ModuleTreeLinksExtensionPoint
 		return $tree;
 	}
 
-	protected function get_module_additional_actions_tree_links() {}
+	protected function get_add_item_url()
+	{
+		return ItemsUrlBuilder::add(AppContext::get_request()->get_getint('id_category', Category::ROOT_CATEGORY), $this->module_id);
+	}
 
-	protected function get_module_additional_items_actions_tree_links() {}
+	private function check_write_authorization()
+	{
+		return ModulesManager::get_module($this->module_id)->get_configuration()->has_contribution() ? ($this->authorizations->write() || $this->authorizations->contribution()) : $this->authorizations->write();
+	}
+
+	protected function get_module_additional_actions_tree_links(&$tree) {}
+
+	protected function get_module_additional_items_actions_tree_links(&$tree) {}
 }
 ?>
