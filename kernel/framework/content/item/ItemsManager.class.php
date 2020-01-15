9a563b46a50e0417b45cd77687b525ae24b9d2f5
j1.seth <julien.briswalter@gmail.com>
2020-01-15 22:55:18 +0100

    [FEATURE] ItemsManager

---
+++ kernel/framework/content/item/ItemsManager.class.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * @package     Content
+ * @subpackage  Item\controllers
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Julien BRISWALTER <j1.seth@phpboost.com>
+ * @version     PHPBoost 5.3 - last update: 2020 01 15
+ * @since       PHPBoost 5.3 - 2019 12 20
+*/
+
+class ItemsManager
+{
+	protected static $db_querier;
+	protected static $module_id;
+	protected static $module;
+	protected static $items_table;
+
+	public static function __static()
+	{
+		self::$db_querier  = PersistenceContext::get_querier();
+		self::$module_id   = Environment::get_running_module_name();
+		self::$module      = ModulesManager::get_module(self::$module_id);
+		self::$items_table = self::$module->get_configuration()->get_items_table_name();
+	}
+
+	public function __construct($module_id = '')
+	{
+		if ($module_id)
+		{
+			self::$module_id   = $module_id;
+			self::$module      = ModulesManager::get_module(self::$module_id);
+			self::$items_table = self::$module->get_configuration()->get_items_table_name();
+		}
+	}
+
+	 /**
+	 * @desc Count items number.
+	 * @param string $condition (optional) : Restriction to apply to the list of items
+	 * @param array $parameters (optional) : Parameters list to apply to the condition
+	 */
+	public static function count($condition = '', $parameters = array())
+	{
+		return self::$db_querier->count(self::$items_table, $condition, $parameters);
+	}
+
+	 /**
+	 * @desc Create a new item.
+	 * @param string[] $item new Item
+	 */
+	public static function add(Item $item)
+	{
+		$result = self::$db_querier->insert(self::$items_table, $item->get_properties());
+		return $result->get_last_inserted_id();
+	}
+
+	 /**
+	 * @desc Update an item.
+	 * @param string[] $item Item to update
+	 */
+	public static function update(Item $item)
+	{
+		self::$db_querier->update(self::$items_table, $item->get_properties(), 'WHERE id=:id', array('id', $item->get_id()));
+	}
+
+	 /**
+	 * @desc Delete an item.
+	 * @param int $id Item identifier
+	 */
+	public static function delete(int $id)
+	{
+		if (AppContext::get_current_user()->is_readonly())
+		{
+			$controller = PHPBoostErrors::user_in_read_only();
+			DispatchManager::redirect($controller);
+		}
+		
+		self::$db_querier->delete(self::$items_table, 'WHERE id=:id', array('id' => $id));
+		
+		self::$db_querier->delete(DB_TABLE_EVENTS, 'WHERE module=:module AND id_in_module=:id', array('module' => self::$module_id, 'id' => $id));
+		
+		CommentsService::delete_comments_topic_module(self::$module_id, $id);
+		KeywordsService::get_keywords_manager()->delete_relations($id);
+		NotationService::delete_notes_id_in_module(self::$module_id, $id);
+	}
+
+	 /**
+	 * @desc Return the item with all its properties.
+	 * @param int $id Item identifier
+	 */
+	public static function get_item(int $id)
+	{
+		$row = self::$db_querier->select_single_row_query('SELECT :module_id.*, member.*, notes.average_notes, notes.number_notes, note.note
+		FROM ' . self::$items_table . ' :module_id
+		LEFT JOIN ' . DB_TABLE_MEMBER . ' member ON member.user_id = :module_id.author_user_id
+		LEFT JOIN ' . DB_TABLE_AVERAGE_NOTES . ' notes ON notes.id_in_module = :module_id.id AND notes.module_name = :module_id
+		LEFT JOIN ' . DB_TABLE_NOTE . ' note ON note.id_in_module = :module_id.id AND note.module_name = :module_id AND note.user_id = :current_user_id
+		WHERE :module_id.id=:id', array(
+			'module_id'       => self::$module_id, 
+			'id'              => $id,
+			'current_user_id' => AppContext::get_current_user()->get_id()
+		));
+
+		$item = self::get_item_class();
+		$item->set_properties($row);
+		return $item;
+	}
+
+	 /**
+	 * @desc Updates the views number of the item.
+	 * @param string[] $item Item to update
+	 */
+	public static function update_views_number(Item $item)
+	{
+		self::$db_querier->update(self::$items_table, array('views_number' => $item->get_views_number() + 1), 'WHERE id=:id', array('id' => $item->get_id()));
+	}
+
+	 /**
+	 * @desc Return the list of items correspnding to the condition.
+	 * @param int $number_items_per_page Number of items to display
+	 * @param int $display_from First item to take into account
+	 * @param string $sort_field Field on which apply the sorting
+	 * @param string $sort_mode Sort mode (asc or desc)
+	 * @param string $condition Restriction to apply to the item
+	 * @param array $parameters Parameters of the condition
+	 */
+	public static function get_items(int $number_items_per_page, int $display_from, $sort_field, $sort_mode, $condition = '', array $parameters = array())
+	{
+		$now = new Date();
+		$items = array();
+		
+		$result = self::$db_querier->select('SELECT :module_id.*, member.*, com.number_comments, notes.number_notes, notes.average_notes, note.note
+		FROM ' . self::$items_table . ' :module_id
+		LEFT JOIN ' . DB_TABLE_KEYWORDS_RELATIONS . ' relation ON relation.module_id = :module_id AND relation.id_in_module = :module_id.id
+		LEFT JOIN ' . DB_TABLE_MEMBER . ' member ON member.user_id = :module_id.author_user_id
+		LEFT JOIN ' . DB_TABLE_COMMENTS_TOPIC . ' com ON com.id_in_module = :module_id.id AND com.module_id = :module_id
+		LEFT JOIN ' . DB_TABLE_AVERAGE_NOTES . ' notes ON notes.id_in_module = :module_id.id AND notes.module_name = :module_id
+		LEFT JOIN ' . DB_TABLE_NOTE . ' note ON note.id_in_module = :module_id.id AND note.module_name = :module_id AND note.user_id = :user_id
+		' . $condition . '
+		ORDER BY ' . $sort_field . ' ' . $sort_mode . '
+		LIMIT :number_items_per_page OFFSET :display_from', array_merge($parameters, array(
+			'user_id' => AppContext::get_current_user()->get_id(),
+			'timestamp_now' => $now->get_timestamp(),
+			'number_items_per_page' => $number_items_per_page,
+			'display_from' => $display_from
+		)));
+		
+		while ($row = $result->fetch())
+		{
+			$item = self::get_item_class();
+			$item->set_properties($row);
+			$items[] = $item;
+		}
+		$result->dispose();
+		
+		return $items;
+	}
+
+	 /**
+	 * @desc Clear caches files
+	 */
+	public static function clear_cache()
+	{
+		Feed::clear_cache(self::$module_id);
+		KeywordsCache::invalidate();
+		
+		if (self::$module->get_configuration()->has_categories())
+			CategoriesService::get_categories_manager()->regenerate_cache();
+		
+		self::clear_module_cache();
+	}
+
+	 /**
+	 * @desc Clear module caches files if needed
+	 */
+	protected static function clear_module_cache()
+	{
+		$cache_classes = array(ucfirst(self::$module_id) . 'Cache', ucfirst(self::$module_id) . 'MiniMenuCache');
+		foreach ($cache_classes as $cache_class)
+		{
+			if (class_exists($cache_class) && is_subclass_of($cache_class, 'CacheData'))
+				call_user_func($cache_class .'::invalidate');
+		}
+	}
+	
+	/**
+	 * Get the authorizations of the module.
+	 * @return mixed[] The array of authorizations of the module.
+	 */
+	public function get_heritated_authorizations()
+	{
+		return self::$module->get_configuration()->get_configuration_parameters()->get_authorizations();
+	}
+
+	/**
+	 * @return string module identifier.
+	 */
+	public function get_module_id() {
+		return self::$module_id;
+	}
+
+	/**
+	 * @return mixed[] new Item
+	 */
+	protected static function get_item_class()
+	{
+		$class_name = self::$module->get_configuration()->get_item_name();
+		return new $class_name();
+	}
+}
+?>
