5ab0ced9644f7ae3772202582f0516dc72b4e12d
j1.seth <julien.briswalter@gmail.com>
2020-01-23 12:07:38 +0100

    [FIX] Articles config lang

--- articles/controllers/AdminArticlesConfigController.class.php
+++ articles/controllers/AdminArticlesConfigController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Patrick DUBEAU <daaxwizeman@gmail.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 18
+ * @version     PHPBoost 5.3 - last update: 2020 01 23
  * @since       PHPBoost 4.0 - 2013 02 27
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -98,7 +98,7 @@ class AdminArticlesConfigController extends AdminModuleController
 				}'))
 		));
 
-		$fieldset->add_field(new FormFieldNumberEditor('items_per_row', $this->admin_common_lang['config.items.number.per.row'], $this->config->get_items_per_row(),
+		$fieldset->add_field(new FormFieldNumberEditor('items_per_row', $this->admin_common_lang['config.items.per.row'], $this->config->get_items_per_row(),
 			array(
 				'hidden' => $this->config->get_display_type() !== ArticlesConfig::GRID_VIEW,
 				'min' => 1, 'max' => 4,
