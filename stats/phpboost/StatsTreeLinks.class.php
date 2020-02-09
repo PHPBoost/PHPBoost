c71b02841f168874f7765a1c18e546608db18c17
Labsoweb\babsolune <babsolune@phpboost.com>
2020-02-09 09:27:23 +0100

    [UPD] stats - duplicated link

--- stats/phpboost/StatsTreeLinks.class.php
+++ stats/phpboost/StatsTreeLinks.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 15
+ * @version     PHPBoost 5.3 - last update: 2020 02 08
  * @since       PHPBoost 5.2 - 2019 12 15
 */
 
@@ -13,7 +13,6 @@ class StatsTreeLinks implements ModuleTreeLinksExtensionPoint
 	{
 		$tree = new ModuleTreeLinks();
 
-		$tree->add_link(new AdminModuleLink(LangLoader::get_message('home', 'main'), new Url('/stats/index.php')));
 		$tree->add_link(new AdminModuleLink(LangLoader::get_message('configuration', 'admin'), new Url('/stats/admin_stats.php')));
 		$tree->add_link(new AdminModuleLink(LangLoader::get_message('module.documentation', 'admin-modules-common'), ModulesManager::get_module('stats')->get_configuration()->get_documentation()));
 
