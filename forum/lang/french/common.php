20630ef2821f8ea09807d8ecb3bf878b475a29c7
mipel85 <mipel@phpboost.com>
2020-02-10 15:36:53 +0100

    [UPD] Forum lang

--- forum/lang/french/common.php
+++ forum/lang/french/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 12 10
+ * @version     PHPBoost 5.3 - last update: 2020 02 10
  * @since       PHPBoost 4.1 - 2015 02 25
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -28,13 +28,13 @@ $lang['forum.links'] = 'Liens';
 $lang['forum.message.options'] = 'Options du message';
 $lang['forum.messages'] = 'Messages sur le forum';
 
-//config
+// Configuration
 $lang['config.forum.name'] = 'Nom du forum';
 $lang['config.number.topics.per.page'] = 'Nombre de sujets par page';
 $lang['config.number.messages.per.page'] = 'Nombre de messages par page';
 $lang['config.read.messages.storage.duration'] = 'Durée pour laquelle les messages lus par les membres sont stockés';
 $lang['config.read.messages.storage.duration.explain'] = 'En jours. A régler suivant le nombre de messages par jour.';
-$lang['config.max.topic.number.in.favorite'] = 'Nombre de sujets en favoris maximum pour chaque membre';
+$lang['config.max.topic.number.in.favorite'] = 'Nombre de sujets maximum en favoris pour chaque membre';
 $lang['config.edit.mark.enabled'] = 'Marqueurs d\'édition des messages';
 $lang['config.multiple.posts.allowed'] = 'Autoriser les utilisateurs à poster plusieurs messages consécutifs';
 $lang['config.multiple.posts.allowed.explain'] = 'Si l\'option est désactivée, le dernier message de l\'utilisateur sera automatiquement complété avec le nouveau contenu lors de l\'ajout d\'un message';
@@ -43,9 +43,9 @@ $lang['config.message.before.topic.title.displayed'] = 'Afficher le message deva
 $lang['config.message.before.topic.title'] = 'Message devant le titre du topic';
 $lang['config.message.when.topic.is.unsolved'] = 'Message devant le titre du topic si statut non changé';
 $lang['config.message.when.topic.is.solved'] = 'Message devant le titre du topic si statut changé';
-$lang['config.message.before.topic.title.icon.displayed'] = 'Afficher l\'icône associée';
+$lang['config.message.before.topic.title.icon.displayed'] = 'Afficher l\'icône associé';
 
-//Categories
+// Categories
 $lang['category.status.locked'] = 'Verrouillé';
 
 //Extended Field
@@ -62,6 +62,6 @@ $lang['extended.field.signing.explain'] = 'Apparaît sous chacun de vos messages
 $lang['authorizations.read.topics.content'] = 'Autorisation d\'afficher le contenu des topics';
 $lang['authorizations.flood'] = 'Autorisation de flooder';
 $lang['authorizations.hide.edition.mark'] = 'Désactivation du marqueur d\'édition des messages';
-$lang['authorizations.unlimited.topics.tracking'] = 'Désactivation de la limite de sujet suivis';
-$lang['authorizations.multiple.posts'] = 'Utilisateurs autorisés à à poster plusieurs messages consécutifs';
+$lang['authorizations.unlimited.topics.tracking'] = 'Désactivation de la limite de sujets suivis';
+$lang['authorizations.multiple.posts'] = 'Utilisateurs autorisés à poster plusieurs messages consécutifs';
 ?>
