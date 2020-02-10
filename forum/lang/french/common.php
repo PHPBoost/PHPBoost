b6e33d49a2f5281e6a81d67b900711f5ec5af0e2
mipel85 <mipel@phpboost.com>
2020-02-10 15:41:51 +0100

    [UPD] Forum lang

--- forum/lang/french/common.php
+++ forum/lang/french/common.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2019 10 25
+ * @version   	PHPBoost 5.2 - last update: 2020 02 10
  * @since   	PHPBoost 4.1 - 2015 02 25
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -37,7 +37,7 @@ $lang['config.number_topics_per_page'] = 'Nombre de sujets par page';
 $lang['config.number_messages_per_page'] = 'Nombre de messages par page';
 $lang['config.read_messages_storage_duration'] = 'Durée pour laquelle les messages lus par les membres sont stockés';
 $lang['config.read_messages_storage_duration.explain'] = 'En jours. A régler suivant le nombre de messages par jour.';
-$lang['config.max_topic_number_in_favorite'] = 'Nombre de sujets en favoris maximum pour chaque membre';
+$lang['config.max_topic_number_in_favorite'] = 'Nombre de sujets maximum en favoris pour chaque membre';
 $lang['config.edit_mark_enabled'] = 'Marqueurs d\'édition des messages';
 $lang['config.multiple_posts_allowed'] = 'Autoriser les utilisateurs à poster plusieurs messages consécutifs';
 $lang['config.multiple_posts_allowed.explain'] = 'Si l\'option est désactivée, le dernier message de l\'utilisateur sera automatiquement complété avec le nouveau contenu lors de l\'ajout d\'un message';
@@ -46,7 +46,7 @@ $lang['config.message_before_topic_title_displayed'] = 'Afficher le message deva
 $lang['config.message_before_topic_title'] = 'Message devant le titre du topic';
 $lang['config.message_when_topic_is_unsolved'] = 'Message devant le titre du topic si statut non changé';
 $lang['config.message_when_topic_is_solved'] = 'Message devant le titre du topic si statut changé';
-$lang['config.message_before_topic_title_icon_displayed'] = 'Afficher l\'icône associée';
+$lang['config.message_before_topic_title_icon_displayed'] = 'Afficher l\'icône associé';
 $lang['config.message_before_topic_title_icon_displayed.explain'] = '<i class="fa fa-msg-display"></i> / <i class="fa fa-msg-not-display"></i>';
 
 //Categories
@@ -67,5 +67,5 @@ $lang['authorizations.read_topics_content'] = 'Autorisation d\'afficher le conte
 $lang['authorizations.flood'] = 'Autorisation de flooder';
 $lang['authorizations.hide_edition_mark'] = 'Désactivation du marqueur d\'édition des messages';
 $lang['authorizations.unlimited_topics_tracking'] = 'Désactivation de la limite de sujet suivis';
-$lang['authorizations.multiple_posts'] = 'Utilisateurs autorisés à à poster plusieurs messages consécutifs';
+$lang['authorizations.multiple_posts'] = 'Utilisateurs autorisés à poster plusieurs messages consécutifs';
 ?>
