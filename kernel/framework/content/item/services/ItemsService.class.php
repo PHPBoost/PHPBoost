7c4456f6d08c25bdb9158d949c4247861960222c
j1.seth <julien.briswalter@gmail.com>
2020-02-14 22:33:53 +0100

    [UPD] ItemsService

--- kernel/framework/content/item/services/ItemsService.class.php
+++ kernel/framework/content/item/services/ItemsService.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 16
+ * @version     PHPBoost 5.3 - last update: 2020 02 14
  * @since       PHPBoost 5.3 - 2020 01 08
 */
 
@@ -37,7 +37,7 @@ class ItemsService
 		$module_lang = LangLoader::get($filename, $module_id);
 		$parameters = array();
 		
-		foreach (array('all.items', 'last.items', 'an.item', 'the.item', 'items', 'item') as $element)
+		foreach (array('all.items', 'last.items', 'an.item', 'the.item', 'items.number', 'items', 'item') as $element)
 		{
 			$parameters[str_replace('.', '_', TextHelper::ucfirst($element))] = isset($module_lang[$element]) ? TextHelper::ucfirst($module_lang[$element]) : TextHelper::ucfirst($items_lang[$element]);
 			$parameters[str_replace('.', '_', TextHelper::lcfirst($element))] = isset($module_lang[$element]) ? TextHelper::lcfirst($module_lang[$element]) : TextHelper::lcfirst($items_lang[$element]);
