55f9eb222ccdcadfc7a0fd2c8b5eadfdfeadb3ac
j1.seth <julien.briswalter@gmail.com>
2020-02-18 14:50:49 +0100

    [FIX] Newsletter

--- newsletter/services/NewsletterStream.class.php
+++ newsletter/services/NewsletterStream.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2016 02 11
+ * @version     PHPBoost 5.3 - last update: 2020 02 18
  * @since       PHPBoost 4.1 - 2015 12 19
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -11,5 +11,10 @@
 class NewsletterStream extends RichCategory
 {
 	protected $allowed_to_have_childs = false;
+	
+	public static function __static()
+	{
+		parent::__static();
+	}
 }
 ?>
