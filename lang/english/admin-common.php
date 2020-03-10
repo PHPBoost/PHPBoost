cc7804928b5fd5f40a23e799f498089a675fd7f3
Labsoweb\babsolune <babsolune@phpboost.com>
2020-03-10 05:10:40 +0100

    [UPD] common lang variables

--- lang/english/admin-common.php
+++ lang/english/admin-common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 29
+ * @version     PHPBoost 5.3 - last update: 2020 30 10
  * @since       PHPBoost 4.0 - 2013 12 13
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -32,18 +32,21 @@ $lang['author'] = 'Author';
 $lang['compatibility'] = 'Compatibility';
 $lang['not_compatible'] = 'Not compatible';
 
-$lang['config.items_number_per_page'] = 'Items number per page';
-$lang['config.categories_number_per_page'] = 'Categories number per page';
-$lang['config.columns_number_per_line'] = 'Columns number per line';
-$lang['config.columns_number_per_line.description'] = 'Categories and Items';
+$lang['config.items_number_per_page'] = 'Items number per page'; // To be deleted
+$lang['config.categories_number_per_page'] = 'Categories number per page'; // To be deleted
+$lang['config.categories.per.page'] = 'Categories number per page';
+$lang['config.columns_number_per_line'] = 'Columns number per line'; // To be deleted
+$lang['config.columns_number_per_line.description'] = 'Categories and Items'; // To be deleted
 $lang['config.categories.per.row'] = 'Categories number per row';
 $lang['config.items.per.row'] = 'Items number per row';
-$lang['config.display_summary_to_guests'] = 'Display summary of files to guests if they don\'t have read permission';
+$lang['config.items.per.page'] = 'Items number per page';
+$lang['config.display.summary.to.guests'] = 'Display summary of files to guests if they don\'t have read permission';
 $lang['config.full.item.display'] = 'Full item display';
-$lang['config.items_default_sort'] = 'Items display order per default'; // To be deleted whan all modules will use the new variable
+$lang['config.items_default_sort'] = 'Items display order per default'; // To be deleted
 $lang['config.items.default.sort.field'] = 'Default items display order';
 $lang['config.items.default.sort.mode'] = 'Default items display direction';
-$lang['config.author_displayed'] = 'Display author\'s name';
+$lang['config.author_displayed'] = 'Display author\'s name'; // To be deleted
+$lang['config.author.displayed'] = 'Display author\'s name';
 $lang['config.views.number.enabled'] = 'Display views number';
 $lang['config.comments_enabled'] = 'Enable comments';
 $lang['config.notation_enabled'] = 'Enable notation';
@@ -53,7 +56,9 @@ $lang['config.display.type'] = 'Display type';
 $lang['config.display.type.grid'] = 'Display in grid view';
 $lang['config.display.type.list'] = 'Display in list view';
 $lang['config.display.type.table'] = 'Display in table view';
+$lang['config.characters.number.to.cut'] = 'Characters number to cut';
 $lang['config.root_category_description'] = 'Root category description';
+$lang['config.item.default.content'] = 'Item default content';
 
 $lang['config.authorizations.explain'] = 'Global module authorizations. You can change these authorizations on each category.';
 
--- lang/french/admin-common.php
+++ lang/french/admin-common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 29
+ * @version     PHPBoost 5.3 - last update: 2020 03 10
  * @since       PHPBoost 4.0 - 2013 12 13
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -32,20 +32,21 @@ $lang['author'] = 'Auteur';
 $lang['compatibility'] = 'Compatibilité';
 $lang['not_compatible'] = 'Non compatible';
 
-$lang['config.items_number_per_page'] = 'Nombre d\'éléments par page';
-$lang['config.categories_number_per_page'] = 'Nombre de catégories par page';
+$lang['config.items_number_per_page'] = 'Nombre d\'éléments par page'; // To be deleted
+$lang['config.categories_number_per_page'] = 'Nombre de catégories par page'; // To be deleted
 $lang['config.categories.per.page'] = 'Nombre de catégories par page';
 $lang['config.columns_number_per_line'] = 'Nombre de colonnes d\'affichage par ligne';
 $lang['config.columns_number_per_line.description'] = 'Catégories et éléments';
 $lang['config.categories.per.row'] = 'Nombre de catégories par ligne';
 $lang['config.items.per.row'] = 'Nombre d\'éléments par ligne';
 $lang['config.items.per.page'] = 'Nombre d\'éléments par page';
-$lang['config.display.summary.to.guests'] = 'Afficher le résumé des éléments aux visiteurs s\'ils n\'ont pas l\'autorisation de lecture';
+$lang['config.display.summary.to.guests'] = 'Afficher le résumé des éléments aux visiteurs non autorisés en lecture';
 $lang['config.full.item.display'] = 'Affichage complet des éléments';
-$lang['config.items_default_sort'] = 'Ordre d\'affichage des éléments par défaut'; // To be deleted whan all modules will use the new variable
+$lang['config.items_default_sort'] = 'Ordre d\'affichage des éléments par défaut'; // To be deleted
 $lang['config.items.default.sort.field'] = 'Ordre d\'affichage des éléments par défaut';
 $lang['config.items.default.sort.mode'] = 'Sens d\'affichage des éléments par défaut';
-$lang['config.author_displayed'] = 'Afficher le nom de l\'auteur';
+$lang['config.author_displayed'] = 'Afficher le nom de l\'auteur'; // To be deleted
+$lang['config.author.displayed'] = 'Afficher le nom de l\'auteur';
 $lang['config.views.number.enabled'] = 'Afficher le nombre de vues';
 $lang['config.comments_enabled'] = 'Activer les commentaires';
 $lang['config.notation_enabled'] = 'Activer la notation';
@@ -55,7 +56,9 @@ $lang['config.display.type'] = 'Type d\'affichage';
 $lang['config.display.type.grid'] = 'Affichage en grille';
 $lang['config.display.type.list'] = 'Affichage en liste';
 $lang['config.display.type.table'] = 'Affichage en tableau';
+$lang['config.characters.number.to.cut'] = 'Nombre de caractères pour couper l\'élément';
 $lang['config.root_category_description'] = 'Description de la catégorie racine';
+$lang['config.item.default.content'] = 'Contenu par défaut d\'un élément';
 
 $lang['config.authorizations.explain'] = 'Autorisations globales du module. Vous pouvez changer ces autorisations localement sur chaque catégorie.';
 
