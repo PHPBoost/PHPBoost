c9a6653d7dbf94f87b4cfc3b8515f642b59df151
mipel85 <mipel@phpboost.com>
2020-03-31 13:43:08 +0200

    [FIX] Newsletter - little error corrected

--- newsletter/controllers/NewsletterSubscribersListController.class.php
+++ newsletter/controllers/NewsletterSubscribersListController.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2019 10 11
+ * @version   	PHPBoost 5.2 - last update: 2020 03 31
  * @since   	PHPBoost 3.0 - 2011 03 11
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
