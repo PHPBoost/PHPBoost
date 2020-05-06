18ed8293faef4972d7b50db5929cb66793b618cc
j1.seth <julien.briswalter@gmail.com>
2020-05-06 14:48:23 +0200

    [FIX] Categories

--- gallery/services/GalleryCategoriesCache.class.php
+++ gallery/services/GalleryCategoriesCache.class.php
@@ -3,12 +3,32 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 05 06
+ * @version     PHPBoost 5.3 - last update: 2019 12 29
  * @since       PHPBoost 4.1 - 2015 02 10
 */
 
-class GalleryCategoriesCache extends DefaultRichCategoriesCache
+class GalleryCategoriesCache extends CategoriesCache
 {
+	public function get_table_name()
+	{
+		return GallerySetup::$gallery_cats_table;
+	}
+
+	public function get_table_name_containing_items()
+	{
+		return GallerySetup::$gallery_table;
+	}
+
+	public function get_category_class()
+	{
+		return CategoriesManager::RICH_CATEGORY_CLASS;
+	}
+
+	public function get_module_identifier()
+	{
+		return 'gallery';
+	}
+
 	protected function get_category_elements_number($id_category)
 	{
 		$pics_aprob = GalleryService::count('WHERE id_category = :id_category AND aprob = 1', array('id_category' => $id_category));
@@ -20,9 +40,15 @@ class GalleryCategoriesCache extends DefaultRichCategoriesCache
 		);
 	}
 
-	protected function get_root_category_description()
+	public function get_root_category()
 	{
-		return StringVars::replace_vars(LangLoader::get_message('gallery.seo.description.root', 'common', 'gallery'), array('site' => GeneralConfig::load()->get_site_name()));
+		$root = new RichRootCategory();
+		$root->set_authorizations(GalleryConfig::load()->get_authorizations());
+		$root->set_description(
+			StringVars::replace_vars(LangLoader::get_message('gallery.seo.description.root', 'common', 'gallery'),
+			array('site' => GeneralConfig::load()->get_site_name()
+		)));
+		return $root;
 	}
 }
 ?>
