7ee20bf7965fbc054dcda9ee93d72d6017b8ec06
j1.seth <julien.briswalter@gmail.com>
2020-05-08 16:30:36 +0200

    [FIX] DownloadConfigUpdate

--- update/services/modules/config/DownloadConfigUpdateVersion.class.php
+++ update/services/modules/config/DownloadConfigUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 05 06
+ * @version     PHPBoost 5.3 - last update: 2020 05 08
  * @since       PHPBoost 5.3 - 2020 05 06
 */
 
@@ -21,6 +21,7 @@ class DownloadConfigUpdateVersion extends ConfigUpdateVersion
 		if (class_exists('DownloadConfig') && !empty($old_config))
 		{
 			$config = DownloadConfig::load();
+			
 			switch ($old_config->get_items_default_sort_field())
 			{
 				case 'number_downloads':
@@ -33,7 +34,22 @@ class DownloadConfigUpdateVersion extends ConfigUpdateVersion
 					$config->set_items_default_sort_field($old_config->get_items_default_sort_field());
 				break;
 			}
+			
 			$config->set_items_default_sort_mode(in_array(TextHelper::strtoupper($old_config->get_items_default_sort_field()), array(Item::ASC, Item::DESC)) ? TextHelper::strtolower($old_config->get_items_default_sort_field()) : TextHelper::strtolower(Item::DESC));
+			
+			switch ($old_config->get_sort_type())
+			{
+				case 'number_downloads':
+					$config->set_sort_type('downloads_number');
+				break;
+				case 'number_view':
+					$config->set_sort_type('views_number');
+				break;
+				default:
+					$config->set_sort_type($old_config->get_sort_type());
+				break;
+			}
+			
 			DownloadConfig::save();
 			
 			return true;
