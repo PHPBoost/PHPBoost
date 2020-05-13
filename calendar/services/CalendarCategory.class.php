1fd1a5ca15ba8dfb9b8267ab19f033fca5ea5314
j1.seth <julien.briswalter@gmail.com>
2020-05-13 09:39:47 +0200

    [UPD] calendar default category color to take event color defined in admin

--- calendar/services/CalendarCategory.class.php
+++ calendar/services/CalendarCategory.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 02 17
+ * @version     PHPBoost 5.3 - last update: 2020 05 13
  * @since       PHPBoost 4.0 - 2013 02 25
 */
 
@@ -13,8 +13,9 @@ class CalendarCategory extends Category
 	{
 		parent::__static();
 		self::add_additional_attribute('color', array('type' => 'string', 'length' => 250, 'default' => "''", 'attribute_field_parameters' => array(
-			'field_class' => 'FormFieldColorPicker',
-			'label' => LangLoader::get_message('calendar.config.category.color', 'common', 'calendar')
+			'field_class'   => 'FormFieldColorPicker',
+			'label'         => LangLoader::get_message('calendar.config.category.color', 'common', 'calendar'),
+			'default_value' => CalendarConfig::load()->get_event_color()
 			)
 		));
 	}
