6afc6667ffaf43f98cb3a34314e36342757266d3
j1.seth <julien.briswalter@gmail.com>
2020-05-14 16:22:53 +0200

    [UPD] Automatic site migration now based on different domain name or different site path only

--- kernel/framework/io/http/HTTPRequestCustom.class.php
+++ kernel/framework/io/http/HTTPRequestCustom.class.php
@@ -6,7 +6,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 22
+ * @version     PHPBoost 5.3 - last update: 2020 05 14
  * @since       PHPBoost 3.0 - 2009 10 17
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor janus57 <janus57@janus57.fr>
@@ -380,9 +380,9 @@ class HTTPRequestCustom
 	}
 
 	// get site domain name (without host)
-	public function get_domain_name()
+	public function get_domain_name($url = '')
 	{
-		$pieces = parse_url(self::get_site_url());
+		$pieces = parse_url(!empty($url) ? $url : self::get_site_url());
 		$domain = isset($pieces['host']) ? $pieces['host'] : '';
 		if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/iu', $domain, $regs))
 			return $regs['domain'];
