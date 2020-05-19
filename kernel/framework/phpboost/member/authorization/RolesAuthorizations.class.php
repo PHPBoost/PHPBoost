0c6c1c73b97b945f2c4e462711e8839fd33d384f
Julien Briswalter <jbriswalter@users.noreply.github.com>
2020-05-19 07:06:29 +0200

    Update RolesAuthorizations.class.php

--- kernel/framework/phpboost/member/authorization/RolesAuthorizations.class.php
+++ kernel/framework/phpboost/member/authorization/RolesAuthorizations.class.php
@@ -6,7 +6,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2016 10 28
+ * @version     PHPBoost 5.3 - last update: 2020 05 19
  * @since       PHPBoost 3.0 - 2010 03 01
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -128,7 +128,7 @@ class RolesAuthorizations
 		{
 			if ($auth)
 			{
-				if ($role[0] == 'm')
+				if (TextHelper::substr($role, 0, 1) == 'm')
 				{
 					$this->users[] = (int)TextHelper::substr($role, 1);
 				}
