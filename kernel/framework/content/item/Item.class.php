bde2db9f73f6c37f2c2285c28af5b83ba7af9641
j1.seth <julien.briswalter@gmail.com>
2020-05-21 00:39:25 +0200

    [UPD] Item

--- kernel/framework/content/item/Item.class.php
+++ kernel/framework/content/item/Item.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 04 30
+ * @version     PHPBoost 5.3 - last update: 2020 05 21
  * @since       PHPBoost 5.3 - 2019 12 20
 */
 
@@ -344,6 +344,16 @@ class Item
 		}
 		else
 			self::$additional_attributes_list[$id] = array('is_url' => false);
+		
+		foreach (array('attribute_pre_content_field_parameters', 'attribute_post_content_field_parameters', 'attribute_other_field_parameters') as $attribute_field)
+		{
+			if (isset($parameters[$attribute_field]))
+			{
+				self::$additional_attributes_list[$id][$attribute_field] = $parameters[$attribute_field];
+				
+				unset($parameters[$attribute_field]);
+			}
+		}
 		
 		self::$additional_attributes_items_table_fields[$id] = $parameters;
 	}
