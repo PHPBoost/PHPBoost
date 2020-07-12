e5c090fc912c97c037836c745cae573b6530fc08
Julien Briswalter <jbriswalter@users.noreply.github.com>
2020-07-12 09:05:36 +0200

    [FIX] ClassLoader

--- kernel/framework/core/ClassLoader.class.php
+++ kernel/framework/core/ClassLoader.class.php
@@ -4,7 +4,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 07 11
+ * @version     PHPBoost 6.0 - last update: 2020 07 12
  * @since       PHPBoost 3.0 - 2009 10 21
  * @contributor mipel <mipel@phpboost.com>
 */
@@ -140,7 +140,7 @@ class ClassLoader
 		$relative_path = Path::get_path_from_root($folder->get_path());
 		
 		$path_folders = explode('/', $relative_path);
-		$module_id = $path_folders[1];
+		$module_id = isset($path_folders[1]) ? $path_folders[1] : '';
 		
 		$files = $folder->get_files($pattern);
 		foreach ($files as $file)
