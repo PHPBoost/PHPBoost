841f168412426f55cde047e5dd95362cac619ccc
Labsoweb\babsolune <babsolune@phpboost.com>
2020-07-13 08:31:14 +0200

    [UPD] headblock file version

--- news/controllers/NewsMemberItemsController.class.php
+++ news/controllers/NewsMemberItemsController.class.php
@@ -4,7 +4,7 @@
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
  * @version     PHPBoost 6.0 - last update: 2020 06 14
- * @since       PHPBoost 5.3 - 2013 06 14
+ * @since       PHPBoost 5.2 - 2013 06 14
 */
 
 class NewsMemberItemsController extends ModuleController
