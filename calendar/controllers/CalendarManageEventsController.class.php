f36bdbf5b971cf5d6bb39dda300da7f7b2c2828d
j1.seth <julien.briswalter@gmail.com>
2020-07-19 08:58:42 +0200

    [FIX] Calendar events management repeat type label

--- calendar/controllers/CalendarManageEventsController.class.php
+++ calendar/controllers/CalendarManageEventsController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 05 08
+ * @version     PHPBoost 6.0 - last update: 2020 07 19
  * @since       PHPBoost 4.0 - 2013 07 25
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -89,7 +89,7 @@ class CalendarManageEventsController extends AdminModuleController
 				new HTMLTableRowCell(new SpanHTMLElement(($category->get_id() == Category::ROOT_CATEGORY ? LangLoader::get_message('none_e', 'common') : $category->get_name()), array('data-color-surround' => $category->get_id() != Category::ROOT_CATEGORY && $category->get_color() ? $category->get_color() : ($category->get_id() == Category::ROOT_CATEGORY ? $config->get_event_color() : '')), 'pinned')),
 				new HTMLTableRowCell($author),
 				new HTMLTableRowCell(LangLoader::get_message('from_date', 'main') . ' ' . $event->get_start_date()->format(Date::FORMAT_DAY_MONTH_YEAR_HOUR_MINUTE) . $br->display() . LangLoader::get_message('to_date', 'main') . ' ' . $event->get_end_date()->format(Date::FORMAT_DAY_MONTH_YEAR_HOUR_MINUTE)),
-				new HTMLTableRowCell($event->belongs_to_a_serie() ? $this->lang['calendar.labels.repeat.' . $event->get_content()->get_repeat_type()] . ' - ' . $event->get_content()->get_repeat_number() . ' ' . $this->lang['calendar.labels.repeat_times'] : LangLoader::get_message('no', 'common')),
+				new HTMLTableRowCell($event->belongs_to_a_serie() ? $this->get_repeat_type_label($event) . ' - ' . $event->get_content()->get_repeat_number() . ' ' . $this->lang['calendar.labels.repeat_times'] : LangLoader::get_message('no', 'common')),
 				new HTMLTableRowCell($event->get_content()->is_approved() ? LangLoader::get_message('yes', 'common') : LangLoader::get_message('no', 'common')),
 				new HTMLTableRowCell($edit_link->display() . $delete_link->display(), 'controls')
 			);
@@ -140,6 +140,32 @@ class CalendarManageEventsController extends AdminModuleController
 
 			AppContext::get_response()->redirect(CalendarUrlBuilder::manage_events(), LangLoader::get_message('process.success', 'status-messages-common'));
 		}
+	}
+
+	private function get_repeat_type_label(CalendarEvent $event)
+	{
+		$label = CalendarEventContent::NEVER;
+
+		switch ($event->get_content()->get_repeat_type())
+		{
+			case CalendarEventContent::DAILY :
+				$label = LangLoader::get_message('every_day', 'date-common');
+				break;
+
+			case CalendarEventContent::WEEKLY :
+				$label = LangLoader::get_message('every_week', 'date-common');
+				break;
+
+			case CalendarEventContent::MONTHLY :
+				$label = LangLoader::get_message('every_month', 'date-common');
+				break;
+
+			case CalendarEventContent::YEARLY :
+				$label = LangLoader::get_message('every_year', 'date-common');
+				break;
+		}
+
+		return $label;
 	}
 
 	private function check_authorizations()
