5729776f55e82f73e141ddf5068b558d160823b3
Labsoweb\babsolune <babsolune@phpboost.com>
2020-07-28 08:49:26 +0200

    [UPD] phpinfo design

--- admin/server/controllers/AdminServerPHPInfoController.class.php
+++ admin/server/controllers/AdminServerPHPInfoController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2018 06 04
+ * @version     PHPBoost 6.0 - last update: 2020 07 28
  * @since       PHPBoost 4.1 - 2015 05 20
  * @contributor mipel <mipel@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -17,8 +17,8 @@ class AdminServerPHPInfoController extends AdminController
 		phpinfo();
 		$phpinfo = ob_get_contents();
 		$phpinfo = preg_replace('`^.*<body>`isu', '', $phpinfo);
-		$phpinfo = str_replace(array('class="e"', 'class="v"', '<table border="0" cellpadding="3" width="600">', '</body></html>'),
-		array('', 'style="overflow:auto;"',  '<table style="table-layout:fixed;">', ''), $phpinfo);
+		$phpinfo = str_replace(array('class="e"', 'class="v"', '<table>', '<th>', '</body></html>'),
+		array('', '',  '<table class="table-no-header phpinfo">', '<td>', ''), $phpinfo);
 		ob_end_clean();
 		ob_start();
 
