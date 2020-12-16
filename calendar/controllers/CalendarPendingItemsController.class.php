f2c8f3c2e1fa31afd217d672a7bbd37faddec919
Labsoweb\babsolune <babsolune@phpboost.com>
2020-12-16 09:20:07 +0100

    [FIX] calendar pending items

--- calendar/controllers/CalendarPendingItemsController.class.php
+++ calendar/controllers/CalendarPendingItemsController.class.php
@@ -3,12 +3,12 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 11
+ * @version     PHPBoost 6.0 - last update: 2020 12 16
  * @since       PHPBoost 4.0 - 2013 09 29
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
-class CalendaryPendingItemsController extends ModuleController
+class CalendarPendingItemsController extends ModuleController
 {
 	private $view;
 	private $items_view;
