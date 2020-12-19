646e80e1f9fb91c5a7918da0f537c14fee854ec4
Labsoweb\babsolune <babsolune@phpboost.com>
2020-12-19 11:21:07 +0100

    [UPD] quotes - writing conventions - files name & update

--- update/services/modules/QuotesModuleUpdateVersion.class.php
+++ update/services/modules/QuotesModuleUpdateVersion.class.php
@@ -3,9 +3,10 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 07 13
+ * @version     PHPBoost 6.0 - last update: 2020 12 19
  * @since       PHPBoost 6.0 - 2019 11 03
  * @contributor xela <xela@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class QuotesModuleUpdateVersion extends ModuleUpdateVersion
@@ -17,18 +18,26 @@ class QuotesModuleUpdateVersion extends ModuleUpdateVersion
 		$this->content_tables = array(array('name' => PREFIX . 'quotes', 'content_field' => 'quote'));
 		self::$delete_old_files_list = array(
 			'/controllers/AdminQuotesManageController.class.php',
+			'/controllers/QuotesDeleteController.class.php',
+			'/controllers/QuotesDisplayAuthorQuotesController.class.php',
+			'/controllers/QuotesDisplayCategoryController.class.php',
+			'/controllers/QuotesDisplayPendingQuotesController.class.php',
+			'/controllers/QuotesFormController.class.php',
+			'/controllers/QuotesManageController.class.php',
 			'/lang/english/config.php',
 			'/lang/french/config.php',
 			'/phpboost/QuotesHomePageExtensionPoint.class.php',
 			'/phpboost/QuotesSitemapExtensionPoint.class.php',
 			'/phpboost/QuotesTreeLinks.class.php',
+			'/services/Quote.class.php',
 			'/services/QuotesAuthorizationsService.class.php',
+			'/templates/QuotesDisplaySeveralQuotesController.tpl',
 			'/util/AdminQuotesDisplayResponse.class.php'
 		);
 		self::$delete_old_folders_list = array(
 			'/controllers/categories'
 		);
-		
+
 		$this->database_columns_to_modify = array(
 			array(
 				'table_name' => PREFIX . 'quotes_cats',
