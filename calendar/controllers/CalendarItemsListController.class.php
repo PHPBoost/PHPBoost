fe436d1367bed6683c1645e52720ab770754a6c6
Labsoweb\babsolune <babsolune@phpboost.com>
2020-12-20 16:53:15 +0100

    [UPD]missing date

--- calendar/controllers/CalendarItemsListController.class.php
+++ calendar/controllers/CalendarItemsListController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 11
+ * @version     PHPBoost 6.0 - last update: 2020 12 20
  * @since       PHPBoost 4.1 - 2015 04 13
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
