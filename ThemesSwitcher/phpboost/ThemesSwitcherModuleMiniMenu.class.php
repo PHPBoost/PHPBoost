ec5235b8dde7eddc57df8832e6f1f02800929ea2
Labsoweb\babsolune <babsolune@phpboost.com>
2020-12-29 08:42:35 +0100

    [FIX] ThemesSwitcher lang

--- ThemesSwitcher/phpboost/ThemesSwitcherModuleMiniMenu.class.php
+++ ThemesSwitcher/phpboost/ThemesSwitcherModuleMiniMenu.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 21
+ * @version     PHPBoost 6.0 - last update: 2020 12 29
  * @since       PHPBoost 3.0 - 2012 02 22
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -29,7 +29,7 @@ class ThemesSwitcherModuleMiniMenu extends ModuleMiniMenu
 
 	public function get_menu_title()
 	{
-		return LangLoader::get_message('switch.theme', 'themeswitcher_common', 'ThemesSwitcher');
+		return LangLoader::get_message('switch.theme', 'common', 'ThemesSwitcher');
 	}
 
 	public function is_displayed()
