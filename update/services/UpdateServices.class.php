559d37b0c61503ff2d6c7a714143483a948a4327
j1.seth <julien.briswalter@gmail.com>
2021-01-14 06:51:26 +0100

    [FIX] TinyMCE

--- update/services/UpdateServices.class.php
+++ update/services/UpdateServices.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 01 05
+ * @version     PHPBoost 6.0 - last update: 2021 01 14
  * @since       PHPBoost 3.0 - 2012 02 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -701,6 +701,10 @@ class UpdateServices
 		$file->delete();
 
 		$folder = new Folder(PATH_TO_ROOT . '/ReCaptcha/lib');
+		if ($folder->exists())
+			$folder->delete();
+
+		$folder = new Folder(PATH_TO_ROOT . '/TinyMCE/lib');
 		if ($folder->exists())
 			$folder->delete();
 	}
