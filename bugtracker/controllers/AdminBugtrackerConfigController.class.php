884aa427a5b5dc2adf03d53f782524ac4000543f
j1.seth <julien.briswalter@gmail.com>
2021-01-25 07:02:34 +0100

    [UPD] Bugtracker

--- bugtracker/controllers/AdminBugtrackerConfigController.class.php
+++ bugtracker/controllers/AdminBugtrackerConfigController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 24
+ * @version     PHPBoost 6.0 - last update: 2021 01 25
  * @since       PHPBoost 3.0 - 2012 10 18
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -23,7 +23,7 @@ class AdminBugtrackerConfigController extends AdminModuleController
 	private $submit_button;
 	private $config;
 
-	private $max_input = 150;
+	private $max_input = 500;
 
 	public function execute(HTTPRequestCustom $request)
 	{
