dafdbc1e10952640c8ee11214829feb35b31ba65
j1.seth <julien.briswalter@gmail.com>
2021-02-02 07:19:13 +0100

    [UPD] ArticlesConfig update in update script

--- update/services/modules/config/ArticlesConfigUpdateVersion.class.php
+++ update/services/modules/config/ArticlesConfigUpdateVersion.class.php
@@ -21,7 +21,31 @@ class ArticlesConfigUpdateVersion extends ConfigUpdateVersion
 		if (class_exists('ArticlesConfig') && !empty($old_config))
 		{
 			$config = ArticlesConfig::load();
-			$items_default_sort_field = $items_default_sort_mode = $display_type = '';
+			$number_articles_per_page = $number_categories_per_page = $number_cols_display_per_line = $number_character_to_cut = $items_default_sort_field = $items_default_sort_mode = $descriptions_displayed_to_guests = $date_updated_displayed = $display_type = '';
+			
+			try {
+				$number_articles_per_page = $old_config->get_property('number_articles_per_page');
+			} catch (PropertyNotFoundException $e) {}
+			if ($number_articles_per_page)
+				$config->set_items_per_page($number_articles_per_page);
+			
+			try {
+				$number_categories_per_page = $old_config->get_property('number_categories_per_page');
+			} catch (PropertyNotFoundException $e) {}
+			if ($number_categories_per_page)
+				$config->set_categories_per_page($number_categories_per_page);
+			
+			try {
+				$number_cols_display_per_line = $old_config->get_property('number_cols_display_per_line');
+			} catch (PropertyNotFoundException $e) {}
+			if ($number_cols_display_per_line)
+				$config->set_items_per_row($number_cols_display_per_line);
+			
+			try {
+				$number_character_to_cut = $old_config->get_property('number_character_to_cut');
+			} catch (PropertyNotFoundException $e) {}
+			if ($number_character_to_cut)
+				$config->set_auto_cut_characters_number($number_character_to_cut);
 			
 			try {
 				$items_default_sort_field = $old_config->get_property('items_default_sort_field');
@@ -56,6 +80,18 @@ class ArticlesConfigUpdateVersion extends ConfigUpdateVersion
 			} catch (PropertyNotFoundException $e) {}
 			if ($items_default_sort_mode)
 			$config->set_items_default_sort_mode(in_array(TextHelper::strtoupper($items_default_sort_mode), array(Item::ASC, Item::DESC)) ? TextHelper::strtolower($items_default_sort_mode) : TextHelper::strtolower(Item::DESC));
+			
+			try {
+				$descriptions_displayed_to_guests = $old_config->get_property('descriptions_displayed_to_guests');
+			} catch (PropertyNotFoundException $e) {}
+			if ($descriptions_displayed_to_guests)
+				$config->set_summary_displayed_to_guests($descriptions_displayed_to_guests);
+			
+			try {
+				$date_updated_displayed = $old_config->get_property('date_updated_displayed');
+			} catch (PropertyNotFoundException $e) {}
+			if ($date_updated_displayed)
+				$config->set_update_date_displayed($date_updated_displayed);
 			
 			try {
 				$display_type = $old_config->get_property('display_type');
