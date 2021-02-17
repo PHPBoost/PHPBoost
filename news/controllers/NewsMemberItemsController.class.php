a3db598565a46496bede534024a000ec53c06bc5
j1.seth <julien.briswalter@gmail.com>
2021-02-17 06:53:45 +0100

    [UPD] News member items optimization

--- news/controllers/NewsMemberItemsController.class.php
+++ news/controllers/NewsMemberItemsController.class.php
@@ -3,8 +3,9 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 02 16
+ * @version     PHPBoost 6.0 - last update: 2021 02 17
  * @since       PHPBoost 5.2 - 2013 06 14
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class NewsMemberItemsController extends ModuleController
@@ -12,7 +13,7 @@ class NewsMemberItemsController extends ModuleController
 	private $view;
 	private $lang;
 	private $config;
-	private $user;
+	private $member;
 
 	public function execute(HTTPRequestCustom $request)
 	{
@@ -20,15 +21,7 @@ class NewsMemberItemsController extends ModuleController
 
 		$this->init();
 
-		$user_id = $request->get_getint('user_id', AppContext::get_current_user()->get_id());
-		try {
-			$this->user = PersistenceContext::get_querier()->select_single_row(PREFIX . 'member', array('*'), 'WHERE user_id=:user_id', array('user_id' => $user_id));
-		} catch (RowNotFoundException $e) {
-			$error_controller = PHPBoostErrors::unexisting_element();
-			DispatchManager::redirect($error_controller);
-		}
-
-		$this->build_view($this->user['user_id']);
+		$this->build_view();
 
 		return $this->generate_response();
 	}
@@ -42,7 +35,7 @@ class NewsMemberItemsController extends ModuleController
 		$this->config = NewsConfig::load();
 	}
 
-	public function build_view($user_id)
+	public function build_view()
 	{
 		$now = new Date();
 		$comments_config = CommentsConfig::load();
@@ -53,12 +46,12 @@ class NewsMemberItemsController extends ModuleController
 		AND (published = 1 OR (published = 2 AND (publishing_start_date > :timestamp_now OR (publishing_end_date != 0 AND publishing_end_date < :timestamp_now))))';
 		$parameters = array(
 			'authorized_categories' => $authorized_categories,
-			'user_id' => $this->user['user_id'],
+			'user_id' => $this->get_member()->get_id(),
 			'timestamp_now' => $now->get_timestamp()
 		);
 
 		$page = AppContext::get_request()->get_getint('page', 1);
-		$pagination = $this->get_pagination($condition, $parameters, $user_id, $page);
+		$pagination = $this->get_pagination($condition, $parameters, $page);
 
 		$result = PersistenceContext::get_querier()->select('SELECT news.*, member.*
 		FROM '. NewsSetup::$news_table .' news
@@ -73,7 +66,7 @@ class NewsMemberItemsController extends ModuleController
 
 		$this->view->put_all(array(
 			'C_MEMBER_ITEMS' => true,
-			'C_MY_ITEMS' => $user_id == AppContext::get_current_user()->get_id(),
+			'C_MY_ITEMS' => $this->is_current_member_displayed(),
 			'C_GRID_VIEW' => $this->config->get_display_type() == NewsConfig::GRID_VIEW,
 			'C_LIST_VIEW' => $this->config->get_display_type() == NewsConfig::LIST_VIEW,
 			'C_FULL_ITEM_DISPLAY' => $this->config->get_full_item_display(),
@@ -84,7 +77,7 @@ class NewsMemberItemsController extends ModuleController
 			'PAGINATION' => $pagination->display(),
 
 			'ITEMS_PER_ROW' => $this->config->get_items_per_row(),
-			'MEMBER_NAME' => $this->user['display_name']
+			'MEMBER_NAME' => $this->get_member()->get_display_name()
 		));
 
 		while ($row = $result->fetch())
@@ -102,12 +95,28 @@ class NewsMemberItemsController extends ModuleController
 		$result->dispose();
 	}
 
-	private function get_pagination($condition, $parameters, $user_id, $page)
+	protected function get_member()
+	{
+		if ($this->member === null)
+		{
+			$this->member = UserService::get_user(AppContext::get_request()->get_getint('user_id', AppContext::get_current_user()->get_id()));
+			if (!$this->member)
+				DispatchManager::redirect(PHPBoostErrors::unexisting_element());
+		}
+		return $this->member;
+	}
+
+	protected function is_current_member_displayed()
+	{
+		return $this->member && $this->member->get_id() == AppContext::get_current_user()->get_id();
+	}
+
+	private function get_pagination($condition, $parameters, $page)
 	{
 		$items_number = NewsService::count($condition, $parameters);
 
 		$pagination = new ModulePagination($page, $items_number, (int)NewsConfig::load()->get_items_per_page());
-		$pagination->set_url(NewsUrlBuilder::display_member_items($user_id, '%d'));
+		$pagination->set_url(NewsUrlBuilder::display_member_items($this->get_member()->get_id(), '%d'));
 
 		if ($pagination->current_page_is_empty() && $page > 1)
 		{
@@ -130,22 +139,17 @@ class NewsMemberItemsController extends ModuleController
 	private function generate_response()
 	{
 		$page = AppContext::get_request()->get_getint('page', 1);
+		$page_title = $this->is_current_member_displayed() ? $this->lang['my.items'] : $this->lang['member.items'] . ' ' . $this->get_member()->get_display_name();
 		$response = new SiteDisplayResponse($this->view);
 
 		$graphical_environment = $response->get_graphical_environment();
-		if($this->user['user_id'] == AppContext::get_current_user()->get_id())
-			$graphical_environment->set_page_title($this->lang['my.items'], $this->lang['module.title'], $page);
-		else
-			$graphical_environment->set_page_title($this->lang['member.items'] . ' ' . $this->user['display_name'], $this->lang['module.title'], $page);
+		$graphical_environment->set_page_title($page_title, $this->lang['module.title'], $page);
 		$graphical_environment->get_seo_meta_data()->set_description(StringVars::replace_vars($this->lang['news.seo.description.member'], array('author' => AppContext::get_current_user()->get_display_name())), $page);
-		$graphical_environment->get_seo_meta_data()->set_canonical_url(NewsUrlBuilder::display_member_items($this->user['user_id'], $page));
+		$graphical_environment->get_seo_meta_data()->set_canonical_url(NewsUrlBuilder::display_member_items($this->get_member()->get_id(), $page));
 
 		$breadcrumb = $graphical_environment->get_breadcrumb();
 		$breadcrumb->add($this->lang['module.title'], NewsUrlBuilder::home());
-		if($this->user['user_id'] == AppContext::get_current_user()->get_id())
-			$breadcrumb->add($this->lang['my.items'], NewsUrlBuilder::display_member_items($this->user['user_id'], $page));
-		else
-			$breadcrumb->add($this->lang['member.items'] . ' ' . $this->user['display_name'], NewsUrlBuilder::display_member_items($this->user['user_id'], $page));
+		$breadcrumb->add($page_title, NewsUrlBuilder::display_member_items($this->get_member()->get_id(), $page));
 
 		return $response;
 	}
