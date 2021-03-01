2b4b6f1b4309b9ecea7d9e6843b01cd967487ae3
j1.seth <julien.briswalter@gmail.com>
2021-03-01 23:21:55 +0100

    [UPD] Calendar update

--- update/services/modules/CalendarModuleUpdateVersion.class.php
+++ update/services/modules/CalendarModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 02 01
+ * @version     PHPBoost 6.0 - last update: 2021 03 01
  * @since       PHPBoost 4.0 - 2014 02 11
  * @contributor xela <xela@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -62,6 +62,12 @@ class CalendarModuleUpdateVersion extends ModuleUpdateVersion
 				)
 			)
 		);
+	}
+
+	protected function execute_module_specific_changes()
+	{
+		// Set registration_limit enabled where max registered members id different of 0
+		$this->querier->update(PREFIX . 'calendar_events_content', array('registration_limit' => 1), 'WHERE max_registered_members <> 0');
 	}
 }
 ?>
