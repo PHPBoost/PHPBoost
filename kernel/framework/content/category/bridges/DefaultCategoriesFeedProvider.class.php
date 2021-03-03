796cece5317c72e44138d20d70b218e1b8fe361b
j1.seth <julien.briswalter@gmail.com>
2021-03-03 22:18:03 +0100

    [UPD] Default Categories feed

--- kernel/framework/content/category/bridges/DefaultCategoriesFeedProvider.class.php
+++ kernel/framework/content/category/bridges/DefaultCategoriesFeedProvider.class.php
@@ -50,8 +50,7 @@ class DefaultCategoriesFeedProvider implements FeedProvider
 			$data->set_auth_bit(Category::READ_AUTHORIZATIONS);
 
 			$condition = 'WHERE id_category IN :categories_id
-			AND (published = ' . Item::PUBLISHED . ($module->get_configuration()->feature_is_enabled('deferred_publication') ? ' OR (published = ' . Item::DEFERRED_PUBLICATION . ' AND (publishing_start_date > :timestamp_now OR (publishing_end_date != 0 AND publishing_end_date < :timestamp_now)))' : '') . ')
-			ORDER BY creation_date DESC';
+			AND (published = ' . Item::PUBLISHED . ($module->get_configuration()->feature_is_enabled('deferred_publication') ? ' OR (published = ' . Item::DEFERRED_PUBLICATION . ' AND (publishing_start_date > :timestamp_now OR (publishing_end_date != 0 AND publishing_end_date < :timestamp_now)))' : '') . ')';
 			
 			$parameters = array(
 				'categories_id' => array_keys(CategoriesService::get_categories_manager($this->module_id)->get_children($id_category, new SearchCategoryChildrensOptions(), true))
