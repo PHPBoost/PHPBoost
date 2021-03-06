73487d03c901924ed1458630376f6fc48d11d05b
Julien Briswalter <jbriswalter@users.noreply.github.com>
2021-03-06 13:59:24 +0100

    [UPD] VariableTemplateSyntaxElement

--- kernel/framework/io/template/parser/syntax/VariableTemplateSyntaxElement.class.php
+++ kernel/framework/io/template/parser/syntax/VariableTemplateSyntaxElement.class.php
@@ -5,15 +5,16 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2014 12 22
+ * @version     PHPBoost 6.0 - last update: 2021 03 06
  * @since       PHPBoost 3.0 - 2010 07 08
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class VariableTemplateSyntaxElement extends AbstractTemplateSyntaxElement
 {
 	public static function is_element(StringInputStream $input)
 	{
-		return $input->assert_next('\s*(?:@(?:H\|)?)?(?:[a-z0-9A-Z_]\w+\.)*[a-z0-9A-Z_]\w+\s*');
+		return $input->assert_next('\s*(?:@(?:H\|)?)?(?:[a-z0-9A-Z_-]\w+\.)*[a-z0-9A-Z_-]\w+\s*');
 	}
 
 	public function parse(TemplateSyntaxParserContext $context, StringInputStream $input, StringOutputStream $output)
