46e2a02b055c3789721a243148bfa70fc7a3d488
Julien Briswalter <jbriswalter@users.noreply.github.com>
2021-03-06 13:53:01 +0100

    [UPD] LangVarTemplateSyntaxElement

--- kernel/framework/io/template/parser/syntax/LangVarTemplateSyntaxElement.class.php
+++ kernel/framework/io/template/parser/syntax/LangVarTemplateSyntaxElement.class.php
@@ -5,21 +5,22 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2014 12 22
+ * @version     PHPBoost 6.0 - last update: 2021 03 06
  * @since       PHPBoost 3.0 - 2010 10 01
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class LangVarTemplateSyntaxElement extends AbstractTemplateSyntaxElement
 {
     public static function is_element(StringInputStream $input)
     {
-        return $input->assert_next('@(?:H\|)?[a-z0-9A-Z_][\w_.]*');
+        return $input->assert_next('@(?:H\|)?[a-z0-9A-Z_-][\w_.]*');
     }
 
     public function parse(TemplateSyntaxParserContext $context, StringInputStream $input, StringOutputStream $output)
     {
         $matches = array();
-        if ($input->consume_next('@(?P<html>H\|)?(?P<msg>[a-z0-9A-Z_][\w_.]*)', '', $matches))
+        if ($input->consume_next('@(?P<html>H\|)?(?P<msg>[a-z0-9A-Z_-][\w_.]*)', '', $matches))
         {
             $is_html = $matches['html'];
             $msg = $matches['msg'];
