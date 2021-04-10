b4afe3cb18f2f75e78cc1195367fd0da6fab40c4
j1.seth <julien.briswalter@gmail.com>
2021-04-10 08:44:10 +0200

    [FIX] Items configuration

--- kernel/framework/content/item/controllers/DefaultConfigurationController.class.php
+++ kernel/framework/content/item/controllers/DefaultConfigurationController.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 04 09
+ * @version     PHPBoost 6.0 - last update: 2021 04 10
  * @since       PHPBoost 6.0 - 2020 02 11
  * @contributor xela <xela@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -299,7 +299,7 @@ class DefaultConfigurationController extends AbstractAdminItemController
 				{
 					$this->additional_fields_list[] = $parameter;
 					$parameter_get_method = 'get_' . $parameter;
-					if ($configuration_class->hasMethod($parameter_get_method))
+					if (is_string($parameter))
 					{
 						$type = gettype($configuration_class->getMethod('get_default_value')->invoke($this->config, $parameter));
 
