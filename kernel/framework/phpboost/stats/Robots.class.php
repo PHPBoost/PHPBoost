bcca4ade71f4a847701cf0dd1ce3a29086d2e5ae
j1.seth <julien.briswalter@gmail.com>
2021-04-27 06:41:43 +0200

    [UPD] Robots list

--- kernel/framework/phpboost/stats/Robots.class.php
+++ kernel/framework/phpboost/stats/Robots.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 04 15
+ * @version     PHPBoost 6.0 - last update: 2021 04 27
  * @since       PHPBoost 4.0 - 2013 01 06
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -52,6 +52,7 @@ class Robots
 		return array(
 			'360Spider',
 			'Applebot',
+			'AppEngine-Google',
 			'YisouSpider',
 		);
 	}
