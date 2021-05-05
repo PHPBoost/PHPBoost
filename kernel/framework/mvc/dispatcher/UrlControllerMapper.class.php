f2f56090402c9e45c6e471bda333e48e07f818ce
j1.seth <julien.briswalter@gmail.com>
2021-05-05 07:01:17 +0200

    [UPD] ModuleDispatchManager

--- kernel/framework/mvc/dispatcher/UrlControllerMapper.class.php
+++ kernel/framework/mvc/dispatcher/UrlControllerMapper.class.php
@@ -6,15 +6,17 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2016 11 15
+ * @version     PHPBoost 6.0 - last update: 2021 05 05
  * @since       PHPBoost 3.0 - 2009 06 08
  * @contributor mipel <mipel@phpboost.com>
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class UrlControllerMapper extends AbstractUrlMapper
 {
 	private $classname;
 	private $parameters_names;
+	private $module_id;
 
 	/**
 	 * build a new UrlDispatcherItem
@@ -22,12 +24,14 @@ class UrlControllerMapper extends AbstractUrlMapper
 	 * @param string $capture_regex the regular expression matching the url
 	 * and capturing the controller method parameters. By default, match the empty url <code>/</code>
      * @param string $parameters_names the names of the parameters in the capture order
+	 * @param string $module_id id of the current module (optional).
 	 * @throws NoSuchControllerException
 	 */
-	public function __construct($classname, $capture_regex = '`^/?$`u', $parameters_names = array())
+	public function __construct($classname, $capture_regex = '`^/?$`u', $parameters_names = array(), $module_id = '')
 	{
 		$this->classname =& $classname;
 		$this->parameters_names = $parameters_names;
+		$this->module_id = $module_id;
 		parent::__construct($capture_regex);
 	}
 
@@ -59,7 +63,7 @@ class UrlControllerMapper extends AbstractUrlMapper
 
 	private function do_call()
 	{
-		$controller = new $this->classname();
+		$controller = $this->module_id ? new $this->classname($this->module_id) : new $this->classname();
 		if (!($controller instanceof Controller))
 		{
 			throw new NoSuchControllerException($this->classname);
--- kernel/framework/phpboost/module/ModuleDispatchManager.class.php
+++ kernel/framework/phpboost/module/ModuleDispatchManager.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 03 03
+ * @version     PHPBoost 6.0 - last update: 2021 05 05
  * @since       PHPBoost 6.0 - 2020 02 07
 */
 
@@ -14,49 +14,54 @@ class ModuleDispatchManager extends DispatchManager
 	/**
 	 * Redirect the request to the right controller using the url controller mappes list
 	 * @param UrlControllerMapper[] $url_controller_mappers the url controllers mapper list
+	 * @param string $module_id id of the current module. If not set module id is retrieved from running module name in Environment class.
 	 */
-	public static function dispatch($url_controller_mappers = array())
+	public static function dispatch($url_controller_mappers = array(), $module_id = '')
 	{
-		$module_configuration = ModulesManager::get_module(Environment::get_running_module_name())->get_configuration();
+		$module_configuration = ModulesManager::get_module(!empty($module_id) ? $module_id : Environment::get_running_module_name())->get_configuration();
 		
 		if ($module_configuration->has_categories())
 		{
 			//Categories
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesManagementController', '`^/categories/?$`');
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesFormController', '`^/categories/add/?([0-9]+)?/?$`', array('id_parent'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesFormController', '`^/categories/([0-9]+)/edit/?$`', array('id'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultDeleteCategoryController', '`^/categories/([0-9]+)/delete/?$`', array('id'));
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesManagementController', '`^/categories/?$`', array(), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesFormController', '`^/categories/add/?([0-9]+)?/?$`', array('id_parent'), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultCategoriesFormController', '`^/categories/([0-9]+)/edit/?$`', array('id'), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultDeleteCategoryController', '`^/categories/([0-9]+)/delete/?$`', array('id'), $module_id);
 		}
 
 		if ($module_configuration->get_configuration_name())
 		{
 			//Configuration
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultConfigurationController', '`^/admin(?:/config)?/?$`');
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultConfigurationController', '`^/admin(?:/config)?/?$`', array(), $module_id);
 		}
 
 		if ($module_configuration->has_items() || $module_configuration->has_rich_items())
 		{
 			//Items management
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemsManagementController', '`^/manage/?$`');
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultDeleteItemController', '`^/([0-9]+)/delete/?$`', array('id'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemFormController', '`^/add/?([0-9]+)?/?$`', array('id_category'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemFormController', '`^(?:/([0-9]+))/edit/?$`', array('id'));
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemsManagementController', '`^/manage/?$`', array(), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultDeleteItemController', '`^/([0-9]+)/delete/?$`', array('id'), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemFormController', '`^/add/?([0-9]+)?/?$`', array('id_category'), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultItemFormController', '`^(?:/([0-9]+))/edit/?$`', array('id'), $module_id);
 
 			//Item display
 			if ($module_configuration->has_categories())
-				$url_controller_mappers[] = new UrlControllerMapper('DefaultDisplayItemController', '`^(?:/([0-9]+)-([a-z0-9-_]+)/([0-9]+)-([a-z0-9-_]+))/?$`', array('id_category', 'rewrited_name_category', 'id', 'rewrited_title'));
+				$url_controller_mappers[] = new UrlControllerMapper('DefaultDisplayItemController', '`^(?:/([0-9]+)-([a-z0-9-_]+)/([0-9]+)-([a-z0-9-_]+))/?$`', array('id_category', 'rewrited_name_category', 'id', 'rewrited_title'), $module_id);
 			else
-				$url_controller_mappers[] = new UrlControllerMapper('DefaultDisplayItemController', '`^(?:/([0-9]+)-([a-z0-9-_]+))/?$`', array('id', 'rewrited_title'));
+				$url_controller_mappers[] = new UrlControllerMapper('DefaultDisplayItemController', '`^(?:/([0-9]+)-([a-z0-9-_]+))/?$`', array('id', 'rewrited_title'), $module_id);
 			
 			//Items display
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/member/([0-9]+)/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('user_id', 'field', 'sort', 'page'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/pending/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('field', 'sort', 'page'));
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/member/?([0-9]+)?/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('user_id', 'field', 'sort', 'page'), $module_id);
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/pending/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('field', 'sort', 'page'), $module_id);
 			if ($module_configuration->feature_is_enabled('keywords'))
-				$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/tag/([a-z0-9-_]+)/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('tag', 'field', 'sort', 'page'));
+				$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/tag/?([a-z0-9-_]+)/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('tag', 'field', 'sort', 'page'), $module_id);
 			
 			//Categories and home page display
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/([0-9]+)-([a-z0-9-_]+)/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?([0-9]+)?/?$`', array('id_category', 'rewrited_name', 'field', 'sort', 'page', 'subcategories_page'));
-			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/?$`');
+			if ($module_configuration->has_categories())
+				$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/([0-9]+)-([a-z0-9-_]+)/?([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?([0-9]+)?/?$`', array('id_category', 'rewrited_name', 'field', 'sort', 'page', 'subcategories_page'), $module_id);
+			else
+				$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/([a-z_]+)?/?([a-z]+)?/?([0-9]+)?/?$`', array('field', 'sort', 'page'), $module_id);
+
+			$url_controller_mappers[] = new UrlControllerMapper('DefaultSeveralItemsController', '`^/?$`', array(), $module_id);
 		}
 		
 		parent::dispatch($url_controller_mappers);
