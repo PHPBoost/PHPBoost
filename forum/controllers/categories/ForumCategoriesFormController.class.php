ae4277d8c67520069da1870eaf623180267f75fb
babsolune <babsolune@phpboost.com>
2021-05-21 08:55:11 +0200

    [UPD] forum - category form lang

--- forum/controllers/categories/ForumCategoriesFormController.class.php
+++ forum/controllers/categories/ForumCategoriesFormController.class.php
@@ -90,7 +90,7 @@ class ForumCategoriesFormController extends DefaultCategoriesFormController
 
 		$fieldset->add_field(self::get_categories_manager()->get_select_categories_form_field('id_parent', self::$form_lang['form.category'], $this->get_category()->get_id_parent(), $search_category_children_options, array('required' => true, 'hidden' => $this->get_category()->get_type() == ForumCategory::TYPE_CATEGORY)));
 
-		$fieldset->add_field(new FormFieldCheckbox('status', LangLoader::get_message('category.status.locked', 'common', 'forum'), $this->get_category()->get_status(),
+		$fieldset->add_field(new FormFieldCheckbox('status', LangLoader::get_message('forum.category.status.locked', 'common', 'forum'), $this->get_category()->get_status(),
 			array('hidden' => $this->get_category()->get_type() != ForumCategory::TYPE_FORUM)
 		));
 
@@ -104,17 +104,21 @@ class ForumCategoriesFormController extends DefaultCategoriesFormController
 		$root_auth = self::get_categories_manager()->get_categories_cache()->get_category(Category::ROOT_CATEGORY)->get_authorizations();
 
 		$fieldset_authorizations->add_field(new FormFieldCheckbox('special_authorizations', self::$form_lang['form.authorizations'], !$this->get_category()->auth_is_equals($root_auth),
-		array('description' => self::$lang['category.form.authorizations.description'], 'events' => array('click' => '
-		if (HTMLForms.getField("special_authorizations").getValue()) {
-			jQuery("#' . __CLASS__ . '_authorizations").show();
-			if (HTMLForms.getField("type").getValue() == ' . ForumCategory::TYPE_CATEGORY . ' || HTMLForms.getField("type").getValue() == ' . ForumCategory::TYPE_URL . ') {
-				jQuery("#' . __CLASS__ . '_authorizations > div").eq(1).hide();
-				jQuery("#' . __CLASS__ . '_authorizations > div").eq(2).hide();
-			}
-		} else {
-			jQuery("#' . __CLASS__ . '_authorizations").hide();
-		}')
-		)));
+			array(
+				'description' => self::$category_lang['category.form.authorizations.clue'],
+				'events' => array('click' => '
+					if (HTMLForms.getField("special_authorizations").getValue()) {
+						jQuery("#' . __CLASS__ . '_authorizations").show();
+						if (HTMLForms.getField("type").getValue() == ' . ForumCategory::TYPE_CATEGORY . ' || HTMLForms.getField("type").getValue() == ' . ForumCategory::TYPE_URL . ') {
+							jQuery("#' . __CLASS__ . '_authorizations > div").eq(1).hide();
+							jQuery("#' . __CLASS__ . '_authorizations > div").eq(2).hide();
+						}
+					} else {
+						jQuery("#' . __CLASS__ . '_authorizations").hide();
+					}'
+				)
+			)
+		));
 
 		// Autorisations cachées à l'édition Si le type est catégorie ou url
 		$fieldset_authorizations->add_field(new FormFieldFree('hide_authorizations', '', '
