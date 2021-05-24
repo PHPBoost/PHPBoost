6c989e06c52e4d59942713c3ba6d459c57718621
babsolune <babsolune@phpboost.com>
2021-05-24 11:11:58 +0200

    [UPD] faq headblock

--- faq/phpboost/FaqExtensionPointProvider.class.php
+++ faq/phpboost/FaqExtensionPointProvider.class.php
@@ -3,11 +3,12 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 12 27
+ * @version     PHPBoost 6.0 - last update: 2020 05 24
  * @since       PHPBoost 4.0 - 2014 09 02
  * @contributor Kevin MASSY <reidlos@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor xela <xela@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class FaqExtensionPointProvider extends ExtensionPointProvider
