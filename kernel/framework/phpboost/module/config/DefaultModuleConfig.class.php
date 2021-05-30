7c5349168293946fce9ce33ee55f1047631be4e6
Xela <xela@phpboost.com>
2021-05-30 11:55:56 +0200

    [UPD] Docblock

--- kernel/framework/phpboost/module/config/DefaultModuleConfig.class.php
+++ kernel/framework/phpboost/module/config/DefaultModuleConfig.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 03 02
+ * @version     PHPBoost 6.0 - last update: 2021 05 30
  * @since       PHPBoost 6.0 - 2020 01 10
  * @contributor xela <xela@phpboost.com>
 */
