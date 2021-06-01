f9ef396d04bb7c2ccc3c3ad2b398ac29bf814f70
mipel85 <mipel85@phpboost.com>
2021-06-01 18:36:33 +0200

    [UPD] delete public static function in neomodules + last update

--- poll/phpboost/PollConfig.class.php
+++ poll/phpboost/PollConfig.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      xela <xela@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 04 21
+ * @version     PHPBoost 6.0 - last update: 2021 06 01
  * @since       PHPBoost 6.0 - 2020 05 14
 */
 
