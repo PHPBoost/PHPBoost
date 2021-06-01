0be3a5de89d4ce396c6d22df36ae36497112d800
j1.seth <julien.briswalter@gmail.com>
2021-06-01 23:26:16 +0200

    [FIX] PollAuthorizationsService

--- poll/services/PollAuthorizationsService.class.php
+++ poll/services/PollAuthorizationsService.class.php
@@ -3,11 +3,11 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      xela <xela@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 27
+ * @version     PHPBoost 6.0 - last update: 2021 06 01
  * @since       PHPBoost 6.0 - 2020 05 14
 */
 
-class PollAuthorizationsService extends ItemsAuthorizationsService
+class PollAuthorizationsService extends CategoriesAuthorizationsService
 {
 	public function vote()
 	{
