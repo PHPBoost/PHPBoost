71cb72cf86f90e69dbf147e7a5c754f9d4ad2950
mipel85 <mipel85@phpboost.com>
2021-06-01 18:38:16 +0200

    [UPD] delete public static function in neomodules + last update

--- articles/phpboost/ArticlesConfig.class.php
+++ articles/phpboost/ArticlesConfig.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Patrick DUBEAU <daaxwizeman@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2021 04 21
+ * @version     PHPBoost 6.0 - last update: 2021 06 01
  * @since       PHPBoost 4.0 - 2013 02 27
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
