3d0c7b71d31cc42fd51bf14cfac277c4e19268f3
babsolune <babsolune@phpboost.com>
2021-06-05 05:28:14 +0200

    [FIX] dead link message

--- user/controllers/UserContributionSuccessController.class.php
+++ user/controllers/UserContributionSuccessController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 04 17
+ * @version     PHPBoost 6.0 - last update: 2021 06 05
  * @since       PHPBoost 3.0 - 2011 10 07
 */
 
@@ -11,9 +11,9 @@ class UserContributionSuccessController extends UserErrorController
 {
 	public function __construct()
 	{
-		$lang = LangLoader::get('user-lang');
+		$lang = LangLoader::get('contribution-lang');
 
-		parent::__construct($lang['user.contribution.confirmed'], $lang['user.contribution.confirmed.messages'], self::SUCCESS);
+		parent::__construct($lang['contribution.confirmed'], $lang['contribution.confirmed.messages'], self::SUCCESS);
 	}
 }
 ?>
