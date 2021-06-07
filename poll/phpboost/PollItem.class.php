b4de5e3ddfa6917b03665e88128e374616e79fe6
Xela <xela@phpboost.com>
2021-06-07 08:42:12 +0200

    [UPD] Docblock date

--- poll/phpboost/PollItem.class.php
+++ poll/phpboost/PollItem.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      xela <xela@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 05 04
+ * @version     PHPBoost 6.0 - last update: 2021 06 07
  * @since       PHPBoost 6.0 - 2020 05 14
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
