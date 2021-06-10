abc61fd876f119b18a7c8c9cc14032a90265cb04
babsolune <babsolune@phpboost.com>
2021-06-10 06:23:48 +0200

    [UPD] lang correction

--- lang/english/addon-lang.php
+++ lang/english/addon-lang.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 08
+ * @version     PHPBoost 6.0 - last update: 2021 06 09
  * @since       PHPBoost 3.0 - 2012 04 12
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -23,8 +23,8 @@ $lang['addon.multiple.uninstall']  = 'Uninstall selection';
 $lang['addon.multiple.enable']     = 'Enable selection';
 $lang['addon.multiple.disable']    = 'Disable selection';
 $lang['addon.multiple.upgrade']    = 'Upgrade selection';
-$lang['addon.install']             = 'Installer';
-$lang['addon.uninstall']           = 'Uninstaller';
+$lang['addon.install']             = 'Install';
+$lang['addon.uninstall']           = 'Uninstall';
 $lang['addon.compatible']          = 'Compatible';
 $lang['addon.not.compatible']      = 'Incompatible';
 $lang['addon.compatibility']       = 'Compatibilité';
