f5aa441d0a5fb1df1c2c2b7cd923cb06a5dd32f3
babsolune <babsolune@phpboost.com>
2021-06-11 06:52:55 +0200

    [UPD] neoitem alt2

--- lang/french/item-lang.php
+++ lang/french/item-lang.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 10
+ * @version     PHPBoost 6.0 - last update: 2021 06 11
  * @since       PHPBoost 6.0 - 2020 01 08
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
  * @contributor xela <xela@phpboost.com>
@@ -29,6 +29,7 @@ $lang['items.number'] = 'Nombre de :items';
 $lang['items.number.alt2'] = 'Nombre d\':items';
 $lang['completed.item'] = 'Ce :item est terminé';
 $lang['completed.item.alt'] = 'Cette :item est terminée';
+$lang['completed.item.alt2'] = 'Cet :item est terminée';
 $lang['items.no.element'] = 'Aucun :item n\'est disponible pour le moment';
 $lang['items.no.element.alt'] = 'Aucune :item n\'est disponible pour le moment';
 
