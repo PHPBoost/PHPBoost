b700f800f61dfd5057e5d5a0e666b93aad0c44c2
babsolune <babsolune@phpboost.com>
2021-06-16 09:54:35 +0200

    [FIX] admin themes - lang

--- admin/themes/controllers/AdminThemesInstalledListController.class.php
+++ admin/themes/controllers/AdminThemesInstalledListController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 06
+ * @version     PHPBoost 6.0 - last update: 2021 06 16
  * @since       PHPBoost 3.0 - 2011 04 20
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -151,7 +151,7 @@ class AdminThemesInstalledListController extends AdminController
 				}
 				$theme_number++;
 			}
-			AppContext::get_response()->redirect(AdminThemeUrlBuilder::list_installed_theme(), LangLoader::get_message('warning.warning.process.success', 'warning-lang'));
+			AppContext::get_response()->redirect(AdminThemeUrlBuilder::list_installed_theme(), LangLoader::get_message('warning.process.success', 'warning-lang'));
 		}
 		else
 		{
@@ -166,7 +166,7 @@ class AdminThemesInstalledListController extends AdminController
 					$user_accounts_config->set_default_theme($theme->get_id());
 					UserAccountsConfig::save();
 
-					AppContext::get_response()->redirect(AdminThemeUrlBuilder::list_installed_theme(), LangLoader::get_message('warning.warning.process.success', 'warning-lang'));
+					AppContext::get_response()->redirect(AdminThemeUrlBuilder::list_installed_theme(), LangLoader::get_message('warning.process.success', 'warning-lang'));
 				}
 				else if ($request->get_string('delete-' . $theme->get_id(), ''))
 				{
