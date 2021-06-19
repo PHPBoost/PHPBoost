d7b359fd29bed4a4f365987088507d4bbfe35ff7
Xela <xela@phpboost.com>
2021-06-19 23:34:27 +0200

    [FIX] TextHelper - to_js_string method

--- kernel/framework/helper/TextHelper.class.php
+++ kernel/framework/helper/TextHelper.class.php
@@ -5,11 +5,12 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2018 11 09
+ * @version     PHPBoost 6.0 - last update: 2021 06 19
  * @since       PHPBoost 3.0 - 2010 01 24
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
+ * @contributor xela <xela@phpboost.com>
 */
 
 class TextHelper
@@ -112,7 +113,7 @@ class TextHelper
 	public static function to_js_string($string, $add_quotes = true)
 	{
 		$bounds = $add_quotes ? '\'' : '';
-		return $bounds . str_replace(array("\r\n", "\r", "\n", '"'), array('\n', '\n', '\n', '&quot;'), addcslashes($string, '\'')) . $bounds;
+		return $bounds . str_replace(array("\r\n", "\r", "\n", '"', '\\\\\''), array('\n', '\n', '\n', '&quot;', '\\\''), addcslashes($string, '\'')) . $bounds;
 	}
 
 	/**
