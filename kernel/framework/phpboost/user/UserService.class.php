63b278aeaa05bb38ba419d728f37622f5853967c
elenwii <elenwii@phpboost.com>
2021-06-23 23:27:22 +0200

    [FIX] lang variable

--- kernel/framework/phpboost/user/UserService.class.php
+++ kernel/framework/phpboost/user/UserService.class.php
@@ -6,7 +6,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 21
+ * @version     PHPBoost 6.0 - last update: 2021 06 23
  * @since       PHPBoost 3.0 - 2012 03 31
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -249,7 +249,7 @@ class UserService
 				return $lang['user.robot'];
 			break;
 			case User::VISITOR_LEVEL:
-				return $lang['user.visitor'];
+				return $lang['user.guest'];
 			break;
 			case User::MEMBER_LEVEL:
 				return $lang['user.member'];
