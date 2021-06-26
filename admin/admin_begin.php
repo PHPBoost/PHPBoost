6e36792c517f32313150d621e6fe4d88de657b9a
babsolune <babsolune@phpboost.com>
2021-06-26 09:31:26 +0200

    [FIX] admin lang

--- admin/admin_begin.php
+++ admin/admin_begin.php
@@ -3,18 +3,17 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2016 10 28
+ * @version     PHPBoost 6.0 - last update: 2021 06 26
  * @since       PHPBoost 1.2 - 2005 06 20
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 if (!defined('PATH_TO_ROOT'))
 	define('PATH_TO_ROOT', '..');
 
 require_once(PATH_TO_ROOT . '/kernel/begin.php');
-
-require_once(PATH_TO_ROOT . '/lang/' . AppContext::get_current_user()->get_locale() . '/admin.php');
 
 if (!AppContext::get_current_user()->is_admin())
 {
