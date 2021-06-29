b152bef5518aadae9506c37171d350f4a7f9be8d
babsolune <babsolune@phpboost.com>
2021-06-29 17:40:09 +0200

    [UPD] lang - optimization

--- lang/english/languages.php
+++ lang/english/languages.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 11 24
+ * @version     PHPBoost 6.0 - last update: 2021 06 29
  * @since       PHPBoost 5.2 - 2019 11 24
 */
 
@@ -11,58 +11,57 @@
 #                     English                      #
 ####################################################
 $lang = array(
-    'en'  => 'English',
-
-    'de'  => 'German',
     'ar'  => 'Arabian',
-    'hy'  => 'Armenian',
     'az'  => 'Azeri',
-    'eu'  => 'Basque',
     'be'  => 'Belarusian',
     'bu'  => 'Bulgarian',
     'ca'  => 'Catalan',
+    'ce'  => 'Chechen',
+    'cs'  => 'Czech',
     'ceb' => 'Cebuano',
-    'zh'  => 'Chinese',
-    'hr'  => 'Croatian',
     'da'  => 'Danish',
-    'es'  => 'Spanish',
+    'de'  => 'German',
+    'el'  => 'Greek',
+    'en'  => 'English',
     'eo'  => 'Esperanto',
+    'es'  => 'Spanish',
     'et'  => 'Estonian',
+    'eu'  => 'Basque',
+    'fa'  => 'Persian',
     'fi'  => 'Finnish',
     'fr'  => 'French',
     'gl'  => 'Galician',
-    'ka'  => 'Georgian',
-    'el'  => 'Greek',
-    'nl'  => 'Dutch',
     'he'  => 'Hebrew',
     'hi'  => 'Hindi',
+    'hr'  => 'Croatian',
     'hu'  => 'Hungarian',
+    'hy'  => 'Armenian',
     'id'  => 'Indonesian',
     'it'  => 'Italian',
     'ja'  => 'Japanese',
+    'ka'  => 'Georgian',
     'kk'  => 'Kazakh',
     'ko'  => 'Korean',
     'lt'  => 'Lithuanian',
     'ms'  => 'Malay',
-    'no'  => 'Norwegian (no)',
+    'nl'  => 'Dutch',
     'nn'  => 'Norwegian (nn)',
-    'ur'  => 'Urdu',
-    'fa'  => 'Persian',
+    'no'  => 'Norwegian (no)',
     'pl'  => 'Polish',
     'pt'  => 'Portuguese',
     'ro'  => 'Romanian',
     'ru'  => 'Russian',
-    'sr'  => 'Serbian',
     'sh'  => 'Serbo-Croatian',
     'sk'  => 'Slovak',
     'sl'  => 'Slovenian',
+    'sr'  => 'Serbian',
     'sv'  => 'Swedish',
-    'cs'  => 'Czech',
-    'ce'  => 'Chechen',
     'th'  => 'Thai',
     'tr'  => 'Turkish',
     'uk'  => 'Ukrainian',
+    'ur'  => 'Urdu',
     'vi'  => 'Vietnamian',
-    'war' => 'Winaray'
+    'war' => 'Winaray',
+    'zh'  => 'Chinese',
 );
 ?>
--- lang/french/languages.php
+++ lang/french/languages.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 11 24
+ * @version     PHPBoost 6.0 - last update: 2021 06 29
  * @since       PHPBoost 5.2 - 2019 11 24
 */
 
@@ -11,58 +11,57 @@
 #                     French                       #
 ####################################################
 $lang = array(
-    'fr'  => 'Français',
-
-    'de'  => 'Allemand',
-    'en'  => 'Anglais',
     'ar'  => 'Arabe',
-    'hy'  => 'Arménien',
     'az'  => 'Azéri',
-    'eu'  => 'Basque',
     'be'  => 'Bélarusse',
     'bu'  => 'Bulgare',
     'ca'  => 'Catalan',
+    'ce'  => 'Tchétchène',
+    'cs'  => 'Tchèque',
     'ceb' => 'Cebuano',
-    'zh'  => 'Chinois',
-    'hr'  => 'Croate',
     'da'  => 'Danois',
-    'es'  => 'Espagnol',
-    'eo'  => 'Espéranto',
-    'et'  => 'Estonien',
-    'fi'  => 'Finlandais',
-    'gl'  => 'Galicien',
-    'ka'  => 'Géorgien',
+    'de'  => 'Allemand',
     'el'  => 'Grec',
-    'nl'  => 'Hollandais',
+    'en'  => 'Anglais',
+    'eo'  => 'Espéranto',
+    'es'  => 'Espagnol',
+    'et'  => 'Estonien',
+    'eu'  => 'Basque',
+    'fa'  => 'Persan',
+    'fi'  => 'Finlandais',
+    'fr'  => 'Français',
+    'gl'  => 'Galicien',
     'he'  => 'Hebreu',
     'hi'  => 'Hindi',
+    'hr'  => 'Croate',
     'hu'  => 'Hongrois',
+    'hy'  => 'Arménien',
     'id'  => 'Indonésien',
     'it'  => 'Italien',
     'ja'  => 'Japonais',
+    'ka'  => 'Géorgien',
     'kk'  => 'Kazakh',
     'ko'  => 'Koréen',
     'lt'  => 'Lituanien',
     'ms'  => 'Malais',
-    'no'  => 'Norvegien (no)',
+    'nl'  => 'Hollandais',
     'nn'  => 'Norvegien (nn)',
-    'ur'  => 'Ourdou',
-    'fa'  => 'Persan',
+    'no'  => 'Norvegien (no)',
     'pl'  => 'Polonais',
     'pt'  => 'Portugais',
     'ro'  => 'Roumain',
     'ru'  => 'Russe',
-    'sr'  => 'Serbe',
     'sh'  => 'Serbo-Croate',
     'sk'  => 'Slovaque',
     'sl'  => 'Slovène',
+    'sr'  => 'Serbe',
     'sv'  => 'Suédois',
-    'cs'  => 'Tchèque',
-    'ce'  => 'Tchétchène',
     'th'  => 'Thai',
     'tr'  => 'Turc',
     'uk'  => 'Ukrainien',
+    'ur'  => 'Ourdou',
     'vi'  => 'Vietnamien',
-    'war' => 'Winaray'
+    'war' => 'Winaray',
+    'zh'  => 'Chinois',
 );
 ?>
