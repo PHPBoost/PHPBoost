a8f9ae7a7c3c9db6eafbbbca6ba046fe07480360
babsolune <babsolune@phpboost.com>
2021-07-02 07:43:58 +0200

    [UPD] search - delete old files

---
+++ update/services/modules/SearchModuleUpdateVersion.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2021 07 02
+*/
+#################################################*/
+
+class SearchModuleUpdateVersion extends ModuleUpdateVersion
+{
+	public function __construct()
+	{
+		parent::__construct('search');
+
+		self::$delete_old_files_list = array(
+			'/lang/english/search_english.php',
+			'/lang/french/search_french.php'
+		);
+	}
+}
+?>
