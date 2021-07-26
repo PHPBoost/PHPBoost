59d7bcbcd4b4df78fdd5d4e6ae933aaa74f59805
babsolune <babsolune@phpboost.com>
2021-07-26 18:13:58 +0200

    [UPD] offload

--- news/lang/english/install.php
+++ news/lang/english/install.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 17
+ * @version     PHPBoost 6.0 - last update: 2021 07 26
  * @since       PHPBoost 4.0 - 2012 04 09
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor janus57 <janus57@janus57.fr>
@@ -39,10 +39,10 @@ $lang['items'][] = array(
     <ul class="formatter-ul">
         <li class="formatter-li"><a class="offload" href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">Put your website under maintenance</a> and you won\'t be disturbed while you\'re working on it.
         </li><li class="formatter-li">Now\'s the time to setup the <a class="offload" href="' . AdminConfigUrlBuilder::general_config()->relative() . '">main configurations</a> of the website.
-        </li><li class="formatter-li"><a class="offload" href="' . AdminModulesUrlBuilder::list_installed_modules()->relative() . '">Configure the installed modules</a> and give them access rights (If you have not installed the complete package, all modules are available on the <a href="https://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
+        </li><li class="formatter-li"><a class="offload" href="' . AdminModulesUrlBuilder::list_installed_modules()->relative() . '">Configure the installed modules</a> and give them access rights (If you have not installed the complete package, all modules are available on the <a class="offload" href="https://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
         </li><li class="formatter-li"><a class="offload" href="' . AdminContentUrlBuilder::content_configuration()->relative() . '">Choose the default content language formatting</a>.
         </li><li class="formatter-li"><a class="offload" href="' . AdminMembersUrlBuilder::configuration()->relative() . '">Configure the members settings</a>.
-        </li><li class="formatter-li"><a class="offload" href="' . AdminThemeUrlBuilder::list_installed_theme()->relative() . '">Choose the website style</a> to change the look of your site (You can find more styles on the <a href="https://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
+        </li><li class="formatter-li"><a class="offload" href="' . AdminThemeUrlBuilder::list_installed_theme()->relative() . '">Choose the website style</a> to change the look of your site (You can find more styles on the <a class="offload" href="https://www.phpboost.com/download/">PHPBoost website</a> in the resources section.
         </li><li class="formatter-li">Before giving back access to your members, take time to add content to your website!
         </li><li class="formatter-li">Finally, Finally, <a class="offload" href="' . AdminMaintainUrlBuilder::maintain()->relative() . '">put your site online</a> in order to restore access to your site to your visitors.<br />
         </li>
