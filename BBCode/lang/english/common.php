c4695da50dde48f20b82d109f7c77cdbf45ac02d
babsolune <babsolune@phpboost.com>
2021-09-06 12:05:35 +0200

    [UPD] BBCode - lang optimizations

--- BBCode/lang/english/common.php
+++ BBCode/lang/english/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 07 05
+ * @version     PHPBoost 6.0 - last update: 2021 09 06
  * @since       PHPBoost 4.1 - 2014 08 12
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -14,132 +14,132 @@
 #                     English                      #
 ####################################################
 
-$lang['bbcode.tags.add'] = 'Insert';
-$lang['bbcode.tags.choice'] = 'Select';
+$lang['bbcode.tags.add']     = 'Insert';
+$lang['bbcode.tags.choice']  = 'Select';
 $lang['bbcode.tags.options'] = 'Add options to the tag';
 $lang['bbcode.preview.text'] = 'Text preview';
-$lang['bbcode.class'] = 'Classes';
-$lang['bbcode.style'] = 'Style';
+$lang['bbcode.class']        = 'Classes';
+$lang['bbcode.style']        = 'Style';
 
 // Text formatting
-$lang['bbcode.bold'] = 'Bold';
-$lang['bbcode.italic'] = 'Italic';
+$lang['bbcode.bold']      = 'Bold';
+$lang['bbcode.italic']    = 'Italic';
 $lang['bbcode.underline'] = 'Underlined';
-$lang['bbcode.strike'] = 'Strike';
-$lang['bbcode.color'] = 'Color';
-$lang['bbcode.bgcolor'] = 'Background color';
+$lang['bbcode.strike']    = 'Strike';
+$lang['bbcode.color']     = 'Color';
+$lang['bbcode.bgcolor']   = 'Background color';
 
-$lang['bbcode.size'] = 'Size';
+$lang['bbcode.size']        = 'Size';
 $lang['bbcode.size.picker'] = 'Define text size (px)';
-$lang['bbcode.font'] = 'Font';
+$lang['bbcode.font']        = 'Font';
 
-$lang['bbcode.align'] = 'Align';
-$lang['bbcode.left'] = 'Align left';
-$lang['bbcode.center'] = 'Align centrer';
-$lang['bbcode.right'] = 'Align right';
+$lang['bbcode.align']   = 'Align';
+$lang['bbcode.left']    = 'Align left';
+$lang['bbcode.center']  = 'Align centrer';
+$lang['bbcode.right']   = 'Align right';
 $lang['bbcode.justify'] = 'Justify';
 
-$lang['bbcode.positions'] = 'Positioning';
-$lang['bbcode.float.left'] = 'Float left';
+$lang['bbcode.positions']   = 'Positioning';
+$lang['bbcode.float.left']  = 'Float left';
 $lang['bbcode.float.right'] = 'Float right';
-$lang['bbcode.indent'] = 'Indentation';
-$lang['bbcode.sup'] = 'Sup';
-$lang['bbcode.sub'] = 'Sub';
+$lang['bbcode.indent']      = 'Indentation';
+$lang['bbcode.sup']         = 'Sup';
+$lang['bbcode.sub']         = 'Sub';
 
 // HTML formatting
-$lang['bbcode.title'] = 'Titration';
+$lang['bbcode.title']       = 'Titration';
 $lang['bbcode.title.label'] = 'Title';
 
-$lang['bbcode.list'] = 'List';
+$lang['bbcode.list']         = 'List';
 $lang['bbcode.list.ordered'] = 'Ordered list';
 
-$lang['bbcode.table'] = 'Table';
-$lang['bbcode.head.table'] = 'Table header';
+$lang['bbcode.table']        = 'Table';
+$lang['bbcode.head.table']   = 'Table header';
 $lang['bbcode.insert.table'] = 'Insert table';
-$lang['bbcode.lines'] = 'Number of rows';
-$lang['bbcode.cols'] = 'Number of columns';
+$lang['bbcode.lines']        = 'Number of rows';
+$lang['bbcode.cols']         = 'Number of columns';
 
-$lang['bbcode.container'] = 'Container';
-$lang['bbcode.paragraph'] = 'Paragraph';
-$lang['bbcode.block'] = 'Block';
-$lang['bbcode.custom.div'] = 'Customizable container';
+$lang['bbcode.container']        = 'Container';
+$lang['bbcode.paragraph']        = 'Paragraph';
+$lang['bbcode.block']            = 'Block';
+$lang['bbcode.custom.div']       = 'Customizable container';
 $lang['bbcode.custom.div.alert'] = 'You have to know about html/css languages and/or properties of the template.';
-$lang['bbcode.custom.div.id'] = 'Identifier';
-$lang['bbcode.fieldset'] = 'Fieldset';
-$lang['bbcode.fieldset.legend'] = 'Legend of fieldset';
-$lang['bbcode.paragraph.title'] = 'Paragraph : [p]text[/p]';
-$lang['bbcode.abbr'] = 'Abbreviation';
-$lang['bbcode.abbr.label'] = 'Full name';
+$lang['bbcode.custom.div.id']    = 'Identifier';
+$lang['bbcode.fieldset']         = 'Fieldset';
+$lang['bbcode.fieldset.legend']  = 'Legend of fieldset';
+$lang['bbcode.paragraph.title']  = 'Paragraph : [p]text[/p]';
+$lang['bbcode.abbr']             = 'Abbreviation';
+$lang['bbcode.abbr.label']       = 'Full name';
 
-$lang['bbcode.quote'] = 'Quote';
+$lang['bbcode.quote']        = 'Quote';
 $lang['bbcode.quote.author'] = 'Quote author';
 
-$lang['bbcode.hide'] = 'Hidden content';
-$lang['bbcode.hide.all'] = 'Hide content and show it on click';
-$lang['bbcode.hide.member'] = 'Hide content to visitors';
-$lang['bbcode.hide.teaser'] = 'Hide end of content to visitors';
-$lang['bbcode.hide.moderator'] = 'Hide content to members';
+$lang['bbcode.hide']           = 'Hidden content';
+$lang['bbcode.hide.all']       = 'Hiding content and showing it on click';
+$lang['bbcode.hide.member']    = 'Hiding content to visitors';
+$lang['bbcode.hide.teaser']    = 'Hiding end of content to visitors';
+$lang['bbcode.hide.moderator'] = 'Hiding content to members';
 
 $lang['bbcode.style'] = 'Style [style=x]text[/style]';
 
 // Links
-$lang['bbcode.link'] = 'Links';
-$lang['bbcode.link.url'] = 'Link address';
+$lang['bbcode.link']       = 'Links';
+$lang['bbcode.link.url']   = 'Link address';
 $lang['bbcode.link.label'] = 'Link label';
 
-$lang['bbcode.mail'] = 'Mail address';
+$lang['bbcode.mail']       = 'Mail address';
 $lang['bbcode.mail.label'] = 'Mail label';
 
-$lang['bbcode.wikipedia'] = 'Wikipedia link';
-$lang['bbcode.wikipedia.word'] = 'Word';
-$lang['bbcode.wikipedia.add.lang'] = 'Define a language in the link';
+$lang['bbcode.wikipedia']               = 'Wikipedia link';
+$lang['bbcode.wikipedia.word']          = 'Word';
+$lang['bbcode.wikipedia.add.lang']      = 'Define a language in the link';
 $lang['bbcode.wikipedia.add.lang.clue'] = 'If not <strong>en</strong> is automaticaly added';
-$lang['bbcode.wikipedia.lang'] = 'Language';
+$lang['bbcode.wikipedia.lang']          = 'Language';
 
-$lang['bbcode.feed'] = 'RSS flux';
+$lang['bbcode.feed']        = 'RSS flux';
 $lang['bbcode.feed.module'] = 'Module name';
 $lang['bbcode.feed.select'] = 'Pick a module';
 $lang['bbcode.feed.number'] = 'Items number to display';
 
-$lang['bbcode.anchor'] = 'Anchor somewhere in the page [anchor=x]text[/anchor]';
+$lang['bbcode.anchor']      = 'Anchor somewhere in the page [anchor =x]text[/anchor]';
 $lang['bbcode.anchor.name'] = 'Anchor name (format : anchor-name)?';
-$lang['bbcode.anchor.url'] = 'Add the link to the anchor';
+$lang['bbcode.anchor.url']  = 'Add the link to the anchor';
 
-$lang['bbcode.sound'] = 'Sound';
+$lang['bbcode.sound']     = 'Sound';
 $lang['bbcode.sound.url'] = 'Audio file url';
 
-$lang['bbcode.movie'] = 'Video';
-$lang['bbcode.movie.url'] = 'Video file url';
+$lang['bbcode.movie']        = 'Video';
+$lang['bbcode.movie.url']    = 'Video file url';
 $lang['bbcode.movie.format'] = 'You can also add hosted video url from Youtube, Dailymotion or Vimeo';
-$lang['bbcode.movie.width'] = 'Video width';
+$lang['bbcode.movie.width']  = 'Video width';
 $lang['bbcode.movie.height'] = 'Video height';
 $lang['bbcode.movie.poster'] = 'Add thumbnail/poster';
 
-$lang['bbcode.lightbox'] = 'Add a picture with lightbox effect';
-$lang['bbcode.picture.url'] = 'Picture url';
+$lang['bbcode.lightbox']        = 'Add a picture with lightbox effect';
+$lang['bbcode.picture.url']     = 'Picture url';
 $lang['bbcode.thumbnail.width'] = 'Max width of the thumbnail (px)';
 
-$lang['bbcode.picture.width'] = 'Max width of the picture (px)';
-$lang['bbcode.figure'] = 'Add a picture with description';
-$lang['bbcode.picture.alt'] = 'Alternative text';
+$lang['bbcode.picture.width']  = 'Max width of the picture (px)';
+$lang['bbcode.figure']         = 'Add a picture with description';
+$lang['bbcode.picture.alt']    = 'Alternative text';
 $lang['bbcode.figure.caption'] = 'Picture description';
 
 $lang['bbcode.upload'] = 'Attach files';
 
-$lang['bbcode.smileys'] = 'Smileys';
-$lang['bbcode.fa'] = 'Font Awesome Icon';
-$lang['bbcode.fa.tag'] = '[fa=fa-style,fa-option]codename (without "fa-")[/fa]';
-$lang['bbcode.code'] = 'Code';
-$lang['bbcode.code.custom.name'] = 'Path to a file';
-$lang['bbcode.code.name'] = 'Language';
-$lang['bbcode.code.line'] = 'Display code on single line';
-$lang['bbcode.text'] = 'Text';
+$lang['bbcode.smileys']            = 'Smileys';
+$lang['bbcode.fa']                 = 'Font Awesome Icon';
+$lang['bbcode.fa.tag']             = '[fa=fa-style,fa-option]codename (without "fa-")[/fa]';
+$lang['bbcode.code']               = 'Code';
+$lang['bbcode.code.custom.name']   = 'Path to a file';
+$lang['bbcode.code.name']          = 'Language';
+$lang['bbcode.code.line']          = 'Display code on single line';
+$lang['bbcode.text']               = 'Text';
 $lang['bbcode.phpboost.languages'] = 'PHPBoost';
-$lang['bbcode.web'] = 'Web';
-$lang['bbcode.script'] = 'Script';
-$lang['bbcode.prog'] = 'Programming';
-$lang['bbcode.math'] = 'Insert mathematics code';
-$lang['bbcode.html'] = 'Html code';
+$lang['bbcode.web']                = 'Web';
+$lang['bbcode.script']             = 'Script';
+$lang['bbcode.prog']               = 'Programming';
+$lang['bbcode.math']               = 'Insert mathematics code';
+$lang['bbcode.html']               = 'Html code';
 
 $lang['bbcode.help'] = 'BBcode help';
 ?>
--- BBCode/lang/french/common.php
+++ BBCode/lang/french/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 07 05
+ * @version     PHPBoost 6.0 - last update: 2021 09 06
  * @since       PHPBoost 4.1 - 2014 08 12
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -15,132 +15,132 @@
 #                     French                       #
 ####################################################
 
-$lang['bbcode.tags.add'] = 'Insérer';
-$lang['bbcode.tags.choice'] = 'Choisir';
+$lang['bbcode.tags.add']     = 'Insérer';
+$lang['bbcode.tags.choice']  = 'Choisir';
 $lang['bbcode.tags.options'] = 'Ajouter des options à la balise';
 $lang['bbcode.preview.text'] = 'Aperçu du texte';
-$lang['bbcode.class'] = 'Classes';
-$lang['bbcode.style'] = 'Style';
+$lang['bbcode.class']        = 'Classes';
+$lang['bbcode.style']        = 'Style';
 
 // Text formatting
-$lang['bbcode.bold'] = 'Texte en gras';
-$lang['bbcode.italic'] = 'Texte en italique';
+$lang['bbcode.bold']      = 'Texte en gras';
+$lang['bbcode.italic']    = 'Texte en italique';
 $lang['bbcode.underline'] = 'Texte souligné';
-$lang['bbcode.strike'] = 'Texte barré';
-$lang['bbcode.color'] = 'Couleur du texte';
-$lang['bbcode.bgcolor'] = 'Couleur du fond';
+$lang['bbcode.strike']    = 'Texte barré';
+$lang['bbcode.color']     = 'Couleur du texte';
+$lang['bbcode.bgcolor']   = 'Couleur du fond';
 
-$lang['bbcode.size'] = 'Taille du texte';
+$lang['bbcode.size']        = 'Taille du texte';
 $lang['bbcode.size.picker'] = 'Choisir la taille du texte (px)';
-$lang['bbcode.font'] = 'Police d\'écriture';
+$lang['bbcode.font']        = 'Police d\'écriture';
 
-$lang['bbcode.align'] = 'Alignement';
-$lang['bbcode.left'] = 'Aligner à gauche';
-$lang['bbcode.center'] = 'Centrer';
-$lang['bbcode.right'] = 'Aligner à droite';
+$lang['bbcode.align']   = 'Alignement';
+$lang['bbcode.left']    = 'Aligner à gauche';
+$lang['bbcode.center']  = 'Centrer';
+$lang['bbcode.right']   = 'Aligner à droite';
 $lang['bbcode.justify'] = 'Justifier';
 
-$lang['bbcode.positions'] = 'Positionnement';
-$lang['bbcode.float.left'] = 'Flottant à gauche';
+$lang['bbcode.positions']   = 'Positionnement';
+$lang['bbcode.float.left']  = 'Flottant à gauche';
 $lang['bbcode.float.right'] = 'Flottant à droite';
-$lang['bbcode.indent'] = 'Indentation';
-$lang['bbcode.sup'] = 'Exposant';
-$lang['bbcode.sub'] = 'Indice';
+$lang['bbcode.indent']      = 'Indentation';
+$lang['bbcode.sup']         = 'Exposant';
+$lang['bbcode.sub']         = 'Indice';
 
 // HTML formatting
-$lang['bbcode.title'] = 'Titrage';
+$lang['bbcode.title']       = 'Titrage';
 $lang['bbcode.title.label'] = 'Titre';
 
-$lang['bbcode.list'] = 'Liste';
+$lang['bbcode.list']         = 'Liste';
 $lang['bbcode.list.ordered'] = 'Liste ordonnée';
 
-$lang['bbcode.table'] = 'Tableau';
-$lang['bbcode.head.table'] = 'Entête';
+$lang['bbcode.table']        = 'Tableau';
+$lang['bbcode.head.table']   = 'Entête';
 $lang['bbcode.insert.table'] = 'Insérer le tableau';
-$lang['bbcode.lines'] = 'Nombre de lignes';
-$lang['bbcode.cols'] = 'Nombre de colonnes';
+$lang['bbcode.lines']        = 'Nombre de lignes';
+$lang['bbcode.cols']         = 'Nombre de colonnes';
 
-$lang['bbcode.container'] = 'Conteneur';
-$lang['bbcode.paragraph'] = 'Paragraphe';
-$lang['bbcode.block'] = 'Bloc';
-$lang['bbcode.custom.div'] = 'Conteneur personnalisable';
+$lang['bbcode.container']        = 'Conteneur';
+$lang['bbcode.paragraph']        = 'Paragraphe';
+$lang['bbcode.block']            = 'Bloc';
+$lang['bbcode.custom.div']       = 'Conteneur personnalisable';
 $lang['bbcode.custom.div.alert'] = 'Ces critères nécessitent la connaissance des langages HTML/CSS et les propriétés du thème utilisé.';
-$lang['bbcode.custom.div.id'] = 'Identifiant';
-$lang['bbcode.fieldset'] = 'Bloc champs';
-$lang['bbcode.fieldset.legend'] = 'Titre du bloc champs';
-$lang['bbcode.abbr'] = 'Abréviation';
-$lang['bbcode.abbr.label'] = 'Nom complet';
+$lang['bbcode.custom.div.id']    = 'Identifiant';
+$lang['bbcode.fieldset']         = 'Bloc champs';
+$lang['bbcode.fieldset.legend']  = 'Titre du bloc champs';
+$lang['bbcode.abbr']             = 'Abréviation';
+$lang['bbcode.abbr.label']       = 'Nom complet';
 
-$lang['bbcode.quote'] = 'Citation';
+$lang['bbcode.quote']        = 'Citation';
 $lang['bbcode.quote.author'] = 'Auteur de la citation';
 
-$lang['bbcode.hide'] = 'Contenu Caché';
-$lang['bbcode.moderator'] = 'Contenu réservé aux modérateurs';
-$lang['bbcode.hide.all'] = 'Cache le contenu et l\'affiche au clic';
-$lang['bbcode.hide.member'] = 'Cache le contenu aux visiteurs';
-$lang['bbcode.hide.teaser'] = 'Cache la fin du contenu aux visiteurs';
-$lang['bbcode.hide.moderator'] = 'Cache le contenu aux membres';
+$lang['bbcode.hide']           = 'Contenu Caché';
+$lang['bbcode.moderator']      = 'Contenu réservé aux modérateurs';
+$lang['bbcode.hide.all']       = 'Cacher le contenu et l\'afficher au clic';
+$lang['bbcode.hide.member']    = 'Cacher le contenu aux visiteurs';
+$lang['bbcode.hide.teaser']    = 'Cacher la fin du contenu aux visiteurs';
+$lang['bbcode.hide.moderator'] = 'Cacher le contenu aux membres';
 
 $lang['bbcode.style'] = 'Style';
 
 // Links
-$lang['bbcode.link'] = 'Lien';
-$lang['bbcode.link.url'] = 'Adresse du lien';
+$lang['bbcode.link']       = 'Lien';
+$lang['bbcode.link.url']   = 'Adresse du lien';
 $lang['bbcode.link.label'] = 'Texte du lien';
 
-$lang['bbcode.mail'] = 'Adresse email';
+$lang['bbcode.mail']       = 'Adresse email';
 $lang['bbcode.mail.label'] = 'Texte de la balise';
 
-$lang['bbcode.wikipedia'] = 'Lien wikipedia';
-$lang['bbcode.wikipedia.word'] = 'Mot';
-$lang['bbcode.wikipedia.add.lang'] = 'Définir une langue dans le lien';
+$lang['bbcode.wikipedia']               = 'Lien wikipedia';
+$lang['bbcode.wikipedia.word']          = 'Mot';
+$lang['bbcode.wikipedia.add.lang']      = 'Définir une langue dans le lien';
 $lang['bbcode.wikipedia.add.lang.clue'] = 'Sinon <strong>fr</strong> est ajouté automatiquement';
-$lang['bbcode.wikipedia.lang'] = 'Langue';
+$lang['bbcode.wikipedia.lang']          = 'Langue';
 
-$lang['bbcode.feed'] = 'Flux RSS';
+$lang['bbcode.feed']        = 'Flux RSS';
 $lang['bbcode.feed.module'] = 'Nom du module';
 $lang['bbcode.feed.select'] = 'Choisir un module';
 $lang['bbcode.feed.number'] = 'Nombre d\'éléments à afficher';
 
-$lang['bbcode.anchor'] = 'Ancre vers un endroit de la page';
+$lang['bbcode.anchor']      = 'Ancre vers un endroit de la page';
 $lang['bbcode.anchor.name'] = 'Nom de l\'ancre';
-$lang['bbcode.anchor.url'] = 'Ajouter le lien vers l\'ancre';
+$lang['bbcode.anchor.url']  = 'Ajouter le lien vers l\'ancre';
 
-$lang['bbcode.sound'] = 'Son';
+$lang['bbcode.sound']     = 'Son';
 $lang['bbcode.sound.url'] = 'Adresse du fichier audio';
 
-$lang['bbcode.movie'] = 'Vidéo';
-$lang['bbcode.movie.url'] = 'Adresse de la vidéo';
+$lang['bbcode.movie']        = 'Vidéo';
+$lang['bbcode.movie.url']    = 'Adresse de la vidéo';
 $lang['bbcode.movie.format'] = 'Vous pouvez ajouter l\'adresse d\'une vidéo hébergée, youtube, dailymotion ou vimeo.';
-$lang['bbcode.movie.width'] = 'Largeur de la vidéo';
+$lang['bbcode.movie.width']  = 'Largeur de la vidéo';
 $lang['bbcode.movie.height'] = 'Hauteur de la vidéo';
 $lang['bbcode.movie.poster'] = 'Ajouter une vignette';
 
-$lang['bbcode.lightbox'] = 'Ajouter une image avec un effet lightbox';
-$lang['bbcode.picture.url'] = 'Adresse de l\'image';
+$lang['bbcode.lightbox']        = 'Ajouter une image avec un effet lightbox';
+$lang['bbcode.picture.url']     = 'Adresse de l\'image';
 $lang['bbcode.thumbnail.width'] = 'Largeur max de la vignette (px)';
 
-$lang['bbcode.picture.width'] = 'Largeur max de l\'image (px)';
-$lang['bbcode.figure'] = 'Ajouter une image avec description';
-$lang['bbcode.picture.alt'] = 'Texte alternatif';
+$lang['bbcode.picture.width']  = 'Largeur max de l\'image (px)';
+$lang['bbcode.figure']         = 'Ajouter une image avec description';
+$lang['bbcode.picture.alt']    = 'Texte alternatif';
 $lang['bbcode.figure.caption'] = 'Description de l\'image';
 
 $lang['bbcode.upload'] = 'Attacher un fichier';
 
-$lang['bbcode.smileys'] = 'Smileys';
-$lang['bbcode.fa'] = 'Icônes Font Awesome';
-$lang['bbcode.fa.tag'] = '[fa=fa-style,fa-option]nom de l\'icône (sans le "fa-")[/fa]';
-$lang['bbcode.code'] = 'Code';
-$lang['bbcode.code.custom.name'] = 'Chemin vers un fichier';
-$lang['bbcode.code.name'] = 'Langage';
-$lang['bbcode.code.line'] = 'Afficher sur une seule ligne';
-$lang['bbcode.text'] = 'Texte';
+$lang['bbcode.smileys']            = 'Smileys';
+$lang['bbcode.fa']                 = 'Icônes Font Awesome';
+$lang['bbcode.fa.tag']             = '[fa=fa-style,fa-option]nom de l\'icône (sans le "fa-")[/fa]';
+$lang['bbcode.code']               = 'Code';
+$lang['bbcode.code.custom.name']   = 'Chemin vers un fichier';
+$lang['bbcode.code.name']          = 'Langage';
+$lang['bbcode.code.line']          = 'Afficher sur une seule ligne';
+$lang['bbcode.text']               = 'Texte';
 $lang['bbcode.phpboost.languages'] = 'PHPBoost';
-$lang['bbcode.web'] = 'Web';
-$lang['bbcode.script'] = 'Script';
-$lang['bbcode.prog'] = 'Programmation';
-$lang['bbcode.math'] = 'Insérer du code mathématique';
-$lang['bbcode.html'] = 'Insérer du code html';
+$lang['bbcode.web']                = 'Web';
+$lang['bbcode.script']             = 'Script';
+$lang['bbcode.prog']               = 'Programmation';
+$lang['bbcode.math']               = 'Insérer du code mathématique';
+$lang['bbcode.html']               = 'Insérer du code html';
 
 $lang['bbcode.help'] = 'Aide BBcode';
 ?>
