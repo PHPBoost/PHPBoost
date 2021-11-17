7b2c5996d5779c90a4cdfc2553d21ef249de572b
j1.seth <julien.briswalter@gmail.com>
2021-11-17 07:05:04 +0100

    [UPD] ModuleExtensionPointProvider

--- kernel/framework/phpboost/module/ModuleExtensionPointProvider.class.php
+++ kernel/framework/phpboost/module/ModuleExtensionPointProvider.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2021 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 05 12
+ * @version     PHPBoost 6.0 - last update: 2021 11 17
  * @since       PHPBoost 6.0 - 2020 01 02
 */
 
@@ -51,6 +51,12 @@ class ModuleExtensionPointProvider extends ExtensionPointProvider
 			return $module_css_files;
 		}
 		return false;
+	}
+
+	public function hook()
+	{
+		if ($class = $this->get_class('Hook'))
+			return $class;
 	}
 
 	public function menus()
