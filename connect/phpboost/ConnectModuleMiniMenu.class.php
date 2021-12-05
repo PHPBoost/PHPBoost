e58ba64e08446945077e12a1e77f522f03a15a75
babsolune <babsolune@phpboost.com>
2021-12-05 08:39:54 +0100

    [UPD] connect - lang

--- connect/phpboost/ConnectModuleMiniMenu.class.php
+++ connect/phpboost/ConnectModuleMiniMenu.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2021 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 12
+ * @version     PHPBoost 6.0 - last update: 2021 12 05
  * @since       PHPBoost 3.0 - 2011 10 08
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -27,7 +27,7 @@ class ConnectModuleMiniMenu extends ModuleMiniMenu
 		if (!Url::is_current_url('/login'))
 		{
 			$view = new FileTemplate('connect/connect_mini.tpl');
-			$view->add_lang(LangLoader::get('user-lang'));
+			$view->add_lang(LangLoader::get_all_langs());
 			$user = AppContext::get_current_user();
 			MenuService::assign_positions_conditions($view, $this->get_block());
 
