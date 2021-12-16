f0ca74f125376effbd323c38a947fd88e19f2b51
babsolune <babsolune@phpboost.com>
2021-12-16 03:41:02 +0100

    [UPD] forum - lang

--- forum/phpboost/ForumSearchable.class.php
+++ forum/phpboost/ForumSearchable.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2021 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 12 15
+ * @version     PHPBoost 6.0 - last update: 2021 12 16
  * @since       PHPBoost 3.0 - 2012 02 21
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -191,10 +191,7 @@ class ForumSearchable extends AbstractSearchableExtensionPoint
 	public function parse_search_result($result_data)
 	{
 		$view = new FileTemplate('forum/forum_generic_results.tpl');
-		$view->add_lang(array_merge(
-			LangLoader::get('common', 'forum'),
-			LangLoader::get('common-lang')
-		));
+		$view->add_lang(LangLoader::get_all_langs('forum'));
 
 		$rewrited_title = ServerEnvironmentConfig::load()->is_url_rewriting_enabled() ? '+' . Url::encode_rewrite($result_data['title']) : '';
 
