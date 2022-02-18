e5aa8eb448ba5d06d09dec7ad601597a21c05169
j1.seth <julien.briswalter@gmail.com>
2022-02-18 07:03:11 +0100

    [FIX] ClassLoader

--- kernel/framework/core/ClassLoader.class.php
+++ kernel/framework/core/ClassLoader.class.php
@@ -4,9 +4,10 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 02 28
+ * @version     PHPBoost 6.0 - last update: 2022 02 18
  * @since       PHPBoost 3.0 - 2009 10 21
  * @contributor mipel <mipel@phpboost.com>
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class ClassLoader
@@ -29,6 +30,10 @@ class ClassLoader
 	public static function init_autoload()
 	{
 		spl_autoload_register(array(get_class(), 'autoload'));
+		if (!self::inc(PATH_TO_ROOT . self::$cache_file))
+		{
+			self::generate_classlist();
+		}
 	}
 
 	/**
@@ -37,32 +42,15 @@ class ClassLoader
 	 */
 	public static function autoload($classname)
 	{
-		if (!isset(self::$autoload[$classname]) || !self::inc(PATH_TO_ROOT . self::$autoload[$classname]))
-		{
-			self::generate_classlist();
-			if (isset(self::$autoload[$classname]) && file_exists(PATH_TO_ROOT . self::$autoload[$classname]))
-			{
-				require_once PATH_TO_ROOT . self::$autoload[$classname];
-			}
-		}
+		if (!self::is_class_registered($classname) || !self::inc(PATH_TO_ROOT . self::$autoload[$classname]))
+			throw new Exception('Class ' . $classname . ' does not exist');
+		
 		self::call_static_initializer($classname);
 	}
 
 	public static function is_class_registered_and_valid($classname)
 	{
-		if (!self::is_class_registered($classname))
-		{
-			return false;
-		}
-		elseif (!file_exists(PATH_TO_ROOT . self::$autoload[$classname]))
-		{
-			self::generate_classlist();
-			return self::is_class_registered($classname);
-		}
-		else
-		{
-			return true;
-		}
+		return self::is_class_registered($classname) && file_exists(PATH_TO_ROOT . self::$autoload[$classname]);
 	}
 
 	/**
