b852015a94e65beabc4fd09d76420694da191e16
babsolune <babsolune@phpboost.com>
2022-03-02 06:32:38 +0100

    [UPD] install category thumbnail

--- gallery/phpboost/GallerySetup.class.php
+++ gallery/phpboost/GallerySetup.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 01 07
+ * @version     PHPBoost 6.0 - last update: 2022 03 02
  * @since       PHPBoost 3.0 - 2010 01 17
  * @contributor Arnaud GENET <elenwii@phpboost.com>
 */
@@ -89,7 +89,7 @@ class GallerySetup extends DefaultModuleSetup
 			'rewrited_name' => Url::encode_rewrite($this->messages['default.cat.name']),
 			'name' => $this->messages['default.cat.name'],
 			'description' => $this->messages['default.cat.description'],
-			'thumbnail' => '/gallery/gallery.png'
+			'thumbnail' => '/templates/__default__/images/default_category.webp'
 		));
 	}
 
