bdf14d9a5d9508ef8d7514d87e9dc58eea4e05c4
j1.seth <julien.briswalter@gmail.com>
2022-03-29 06:19:22 +0200

    [FIX] Update

--- update/services/UpdateServices.class.php
+++ update/services/UpdateServices.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 03 28
+ * @version     PHPBoost 6.0 - last update: 2022 03 29
  * @since       PHPBoost 3.0 - 2012 02 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -446,10 +446,13 @@ class UpdateServices
 						$has_update_class = true;
 				}
 
-				if (!ModulesManager::is_module_installed($module_id) && $has_update_class && $module_update_class)
+				if (!ModulesManager::is_module_installed($module_id))
 				{
-					$module_update_class::delete_old_files();
-					$module_update_class::delete_old_folders();
+					if ($has_update_class && $module_update_class)
+					{
+						$module_update_class::delete_old_files();
+						$module_update_class::delete_old_folders();
+					}
 				}
 				else
 				{
