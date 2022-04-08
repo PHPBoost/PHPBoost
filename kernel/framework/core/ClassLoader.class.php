e964dd0032f5ca7801d234a9385c44cb6d0d7890
j1.seth <julien.briswalter@gmail.com>
2022-04-09 00:38:13 +0200

    [UPD] ClassLoader

--- kernel/framework/core/ClassLoader.class.php
+++ kernel/framework/core/ClassLoader.class.php
@@ -4,7 +4,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 03 29
+ * @version     PHPBoost 6.0 - last update: 2022 04 09
  * @since       PHPBoost 3.0 - 2009 10 21
  * @contributor mipel <mipel@phpboost.com>
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
@@ -42,7 +42,11 @@ class ClassLoader
 	public static function autoload($classname)
 	{
 		if (!self::is_class_registered($classname) || !self::inc(PATH_TO_ROOT . self::$autoload[$classname]))
-			throw new Exception('Class ' . $classname . ' does not exist');
+		{
+			self::generate_classlist();
+			if (!self::is_class_registered($classname) || !self::inc(PATH_TO_ROOT . self::$autoload[$classname]))
+				throw new Exception('Class ' . $classname . ' does not exist');
+		}
 		
 		self::call_static_initializer($classname);
 	}
