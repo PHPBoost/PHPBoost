7c7f862bd9e17e3a199a9e79e3cc17fc220538d0
j1.seth <julien.briswalter@gmail.com>
2022-04-18 07:47:46 +0200

    [UPD] Customization CSS

--- customization/controllers/AdminCustomizeEditorCSSFilesController.class.php
+++ customization/controllers/AdminCustomizeEditorCSSFilesController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 04 16
+ * @version     PHPBoost 6.0 - last update: 2022 04 18
  * @since       PHPBoost 3.0 - 2011 09 26
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -73,8 +73,9 @@ class AdminCustomizeEditorCSSFilesController extends DefaultAdminModuleControlle
 				new FormFieldSimpleSelectChoice('select_file', $this->lang['customization.editor.files.select'], $file_selected,
 					$this->list_files($theme_selected),
 					array(
-						'class' => 'third-field',
-						'events' => array('change' => 'document.location.href = "' . AdminCustomizeUrlBuilder::editor_css_file($theme_selected)->rel() . '" + "/" + HTMLForms.getField(\'select_file\').getValue()')
+						'class'       => 'third-field',
+						'description' => $this->lang['customization.editor.files.select.clue'],
+						'events'      => array('change' => 'document.location.href = "' . AdminCustomizeUrlBuilder::editor_css_file($theme_selected)->rel() . '" + "/" + HTMLForms.getField(\'select_file\').getValue()')
 					)
 				)
 			);
@@ -257,8 +258,28 @@ class AdminCustomizeEditorCSSFilesController extends DefaultAdminModuleControlle
 			{
 				foreach ($folder->get_files('`\.css$`') as $file)
 				{
-					$files[] = new FormFieldSelectChoiceOption(LangLoader::get_message('common.module', 'common-lang') . ' ' . ModulesManager::get_module($module->get_id())->get_configuration()->get_name() . ' : ' . $file->get_name(), $module->get_id() . '/' . str_replace('@', '', $file->get_name()));
+					$is_override = false;
+					$css_file = new File(PATH_TO_ROOT . $this->templates_path . $theme_selected . $this->css_modules_files_path . $module->get_id() . '/' . $file->get_name());
+					if ($css_file->exists())
+						$is_override = true;
+					
+					$files[] = new FormFieldSelectChoiceOption(LangLoader::get_message('common.module', 'common-lang') . ' ' . ModulesManager::get_module($module->get_id())->get_configuration()->get_name() . ' : ' . $file->get_name() . ($is_override ? ' *' : ''), $module->get_id() . '/' . str_replace('@', '', $file->get_name()));
 				}
+			}
+		}
+
+		// User CSS files
+		$folder = new Folder(PATH_TO_ROOT . '/user/templates');
+		if ($folder->exists())
+		{
+			foreach ($folder->get_files('`\.css$`') as $file)
+			{
+				$is_override = false;
+				$css_file = new File(PATH_TO_ROOT . $this->templates_path . $theme_selected . $this->css_modules_files_path . 'user/' . $file->get_name());
+				if ($css_file->exists())
+					$is_override = true;
+				
+				$files[] = new FormFieldSelectChoiceOption(LangLoader::get_message('user.users', 'user-lang') . ' : ' . $file->get_name() . ($is_override ? ' *' : ''), 'user/' . $file->get_name());
 			}
 		}
 
