f2d1dd79b161129218dd56d288b437ffab765068
babsolune <babsolune@phpboost.com>
2022-04-21 10:26:06 +0200

    [FIX] shoutbox - delete item

--- shoutbox/controllers/ShoutboxDeleteController.class.php
+++ shoutbox/controllers/ShoutboxDeleteController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 04 17
+ * @version     PHPBoost 6.0 - last update: 2022 04 21
  * @since       PHPBoost 4.1 - 2014 10 14
 */
 
@@ -18,7 +18,7 @@ class ShoutboxDeleteController extends ModuleController
 		$this->check_authorizations($item);
 
 		ShoutboxService::delete($item->get_id());
-		
+
 		HooksService::execute_hook_action('delete', self::$module_id, $item->get_properties());
 
 		AppContext::get_response()->redirect(($request->get_url_referrer() ? $request->get_url_referrer() : ShoutboxUrlBuilder::home()), LangLoader::get_message('shoutbox.message.success.delete', 'common', 'shoutbox'));
@@ -29,14 +29,14 @@ class ShoutboxDeleteController extends ModuleController
 		$id = $request->get_getint('id', 0);
 
 		try {
-			return ShoutboxService::get_message($id);
+			return ShoutboxService::get_item($id);
 		} catch (RowNotFoundException $e) {
 			$error_controller = PHPBoostErrors::unexisting_page();
 			DispatchManager::redirect($error_controller);
 		}
 	}
 
-	private function check_authorizations(ShoutboxMessage $item)
+	private function check_authorizations(ShoutboxItem $item)
 	{
 		if (!$item->is_authorized_to_delete())
 		{
