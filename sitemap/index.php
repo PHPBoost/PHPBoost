863958d5d08328eacec8c7eebd49e7c0b36e922f
Julien Briswalter <jbriswalter@users.noreply.github.com>
2022-04-24 17:10:41 +0200

    [FIX] Sitemap

--- sitemap/index.php
+++ sitemap/index.php
@@ -4,8 +4,9 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2014 12 22
+ * @version     PHPBoost 6.0 - last update: 2022 04 24
  * @since       PHPBoost 3.0 - 2009 12 08
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 define('PATH_TO_ROOT', '..');
@@ -15,7 +16,7 @@ require_once PATH_TO_ROOT . '/kernel/init.php';
 $url_controller_mappers = array(
 	new UrlControllerMapper('ViewXMLSitemapController', '`^/view/xml?$`'),
 	new UrlControllerMapper('ViewSitemapController', '`^(?:/view)?/?$`'),
-	new UrlControllerMapper('AdminSitemapController', '`^/admin/?$`'),
+	new UrlControllerMapper('AdminSitemapController', '`^/admin(?:/config)?/?$`'),
 	new UrlControllerMapper('GenerateXMLSitemapController', '`^/admin/generate?$`')
 );
 
