dd260efaae1794e407813fd074ea5c5bde76ce89
j1.seth <julien.briswalter@gmail.com>
2022-05-18 06:47:44 +0200

    [UPD] Repository

--- kernel/framework/phpboost/update/Repository.class.php
+++ kernel/framework/phpboost/update/Repository.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2019 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 5.2 - last update: 2019 03 26
+ * @version     PHPBoost 5.2 - last update: 2022 05 18
  * @since       PHPBoost 2.0 - 2008 08 17
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -22,14 +22,44 @@ class Repository
 	public function __construct($url)
 	{
 		$this->url = $url;
-		if (function_exists('simplexml_load_file'))
+		$server_configuration = new ServerConfiguration();
+		if (function_exists('simplexml_load_string') && $server_configuration->has_curl_library())
 		{
-			$this->xml = @simplexml_load_file($this->url);
+			libxml_use_internal_errors(true);
+			$this->xml = simplexml_load_string($this->get_xml_content_with_curl($this->url));
 			if ($this->xml == false)
-			{
 				$this->xml = null;
-			}
 		}
+		else if (function_exists('simplexml_load_file') && $server_configuration->has_allow_url_fopen())
+		{
+			libxml_use_internal_errors(true);
+			$this->xml = simplexml_load_file($this->url);
+			if ($this->xml == false)
+				$this->xml = null;
+		}
+	}
+	
+	private function get_xml_content_with_curl($url)
+	{
+		$curl = curl_init();
+		$header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
+		$header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+		$header[] = "Cache-Control: max-age=0";
+		$header[] = "Connection: keep-alive";
+		$header[] = "Keep-Alive: 300";
+		$header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
+		curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+
+		$html = curl_exec($curl);
+		curl_close($curl);
+
+		return $html;
 	}
 
 	/**
