ef3ad1d2e514d291d83564876356beb8e2ea1b02
j1.seth <julien.briswalter@gmail.com>
2022-05-18 07:12:07 +0200

    [UPD] Forum delete message hook

--- forum/Forum.class.php
+++ forum/Forum.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 02 11
+ * @version     PHPBoost 6.0 - last update: 2022 05 18
  * @since       PHPBoost 2.0 - 2007 12 10
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -316,7 +316,7 @@ class Forum
 				$topic = array();
 			}
 
-			HooksService::execute_hook_action('delete', 'forum', array_merge($topic, array('id' => $idmsg, 'idtopic' => $idtopic, 'url' => Url::to_rel('/forum/topic' . url('.php?id=' . $idtopic . $msg_page, '-' . $idtopic .  $msg_page_rewrite . '.php', '&') . '#m' . $previous_msg_id))));
+			HooksService::execute_hook_action('delete', 'forum', array_merge($topic, array('id' => $idmsg, 'idtopic' => $idtopic, 'id_category' => $id_category, 'msg_user_id' => $msg_user_id, 'url' => Url::to_rel('/forum/topic' . url('.php?id=' . $idtopic . $msg_page, '-' . $idtopic .  $msg_page_rewrite . '.php', '&') . '#m' . $previous_msg_id))));
 
 			return array($nbr_msg, $previous_msg_id);
 		}
