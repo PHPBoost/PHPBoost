032010aac24e72ed156aaffdf350cde815e98d6c
j1.seth <julien.briswalter@gmail.com>
2022-05-22 09:19:58 +0200

    [UPD] redirect to https if available on install and update

--- install/controllers/InstallController.class.php
+++ install/controllers/InstallController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 04 22
+ * @version     PHPBoost 6.0 - last update: 2022 05 22
  * @since       PHPBoost 3.0 - 2010 09 12
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -16,11 +16,18 @@ abstract class InstallController extends AbstractController
 
 	protected function load_lang(HTTPRequestCustom $request)
 	{
+		$this->redirect_to_https_if_needed($request);
 		$locale = TextHelper::htmlspecialchars($request->get_string('lang', ''));
 		$this->locale = in_array($locale, InstallationServices::get_available_langs()) ? $locale : InstallationServices::get_default_lang();
 		LangLoader::set_locale($this->locale);
 		InstallUrlBuilder::set_locale($this->locale);
 		$this->lang = LangLoader::get_all_langs('install', $this->locale);
+	}
+
+	protected function redirect_to_https_if_needed(HTTPRequestCustom $request)
+	{
+		if (!$request->get_is_https() && Url::check_url_validity(str_replace('http://', 'https://', $request->get_site_url())))
+			AppContext::get_response()->redirect(str_replace('http://', 'https://', $request->get_current_url()));
 	}
 }
 ?>
--- update/controllers/UpdateController.class.php
+++ update/controllers/UpdateController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2014 12 22
+ * @version     PHPBoost 6.0 - last update: 2022 05 22
  * @since       PHPBoost 3.0 - 2012 02 29
 */
 
@@ -15,10 +15,17 @@ abstract class UpdateController extends AbstractController
 
 	protected function load_lang(HTTPRequestCustom $request)
 	{
+		$this->redirect_to_https_if_needed($request);
 		$locale = TextHelper::htmlspecialchars($request->get_string('lang', UpdateController::DEFAULT_LOCALE));
 		LangLoader::set_locale($locale);
 		UpdateUrlBuilder::set_locale($locale);
 		$this->lang = LangLoader::get('update', 'update');
+	}
+
+	protected function redirect_to_https_if_needed(HTTPRequestCustom $request)
+	{
+		if (!$request->get_is_https() && Url::check_url_validity(str_replace('http://', 'https://', $request->get_site_url())))
+			AppContext::get_response()->redirect(str_replace('http://', 'https://', $request->get_current_url()));
 	}
 }
 ?>
