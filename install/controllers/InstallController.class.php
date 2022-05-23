430d47a47ed5fb052f2d7a77e0e93df317dae27b
j1.seth <julien.briswalter@gmail.com>
2022-05-23 23:30:57 +0200

    [FIX] Redirect to https if exists on install and update

--- install/controllers/InstallController.class.php
+++ install/controllers/InstallController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 05 22
+ * @version     PHPBoost 6.0 - last update: 2022 05 23
  * @since       PHPBoost 3.0 - 2010 09 12
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -26,8 +26,26 @@ abstract class InstallController extends AbstractController
 
 	protected function redirect_to_https_if_needed(HTTPRequestCustom $request)
 	{
-		if (!$request->get_is_https() && Url::check_url_validity(str_replace('http://', 'https://', $request->get_site_url())))
-			AppContext::get_response()->redirect(str_replace('http://', 'https://', $request->get_current_url()));
+		if (!$request->get_is_https() && @extension_loaded('curl'))
+		{
+			$status = 0;
+			$curl = curl_init(str_replace('http://', 'https://', $request->get_site_url()));
+			curl_setopt($curl, CURLOPT_NOBODY, true);
+			$result = curl_exec($curl);
+
+			if ($result !== false)
+			{
+				$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			}
+			curl_close($curl);
+			
+			if ($status >= 200 && $status < 400)
+			{
+				header('HTTP/1.1 301 Moved Permanently');
+				header('Location: ' . str_replace('http://', 'https://', $request->get_current_url()));
+				exit;
+			}
+		}
 	}
 }
 ?>
--- update/controllers/UpdateController.class.php
+++ update/controllers/UpdateController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 05 22
+ * @version     PHPBoost 6.0 - last update: 2022 05 23
  * @since       PHPBoost 3.0 - 2012 02 29
 */
 
@@ -24,8 +24,26 @@ abstract class UpdateController extends AbstractController
 
 	protected function redirect_to_https_if_needed(HTTPRequestCustom $request)
 	{
-		if (!$request->get_is_https() && Url::check_url_validity(str_replace('http://', 'https://', $request->get_site_url())))
-			AppContext::get_response()->redirect(str_replace('http://', 'https://', $request->get_current_url()));
+		if (!$request->get_is_https() && @extension_loaded('curl'))
+		{
+			$status = 0;
+			$curl = curl_init(str_replace('http://', 'https://', $request->get_site_url()));
+			curl_setopt($curl, CURLOPT_NOBODY, true);
+			$result = curl_exec($curl);
+
+			if ($result !== false)
+			{
+				$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			}
+			curl_close($curl);
+			
+			if ($status >= 200 && $status < 400)
+			{
+				header('HTTP/1.1 301 Moved Permanently');
+				header('Location: ' . str_replace('http://', 'https://', $request->get_current_url()));
+				exit;
+			}
+		}
 	}
 }
 ?>
