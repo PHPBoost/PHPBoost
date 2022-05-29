0c553bcf6ba524a32dda7591483a30d638ba3161
Julien Briswalter <jbriswalter@users.noreply.github.com>
2022-05-29 23:08:49 +0200

    [FIX] Forum topic

--- forum/topic.php
+++ forum/topic.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 05 25
+ * @version     PHPBoost 6.0 - last update: 2022 05 29
  * @since       PHPBoost 1.2 - 2005 10 26
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -356,7 +356,7 @@ while ( $row = $result->fetch() )
 
 	foreach ($user_additional_informations as $info)
 	{
-		$this->view->assign_block_vars('msg.additional_informations', array(
+		$view->assign_block_vars('msg.additional_informations', array(
 			'VALUE' => $info
 		));
 	}
