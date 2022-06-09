7ca466484f8d010fbe7e96dbacf2ce223887df3d
j1.seth <julien.briswalter@gmail.com>
2022-06-09 06:44:03 +0200

    [UPD] ServerConfiguration

--- kernel/framework/phpboost/environment/ServerConfiguration.class.php
+++ kernel/framework/phpboost/environment/ServerConfiguration.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2019 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 5.2 - last update: 2019 09 27
+ * @version     PHPBoost 5.2 - last update: 2022 06 09
  * @since       PHPBoost 3.0 - 2010 05 30
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -15,9 +15,12 @@
 class ServerConfiguration
 {
 	const MIN_PHP_VERSION = '5.6';
-	const RECOMMENDED_PHP_VERSION = '7.1';
+	const RECOMMENDED_PHP_VERSION = '7.4';
 	private static $mod_rewrite = 'mod_rewrite';
 
+	/**
+	 * @return string system php version.
+	 */
 	public static function get_phpversion()
 	{
 		$system_phpversion = phpversion();
@@ -28,13 +31,36 @@ class ServerConfiguration
 		}
 		return $system_phpversion;
 	}
-
+	
 	/**
-	 * @return int upload max filesize in bytes.
-	 */
+	* Detects max size of file cab be uploaded to server
+	*
+	* Based on php.ini parameters "upload_max_filesize", "post_max_size" &
+	* "memory_limit". Valid for single file upload form. May be used
+	* as MAX_FILE_SIZE hidden input or to inform user about max allowed file size.
+	*
+	* @return int Max file size in bytes
+	*/
 	public static function get_upload_max_filesize()
 	{
-		return (int)str_replace('M', '', ini_get('upload_max_filesize')) * pow(1024, 2);
+		$normalize = function($size)
+		{
+			if (preg_match('/^([\d\.]+)([KMG])$/i', $size, $match))
+			{
+				$pos = array_search($match[2], array("K", "M", "G"));
+				if ($pos !== false)
+				{
+					$size = $match[1] * pow(1024, $pos + 1);
+				}
+			}
+			return $size;
+		};
+		
+		$max_upload = $normalize(ini_get('upload_max_filesize'));
+		$max_post = $normalize(ini_get('post_max_size'));
+		$memory_limit = $normalize(ini_get('memory_limit'));
+		$maxFileSize = min($max_upload, $max_post, $memory_limit);
+		return $maxFileSize;
 	}
 
 	/**
@@ -67,6 +93,22 @@ class ServerConfiguration
 	public function has_mbstring_library()
 	{
 		return @extension_loaded('mbstring');
+	}
+
+	/**
+	 * @return true if zip library is available, else false.
+	 */
+	public function has_zip_library()
+	{
+		return @extension_loaded('zip');
+	}
+
+	/**
+	 * @return true if allow_url_fopen directive is enabled, else false.
+	 */
+	public function has_allow_url_fopen()
+	{
+		return ini_get('allow_url_fopen');
 	}
 
 	/**
