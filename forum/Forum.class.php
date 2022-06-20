5078886b3e0d739d77d9c2f6ec99406c0e9056ec
j1.seth <julien.briswalter@gmail.com>
2022-06-20 23:18:02 +0200

    [UPD] Forum

--- forum/Forum.class.php
+++ forum/Forum.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 05 18
+ * @version     PHPBoost 6.0 - last update: 2022 06 20
  * @since       PHPBoost 2.0 - 2007 12 10
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -307,6 +307,8 @@ class Forum
 				$msg_page = ($msg_page > 1) ? '&pt=' . $msg_page : '';
 				forum_history_collector(H_DELETE_MSG, $msg_user_id, 'topic' . url('.php?id=' . $idtopic . $msg_page, '-' . $idtopic .  $msg_page_rewrite . '.php', '&') . '#m' . $previous_msg_id);
 			}
+			else
+				$msg_page = $msg_page_rewrite = '';
 			forum_generate_feeds(); //Regénération des flux flux
 			ForumCategoriesCache::invalidate();
 
