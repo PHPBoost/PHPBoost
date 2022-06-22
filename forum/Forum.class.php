34cd61bfe75f2eb12f58424d948d76f2f37c987a
j1.seth <julien.briswalter@gmail.com>
2022-06-22 06:56:06 +0200

    [FIX]  #2025  Suppression d'un sujet

--- forum/Forum.class.php
+++ forum/Forum.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 06 20
+ * @version     PHPBoost 6.0 - last update: 2022 06 22
  * @since       PHPBoost 2.0 - 2007 12 10
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -740,7 +740,7 @@ class Forum
 
 		$last_topic_id = 0;
 		try {
-			$last_topic_id = PersistenceContext::get_querier()->get_column_value(ForumSetup::$forum_topics_table, 'id', 'WHERE last_timestamp = :timestamp', array('timestamp' => $last_timestamp));
+			$last_topic_id = PersistenceContext::get_querier()->get_column_value(ForumSetup::$forum_topics_table, 'MAX(id)', 'WHERE last_timestamp = :timestamp', array('timestamp' => $last_timestamp));
 		} catch (RowNotFoundException $e) {}
 
 		PersistenceContext::get_querier()->update(ForumSetup::$forum_cats_table, array('last_topic_id' => (int)$last_topic_id ), 'WHERE id = :id', array('id' => $id_category));
