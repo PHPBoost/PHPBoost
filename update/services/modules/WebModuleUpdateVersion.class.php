6a8b2bf3da7610d84c24a0ffbba90dc1255f5098
babsolune <babsolune@phpboost.com>
2022-10-11 06:05:52 +0200

    [UPD] update - web

--- update/services/modules/WebModuleUpdateVersion.class.php
+++ update/services/modules/WebModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 16
+ * @version     PHPBoost 6.0 - last update: 2022 10 11
  * @since       PHPBoost 4.0 - 2014 05 22
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
@@ -23,6 +23,7 @@ class WebModuleUpdateVersion extends ModuleUpdateVersion
 			'/controllers/WebDisplayWebLinkTagController.class.php',
 			'/controllers/WebFormController.class.php',
 			'/controllers/WebManageController.class.php',
+			'/controllers/WebVisitWebLinkController.class.php',
 			'/lang/english/config.php',
 			'/lang/french/config.php',
 			'/phpboost/WebComments.class.php',
