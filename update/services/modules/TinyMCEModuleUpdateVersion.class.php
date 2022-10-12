bab5e40e91d6a4af8ea2448eb19c60dc6c6b895f
babsolune <babsolune@phpboost.com>
2022-10-12 10:47:10 +0200

    [UPD] update - tinymce

---
+++ update/services/modules/TinyMCEModuleUpdateVersion.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2022 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2022 10 12
+ * @since       PHPBoost 4.0 - 2014 05 22
+*/
+
+class TinyMCEModuleUpdateVersion extends ModuleUpdateVersion
+{
+	public function __construct()
+	{
+		parent::__construct('TinyMCE');
+
+		self::$delete_old_folders_list = array(
+			'/templates/js/tinymce/plugins/smileys',
+		);
+}
+?>
