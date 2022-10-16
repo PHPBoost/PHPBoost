41e58b365ba39205b37876d1fde63d40b225e52d
babsolune <babsolune@phpboost.com>
2022-10-16 06:06:58 +0200

    [UPD] update - bbcode content

--- update/services/UpdateServices.class.php
+++ update/services/UpdateServices.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 06
+ * @version     PHPBoost 6.0 - last update: 2022 10 16
  * @since       PHPBoost 3.0 - 2012 02 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -659,7 +659,7 @@ class UpdateServices
 
 			$result = self::$db_querier->select('SELECT ' . $id . ', ' . $contents . '
 				FROM ' . $table . '
-				WHERE (' . $contents . ' LIKE "%class=\"offload\"%") OR (' . $contents . ' LIKE "%class=\"success\"%") OR (' . $contents . ' LIKE "%class=\"question\"%") OR (' . $contents . ' LIKE "%class=\"notice\"%") OR (' . $contents . ' LIKE "%class=\"warning\"%") OR (' . $contents . ' LIKE "%class=\"error\"%") OR (' . $contents . ' LIKE "%title=\"%")'
+				WHERE (' . $contents . ' LIKE "%<a%") OR  (' . $contents . ' LIKE "%class=\"message-helper\"%") OR (' . $contents . ' LIKE "%class=\"success\"%") OR (' . $contents . ' LIKE "%class=\"question\"%") OR (' . $contents . ' LIKE "%class=\"notice\"%") OR (' . $contents . ' LIKE "%class=\"warning\"%") OR (' . $contents . ' LIKE "%class=\"error\"%") OR (' . $contents . ' LIKE "%title=\"%")'
 			);
 
 			$selected_rows = $result->get_rows_count();
