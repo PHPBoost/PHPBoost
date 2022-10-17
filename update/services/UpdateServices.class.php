dcf1ed7b32cf86124e1c3375202b08797a0a8df2
babsolune <babsolune@phpboost.com>
2022-10-17 12:16:42 +0200

    [UPD] update

--- update/services/UpdateServices.class.php
+++ update/services/UpdateServices.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 16
+ * @version     PHPBoost 6.0 - last update: 2022 10 17
  * @since       PHPBoost 3.0 - 2012 02 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -837,6 +837,10 @@ class UpdateServices
 			$folder->delete();
 
 		$folder = new Folder(PATH_TO_ROOT . '/kernel/lib/php/google');
+		if ($folder->exists())
+			$folder->delete();
+
+		$folder = new Folder(PATH_TO_ROOT . '/kernel/lib/js/html5shiv');
 		if ($folder->exists())
 			$folder->delete();
 
