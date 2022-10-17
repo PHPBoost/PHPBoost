149d9a302d79c048c26e68f6754f6060ecd25d74
babsolune <babsolune@phpboost.com>
2022-10-17 18:45:15 +0200

    [UPD] customization - indent

--- customization/lang/english/common.php
+++ customization/lang/english/common.php
@@ -14,37 +14,37 @@
 #                    English                       #
 ####################################################
 
-$lang['customization.interface'] = 'Interface';
-$lang['customization.interface.title'] = 'Customization interface';
-$lang['customization.interface.all.themes'] = 'All themes';
-$lang['customization.interface.select.theme'] = 'Select the theme you want to attribute changes';
-$lang['customization.interface.theme.choice'] = 'Theme choice';
-$lang['customization.interface.logo.current.change'] = 'Change Logo';
+$lang['customization.interface']                                 = 'Interface';
+$lang['customization.interface.title']                           = 'Customization interface';
+$lang['customization.interface.all.themes']                      = 'All themes';
+$lang['customization.interface.select.theme']                    = 'Select the theme you want to attribute changes';
+$lang['customization.interface.theme.choice']                    = 'Theme choice';
+$lang['customization.interface.logo.current.change']             = 'Change Logo';
 $lang['customization.interface.logo.current.change.description'] = 'The logo size must be 90 x 90 pixels. If not, you must modify <a href="../customization/?url=/editor/base/design.css">the css class</a> accordingly';
-$lang['customization.interface.logo.current'] = 'Current logo';
-$lang['customization.interface.logo.current.null'] = 'You currently have no logo';
-$lang['customization.interface.logo.current.erased'] = 'The logo that you saved is visibly removed from the server, please replace it with another';
-$lang['customization.interface.logo.error'] = 'The logo could not be changed';
-$lang['customization.interface.logo.use.default'] = 'Delete the current image and use the default logo';
+$lang['customization.interface.logo.current']                    = 'Current logo';
+$lang['customization.interface.logo.current.null']               = 'You currently have no logo';
+$lang['customization.interface.logo.current.erased']             = 'The logo that you saved is visibly removed from the server, please replace it with another';
+$lang['customization.interface.logo.error']                      = 'The logo could not be changed';
+$lang['customization.interface.logo.use.default']                = 'Delete the current image and use the default logo';
 
-$lang['customization.favicon'] = 'Favicon';
-$lang['customization.favicon.title'] = 'Customizing the favicon';
-$lang['customization.favicon.current'] = 'Current favicon';
-$lang['customization.favicon.current.null'] = 'You currently have no favicon';
+$lang['customization.favicon']                = 'Favicon';
+$lang['customization.favicon.title']          = 'Customizing the favicon';
+$lang['customization.favicon.current']        = 'Current favicon';
+$lang['customization.favicon.current.null']   = 'You currently have no favicon';
 $lang['customization.favicon.current.change'] = 'Change your favicon';
 $lang['customization.favicon.current.erased'] = 'The favicon that you saved is visibly removed from the server, please replace it with another';
-$lang['customization.favicon.error'] = 'The favicon could not be changed';
+$lang['customization.favicon.error']          = 'The favicon could not be changed';
 
-$lang['customization.editor.files.select'] = 'Select the file you want to edit';
+$lang['customization.editor.files.select']      = 'Select the file you want to edit';
 $lang['customization.editor.files.select.clue'] = 'Filename ending with a * indicates there is an override in the selected theme';
-$lang['customization.editor.files.content'] = 'File Contents';
+$lang['customization.editor.files.content']     = 'File Contents';
 
-$lang['customization.css.files'] = 'CSS Files';
+$lang['customization.css.files']        = 'CSS Files';
 $lang['customization.editor.css.files'] = 'CSS Files Editor';
 
-$lang['customization.tpl.files'] = 'TPL Files';
+$lang['customization.tpl.files']        = 'TPL Files';
 $lang['customization.editor.tpl.files'] = 'TPL Files Editor';
 
-$lang['customization.default.theme'] = 'Default theme';
+$lang['customization.default.theme']   = 'Default theme';
 $lang['customization.remove.override'] = 'Remove file override in the theme';
 ?>
