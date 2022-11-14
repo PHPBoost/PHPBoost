4f8f0dc80f14ab53e8659e7cdb635a5e296be596
babsolune <babsolune@phpboost.com>
2022-11-14 10:14:36 +0100

    [FIX] save groups authorizations

--- kernel/framework/builder/form/field/FormFieldAuthorizationsSetter.class.php
+++ kernel/framework/builder/form/field/FormFieldAuthorizationsSetter.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2018 11 05
+ * @version     PHPBoost 6.0 - last update: 2022 11 14
  * @since       PHPBoost 3.0 - 2010 03 01
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -90,7 +90,7 @@ class FormFieldAuthorizationsSetter extends AbstractFormField
 		$g_auths = array();
 		foreach ($auths as $role => $auth)
 		{
-			if ($role[0] == 'r' || $role != '0')
+			if ($role != '0' || $role != '' || $role != NULL)
 			{
 				$g_auths[$role] = $auth;
 			}
