a785d9ed2e4640dd5a955518ab3e91d2022691d7
j1.seth <julien.briswalter@gmail.com>
2023-01-16 00:16:45 +0100

    [FIX] Download comments

--- download/phpboost/DownloadCommentsTopic.class.php
+++ download/phpboost/DownloadCommentsTopic.class.php
@@ -3,40 +3,22 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 10 30
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 4.0 - 2014 08 24
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
-class DownloadCommentsTopic extends CommentsTopic
+class DownloadCommentsTopic extends DefaultCommentsTopic
 {
-	private $item;
-
 	public function __construct(DownloadItem $item = null)
 	{
 		parent::__construct('download');
 		$this->item = $item;
 	}
 
-	public function get_authorizations()
+	protected function get_item_from_manager()
 	{
-		$authorizations = new CommentsAuthorizations();
-		$authorizations->set_authorized_access_module(DownloadAuthorizationsService::check_authorizations($this->get_item()->get_id_category())->read());
-		return $authorizations;
-	}
-
-	public function is_displayed()
-	{
-		return $this->get_item()->is_published();
-	}
-
-	private function get_item()
-	{
-		if ($this->item === null)
-		{
-			$this->item = DownloadService::get_item($this->get_id_in_module());
-		}
-		return $this->item;
+		return DownloadService::get_item($this->get_id_in_module());
 	}
 }
 ?>
--- download/phpboost/DownloadExtensionPointProvider.class.php
+++ download/phpboost/DownloadExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 02 18
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 4.0 - 2014 08 24
  * @contributor Kevin MASSY <reidlos@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -13,6 +13,11 @@
 
 class DownloadExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
+	public function comments()
+	{
+		return new CommentsTopics(array(new DownloadCommentsTopic()));
+	}
+	
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), DownloadCategoryController::get_view());
