25271c72e7d43692f4bfa1754d6a04f6f4b93ae3
j1.seth <julien.briswalter@gmail.com>
2023-01-16 00:17:32 +0100

    [FIX] Web comments

--- web/phpboost/WebCommentsTopic.class.php
+++ web/phpboost/WebCommentsTopic.class.php
@@ -3,40 +3,22 @@
   * @copyright   &copy; 2005-2023 PHPBoost
   * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
   * @author      Julien BRISWALTER <j1.seth@phpboost.com>
-  * @version     PHPBoost 6.0 - last update: 2021 10 30
+  * @version     PHPBoost 6.0 - last update: 2023 01 16
   * @since       PHPBoost 4.1 - 2014 08 21
   * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
  */
 
-class WebCommentsTopic extends CommentsTopic
+class WebCommentsTopic extends DefaultCommentsTopic
 {
-	private $item;
-
 	public function __construct(WebItem $item = null)
 	{
 		parent::__construct('web');
 		$this->item = $item;
 	}
 
-	public function get_authorizations()
+	protected function get_item_from_manager()
 	{
-		$authorizations = new CommentsAuthorizations();
-		$authorizations->set_authorized_access_module(CategoriesAuthorizationsService::check_authorizations($this->get_item()->get_id_category(), 'web')->read());
-		return $authorizations;
-	}
-
-	public function is_displayed()
-	{
-		return $this->get_item()->is_published();
-	}
-
-	private function get_item()
-	{
-		if ($this->item === null)
-		{
-			$this->item = WebService::get_item($this->get_id_in_module());
-		}
-		return $this->item;
+		return WebService::get_item($this->get_id_in_module());
 	}
 }
 ?>
--- web/phpboost/WebExtensionPointProvider.class.php
+++ web/phpboost/WebExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 02 09
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 4.1 - 2014 08 21
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Kevin MASSY <reidlos@phpboost.com>
@@ -13,6 +13,11 @@
 
 class WebExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
+	public function comments()
+	{
+		return new CommentsTopics(array(new WebCommentsTopic()));
+	}
+	
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), WebCategoryController::get_view());
