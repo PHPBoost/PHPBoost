6ac94e126ecdd31d0814d8b04a423527305fe0e7
babsolune <babsolune@phpboost.com>
2023-02-19 09:55:22 +0100

    [UPD] shoutbox indent

--- shoutbox/services/ShoutboxItem.class.php
+++ shoutbox/services/ShoutboxItem.class.php
@@ -134,22 +134,22 @@ class ShoutboxItem
 		$user_group_color = User::get_group_color($user->get_groups(), $user->get_level(), true);
 
 		return array_merge(Date::get_array_tpl_vars($this->creation_date, 'date'),array(
-			'C_EDIT' => $this->is_authorized_to_edit(),
-			'C_DELETE' => $this->is_authorized_to_delete(),
-			'C_AUTHOR_EXISTS' => $user->get_id() != User::VISITOR_LEVEL,
+			'C_EDIT'             => $this->is_authorized_to_edit(),
+			'C_DELETE'           => $this->is_authorized_to_delete(),
+			'C_AUTHOR_EXISTS'    => $user->get_id() != User::VISITOR_LEVEL,
 			'C_USER_GROUP_COLOR' => !empty($user_group_color),
 
 			//Message
-			'ID' => $this->id,
-			'CONTENT' => FormatingHelper::second_parse($this->content),
-			'PSEUDO' => $this->login ? $this->login : $user->get_display_name(),
+			'ID'               => $this->id,
+			'CONTENT'          => FormatingHelper::second_parse($this->content),
+			'PSEUDO'           => $this->login ? $this->login : $user->get_display_name(),
 			'USER_LEVEL_CLASS' => UserService::get_level_class($user->get_level()),
 			'USER_GROUP_COLOR' => $user_group_color,
 
-			'U_ANCHOR' => ShoutboxUrlBuilder::home($page, $this->id)->rel(),
+			'U_ANCHOR'         => ShoutboxUrlBuilder::home($page, $this->id)->rel(),
 			'U_AUTHOR_PROFILE' => UserUrlBuilder::profile($this->get_author_user()->get_id())->rel(),
-			'U_EDIT' => ShoutboxUrlBuilder::edit($this->id, $page)->rel(),
-			'U_DELETE' => ShoutboxUrlBuilder::delete($this->id)->rel()
+			'U_EDIT'           => ShoutboxUrlBuilder::edit($this->id, $page)->rel(),
+			'U_DELETE'         => ShoutboxUrlBuilder::delete($this->id)->rel()
 		));
 	}
 }
