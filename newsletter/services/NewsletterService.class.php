73ff742e52d3fa36a8004483bf3c1ca331fb46eb
babsolune <babsolune@phpboost.com>
2023-07-04 11:39:31 +0200

    [FIX] Newsletter hook

--- newsletter/services/NewsletterService.class.php
+++ newsletter/services/NewsletterService.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 12 16
+ * @version     PHPBoost 6.0 - last update: 2023 07 04
  * @since       PHPBoost 3.0 - 2011 02 08
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -42,9 +42,8 @@ class NewsletterService
 					'title'         => $subject,
 					'content'       => $content,
 					'language_type' => $language_type,
-					'url'           => NewsletterUrlBuilder::archive($archive_id),
+					'url'           => NewsletterUrlBuilder::archive($archive_id)->rel(),
 				);
-
 				HooksService::execute_hook_action('add', 'newsletter', $properties);
 			}
 		}
