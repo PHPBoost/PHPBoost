1414b520cc883cdcea3e475ba321b605b96b9ca0
babsolune <babsolune@phpboost.com>
2023-07-09 12:14:58 +0200

    articles -  [UPD] array nomenclature + [FIX] form display

--- articles/lang/english/install.php
+++ articles/lang/english/install.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Patrick DUBEAU <daaxwizeman@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2021 07 31
+ * @version     PHPBoost 6.0 - last update: 2023 07 09
  * @since       PHPBoost 4.0 - 2013 02 27
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -14,14 +14,14 @@
 #                       English                     #
 #####################################################
 
-$lang['categories'] = $lang['items'] = array();
+$lang['categories'] = $lang['items'] = [];
 
-$lang['categories'][] = array(
+$lang['categories'][] = [
 	'category.name'        => 'First category',
 	'category.description' => 'Article demonstration'
-);
+];
 
-$lang['items'][] = array(
+$lang['items'][] = [
 	'item.title'   => 'How to begin with the articles module',
 	'item.content' => '
 		<p>This brief article will give you some simple tips to take control of this module.</p>
@@ -38,5 +38,5 @@ $lang['items'][] = array(
 		<br />
 		Enjoy using this module.',
 	'item.summary' => ''
-);
+];
 ?>
--- articles/phpboost/ArticlesConfig.class.php
+++ articles/phpboost/ArticlesConfig.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Patrick DUBEAU <daaxwizeman@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 01
+ * @version     PHPBoost 6.0 - last update: 2023 07 09
  * @since       PHPBoost 4.0 - 2013 02 27
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -14,12 +14,12 @@ class ArticlesConfig extends DefaultRichModuleConfig
 {
 	public function get_additional_default_values()
 	{
-		return array(
+		return [
 			self::DISPLAY_TYPE         => self::GRID_VIEW,
 			self::ITEMS_PER_PAGE       => 10,
 			self::CATEGORIES_PER_ROW   => 2,
 			self::VIEWS_NUMBER_ENABLED => true
-		);
+        ];
 	}
 }
 ?>
