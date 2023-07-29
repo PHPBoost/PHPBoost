e5f4e1d6f1ddcf598c3f3e4b24c90e2dff605489
babsolune <babsolune@phpboost.com>
2023-07-29 09:30:14 +0200

    [UPD] install -- lang

--- install/distribution/distribution_full_english.php
+++ install/distribution/distribution_full_english.php
@@ -3,13 +3,16 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2014 12 22
+ * @version     PHPBoost 6.0 - last update: 2023 07 29
  * @since       PHPBoost 2.0 - 2008 10 12
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 $lang = array(
 	'distribution.name' => 'Full pack',
-	'distribution.description' => '<p>You are going to install the <strong>full pack</strong> distribution of PHPBoost.</p>
-<p>This distribution contains all the official modules developed by the PHPBoost team. It will enable you to make many different things thanks to the large number of modules.</p>'
+	'distribution.description' => '
+        <p>You are about to install the <strong>full pack</strong> distribution of PHPBoost.</p>
+        <p>This distribution contains all the official modules published by the PHPBoost development team. It should meet a wide variety of needs thanks to the diversity of its modules.</p>
+    '
 );
 ?>
--- install/distribution/distribution_pdk_english.php
+++ install/distribution/distribution_pdk_english.php
@@ -3,14 +3,16 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 08 02
+ * @version     PHPBoost 6.0 - last update: 2023 07 29
  * @since       PHPBoost 2.0 - 2009 06 06
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 $lang = array(
 	'distribution.name' => 'PDK',
-	'distribution.description' => '<p>You are going to install the <strong><acronym aria-label="PHPBoost Development Kit">PDK</acronym></strong> distribution of PHPBoost.</p>
-<p>This distribution fits very well to developers who want to create and integrate a module in PHPBoost. It contains tools enabling you to manage the database and the PHPBoost framework documentation.</p>'
+	'distribution.description' => '
+        <p>You are about to install the <strong><abbr aria-label="PHPBoost Development Kit">PDK</abbr></strong> distribution of PHPBoost.</p>
+        <p>This distribution perfectly suited for developers who want to create and integrate a module in PHPBoost. It contains tools enabling you to manage the database and the PHPBoost framework documentation.</p>
+    '
 );
 ?>
