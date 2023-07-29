07bd077c7080f109ea21ab8fa8112f2fe36f9944
babsolune <babsolune@phpboost.com>
2023-07-29 09:29:58 +0200

    [UPD] install -- lang

--- install/distribution/distribution_pdk_french.php
+++ install/distribution/distribution_pdk_french.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 08 02
+ * @version     PHPBoost 6.0 - last update: 2023 07 29
  * @since       PHPBoost 2.0 - 2009 06 06
  * @contributor Arnaud GENET <elenwii@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -11,7 +11,9 @@
 
 $lang = array(
 	'distribution.name' => 'PDK',
-	'distribution.description' => '<p>Vous allez installer la distribution <strong><acronym aria-label="PHPBoost Development Kit">PDK</acronym></strong> de PHPBoost.</p>
-<p>Cette distribution est parfaitement adaptée aux développeurs qui souhaitent développer un module afin de l\'intégrer à PHPBoost. Elle contient un outil de gestion de la base de données ainsi que la documentation du framework de PHPBoost.</p>'
+	'distribution.description' => '
+        <p>Vous êtes sur le point d\'installer la distribution <strong><abbr aria-label="PHPBoost Development Kit">PDK</abbr></strong> de PHPBoost.</p>
+        <p>Cette distribution est parfaitement adaptée aux développeurs qui souhaitent créer un module et l\'intégrer à PHPBoost. Elle contient un outil de gestion de la base de données ainsi que la documentation du framework de PHPBoost.</p>
+    '
 );
 ?>
