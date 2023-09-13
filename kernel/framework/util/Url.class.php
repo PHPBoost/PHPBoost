5ed1e0158eae9f38ea6de420fb9a73e5d19bbd14
babsolune <babsolune@phpboost.com>
2023-09-13 06:47:25 +0200

    [FIX] url_rewrite

--- kernel/framework/util/Url.class.php
+++ kernel/framework/util/Url.class.php
@@ -13,7 +13,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2023 07 09
+ * @version     PHPBoost 6.0 - last update: 2023 09 13
  * @since       PHPBoost 2.0 - 2009 01 14
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -207,8 +207,8 @@ class Url
 	 */
 	public static function encode_rewrite($url)
 	{
-		$url = mb_convert_encoding(TextHelper::html_entity_decode($url), 'UTF-8', mb_detect_encoding(TextHelper::html_entity_decode($url)));
-        $url = TextHelper::strtolower(strtr($url, mb_convert_encoding('²ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöø°ÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ()[]\'"~$&%*@ç!?;,:/\^¨€{}<>«»`|+.= #', mb_detect_encoding(TextHelper::html_entity_decode($url)), 'UTF-8'), '2aaaaaaaaaaaaoooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn    --      c  ---    e         --- '));
+		$url = mb_convert_encoding(TextHelper::html_entity_decode($url), 'ISO-8859-1', 'UTF-8');
+        $url = TextHelper::strtolower(strtr($url, mb_convert_encoding('²ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöø°ÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ()[]\'"~$&%*@ç!?;,:/\^¨€{}<>«»`|+.= #', 'ISO-8859-1', 'UTF-8'), '2aaaaaaaaaaaaoooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn    --      c  ---    e         --- '));
 		$url = str_replace(' ', '', $url);
 		$url = str_replace('---', '-', $url);
 		$url = str_replace('--', '-', $url);
