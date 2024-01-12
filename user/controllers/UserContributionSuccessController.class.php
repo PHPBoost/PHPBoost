47794447bc5b56beec43b7b3c4718eeae1e4ad8f
babsolune <babsolune@phpboost.com>
2024-01-12 12:19:48 +0100

    [UPD] redirection button after contribution

--- user/controllers/UserContributionSuccessController.class.php
+++ user/controllers/UserContributionSuccessController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 12 16
+ * @version     PHPBoost 6.0 - last update: 2024 01 12
  * @since       PHPBoost 3.0 - 2011 10 07
 */
 
@@ -14,6 +14,8 @@ class UserContributionSuccessController extends UserErrorController
 		$lang = LangLoader::get_all_langs();
 
 		parent::__construct($lang['contribution.confirmed'], $lang['contribution.confirmed.messages'], self::SUCCESS);
+
+		$this->set_correction_link(LangLoader::get_message('common.back', 'common-lang'), PATH_TO_ROOT);
 	}
 }
 ?>
