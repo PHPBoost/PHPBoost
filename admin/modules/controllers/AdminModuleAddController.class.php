fe6f1e9eb57c6e4558b6d16e2485fbf866fe417b
babsolune <babsolune@phpboost.com>
2024-01-30 06:58:32 +0100

    [UPD] install module - forbidden files

--- admin/modules/controllers/AdminModuleAddController.class.php
+++ admin/modules/controllers/AdminModuleAddController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Patrick DUBEAU <daaxwizeman@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2024 01 11
+ * @version     PHPBoost 6.0 - last update: 2024 01 30
  * @since       PHPBoost 3.0 - 2011 09 20
  * @contributor Kevin MASSY <reidlos@phpboost.com>
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
