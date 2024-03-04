bc841ff73d4009f81167f7a9fef3481c12db98d9
babsolune <babsolune@phpboost.com>
2024-03-04 09:48:45 +0100

    [FIX] online update

--- update/services/modules/OnlineModuleUpdateVersion.class.php
+++ update/services/modules/OnlineModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      xela <xela@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 12 29
+ * @version     PHPBoost 6.0 - last update: 2024 03 04
 */
 #################################################*/
 
@@ -12,11 +12,30 @@ class OnlineModuleUpdateVersion extends ModuleUpdateVersion
 	public function __construct()
 	{
 		parent::__construct('online');
-		
+
 		self::$delete_old_files_list = array(
 			'/phpboost/OnlineHomePageExtensionPoint.class.php',
 			'/util/AdminOnlineDisplayResponse.class.php'
 		);
+	}
+
+	public function execute()
+	{
+		parent::execute();
+		if (ModulesManager::is_module_installed('online'))
+		{
+			$menu_id = 0;
+            try {
+                $menu_id = $this->querier->get_column_value(DB_TABLE_MENUS, 'id', 'WHERE title = "online/OnlineModuleMiniMenu"');
+            } catch (RowNotFoundException $e) {}
+
+            if ($menu_id)
+            {
+                $menu = MenuService::load($menu_id);
+                MenuService::delete($menu);
+                MenuService::generate_cache();
+            }
+		}
 	}
 }
 ?>
