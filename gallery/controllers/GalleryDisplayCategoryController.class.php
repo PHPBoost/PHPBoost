1543acf146a6442cd75b1dbd59862f76595cbdde
Max Koder <mx.koder@gmail.com>
2024-07-16 11:01:48 +0200

    Foutu header

--- gallery/controllers/GalleryDisplayCategoryController.class.php
+++ gallery/controllers/GalleryDisplayCategoryController.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2024 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2023 10 04
+ * @version     PHPBoost 6.0 - last update: 2024 07 16
  * @since       PHPBoost 4.1 - 2015 02 04
  * @contributor Kevin MASSY <reidlos@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
