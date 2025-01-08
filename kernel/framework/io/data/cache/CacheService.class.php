d3558497664a757174f68208ced9ccfb64c7deee
babsolune <babsolune@phpboost.com>
2025-01-08 17:58:14 +0100

    [UPD] @copyright date

--- kernel/framework/io/data/cache/CacheService.class.php
+++ kernel/framework/io/data/cache/CacheService.class.php
@@ -2,7 +2,7 @@
 /**
  * @package     IO
  * @subpackage  Data\cache
- * @copyright   &copy; 2005-2024 PHPBoost
+ * @copyright   &copy; 2005-2025 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
  * @version     PHPBoost 6.0 - last update: 2024 07 06
--- kernel/framework/phpboost/cache/HtaccessFileCache.class.php
+++ kernel/framework/phpboost/cache/HtaccessFileCache.class.php
@@ -4,7 +4,7 @@
  * and is used to change the Apache configuration only in the PHPBoost folder.
  * @package     PHPBoost
  * @subpackage  Cache
- * @copyright   &copy; 2005-2024 PHPBoost
+ * @copyright   &copy; 2005-2025 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
  * @version     PHPBoost 6.0 - last update: 2024 08 22
