91e271c5f26535ae1e41e2cf927ac73e44aa9784
Julien Briswalter <jbriswalter@users.noreply.github.com>
2025-02-27 07:43:21 +0100

    [UPD] FormFieldIconEditor

--- kernel/framework/builder/form/field/FormFieldIconEditor.class.php
+++ kernel/framework/builder/form/field/FormFieldIconEditor.class.php
@@ -6,121 +6,89 @@
  * @copyright   &copy; 2005-2025 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2025 02 25
+ * @version     PHPBoost 6.0 - last update: 2025 02 27
  * @since       PHPBoost 6.0 - 2025 02 21
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class FormFieldIconEditor extends AbstractFormField
 {
-	public function __construct($id, $label, $value = '', array $field_options = [], array $constraints = [])
-    {
-        parent::__construct($id, $label, $value, $field_options, $constraints);
-    }
-
 	public function display()
-    {
-        $template = $this->get_template_to_use();
+    	{
+        	$template = $this->get_template_to_use();
 
 		$view = new FileTemplate('framework/builder/form/fieldelements/FormFieldIconEditor.tpl');
 		$view->add_lang(LangLoader::get_all_langs());
+		
+        	$this->assign_common_template_variables($template);
 
-        $this->assign_common_template_variables($template);
-
-        $value = TextHelper::deserialize($this->get_value());
-        if($value)
-        {
-            foreach($value as $prefix => $icon)
-            {
-                $view->put_all([
-                    'C_PREFIX_FAS' => $prefix === 'fas',
-                    'C_PREFIX_FAR' => $prefix === 'far',
-                    'C_PREFIX_FAB' => $prefix === 'fab',
-                    'C_PREFIX_IBOOST' => $prefix === 'fa iboost',
-                    'ICON_VALUE' => $icon,
-                ]);
-            }
-        }
+	        $value = explode(' ', $this->get_value());
+		$prefix = ($value ? $value[0] : '');
+		$icon = ($value ? $value[1] : '');
 
 		$view->put_all([
-			'NAME' => $this->get_html_id(),
-			'ID' => $this->get_html_id(),
-            'ICON_PREFIX' => $this->get_html_id() . '_prefix',
-            'ICON' => $this->get_html_id() . '_icon',
-            'ICON_LIST' => $this->get_html_id() . '_icon_list',
-            'SELECTED' => $this->get_html_id() . '_selected',
-            'FAS' => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/font-awesome/css/solid.css'),
-            'FAB' => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/font-awesome/css/brand.css'),
-            'IBOOST' => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/icoboost/icoboost.css'),
-        ]);
+			'C_PREFIX_FAS'    => $prefix === 'fas',
+	        	'C_PREFIX_FAR'    => $prefix === 'far',
+	                'C_PREFIX_FAB'    => $prefix === 'fab',
+	                'C_PREFIX_IBOOST' => $prefix === 'fa iboost',
+	                'ICON_VALUE'      => $icon,
+			'ID'              => $this->get_html_id(),
+           		'FAS'             => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/font-awesome/css/solid.css'),
+           		'FAB'             => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/font-awesome/css/brand.css'),
+            		'IBOOST'          => self::get_icon_list(PATH_TO_ROOT . '/templates/__default__/theme/icoboost/icoboost.css')
+        	]);
 
 		$template->assign_block_vars('fieldelements', [
 			'ELEMENT' => $view->render()
-        ]);
+       		 ]);
 
-        return $template;
-    }
-
-    public function validate()
-	{
-		try
-		{
-			$this->retrieve_value();
-			return true;
-		}
-		catch(Exception $ex){}
+		return $template;
 	}
 
 	public function retrieve_value()
-    {
-        $request = AppContext::get_request();
+	{
+		$request = AppContext::get_request();
 
-		$values = [];
-        $icon_id = $this->get_html_id() . '_icon';
-        if ($request->has_postparameter($icon_id))
-        {
-            $prefix_id = $this->get_html_id() . '_prefix';
-            $prefix = $request->get_poststring($prefix_id);
-            $icon = $request->get_poststring($icon_id);
-            if(!empty($prefix) && !empty($icon))
-                $values[$prefix] = $icon;
-        }
-		$this->set_value(TextHelper::serialize($values));
-    }
+		$value = '';
+       		$icon_id = $this->get_html_id() . '_icon';
+	        if ($request->has_postparameter($icon_id))
+	        {
+	           	$prefix_id = $this->get_html_id() . '_prefix';
+	            	$prefix = $request->get_poststring($prefix_id);
+	            	$icon = $request->get_poststring($icon_id);
+	            	$value = ($prefix && $icon ? $prefix . ' ' . $icon : '');
+	        }
+		$this->set_value($value);
+	}
 
 	public function get_icon_list($file)
 	{
-        $icons = [];
-        $css_file = '';
-        if (file_exists($file))
-            $css_file = file_get_contents($file);
+        	$icons = [];
+        	$css_file = (file_exists($file) ? file_get_contents($file) : '');
 		$properties_list = explode('}', $css_file);
-        foreach($properties_list as $property)
-        {
+	        foreach($properties_list as $property)
+	        {
 			$contents = explode('.', $property);
 			foreach($contents as $pseudo_class)
 			{
 				$classes = explode(' ', $pseudo_class);
-                if (in_array('--fa:', $classes))
-                    foreach($classes as $class)
-                    {
-                        if (TextHelper::strpos($class, 'fa-') !== false) {
-                            $icons[] = str_replace('fa-', '', $class);
-                        }
-                    }
+	                	if (in_array('--fa:', $classes))
+				{
+	                    		foreach($classes as $class)
+	                    		{
+	                        		if (TextHelper::strpos($class, 'fa-') !== false)
+	                            			$icons[] = str_replace('fa-', '', $class);
+	                        	}
+	                    	}
 			}
-        }
-        $iconList = '"' . implode('", "', $icons) . '"';
-		return $iconList;
+	        }
+		
+		return '"' . implode('", "', $icons) . '"';
 	}
 
-    protected function compute_options(array &$field_options)
+	protected function get_default_template()
 	{
-        parent::compute_options($field_options);
-    }
-
-    protected function get_default_template()
-    {
-        return new FileTemplate('framework/builder/form/FormField.tpl');
-    }
+		return new FileTemplate('framework/builder/form/FormField.tpl');
+	}
 }
 ?>
