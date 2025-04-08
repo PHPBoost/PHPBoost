ae5a99f3592be74e3d4e89b29338e176fd8ffadc
babsolune <babsolune@phpboost.com>
2025-04-08 05:12:38 +0200

    [UPD] password revealer

--- kernel/framework/builder/form/field/FormFieldPasswordEditor.class.php
+++ kernel/framework/builder/form/field/FormFieldPasswordEditor.class.php
@@ -6,94 +6,96 @@
  * @copyright   &copy; 2005-2025 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2018 06 03
+ * @version     PHPBoost 6.0 - last update: 2025 04 08
  * @since       PHPBoost 3.0 - 2010 02 04
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class FormFieldPasswordEditor extends AbstractFormField
 {
-	private $size = 30;
-	private $maxlength = 255;
-	private $autocomplete = true;
+    private $size = 30;
+    private $maxlength = 255;
+    private $autocomplete = true;
 
-	/**
-	 * Constructs a FormFieldPasswordEditor.
-	 * It has these options in addition to the AbstractFormField ones:
-	 * <ul>
-	 * 	<li>size: The size (width) of the HTML field</li>
-	 * 	<li>maxlength: The maximum length for the field</li>
-	 * </ul>
-	 * @param string $id Field identifier
-	 * @param string $label Field label
-	 * @param string $value Default value
-	 * @param array $field_options Map containing the options
-	 * @param FormFieldConstraint[] $constraints The constraints checked during the validation
-	 */
-	public function __construct($id, $label, $value, array $field_options = array(), array $constraints = array())
-	{
-		parent::__construct($id, $label, $value, $field_options, $constraints);
-		$this->set_css_form_field_class('form-field-password');
-	}
+    /**
+     * Constructs a FormFieldPasswordEditor.
+     * It has these options in addition to the AbstractFormField ones:
+     * <ul>
+     * 	<li>size: The size (width) of the HTML field</li>
+     * 	<li>maxlength: The maximum length for the field</li>
+     * </ul>
+     * @param string $id Field identifier
+     * @param string $label Field label
+     * @param string $value Default value
+     * @param array $field_options Map containing the options
+     * @param FormFieldConstraint[] $constraints The constraints checked during the validation
+     */
+    public function __construct($id, $label, $value, array $field_options = [], array $constraints = [])
+    {
+        parent::__construct($id, $label, $value, $field_options, $constraints);
+        $this->set_css_form_field_class('form-field-password');
+    }
 
-	/**
-	 * @return string The html code for the input.
-	 */
-	public function display()
-	{
-		$template = $this->get_template_to_use();
+    /**
+     * @return string The html code for the input.
+     */
+    public function display()
+    {
+        $template = $this->get_template_to_use();
 
-		$field_tpl = new FileTemplate('framework/builder/form/fieldelements/FormFieldPasswordEditor.tpl');
+        $field_tpl = new FileTemplate('framework/builder/form/fieldelements/FormFieldPasswordEditor.tpl');
+        $field_tpl->add_lang(LangLoader::get('user-lang'));
 
-		$this->assign_common_template_variables($template);
+        $this->assign_common_template_variables($template);
 
-		$field_tpl->put_all(array(
-			'SIZE' => $this->size,
-			'MAX_LENGTH' => $this->maxlength,
-			'NAME' => $this->get_html_id(),
-			'ID' => $this->get_id(),
-			'HTML_ID' => $this->get_html_id(),
-			'VALUE' => $this->get_value(),
-			'CLASS' => $this->get_css_class(),
-			'C_DISABLED' => $this->is_disabled(),
-			'C_AUTOCOMPLETE' => $this->autocomplete
-		));
+        $field_tpl->put_all([
+            'SIZE' => $this->size,
+            'MAX_LENGTH' => $this->maxlength,
+            'NAME' => $this->get_html_id(),
+            'ID' => $this->get_id(),
+            'HTML_ID' => $this->get_html_id(),
+            'VALUE' => $this->get_value(),
+            'CLASS' => $this->get_css_class(),
+            'C_DISABLED' => $this->is_disabled(),
+            'C_AUTOCOMPLETE' => $this->autocomplete
+        ]);
 
-		$template->assign_block_vars('fieldelements', array(
-			'ELEMENT' => $field_tpl->render()
-		));
+        $template->assign_block_vars('fieldelements', [
+            'ELEMENT' => $field_tpl->render()
+        ]);
 
-		return $template;
-	}
+        return $template;
+    }
 
-	protected function compute_options(array &$field_options)
-	{
-		foreach ($field_options as $attribute => $value)
-		{
-			$attribute = TextHelper::strtolower($attribute);
-			switch ($attribute)
-			{
-				case 'size' :
-					$this->size = $value;
-					unset($field_options['size']);
-					break;
-				case 'maxlength' :
-					$this->maxlength = $value;
-					unset($field_options['maxlength']);
-					break;
-				case 'autocomplete' :
-					$this->autocomplete = (bool)$value;
-					unset($field_options['autocomplete']);
-					break;
-			}
-		}
-		parent::compute_options($field_options);
-	}
+    protected function compute_options(array &$field_options)
+    {
+        foreach ($field_options as $attribute => $value)
+        {
+            $attribute = TextHelper::strtolower($attribute);
+            switch ($attribute)
+            {
+                case 'size' :
+                    $this->size = $value;
+                    unset($field_options['size']);
+                    break;
+                case 'maxlength' :
+                    $this->maxlength = $value;
+                    unset($field_options['maxlength']);
+                    break;
+                case 'autocomplete' :
+                    $this->autocomplete = (bool)$value;
+                    unset($field_options['autocomplete']);
+                    break;
+            }
+        }
+        parent::compute_options($field_options);
+    }
 
-	protected function get_default_template()
-	{
-		return new FileTemplate('framework/builder/form/FormField.tpl');
-	}
+    protected function get_default_template()
+    {
+        return new FileTemplate('framework/builder/form/FormField.tpl');
+    }
 }
 ?>
