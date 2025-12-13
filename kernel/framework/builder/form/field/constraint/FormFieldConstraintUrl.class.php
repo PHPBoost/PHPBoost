c0a39eb7d603502206eae04d23617b340e63f4ef
Julien Briswalter <jbriswalter@users.noreply.github.com>
2025-12-13 21:41:25 +0100

    [FIX] Update FormFieldConstraintUrl.class.php

--- kernel/framework/builder/form/field/constraint/FormFieldConstraintUrl.class.php
+++ kernel/framework/builder/form/field/constraint/FormFieldConstraintUrl.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2025 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 02 03
+ * @version     PHPBoost 6.0 - last update: 2025 12 13
  * @since       PHPBoost 3.0 - 2010 02 07
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
@@ -15,7 +15,7 @@
 
 class FormFieldConstraintUrl extends FormFieldConstraintRegex
 {
-	private static $regex = '/(^\/[0-9A-Za-z#!:.?+=&%@!\-\/\._]+$)|(^(ftps?|https?):\/\/([0-9a-z\.-]+)\.{0,1}([a-z\.]{2,9}|[0-9\.]+{0,3})([0-9A-Za-z#!:.?+=&%@!\-\/\._]*)*\/?$)/u';
+	private static $regex = '/(^\/[0-9A-Za-z#!:.?+=&%@!\-\/\._]+$)|(^(ftps?|https?):\/\/([0-9a-z\.-]+)\.{0,1}([a-z\.]{2,9}|[0-9\.]+)([0-9A-Za-z#!:.?+=&%@!\-\/\._]*)*\/?$)/u';
 
 	public function __construct($error_message = '')
 	{
@@ -39,4 +39,5 @@ class FormFieldConstraintUrl extends FormFieldConstraintRegex
 }
 
 ?>
+
 
