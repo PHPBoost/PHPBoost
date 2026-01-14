ff0118d38c8d921e70580b639560eef11df82b1e
babsolune <babsolune@phpboost.com>
2026-01-14 13:15:53 +0100

    [UPD] docblock date

--- kernel/framework/phpboost/cache/HtaccessFileCache.class.php
+++ kernel/framework/phpboost/cache/HtaccessFileCache.class.php
@@ -7,7 +7,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2024 08 22
+ * @version     PHPBoost 6.1 - last update: 2025 01 14
  * @since       PHPBoost 3.0 - 2009 10 22
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor janus57 <janus57@janus57.fr>
--- kernel/framework/phpboost/cache/NginxFileCache.class.php
+++ kernel/framework/phpboost/cache/NginxFileCache.class.php
@@ -7,7 +7,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2023 01 17
+ * @version     PHPBoost 6.1 - last update: 2025 01 14
  * @since       PHPBoost 5.2 - 2019 10 26
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
