5d4210eb96e13987049623fb91fe42afe51c6a61
babsolune <babsolune@phpboost.com>
2026-01-28 04:46:52 +0100

    [FIX] User search

--- kernel/framework/ajax/controllers/AjaxSearchUserAutoCompleteController.class.php
+++ kernel/framework/ajax/controllers/AjaxSearchUserAutoCompleteController.class.php
@@ -5,7 +5,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2022 10 19
+ * @version     PHPBoost 6.1 - last update: 2026 01 28
  * @since       PHPBoost 4.0 - 2013 06 26
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Arnaud GENET <elenwii@phpboost.com>
@@ -15,44 +15,62 @@
 class AjaxSearchUserAutoCompleteController extends AbstractController
 {
 	public function execute(HTTPRequestCustom $request)
-	{
-		$is_admin = AppContext::get_current_user()->check_level(User::ADMINISTRATOR_LEVEL);
-		$number_admins = UserService::count_admin_members();
-		$suggestions = array();
+    {
+        $is_admin = AppContext::get_current_user()->check_level(User::ADMINISTRATOR_LEVEL);
+        $number_admins = UserService::count_admin_members();
+        $suggestions = array();
 
-		try {
-			$result = PersistenceContext::get_querier()->select("SELECT user_id, display_name, level, user_groups FROM " . DB_TABLE_MEMBER . " WHERE display_name LIKE '" . str_replace('*', '%', mysqli_real_escape_string(trim($request->get_value('value', '')))) . "%'");
+        $raw_value = trim($request->get_value('value', ''));
+        // Replace * by % for LIKE, but keep the value as a parameter
+        $like_value = str_replace('*', '%', $raw_value) . '%';
 
-			while($row = $result->fetch())
-			{
-				$user_group_color = User::get_group_color($row['user_groups'], $row['level']);
+        try
+        {
+            $querier = PersistenceContext::get_querier();
 
-				$suggestion = '';
+            // Prepared statement with a bound parameter instead of manual escaping
+            $result = $querier->select(
+                'SELECT user_id, display_name, level, user_groups
+                FROM ' . DB_TABLE_MEMBER . '
+                WHERE display_name LIKE :value',
+                array('value' => $like_value)
+            );
 
-				if ($is_admin)
-				{
-					$edit_link = new EditLinkHTMLElement(UserUrlBuilder::edit_profile($row['user_id']));
+            while ($row = $result->fetch())
+            {
+                $user_group_color = User::get_group_color($row['user_groups'], $row['level']);
 
-					if ($row['level'] != User::ADMINISTRATOR_LEVEL || ($row['level'] == User::ADMINISTRATOR_LEVEL && $number_admins > 1))
-						$delete_link = new DeleteLinkHTMLElement(AdminMembersUrlBuilder::delete($row['user_id']), '', array('title' => LangLoader::get_message('common.delete', 'common-lang'), 'data-confirmation' => 'delete-element'), 'fa fa-delete');
-					else
-						$delete_link = new DeleteLinkHTMLElement('', '', array('onclick' => 'return false;'), 'icon-disabled');
+                $suggestion = '';
 
-					$suggestion .= $edit_link->display() . '&nbsp;' . $delete_link->display() . '&nbsp;';
-				}
+                if ($is_admin)
+                {
+                    $edit_link = new EditLinkHTMLElement(UserUrlBuilder::edit_profile($row['user_id']));
 
-				$profile_link = new LinkHTMLElement(UserUrlBuilder::profile($row['user_id'])->rel(), $row['display_name'], array('style' => (!empty($user_group_color) ? 'color:' . $user_group_color : '')), UserService::get_level_class($row['level']));
+                    if ($row['level'] != User::ADMINISTRATOR_LEVEL || ($row['level'] == User::ADMINISTRATOR_LEVEL && $number_admins > 1))
+                        $delete_link = new DeleteLinkHTMLElement(AdminMembersUrlBuilder::delete($row['user_id']), '', array('title' => LangLoader::get_message('common.delete', 'common-lang'), 'data-confirmation' => 'delete-element'), 'fa fa-delete');
+                    else
+                        $delete_link = new DeleteLinkHTMLElement('', '', array('onclick' => 'return false;'), 'icon-disabled');
 
-				$suggestion .= $profile_link->display();
+                    $suggestion .= $edit_link->display() . '&nbsp;' . $delete_link->display() . '&nbsp;';
+                }
 
-				$suggestions[] = $suggestion;
-			}
-			$result->dispose();
-		} catch (Exception $e) {
-		}
+                $profile_link = new LinkHTMLElement(
+                    UserUrlBuilder::profile($row['user_id'])->rel(),
+                    $row['display_name'],
+                    array('style' => (!empty($user_group_color) ? 'color:' . $user_group_color : '')),
+                    UserService::get_level_class($row['level'])
+                );
 
-		return new JSONResponse(array('suggestions' => $suggestions));
-	}
+                $suggestion .= $profile_link->display();
+
+                $suggestions[] = $suggestion;
+            }
+            $result->dispose();
+        }
+        catch (Exception $e) {}
+
+        return new JSONResponse(array('suggestions' => $suggestions));
+    }
 }
 ?>
 
