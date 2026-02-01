a1f146123c0ce91c2eb99efd7957719cd24513b5
babsolune <babsolune@phpboost.com>
2026-02-01 12:11:04 +0100

    [UPD] docblock version

--- database/cli/CLIDumpCommand.class.php
+++ database/cli/CLIDumpCommand.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Loic ROUCHON <horn@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2026 01 26
+ * @version     PHPBoost 6.1 - last update: 2026 01 26
  * @since       PHPBoost 3.0 - 2010 02 06
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
