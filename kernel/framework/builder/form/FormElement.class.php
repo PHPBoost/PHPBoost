db849a87e974306c062b0265a5d9626ddbd5d721
babsolune <babsolune@phpboost.com>
2026-02-06 18:58:19 +0100

    [UPD] docblock

--- kernel/framework/builder/form/FormElement.class.php
+++ kernel/framework/builder/form/FormElement.class.php
@@ -5,9 +5,10 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Benoit SAUTEL <ben.popeye@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2015 01 19
+ * @version     PHPBoost 6.1 - last update: 2026 02 06
  * @since       PHPBoost 3.0 - 2011 01 05
  * @contributor Kevin MASSY <reidlos@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 interface FormElement extends HTMLElement
