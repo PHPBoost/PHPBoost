841a1a12161e7676049ff7719e3caa9a1ebd8f1d
mipel85 <mipel85@phpboost.com>
2026-06-10 11:08:17 +0200

    [test] - delete fichier bidon pour script sh

--- bidon.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
