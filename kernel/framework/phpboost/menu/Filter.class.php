1ef3eaf1001ef32e5080afb46b9606e5d9a338bc
babsolune <babsolune@phpboost.com>
2026-09-10 06:57:40 +0200

    [FIX] unserialize old menus

--- kernel/framework/phpboost/menu/Filter.class.php
+++ kernel/framework/phpboost/menu/Filter.class.php
@@ -6,7 +6,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Regis VIARRE <crowkait@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2026 05 19
+ * @version     PHPBoost 6.1 - last update: 2026 09 10
  * @since       PHPBoost 3.0 - 2011 03 06
  * @author      Arnaud GENET <elenwii@phpboost.com>
 */
@@ -23,6 +23,21 @@ abstract class Filter
     {
 		$this->pattern = $pattern;
     }
+
+	public function __unserialize(array $data)
+	{
+		foreach ($data as $serialized_property => $value)
+		{
+			$property = str_starts_with($serialized_property, "\0") ? substr($serialized_property, strrpos($serialized_property, "\0") + 1) : $serialized_property;
+			$property = str_starts_with($property, '*') ? substr($property, 1) : $property;
+
+			if (property_exists($this, $property))
+			{
+				$reflection_property = new ReflectionProperty($this, $property);
+				$reflection_property->setValue($this, $value);
+			}
+		}
+	}
 
 	function get_pattern()
 	{
